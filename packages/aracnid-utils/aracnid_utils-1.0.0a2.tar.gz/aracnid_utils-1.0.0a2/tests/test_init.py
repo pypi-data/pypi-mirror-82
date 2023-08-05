023911447a4b5acecb2bcd3726fd2cecf00a14bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:34 +0000

    Add package aracnid-utils/1.0.0a2/aracnid_utils-1.0.0a2.tar.gz

---
+++ packages/aracnid-utils/aracnid_utils-1.0.0a2.tar.gz/aracnid_utils-1.0.0a2/tests/test_init.py
@@ -0,0 +1,8 @@
+"""Test functions for Aracnid Logger import.
+"""
+import aracnid_logger
+
+def test_version():
+    """Tests that Aracnid Logger was imported successfully.
+    """
+    assert aracnid_logger.__version__
