076ba86ab496d4313bd84c9f1bdabe041ccb004e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:18 +0000

    Add package npcnn/0.2.1/npcnn-0.2.1-py3-none-any.whl

---
+++ packages/npcnn/npcnn-0.2.1-py3-none-any.whl/deepctr/models/__init__.py
@@ -0,0 +1,43 @@
+from .afm import AFM
+from .autoint import AutoInt
+from .ccpm import CCPM
+from .dcn import DCN
+from .deepfm import DeepFM
+from .dien import DIEN
+from .din import DIN
+from .fnn import FNN
+from .mlr import MLR
+from .onn import ONN
+from .onn import ONN as NFFM
+from .nfm import NFM
+from .pnn import PNN
+from .wdl import WDL
+from .xdeepfm import xDeepFM
+from .fgcnn import FGCNN
+from .dsin import DSIN
+from .fibinet import FiBiNET
+from .flen import FLEN
+from .fwfm import FwFM
+from .npcnn import NPCNN #tskim 0918
+from .npcnn2 import NPCNN2 #tskim 0921
+from .npcnn3 import NPCNN3 #tskim 0921
+from .npcnn4 import NPCNN4 #tskim 0921
+from .npcnn5 import NPCNN5 #tskim 0921
+from .npcnn6 import NPCNN6 #tskim 0921
+from .npcnn7 import NPCNN7 #tskim 0921
+from .npcnn8 import NPCNN8 #tskim 0921
+from .npcnn9 import NPCNN9 #tskim 0925
+from .nncnn import NNCNN #tskim 1001
+from .nncnn2 import NNCNN2 #tskim 1004
+from .nncnn3 import NNCNN3 #tskim 1005
+from .nncnn4 import NNCNN4 #tskim 1005
+from .nncnn5 import NNCNN5 #tskim 1005
+from .nncnn6 import NNCNN6 #tskim 1005
+from .nncnn7 import NNCNN7 #tskim 1005
+from .nncnn8 import NNCNN8 #tskim 1006
+from .nncnn9 import NNCNN9 #tskim 1006
+from .nncnn10 import NNCNN10 #tskim 1012
+
+__all__ = ["AFM", "CCPM","DCN", "MLR",  "DeepFM", "MLR", "NFM", "DIN", "DIEN", "FNN", "PNN",
+           "WDL", "xDeepFM", "AutoInt", "ONN", "FGCNN", "DSIN", "FiBiNET", 'FLEN', "FwFM", 
+           "NPCNN", "NPCNN2", "NPCNN3", "NPCNN4", "NPCNN5", "NPCNN6", "NPCNN7", "NPCNN8", "NPCNN9", "NNCNN", "NNCNN2", "NNCNN3", "NNCNN3"]
