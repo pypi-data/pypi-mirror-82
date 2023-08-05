247edd7df4e69e7d8fae29a3d6a9ed44d24bd44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package chinadaily/0.1.0/chinadaily-0.1.0.tar.gz

---
+++ packages/chinadaily/chinadaily-0.1.0.tar.gz/chinadaily-0.1.0/chinadaily/cli.py
@@ -0,0 +1,24 @@
+"""Console script for chinadaily."""
+import argparse
+import sys
+from datetime import datetime
+
+from chinadaily import download
+
+
+def main():
+    """Console script for chinadaily."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "chinadaily.cli.main")
+    now = datetime.now()
+    download(now)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
