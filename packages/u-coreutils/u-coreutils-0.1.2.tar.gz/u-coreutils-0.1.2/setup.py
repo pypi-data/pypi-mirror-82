1cdb783c62cf90150cbb05669a3c423385d7a0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:35 +0000

    Add package u-coreutils/0.1.2/u-coreutils-0.1.2.tar.gz

---
+++ packages/u-coreutils/u-coreutils-0.1.2.tar.gz/u-coreutils-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['u_coreutils', 'u_coreutils.cat']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['u-cat = u_coreutils.cat:run']}
+
+setup_kwargs = {
+    'name': 'u-coreutils',
+    'version': '0.1.2',
+    'description': 'GNU coreutils implementation with Python 3.8',
+    'long_description': None,
+    'author': 'duyixian',
+    'author_email': 'duyixian1234@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
