58a471c49e713594a749784fa07f93d36b20a0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:32 +0000

    Add package printnum/0.0.9/printnum-0.0.9.tar.gz

---
+++ packages/printnum/printnum-0.0.9.tar.gz/printnum-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="printnum",
+    version="0.0.9",
+    author="alena-lark",
+    author_email="AllyLarck@yandex.com",
+    description="Printing a number of atoms in a molecule",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alena-lark/PrintNumAtom",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
