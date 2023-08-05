6f08e21ce08d1450925aec4daa3caee9ebf83a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:04 +0000

    Add package chemlib/2.1.2/chemlib-2.1.2.tar.gz

---
+++ packages/chemlib/chemlib-2.1.2.tar.gz/chemlib-2.1.2/chemlib/__init__.py
@@ -0,0 +1,16 @@
+from chemlib.chemistry import PeriodicTable
+
+pte = PeriodicTable()
+SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+AVOGADROS_NUMBER = 6.02e+23
+
+from chemlib.chemistry import Element, Compound, Reaction
+from chemlib.chemistry import Combustion
+
+c = 2.998*10**8    #Speed of light
+h = 6.626*10**-34  #Planck's constant
+R = 1.0974*10**7  #Rydberg constant
+
+from chemlib.quantum_mechanics import Wave
+from chemlib.quantum_mechanics import energy_of_hydrogen_orbital
+
