9b4b72b2663f9c9c377b19fd423691c4d28045ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:07 +0000

    Add package iracema/0.1.4/iracema-0.1.4-py3-none-any.whl

---
+++ packages/iracema/iracema-0.1.4-py3-none-any.whl/iracema/__init__.py
@@ -0,0 +1,14 @@
+"""
+iracema is a python package aimed at the extraction of expressive music
+information from audio signals
+"""
+
+from .timeseries import TimeSeries, Audio
+from .segment import Segment, SegmentList
+
+import iracema.features
+import iracema.harmonics
+import iracema.pitch
+import iracema.plot
+import iracema.segmentation
+import iracema.spectral
