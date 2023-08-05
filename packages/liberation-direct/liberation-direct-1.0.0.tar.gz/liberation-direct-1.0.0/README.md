972e2345739dcb3013ea77ebe3d08bf584bf6be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:39 +0000

    Add package liberation-direct/1.0.0/liberation-direct-1.0.0.tar.gz

---
+++ packages/liberation-direct/liberation-direct-1.0.0.tar.gz/liberation-direct-1.0.0/README.md
@@ -0,0 +1,16 @@
+# liberation-direct
+A parser for Libération's live feed page. 
+
+The French newspaper Libération has a [live feed](http://www.liberation.fr/direct) where they regularly post short summaries of current events.
+This package parses that feed, finds the latest summary and converts it into markdown. 
+
+Example usage to retrieve the summary : 
+
+```python
+from liberation_direct import LiberationDirect
+summary = LiberationDirect().get_news_summary_markdown()
+print(summary)
+```
+
+
+
---
+++ packages/liberation-direct/liberation-direct-1.0.0.tar.gz/liberation-direct-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+from setuptools import setup
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="liberation-direct",
+    version="1.0.0",
+    description="Parse Libération's live news page.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/massoncl/liberation-direct",
+    author="Clément Masson",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=["beautifulsoup4", "markdownify"],
+    packages=["liberation_direct"],
+    include_package_data=True,
+)
