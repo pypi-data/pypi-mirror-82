3215d1efc087db43e653fbbb584b283415b10564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:33 +0000

    Add package geograpy3/0.1.24/geograpy3-0.1.24.tar.gz

---
+++ packages/geograpy3/geograpy3-0.1.24.tar.gz/geograpy3-0.1.24/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+import os 
+from collections import OrderedDict
+
+try:
+    long_description = ""
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+
+except:
+    print('Curr dir:', os.getcwd())
+    long_description = open('../../README.md').read()
+
+setup(name='geograpy3',
+      version='0.1.24',
+      description='Extract countries, regions and cities from a URL or text',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/somnathrakshit/geograpy3',
+      download_url='https://github.com/somnathrakshit/geograpy3',
+      author='Somnath Rakshit',
+      author_email='somnath52@gmail.com',
+      license='Apache',
+      project_urls=OrderedDict(
+        (
+            ("Documentation", "https://geograpy3.netlify.app"),
+            ("Code", "https://github.com/somnathrakshit/geograpy3"),
+            ("Issue tracker", "https://github.com/somnathrakshit/geograpy3/issues"),
+        )
+      ),
+      classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8'
+      ],
+      packages=['geograpy'],
+      install_requires=[
+          'numpy',
+          'nltk',
+          'newspaper3k',
+          'jellyfish',
+          'pycountry',
+	  'pylodstorage'
+      ],
+      scripts=['geograpy/bin/geograpy-nltk'],
+      package_data={
+          'geograpy': ['data/*.csv'],
+      },
+      zip_safe=False)
