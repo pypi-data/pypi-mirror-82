5d51d7c0321a24d061ace74299ef2ed3db3e407a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:43 +0000

    Add package VirxERLU-CLib/1.10.2/VirxERLU-CLib-1.10.2.tar.gz

---
+++ packages/VirxERLU-CLib/VirxERLU-CLib-1.10.2.tar.gz/VirxERLU-CLib-1.10.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+
+module = Extension('virxrlcu', sources=['virxrlcu.c'])
+
+setup(name='VirxERLU-CLib',
+      version='1.10.2',
+      description='C modules for VirxERLU',
+      long_description="""
+Big thanks to ddthj/GoslingUtils for the basis of VirxERLU. This version, however, improves upon many things, including pathfinding, finding shots, and aerials.
+
+VirxERLU-CLib is the C library for VirxERLU (which is Python), and it used for the extra performance on very math-heavy operations, like shot validation.
+https://www.virxcase.dev
+""",
+      ext_modules=[module],
+      license="MIT",
+      author='VirxEC',
+      author_email='virx@virxcase.dev',
+      url="https://github.com/VirxEC/VirxERLU",
+      python_requires='>=3.7'
+      )
