1a71a41fc1c7535c71100acfec20770f981c3c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:04 +0000

    Add package simplysql/0.1.3/simplysql-0.1.3.tar.gz

---
+++ packages/simplysql/simplysql-0.1.3.tar.gz/simplysql-0.1.3/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3.7.6
+
+import setuptools
+from simplysql import __author__, __author_email__, __description__, __name__, __version__, __url__
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(name=__name__,
+                 packages=setuptools.find_packages(),
+                 version=__version__,
+                 license="MIT",
+                 description=__description__,
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 author=__author__,
+                 author_email=__author_email__,
+                 url=__url__,
+                 python_requires=">=3.7",
+                 download_url="https://github.com/Luanee/SimplySQL/archive/v0.1.0.tar.gz",
+                 keywords=["sql", "easy", "queries", "databases", "sqlite3"],
+                 classifiers=["Development Status :: 3 - Alpha",
+                              "Intended Audience :: Developers",
+                              "Topic :: Software Development :: Build Tools",
+                              "License :: OSI Approved :: MIT License",
+                              # TODO: Check compatibility
+                              # "Programming Language :: Python :: 3",
+                              # "Programming Language :: Python :: 3.4",
+                              # "Programming Language :: Python :: 3.5",
+                              "Programming Language :: Python :: 3.7"],
+                 )
