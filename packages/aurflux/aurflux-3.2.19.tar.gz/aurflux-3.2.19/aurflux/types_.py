7424b2fb906670046549e65f5e6ae11d61a1650d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:04 +0000

    Add package aurflux/3.2.19/aurflux-3.2.19.tar.gz

---
+++ packages/aurflux/aurflux-3.2.19.tar.gz/aurflux-3.2.19/aurflux/types_.py
@@ -0,0 +1,27 @@
+import typing as ty
+
+if ty.TYPE_CHECKING:
+   from .context import GuildMessageCtx, AuthorAwareCtx
+   from auth import AuthAwareCtx
+   from .flux import FluxEvent, FluxClient
+
+
+   class CommandFunc(ty.Protocol):
+      def __call__(self, msg_ctx: GuildMessageCtx, auth_ctx: ty.Optional[AuthAwareCtx] = None, cmd_args: str = None, **kwargs): ...
+
+
+   ExtraCtxs: ty.TypeAlias = ty.Literal["auth"]
+
+
+   class GuildCommandCtx:
+      flux: FluxClient
+      msg_ctx: GuildMessageCtx
+      author_ctx: AuthorAwareCtx
+      auth_ctxs: ty.List[AuthAwareCtx]
+
+
+   class GuildCommandEvent(FluxEvent):
+      flux: FluxClient
+      cmd_name: str
+      cmd_ctx: GuildCommandCtx
+      cmd_args: ty.Optional[str]
