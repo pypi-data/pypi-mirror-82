cf0e8da96fc965c9409fc3985001d535087ddba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:53 +0000

    Add package chris-parser/1/chris_parser-1-py3-none-any.whl

---
+++ packages/chris-parser/chris_parser-1-py3-none-any.whl/chris/classes.py
@@ -0,0 +1,17 @@
+class Author():
+    def __init__(self,last_name,first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+class Date():
+    def __init__(self,year,month,day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+class Chris():
+    def __init__(self,quote,author,date,context):
+        self.quote = quote
+        self.author = author
+        self.date = date
+        self.context = context
