0bb4a13525cf61a0c99432997c718cecd4406763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:15 +0000

    Add package common-interfaces/0.1.2/common_interfaces-0.1.2-py3-none-any.whl

---
+++ packages/common-interfaces/common_interfaces-0.1.2-py3-none-any.whl/common_interfaces/__init__.py
@@ -0,0 +1,7 @@
+from .RedisManager import RedisManager
+from .S3Manager import S3Manager
+from .ProcessInventory import ProcessInventory
+
+__all__ = [
+    'RedisManager', 'S3Manager', 'ProcessInventory'
+]
