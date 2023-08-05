aa61d600186b7e214a258afff89d9cb0810f502f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:08 +0000

    Add package pyexcel-pdfr/0.6.0/pyexcel_pdfr-0.6.0-py2.py3-none-any.whl

---
+++ packages/pyexcel-pdfr/pyexcel_pdfr-0.6.0-py2.py3-none-any.whl/pyexcel_pdfr/__init__.py
@@ -0,0 +1,28 @@
+"""
+    pyexcel_pdfr
+    ~~~~~~~~~~~~~~~~~~~
+    Read html table using messytables
+    :copyright: (c) 2015-2017 by Onni Software Ltd & its contributors
+    :license: New BSD License
+"""
+# flake8: noqa
+from pyexcel_io.io import get_data as read_data
+from pyexcel_io.io import isstream
+from pyexcel_io.plugins import IOPluginInfoChainV2
+
+from ._version import __author__, __version__
+
+__FILE_TYPE__ = "pdf"
+IOPluginInfoChainV2(__name__).add_a_reader(
+    relative_plugin_class_path="pdfr.PdfFile",
+    locations=["file"],
+    file_types=[__FILE_TYPE__],
+    stream_type="binary",
+)
+
+
+def get_data(afile, file_type=None, **keywords):
+    """standalone module function for reading module supported file type"""
+    if isstream(afile) and file_type is None:
+        file_type = __FILE_TYPE__
+    return read_data(afile, file_type=file_type, **keywords)
