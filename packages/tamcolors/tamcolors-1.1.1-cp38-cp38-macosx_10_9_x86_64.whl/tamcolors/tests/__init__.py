2bc88bdfa5313eaf78076005b2ac463799f66eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:58 +0000

    Add package tamcolors/1.1.1/tamcolors-1.1.1-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/tamcolors/tamcolors-1.1.1-cp38-cp38-macosx_10_9_x86_64.whl/tamcolors/tests/__init__.py
@@ -0,0 +1,16 @@
+from . import tam_tests
+from . import tam_tools_tests
+from . import tam_io_tests
+from . import tam_basic_tests
+from . import utils_tests
+from . import all_tests
+from . import test_utils
+
+
+__all__ = ("tam_tests",
+           "tam_tools_tests",
+           "tam_io_tests",
+           "all_tests",
+           "tam_basic_tests",
+           "utils_tests",
+           "test_utils")
