f27f991f53dcdc0855651cd97a354104c08967ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package cicdstatemgr/1.2.0/cicdstatemgr-1.2.0.tar.gz

---
+++ packages/cicdstatemgr/cicdstatemgr-1.2.0.tar.gz/cicdstatemgr-1.2.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cicdstatemgr",
+    version="1.2.0",
+    author="bitsofinfo",
+    author_email="bitsofinfo.g@gmail.com",
+    description="State management utility for CICD systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bitsofinfo/cicdstatemgr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'jinja2',
+        'pyyaml',
+        'redis ',
+        'jsonpath-ng', 
+        'requests'
+    ],
+    entry_points = {
+        "console_scripts": ['cicdstatemgr = cicdstatemgr.cli:main']
+    },
+    python_requires='>=3.8',
+)
