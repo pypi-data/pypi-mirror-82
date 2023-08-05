bd38c3fd2cf351607c947fe930db4e87f5a0f8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:51 +0000

    Add package ConnectSQl/1.0/ConnectSQl-1.0.tar.gz

---
+++ packages/ConnectSQl/ConnectSQl-1.0.tar.gz/ConnectSQl-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+with open('DESCRIPTION.txt') as file:
+    long_description = file.read()
+REQUIREMENTS=['mysql-connector-python~=8.0.21'],
+setup(name="ConnectSQl",
+      download_url="https://github.com/kailash1011/ConnectSQl/releases/tag/1.0",
+      version='1.0',
+      description="Connect Access & retrieve MYSQl databases.",
+      url="https://github.com/kailash1011/ConnectSQl",
+      author="Kailash Sharma",
+      author_email="kailashps.1011@gmail.com",
+      license="MIT",
+      classifiers=['Programming Language :: Python :: 3.6'],
+      long_description=long_description
+
+      )
