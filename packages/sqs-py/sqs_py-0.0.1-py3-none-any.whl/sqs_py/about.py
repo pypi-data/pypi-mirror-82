562c2501bbeddea4b82e5f397306b3379ae65f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package sqs-py/0.0.1/sqs_py-0.0.1-py3-none-any.whl

---
+++ packages/sqs-py/sqs_py-0.0.1-py3-none-any.whl/sqs_py/about.py
@@ -0,0 +1,17 @@
+MAJOR = 0
+MINOR = 0
+PATCH = 1
+SUFFIX = ""
+
+NAME = "sqs_py"
+
+AUTHOR = MAINTAINER = {
+    "name": "wengjiyao",
+    "email": "w.j@live.ca",
+}
+
+AUTHORS = MAINTAINERS = (
+    f"{AUTHOR.get('name')} <{AUTHOR.get('email')}>",
+)
+
+__version__ = VERSION = f"{MAJOR}.{MINOR}.{PATCH}{SUFFIX}"
