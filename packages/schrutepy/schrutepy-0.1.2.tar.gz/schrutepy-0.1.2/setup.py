3477efd1607d2406c5b07a23e1bf270a3625bdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package schrutepy/0.1.2/schrutepy-0.1.2.tar.gz

---
+++ packages/schrutepy/schrutepy-0.1.2.tar.gz/schrutepy-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "twine>=3", "wheel", "pip", "pandas"]
+
+setup(
+    name="schrutepy",
+    version="0.1.2",
+    author="Brad Lindblad",
+    author_email="bradley.lindblad@gmail.com",
+    description="The Entire Transcripts from the Office in Tidy Format",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bradlindblad/schrutepy",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
