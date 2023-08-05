19bef22ca6eb068ad1c6a91fab7fc34c4e503672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:40 +0000

    Add package wpdatautil/0.1.0/wpdatautil-0.1.0.tar.gz

---
+++ packages/wpdatautil/wpdatautil-0.1.0.tar.gz/wpdatautil-0.1.0/wpdatautil/pandas/__init__.py
@@ -0,0 +1,15 @@
+"""pandas utilities."""
+
+import pandas as pd
+
+
+def set_pandas_display_options() -> None:
+    """Set pandas display options."""
+    # Ref: https://stackoverflow.com/a/52432757/
+    display = pd.options.display
+
+    display.max_columns = 1000
+    display.max_rows = 1000
+    display.max_colwidth = 199
+    display.width = None
+    # display.precision = 2  # set as needed
