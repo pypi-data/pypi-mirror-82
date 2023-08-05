23c05ebdde57e7475789aed0d579d52538651a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:01 +0000

    Add package CDGARoasters/0.1/CDGARoasters-0.1.tar.gz

---
+++ packages/CDGARoasters/CDGARoasters-0.1.tar.gz/CDGARoasters-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CDGARoasters',
+      version='0.1',
+      description='Profile roasts',
+      url='https://github.com/haywoodIII/cdga_roasterspy',
+      author='Haywood Phillips',
+      author_email='haywoodphillips@gmail.com',
+      license='MIT',
+      packages=['cdga_roasters'],
+      zip_safe=False)
