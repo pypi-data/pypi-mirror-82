3511c94bd67a657fbac258f6f439617cbf45273a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:57 +0000

    Add package mb-cool-library/0.1.4/mb_cool_library-0.1.4.tar.gz

---
+++ packages/mb-cool-library/mb_cool_library-0.1.4.tar.gz/mb_cool_library-0.1.4/mb_cool_library/__init__.py
@@ -0,0 +1,1 @@
+import mb_cool_library.rek
---
+++ packages/mb-cool-library/mb_cool_library-0.1.4.tar.gz/mb_cool_library-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mb_cool_library",
+    version="0.1.4",
+    author="MB",
+    author_email="mb@dev.pl",
+    packages=find_packages(),
+    include_package_data=True,
+    description="Test test test"
+)
