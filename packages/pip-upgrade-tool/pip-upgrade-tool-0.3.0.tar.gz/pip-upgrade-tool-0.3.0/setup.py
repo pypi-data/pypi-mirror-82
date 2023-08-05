ee5c44ed0585051a1b75543098364a1b05822cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:25 +0000

    Add package pip-upgrade-tool/0.3.0/pip-upgrade-tool-0.3.0.tar.gz

---
+++ packages/pip-upgrade-tool/pip-upgrade-tool-0.3.0.tar.gz/pip-upgrade-tool-0.3.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pip-upgrade-tool",
+    version="0.3.0",
+    author="Onur Cetinkol",
+    author_email="realiti44@gmail.com",
+    description="An easy tool for upgrading all of your packages while not breaking dependencies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realiti4/pip-upgrade",
+    entry_points = {
+        'console_scripts': ['pip-upgrade = pip_upgrade:main',
+                            'pip-upgrade-legacy = pip_upgrade:legacy'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    packages=["pip_upgrade"],
+    # install_requires=["pandas", "requests"],
+)
