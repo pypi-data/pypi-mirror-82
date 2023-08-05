a282f4ac0f776f62f4d69fa8e51f4f5245e0f525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package IBATS-Common/0.20.4/IBATS_Common-0.20.4-py3-none-any.whl

---
+++ packages/IBATS-Common/IBATS_Common-0.20.4-py3-none-any.whl/ibats_common/backend/pair.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+"""
+@author  : MG
+@Time    : 2020/10/11 下午2:57
+@File    : pair.py
+@contact : mmmaaaggg@163.com
+@desc    : 配对交易相关函数库
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def pair_plot():
+    pass
+
+if __name__ == "__main__":
+    pass
