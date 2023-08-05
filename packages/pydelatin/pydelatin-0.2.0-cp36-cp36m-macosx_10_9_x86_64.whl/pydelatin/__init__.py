fc12f1601183b667bfc53e8e7c48171100f0d5cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:41 +0000

    Add package pydelatin/0.2.0/pydelatin-0.2.0-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/pydelatin/pydelatin-0.2.0-cp36-cp36m-macosx_10_9_x86_64.whl/pydelatin/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pydelatin."""
+
+__author__ = """Kyle Barron"""
+__email__ = 'kylebarron2@gmail.com'
+__version__ = '0.2.0'
+
+from . import util
+from .delatin import Delatin
