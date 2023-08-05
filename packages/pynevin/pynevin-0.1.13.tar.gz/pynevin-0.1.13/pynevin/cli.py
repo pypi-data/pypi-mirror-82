d6b076b3c89a6fd56bf3664cea4be493442e175c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:52 +0000

    Add package pynevin/0.1.13/pynevin-0.1.13.tar.gz

---
+++ packages/pynevin/pynevin-0.1.13.tar.gz/pynevin-0.1.13/pynevin/cli.py
@@ -0,0 +1,38 @@
+import click
+
+
+# from .visualsim import *
+
+def duck_art():
+    duck = """
+            ,----,
+       ___.`      `,
+       `===  D     :
+         `'.      .'
+            )    (                   ,
+           /      \_________________/|
+          /                          |
+         |                           ;
+         |               _____       /
+         |      \       ______7    ,'
+         |       \    ______7     /
+          \       `-,____7      ,'   jgs
+    ^~^~^~^`\                  /~^~^~^~^
+      ~^~^~^ `----------------' ~^~^~^
+     ~^~^~^~^~^^~^~^~^~^~^~^~^~^~^~^~
+    """
+    return duck
+
+
+@click.command()
+@click.option('--verbose', is_flag=True, help="Will print verbose messages.")
+@click.option('--name', default='', help='Who are you?')
+# @click.option('--visualsim', is_flag=False, help='CEG 4136 Lab Launcher Tool')
+def cli(verbose, name):
+    click.echo(duck_art())
+    click.echo("Hello World. Welcome to PyNevin CLI")
+    if verbose:
+        click.echo("We are in the verbose mode.")
+    # if visualsim_bool:
+    # run()
+    click.echo('Bye {0}'.format(name))
