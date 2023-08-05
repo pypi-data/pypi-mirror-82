7fc850a0728b045e76ef8493dc735bd39a351466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:47 +0000

    Add package dicthandling/0.2b2/dicthandling-0.2b2.tar.gz

---
+++ packages/dicthandling/dicthandling-0.2b2.tar.gz/dicthandling-0.2b2/README.md
@@ -0,0 +1,38 @@
+# dicthandling
+
+`dicthandling` is a python module containing convenient methods handling python
+dictionaries. [Read-the-docs](https://dicthandling.readthedocs.io/en/latest/) for
+a detailed description of its methods.
+
+## Installing
+
+Installing the latest release using pip is recommended.
+
+````shell script
+$ pip install dicthandling
+````
+
+The latest development state can be obtained from gitlab using pip.
+
+````shell script
+$ pip install git+https://gitlab.com/david.scheliga/dicthandling.git@dev
+````
+
+## Authors
+
+* **David Scheliga** 
+    [@gitlab](https://gitlab.com/david.scheliga)
+    [@Linkedin](https://www.linkedin.com/in/david-scheliga-576984171/)
+    - Initial work
+    - Maintainer
+
+## License
+
+This project is licensed under the GNU GENERAL PUBLIC LICENSE - see the
+[LICENSE](https://gitlab.com/david.scheliga/dicthandling/blob/master/LICENSE) file for details
+
+## Acknowledge
+
+- [Code style: black](https://github.com/psf/black)
+- [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
+- [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
---
+++ packages/dicthandling/dicthandling-0.2b2.tar.gz/dicthandling-0.2b2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+import dicthandling
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="dicthandling",
+    version=dicthandling.__version__,
+    author="David Scheliga",
+    author_email="david.scheliga@gmx.de",
+    url="https://gitlab.com/david.scheliga/dicthandling",
+    description="Frequently used methods used for handling nested dictionaries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GNU General Public License v3 (GPLv3)",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="dictionary, nested, handling",
+    py_modules=["dicthandling"],
+    python_requires='>=3.6',
+)
