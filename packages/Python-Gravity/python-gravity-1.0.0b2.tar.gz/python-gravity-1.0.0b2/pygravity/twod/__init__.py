c92a40bb13885cea64793148df8eba9d36e5aa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:32 +0000

    Add package Python-Gravity/1.0.0b2/python-gravity-1.0.0b2.tar.gz

---
+++ packages/Python-Gravity/python-gravity-1.0.0b2.tar.gz/python-gravity-1.0.0b2/pygravity/twod/__init__.py
@@ -0,0 +1,7 @@
+from .vector import Vector2
+from .gravity import GravityContainer, GravityCaster, GravityAcceptor
+from .physics import PhysicsManager
+
+__all__ = ['Vector2',
+           'GravityContainer', 'GravityCaster', 'GravityAcceptor',
+           'PhysicsManager']
