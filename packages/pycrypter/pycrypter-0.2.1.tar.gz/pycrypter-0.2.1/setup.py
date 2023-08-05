3a50e4a7c72b8777743d65f2394883baab5f7661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:45 +0000

    Add package pycrypter/0.2.1/pycrypter-0.2.1.tar.gz

---
+++ packages/pycrypter/pycrypter-0.2.1.tar.gz/pycrypter-0.2.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pycrypter']
+setup_kwargs = {
+    'name': 'pycrypter',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Bekhs',
+    'author_email': 'mypswset@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
