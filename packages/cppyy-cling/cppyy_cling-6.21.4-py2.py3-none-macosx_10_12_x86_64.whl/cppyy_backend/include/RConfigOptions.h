b45338ea9b0b9ed8854973e0593b13ce3c178db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:48 +0000

    Add package cppyy-cling/6.21.4/cppyy_cling-6.21.4-py2.py3-none-macosx_10_12_x86_64.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.21.4-py2.py3-none-macosx_10_12_x86_64.whl/cppyy_backend/include/RConfigOptions.h
@@ -0,0 +1,7 @@
+#ifndef ROOT_RConfigOptions
+#define ROOT_RConfigOptions
+
+#define R__CONFIGUREOPTIONS   "Backtrace_INCLUDE_DIR=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/usr/include CLANG_EXECUTABLE_VERSION=5.0 CMAKE_CACHE_MAJOR_VERSION=3 CMAKE_CACHE_MINOR_VERSION=17 CMAKE_CACHE_PATCH_VERSION=2 DL_LIBRARY_PATH=/usr/lib/libdl.dylib LIBCLANG_LIBRARY_VERSION=5.0 LIBXML2_INCLUDE_DIR=/opt/local/include/libxml2 LIBXML2_LIBRARY=/opt/local/lib/libxml2.dylib LLVM_DYLIB_SYMBOL_VERSIONING=OFF LLVM_INCLUDE_EXAMPLES=OFF LLVM_INCLUDE_RUNTIMES=ON LLVM_INCLUDE_TOOLS=ON LLVM_INCLUDE_UTILS=ON LLVM_VERSION_PRINTER_SHOW_HOST_TARGET_INFO=ON PC_LIBXML_INCLUDEDIR=/opt/local/include PC_LIBXML_INCLUDE_DIRS=/opt/local/include/libxml2 PC_LIBXML_LIBRARIES=xml2 PC_LIBXML_LIBRARY_DIRS=/opt/local/lib PC_LIBXML_STATIC_INCLUDE_DIRS=/opt/local/include/libxml2 PC_LIBXML_STATIC_LIBRARY_DIRS=/opt/local/lib PC_LIBXML_VERSION=2.9.10 ZLIB_INCLUDE_DIR=/Users/wlav/wheelie/cppyy-backend/cling/src/builtins/zlib ZLIB_INCLUDE_DIRS=/Users/wlav/wheelie/cppyy-backend/cling/src/builtins/zlib ZLIB_LIBRARIES=ZLIB::ZLIB ZLIB_LIBRARY=$<TARGET_FILE:ZLIB> ZLIB_VERSION=1.2.8 ZLIB_VERSION_STRING=1.2.8 "
+#define R__CONFIGUREFEATURES  "cxx17  builtin_clang builtin_llvm builtin_zlib libcxx shared"
+
+#endif
