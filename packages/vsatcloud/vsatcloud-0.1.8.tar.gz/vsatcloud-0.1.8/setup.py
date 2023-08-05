48b1c767f67fa5c921cc5ddc3baf843d4259fb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:28 +0000

    Add package vsatcloud/0.1.8/vsatcloud-0.1.8.tar.gz

---
+++ packages/vsatcloud/vsatcloud-0.1.8.tar.gz/vsatcloud-0.1.8/setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name='vsatcloud',  # 包的名字
+    author='vsattech.com',  # 作者
+    version='0.1.8',  # 版本号
+    description='vsat-cloud python sdk',  # 描述
+    long_description="",
+    long_description_content_type="text/markdown",
+    author_email='it@vsattech.com',  # 你的邮箱**
+    url='https://vsat.cloud',  # 可以写github上的地址，或者其他地址
+    packages=setuptools.find_packages(),
+
+    # 依赖包
+    install_requires=['requests >= 2.19.1'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=True,
+)
---
+++ packages/vsatcloud/vsatcloud-0.1.8.tar.gz/vsatcloud-0.1.8/vsatcloud/scene.py
@@ -0,0 +1,55 @@
+import os
+import time
+from abc import ABC, abstractmethod
+
+from .common import load_params, ensure_data_directories_exist, load_step_info, load_resources, get_file_name, \
+    save_step_info, SHARE_PATH
+from .logging import get_logger
+logger = get_logger(__name__)
+
+
+class ProcessingScene(ABC):
+    """
+    Base class for processing Scene.
+    """
+    @abstractmethod
+    def process(self):
+        raise NotImplementedError
+
+    @classmethod
+    def get_instance(cls, params):
+        """
+        获取实例
+        """
+        return cls(params)
+
+    @classmethod
+    def run(cls):
+        """
+        This method is the main entry point for the processing scene.
+        """
+        ensure_data_directories_exist()
+        params: dict = load_params()
+        step_info: dict = load_step_info()
+        resources: dict = load_resources()
+        algorithm_block = cls.get_instance(params)
+        setattr(algorithm_block, "__resources", resources)
+        setattr(algorithm_block, "__input_files", step_info)
+        first_key = ""
+        for key in step_info:
+            first_key = key
+            break
+        print(first_key)
+        print(step_info[first_key])
+        output_file_path = algorithm_block.process(
+            step_info[first_key])
+        if output_file_path is None or not os.path.exists(output_file_path):
+            file_name = "qwertyuiop.text"
+        else:
+            file_name = get_file_name(output_file_path)
+            import shutil
+            shutil.move(output_file_path, SHARE_PATH + "/" + file_name)  # 移动
+        next_step_info = {"input_file_path": SHARE_PATH + "/" + file_name}
+        save_step_info(next_step_info)
+
+
