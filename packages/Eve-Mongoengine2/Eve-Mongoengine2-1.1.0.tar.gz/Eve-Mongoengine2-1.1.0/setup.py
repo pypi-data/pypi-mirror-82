98adc4d0bc722669e376f6761916af91e60657ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:05 +0000

    Add package Eve-Mongoengine2/1.1.0/Eve-Mongoengine2-1.1.0.tar.gz

---
+++ packages/Eve-Mongoengine2/Eve-Mongoengine2-1.1.0.tar.gz/Eve-Mongoengine2-1.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+__version__ = "1.1.0"
+
+setup(
+    name="Eve-Mongoengine2",
+    version=__version__,
+    url="https://github.com/wangsha/eve-mongoengine",
+    author="Wang Sha",
+    description="An Eve extension for Mongoengine ODM support",
+    packages=["eve_mongoengine"],
+    zip_safe=False,
+    test_suite="tests",
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Eve", "Mongoengine", "blinker"],
+)
