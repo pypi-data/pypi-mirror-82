1d07d00a8b3bcaa0b119552fe2f415c1a2a8e6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package pyblas/0.0.1/pyblas-0.0.1.tar.gz

---
+++ packages/pyblas/pyblas-0.0.1.tar.gz/pyblas-0.0.1/pyblas/level1/scopy.py
@@ -0,0 +1,104 @@
+# > \brief \b SCOPY
+#
+#  =========== DOCUMENTATION ===========
+#
+# Online html documentation available at
+#            http://www.netlib.org/lapack/explore-html/
+#
+#  Definition:
+#  ===========
+#
+#       def SCOPY(N,SX,INCX,SY,INCY)
+#
+#       .. Scalar Arguments ..
+#       INTEGER INCX,INCY,N
+#       ..
+#       .. Array Arguments ..
+#       REAL SX(*),SY(*)
+#       ..
+#
+#
+# > \par Purpose:
+#  =============
+# >
+# > \verbatim
+# >
+# >    SCOPY copies a vector, x, to a vector, y.
+# >    uses unrolled loops for increments equal to 1.
+# > \endverbatim
+#
+#  Arguments:
+#  ==========
+#
+# > \param[in] N
+# > \verbatim
+# >          N is INTEGER
+# >         number of elements in input vector(s)
+# > \endverbatim
+# >
+# > \param[in] SX
+# > \verbatim
+# >          SX is REAL array, dimension ( 1 + ( N - 1 )*abs( INCX ) )
+# > \endverbatim
+# >
+# > \param[in] INCX
+# > \verbatim
+# >          INCX is INTEGER
+# >         storage spacing between elements of SX
+# > \endverbatim
+# >
+# > \param[out] SY
+# > \verbatim
+# >          SY is REAL array, dimension ( 1 + ( N - 1 )*abs( INCY ) )
+# > \endverbatim
+# >
+# > \param[in] INCY
+# > \verbatim
+# >          INCY is INTEGER
+# >         storage spacing between elements of SY
+# > \endverbatim
+#
+#  Authors:
+#  ========
+#
+# > \author Univ. of Tennessee
+# > \author Univ. of California Berkeley
+# > \author Univ. of Colorado Denver
+# > \author NAG Ltd.
+#
+# > \date November 2017
+#
+# > \ingroup single_blas_level1
+#
+# > \par Further Details:
+#  =====================
+# >
+# > \verbatim
+# >
+# >     jack dongarra, linpack, 3/11/78.
+# >     modified 12/3/93, array(1) declarations changed to array(*)
+# > \endverbatim
+# >
+#  =====================================================================
+from ..util import slice_
+
+
+def scopy(N, SX, INCX, SY, INCY):
+    #
+    #  -- Reference BLAS level1 routine (version 3.8.0) --
+    #  -- Reference BLAS is a software package provided by Univ. of Tennessee,    --
+    #  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+    #     November 2017
+    #
+    #     .. Scalar Arguments ..
+    # INTEGER INCX,INCY,N
+    #     ..
+    #     .. Array Arguments ..
+    # REAL SX(*),SY(*)
+    #     ..
+    #
+    #  =====================================================================
+
+    if N <= 0:
+        return
+    SY[slice_(N, INCY)] = SX[slice_(N, INCX)]
