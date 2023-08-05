4a905d0a6ecad7ad4880c3afebe26f43cc8641cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package WebpCliWrapper/0.1.3/WebpCliWrapper-0.1.3.tar.gz

---
+++ packages/WebpCliWrapper/WebpCliWrapper-0.1.3.tar.gz/WebpCliWrapper-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='WebpCliWrapper',
+    version='0.1.3',
+    description='A CLI Wrapper to use webp easily',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Florian Erbs',
+    author_email='f@flopana.com',
+    keywords=['webp', 'webp-cli-wrapper'],
+    url='https://gitlab.aptinstall.de/flopana/webp-cli-wrapper',
+    download_url='https://pypi.org/project/WebpCliWrapper/'
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
