536c740b6996bbd6cd814695843d4418f5e1d60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package simplysql/0.1/simplysql-0.1-py3-none-any.whl

---
+++ packages/simplysql/simplysql-0.1-py3-none-any.whl/simplysql/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3.7.6
+
+from simplysql.base_interfaces.database import DataBase
+
+__author__ = "Pit Nahrstedt"
+__author_email = "luanee@web.de"
+__description__ = "A python package for easier handling of SQL files"
+__name__ = "simplysql"
+__version__ = "0.1.0"
+__url__ = "https://github.com/Luanee/simplysql"
+
+name = __name__
