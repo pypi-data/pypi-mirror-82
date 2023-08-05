e4c1245fe52f404a069bbdd11153dace7bac83d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:08 +0000

    Add package pycertainties/1.0.2/pycertainties-1.0.2-py38-none-any.whl

---
+++ packages/pycertainties/pycertainties-1.0.2-py38-none-any.whl/pycertainties/calculations.py
@@ -0,0 +1,101 @@
+from typing import Dict, Iterable, List, Union
+
+import sympy as sp
+from sympy.core.expr import Expr
+from sympy.parsing.sympy_parser import parse_expr
+
+from pycertainties import utilities as utils
+from pycertainties.val import Real, Val
+
+IterableValOrReal = Iterable[Union["Val", Real, "IterableValOrReal"]]  # type: ignore
+ListValOrReal = List[Union["Val", Real, "ListValOrReal"]]  # type: ignore
+
+
+def uncertainty(expr: Union[str, Expr], *variables: str) -> Expr:
+    """
+    Given either a string representation of an equation or sympy expression, and any number of 'variables', which
+    represent values which have an associated uncertainty (vs cosntants, which have no uncertainty), returns a sympy
+    expression representing the uncertainty of the equation.
+
+    Example)
+        repr(uncertainty("x*y + z", "x", "y")) == "sqrt(dx**2*y**2 + dy**2*x**2)"
+    """
+    if isinstance(expr, str):
+        expr = parse_expr(expr)
+
+    return sp.sqrt(
+        sum(
+            sp.diff(expr, sym) ** 2 * sp.symbols("δ" + sym.name) ** 2
+            for sym in expr.free_symbols
+            if sym.name in variables
+        )
+    )
+
+
+def calculate(expr: Union[str, Expr], **values: Union["Val", Real, IterableValOrReal]) -> Union["Val", ListValOrReal]:
+    """
+    Given either a string representation of an equation or sympy expression, and keys corresponding to each symbol in
+    the eqtn/expr mapped to values of the symbols, calculates and returns result of that equation.
+
+    Each value may either be a:
+        1) int/float
+        2) A Val object
+        3) An iterable of more iterables or Val objects
+
+    All iterable values must be of the same shape. The return type depends on if there were any iterable values.
+        1) If there were no iterable values
+            - A single Val object with the result
+        2) If there were any iterable values (all of the same shape)
+            - A list of the same shape as the iterables with scalers of Val types
+
+    Example)
+        calculate("x*y + z", x=Val(3, 0.1), y=Val(3, 1), z=4) == Val(13.0, 3.0149626863362666)
+
+        calculate("x*y + z", x=Val(3, 0.1), y=[Val(3, 1), [Val(5, 1)]], z=4)
+            == [Val(13.0, 3.0149626863362666), [Val(19.0, 3.0413812651491092)]]
+    """
+    if isinstance(expr, str):
+        expr = parse_expr(expr)
+
+    # pylint: disable=W1116
+    constants = {key: value for key, value in values.items() if not isinstance(value, Iterable)}
+    iterables = {key: value for key, value in values.items() if isinstance(value, Iterable)}
+
+    if any(iterables):
+        return utils.operate_recursive(
+            lambda *items: _calculate(expr, **dict(zip(iterables.keys(), items)), **constants),
+            *iterables.values(),
+        )
+    else:
+        return _calculate(expr, **constants)
+
+
+def _calculate(expr: Union[str, Expr], **values: Union[Val, Real]) -> Val:
+    """
+    Performs the same calculations as _calculate(...); however, all values must either be int/floats or Val types.
+    """
+    if isinstance(expr, str):
+        expr = parse_expr(expr)
+
+    uncertainty_expr = uncertainty(expr, *(key for key, value in values.items() if isinstance(value, Val)))
+    to_sub = _substitution_map(**values)
+    return Val(float(expr.subs(to_sub).evalf()), float(uncertainty_expr.subs(to_sub).evalf()))
+
+
+def _substitution_map(**values: Union[Val, Real]) -> Dict[str, Real]:
+    """
+    Given string keys mapping to int/float/Val values, creates a map where:
+        1) If the value is an int/float
+            - The key is mapped to the value
+        2) If the value is a Val object
+            - The key is mapped to Val.value
+            - The string f"δ{key}" is mapped to Val.uncertainty
+    """
+    to_sub = {}
+    for sym, val in values.items():
+        if isinstance(val, Val):
+            to_sub[sym] = val.value
+            to_sub["δ" + sym] = val.uncertainty
+        else:
+            to_sub[sym] = val
+    return to_sub
---
+++ packages/pycertainties/pycertainties-1.0.2-py38-none-any.whl/pycertainties/val.py
@@ -0,0 +1,126 @@
+from dataclasses import dataclass
+from typing import Iterable, Union
+
+import numpy as np
+
+from pycertainties.strings import Real, uncertainty_str
+
+IterableReal = Iterable[Union[Real, "IterableReal"]]  # type: ignore
+RecursiveReal = Union[Real, IterableReal]  # type: ignore
+
+
+@dataclass
+class Val:
+    """
+    This class is number-like type that represents a value and associated uncertainty.
+
+    Implements common mathmetical operators and a few mathmatics functions. Uses the following equation calulating
+    uncertainties:
+        δf(r_i) = √(Σ(df/dr_i^2*δr_i^2))
+
+    Additionally, Val(a, b) comes with appropriate string conversions based on the relative and absolute values of
+    a and b.
+
+    Examples)
+                                  Val(10, 4) - Val(4, 3) == Val(-10.0, 5.0)
+                                       Val(63.5, 1) ** 3 == Val(256047.875, 12096.75)
+                            Val(100, 2) + Val(50.5, 0.2) == Val(150.5, 2.009975124224178)
+                                      Val(100, .1).sin() == Val(-0.5063656411097588, 0.08623188722876839)
+                     Val(4.605170185988092, 0.001).log() == Val(4.605170185988092, 0.001)
+
+                                  str(Val(321.8, .0324)) == "321.80 ± 0.03"
+                                str(Val(321.856, .0324)) == "321.86 ± 0.03"
+                                 str(Val(321.856, 3.86)) == "322 ± 4"
+                               str(Val(-321.856, 11.34)) == "-322 ± 11"
+                         str(Val(3.21856e-10, 3.24e-12)) == "(3.22 ± 0.03)e-10"
+                            str(Val(3.21856e10, 3.24e8)) == "(3.22 ± 0.03)e10"
+                            str(Val(3.21856e10, 1.24e8)) == "(3.219 ± 0.012)e10"
+                str(Val(0.02094495456, 9.541774545e-05)) == "0.020945 ± 0.000095"
+                str(Val(0.02094495456, 9.341774545e-05)) == "0.02094 ± 0.00009"
+        str(Val(3559.8838983606497, 21.815841616631992)) == "3560 ± 20"
+
+    """
+
+    value: Real
+    uncertainty: Real
+
+    def __str__(self) -> str:
+        return uncertainty_str(self.value, self.uncertainty)
+
+    def __repr__(self) -> str:
+        return f"Val({self.value}, {self.uncertainty})"
+
+    def __format__(self, format_spec: str) -> str:
+        return str(self)
+
+    def __mul__(self, other: Union["Val", Real]) -> "Val":
+        if isinstance(other, Val):
+            return Val(
+                self.value * other.value,
+                np.sqrt((self.value ** 2) * (other.uncertainty ** 2) + (other.value ** 2) * (self.uncertainty ** 2)),
+            )
+        else:
+            return Val(self.value * other, self.uncertainty * abs(other))
+
+    def __rmul__(self, other: Real) -> "Val":
+        return self.__mul__(other)
+
+    def __truediv__(self, other: Union["Val", Real]) -> "Val":
+        if isinstance(other, Val):
+            return Val(
+                self.value / other.value,
+                np.sqrt(
+                    (other.uncertainty ** 2) * (self.value ** 2 / other.value ** 4)
+                    + (self.uncertainty ** 2 / other.value ** 2)
+                ),
+            )
+        else:
+            return Val(self.value / other, self.uncertainty / abs(other))
+
+    def __rtruediv__(self, other: Real) -> "Val":
+        return Val(other, 0) / self
+
+    def __sub__(self, other: Union["Val", Real]) -> "Val":
+        if isinstance(other, Val):
+            return Val(self.value - other.value, np.sqrt(self.uncertainty ** 2 + other.uncertainty ** 2))
+        else:
+            return Val(self.value - other, self.uncertainty)
+
+    def __rsub__(self, other: Real) -> "Val":
+        return Val(other, 0) - self
+
+    def __add__(self, other: Union["Val", Real]) -> "Val":
+        if isinstance(other, Val):
+            return Val(self.value + other.value, np.sqrt(self.uncertainty ** 2 + other.uncertainty ** 2))
+        else:
+            return Val(self.value + other, self.uncertainty)
+
+    def __radd__(self, other: "Val") -> "Val":
+        return self.__add__(other)
+
+    def __neg__(self: "Val") -> "Val":
+        return Val(-self.value, self.uncertainty)
+
+    def __pow__(self, power: Union["Val", Real]) -> "Val":
+        if isinstance(power, Val):
+            square = self.value ** (2 * power.value)
+            first = (self.uncertainty ** 2) * ((square * power.value ** 2) / (self.value ** 2))
+            second = (power.uncertainty ** 2) * (square * np.log(self.value) ** 2)
+            return Val(self.value ** power.value, np.sqrt(first + second))
+        else:
+            square = self.value ** (2 * power)
+            return Val(
+                self.value ** power, np.sqrt((self.uncertainty ** 2) * ((square * power ** 2) / (self.value ** 2)))
+            )
+
+    def __rpow__(self, other: Real) -> "Val":
+        return Val(other, 0) ** self
+
+    def log(self) -> "Val":
+        return Val(np.log(self.value), np.sqrt((self.uncertainty ** 2) / (self.value ** 2)))
+
+    def sin(self) -> "Val":
+        return Val(np.sin(self.value), np.sqrt((self.uncertainty ** 2) * (np.cos(self.value) ** 2)))
+
+    def sqrt(self) -> "Val":
+        return self ** (1 / 2)
