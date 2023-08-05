44856c518e30525b127b5efcf2821c042b789ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:56 +0000

    Add package TkinterExtensions/1.3.8/TkinterExtensions-1.3.8-py3-none-any.whl

---
+++ packages/TkinterExtensions/TkinterExtensions-1.3.8-py3-none-any.whl/TkinterExtensions/__init__.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  Created by Tyler Stegmaier.
+#  Copyright (c) 2020.
+#
+# ------------------------------------------------------------------------------
+
+
+from .Extensions import *
+from .Bindings import *
+from .Enumerations import *
+from .Events import *
+from .HID import *
+from .Helpers import *
+from .Widgets import *
+from .debug import *
+from .AnimatedGIF import *
