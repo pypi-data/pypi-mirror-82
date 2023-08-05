136c78414af1c0c2e1ea2b6eedfcc7248fc31182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package pykdc/1.4.0/pykdc-1.4.0.tar.gz

---
+++ packages/pykdc/pykdc-1.4.0.tar.gz/pykdc-1.4.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    reshapedata LLC
+"""
+import platform
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'pykdc',
+    version = '1.4.0',
+    install_requires=[
+        'requests',
+    ],
+    packages=find_packages(),
+    license = 'Apache License',
+    author = 'Reshapedata',
+    author_email = 'hulilei@takewiki.com.cn',
+    url = 'http://www.reshapedata.com',
+    description = 'reshape kdc webapi in py language ',
+    keywords = ['reshapedata', 'pykdc','kdcapi'],
+    python_requires='>=3.6',
+)
