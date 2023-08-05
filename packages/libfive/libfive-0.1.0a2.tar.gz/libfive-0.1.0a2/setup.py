3f1eb814b82c9dc53b28acec201bc6b973c380d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:13 +0000

    Add package libfive/0.1.0a2/libfive-0.1.0a2.tar.gz

---
+++ packages/libfive/libfive-0.1.0a2.tar.gz/libfive-0.1.0a2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['libfive']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'libfive',
+    'version': '0.1.0a2',
+    'description': 'libfive bindings for Python',
+    'long_description': '# libfive-python\n\nThis is a work in progress Python library for using [libfive](https://libfive.com/) from Python.\n',
+    'author': 'Sem Mulder',
+    'author_email': 'sem@mulderke.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/SemMulder/libfive-python',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+from build import *
+build(setup_kwargs)
+
+setup(**setup_kwargs)
