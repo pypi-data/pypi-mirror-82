7429c5ef7e78151ec1a1b8bf23252c71b1108785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:18 +0000

    Add package swehockey-scraper/0.2/swehockey_scraper-0.2.tar.gz

---
+++ packages/swehockey-scraper/swehockey_scraper-0.2.tar.gz/swehockey_scraper-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='swehockey_scraper',
+    version='0.2',
+    description='Functions to scrape ice hockey data and statistics from swehockey',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Marcus Sjölin',
+    author_email='marcussjolin89@gmail.com',
+    keywords=['ice hockey', 'scraping', 'sport analytics', 'shl', 'analys', 'ishockey', 'hockeyallsvenskan'],
+    url='https://github.com/msjoelin/swehockey_scraper',
+    download_url='https://pypi.org/project/swehockey_scraper/'
+)
+
+install_requires = [
+    'numpy',
+    'pandas',
+    'bs4',
+    'datetime'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
