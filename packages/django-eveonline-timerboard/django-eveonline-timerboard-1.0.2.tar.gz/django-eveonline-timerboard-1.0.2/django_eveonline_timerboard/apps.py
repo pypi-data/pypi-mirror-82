f37f234dc562056fbe47c4eaefc2091aa29903a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:31 +0000

    Add package django-eveonline-timerboard/1.0.2/django-eveonline-timerboard-1.0.2.tar.gz

---
+++ packages/django-eveonline-timerboard/django-eveonline-timerboard-1.0.2.tar.gz/django-eveonline-timerboard-1.0.2/django_eveonline_timerboard/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoEveonlineTimerboardConfig(AppConfig):
+    name = 'django_eveonline_timerboard'
+    verbose_name = 'EVE Online Timers'
+    url_slug = 'eveonline'
