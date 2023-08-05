5c0feadd5490b629b6a6ab0b9413ef5f8cd06af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:59 +0000

    Add package conv-lstm/0.1.1/conv_lstm-0.1.1.tar.gz

---
+++ packages/conv-lstm/conv_lstm-0.1.1.tar.gz/conv_lstm-0.1.1/README.md
@@ -0,0 +1,15 @@
+# conv_lstm_pytorch
+
+This version is forked and adapted from https://github.com/ndrplz/ConvLSTM_pytorch.
+
+Original authors are:
+
+- https://github.com/ndrplz/ConvLSTM_pytorch
+- https://github.com/DavideA
+- https://github.com/rogertrullo/pytorch_convlstm
+
+Please checkout the code itself and unit tests for details usage and examples.
+
+## Code style
+
+This repo is managed by poetry, formatted by black, isort, and type checked by mypy.
---
+++ packages/conv-lstm/conv_lstm-0.1.1.tar.gz/conv_lstm-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['conv_lstm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch>=1.5.0']
+
+setup_kwargs = {
+    'name': 'conv-lstm',
+    'version': '0.1.1',
+    'description': 'A PyTorch implementation for convolutional LSTM',
+    'long_description': '# conv_lstm_pytorch\n\nThis version is forked and adapted from https://github.com/ndrplz/ConvLSTM_pytorch.\n\nOriginal authors are:\n\n- https://github.com/ndrplz/ConvLSTM_pytorch\n- https://github.com/DavideA\n- https://github.com/rogertrullo/pytorch_convlstm\n\nPlease checkout the code itself and unit tests for details usage and examples.\n\n## Code style\n\nThis repo is managed by poetry, formatted by black, isort, and type checked by mypy.\n',
+    'author': 'Andrea Palazzi',
+    'author_email': 'andrea.palazzi@unimore.it',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Jimexist/conv_lstm_pytorch',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
