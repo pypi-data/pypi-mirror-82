2b854ff9467a2d8f0143f71689f1b2ec85c7f1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:14 +0000

    Add package footballbrainz-data-models/0.0.4/footballbrainz_data_models-0.0.4.tar.gz

---
+++ packages/footballbrainz-data-models/footballbrainz_data_models-0.0.4.tar.gz/footballbrainz_data_models-0.0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="footballbrainz_data_models",
+    version="0.0.4",
+    description="Data models for footballbrainz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Casimir Desarmeaux",
+    package_dir={"": "footballbrainz_data_models"},
+    packages=setuptools.find_packages(where="footballbrainz_data_models"),
+    install_requires=[],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+)
