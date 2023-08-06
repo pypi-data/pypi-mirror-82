1936a8ee5779d58f539cbaf8ae84c1e55884f071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:28 +0000

    Add package c7n-kube/0.2.6/c7n_kube-0.2.6-py3-none-any.whl

---
+++ packages/c7n-kube/c7n_kube-0.2.6-py3-none-any.whl/c7n_kube/filters.py
@@ -0,0 +1,7 @@
+# Copyright 2017-2018 Capital One Services, LLC
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+
+
+class PolicyFilter:
+    pass
---
+++ packages/c7n-kube/c7n_kube-0.2.6-py3-none-any.whl/c7n_kube/resources/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2017-2018 Capital One Services, LLC
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+#
