23f80d56e98875c51997c29fbf4e52953f97b8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:09 +0000

    Add package funsor/0.3.0/funsor-0.3.0-py3-none-any.whl

---
+++ packages/funsor/funsor-0.3.0-py3-none-any.whl/funsor/integrate.py
@@ -0,0 +1,182 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+from collections import OrderedDict
+from typing import Union
+
+import funsor.ops as ops
+from funsor.cnf import Contraction, GaussianMixture
+from funsor.delta import Delta
+from funsor.gaussian import Gaussian, align_gaussian, _mv, _trace_mm, _vv
+from funsor.tensor import Tensor
+from funsor.terms import (
+    Funsor,
+    FunsorMeta,
+    Number,
+    Subs,
+    Unary,
+    Variable,
+    _convert_reduced_vars,
+    eager,
+    normalize,
+    substitute,
+    to_funsor
+)
+
+
+class IntegrateMeta(FunsorMeta):
+    """
+    Wrapper to convert reduced_vars arg to a frozenset of str.
+    """
+    def __call__(cls, log_measure, integrand, reduced_vars):
+        reduced_vars = _convert_reduced_vars(reduced_vars)
+        return super().__call__(log_measure, integrand, reduced_vars)
+
+
+class Integrate(Funsor, metaclass=IntegrateMeta):
+    """
+    Funsor representing an integral wrt a log density funsor.
+
+    :param Funsor log_measure: A log density funsor treated as a measure.
+    :param Funsor integrand: An integrand funsor.
+    :param reduced_vars: An input name or set of names to reduce.
+    :type reduced_vars: str, Variable, or set or frozenset thereof.
+    """
+    def __init__(self, log_measure, integrand, reduced_vars):
+        assert isinstance(log_measure, Funsor)
+        assert isinstance(integrand, Funsor)
+        assert isinstance(reduced_vars, frozenset)
+        assert all(isinstance(v, str) for v in reduced_vars)
+        inputs = OrderedDict((k, d) for term in (log_measure, integrand)
+                             for (k, d) in term.inputs.items()
+                             if k not in reduced_vars)
+        output = integrand.output
+        fresh = frozenset()
+        bound = reduced_vars
+        super(Integrate, self).__init__(inputs, output, fresh, bound)
+        self.log_measure = log_measure
+        self.integrand = integrand
+        self.reduced_vars = reduced_vars
+
+    def _alpha_convert(self, alpha_subs):
+        assert self.bound.issuperset(alpha_subs)
+        reduced_vars = frozenset(alpha_subs.get(k, k) for k in self.reduced_vars)
+        alpha_subs = {k: to_funsor(v, self.integrand.inputs.get(k, self.log_measure.inputs.get(k)))
+                      for k, v in alpha_subs.items()}
+        log_measure = substitute(self.log_measure, alpha_subs)
+        integrand = substitute(self.integrand, alpha_subs)
+        return log_measure, integrand, reduced_vars
+
+
+@normalize.register(Integrate, Funsor, Funsor, frozenset)
+def normalize_integrate(log_measure, integrand, reduced_vars):
+    return Contraction(ops.add, ops.mul, reduced_vars, log_measure.exp(), integrand)
+
+
+@normalize.register(Integrate,
+                    Contraction[Union[ops.NullOp, ops.LogAddExpOp], ops.AddOp, frozenset, tuple],
+                    Funsor, frozenset)
+def normalize_integrate_contraction(log_measure, integrand, reduced_vars):
+    delta_terms = [t for t in log_measure.terms if isinstance(t, Delta)
+                   and t.fresh.intersection(reduced_vars, integrand.inputs)]
+    for delta in delta_terms:
+        integrand = integrand(**{name: point for name, (point, log_density) in delta.terms
+                                 if name in reduced_vars.intersection(integrand.inputs)})
+    return normalize_integrate(log_measure, integrand, reduced_vars)
+
+
+@eager.register(Contraction, ops.AddOp, ops.MulOp, frozenset,
+                Unary[ops.ExpOp, Union[GaussianMixture, Delta, Gaussian, Number, Tensor]],
+                (Variable, Delta, Gaussian, Number, Tensor, GaussianMixture))
+def eager_contraction_binary_to_integrate(red_op, bin_op, reduced_vars, lhs, rhs):
+
+    if reduced_vars - reduced_vars.intersection(lhs.inputs, rhs.inputs):
+        args = red_op, bin_op, reduced_vars, (lhs, rhs)
+        result = eager.dispatch(Contraction, *args)(*args)
+        if result is not None:
+            return result
+
+    args = lhs.log(), rhs, reduced_vars
+    result = eager.dispatch(Integrate, *args)(*args)
+    if result is not None:
+        return result
+
+    return None
+
+
+@eager.register(Integrate, GaussianMixture, Funsor, frozenset)
+def eager_integrate_gaussianmixture(log_measure, integrand, reduced_vars):
+    real_vars = frozenset(k for k in reduced_vars if log_measure.inputs[k].dtype == 'real')
+    if reduced_vars <= real_vars:
+        discrete, gaussian = log_measure.terms
+        return discrete.exp() * Integrate(gaussian, integrand, reduced_vars)
+    return None
+
+
+########################################
+# Delta patterns
+########################################
+
+@eager.register(Integrate, Delta, Funsor, frozenset)
+def eager_integrate(delta, integrand, reduced_vars):
+    if not reduced_vars & delta.fresh:
+        return None
+    subs = tuple((name, point) for name, (point, log_density) in delta.terms
+                 if name in reduced_vars)
+    new_integrand = Subs(integrand, subs)
+    new_log_measure = Subs(delta, subs)
+    result = Integrate(new_log_measure, new_integrand, reduced_vars - delta.fresh)
+    return result
+
+
+########################################
+# Gaussian patterns
+########################################
+
+@eager.register(Integrate, Gaussian, Variable, frozenset)
+def eager_integrate(log_measure, integrand, reduced_vars):
+    real_vars = frozenset(k for k in reduced_vars if log_measure.inputs[k].dtype == 'real')
+    if real_vars == frozenset([integrand.name]):
+        loc = ops.cholesky_solve(ops.unsqueeze(log_measure.info_vec, -1), log_measure._precision_chol).squeeze(-1)
+        data = loc * ops.unsqueeze(ops.exp(log_measure.log_normalizer.data), -1)
+        data = data.reshape(loc.shape[:-1] + integrand.output.shape)
+        inputs = OrderedDict((k, d) for k, d in log_measure.inputs.items() if d.dtype != 'real')
+        result = Tensor(data, inputs)
+        return result.reduce(ops.add, reduced_vars - real_vars)
+    return None  # defer to default implementation
+
+
+@eager.register(Integrate, Gaussian, Gaussian, frozenset)
+def eager_integrate(log_measure, integrand, reduced_vars):
+    real_vars = frozenset(k for k in reduced_vars if log_measure.inputs[k].dtype == 'real')
+    if real_vars:
+
+        lhs_reals = frozenset(k for k, d in log_measure.inputs.items() if d.dtype == 'real')
+        rhs_reals = frozenset(k for k, d in integrand.inputs.items() if d.dtype == 'real')
+        if lhs_reals == real_vars and rhs_reals <= real_vars:
+            inputs = OrderedDict((k, d) for t in (log_measure, integrand)
+                                 for k, d in t.inputs.items())
+            lhs_info_vec, lhs_precision = align_gaussian(inputs, log_measure)
+            rhs_info_vec, rhs_precision = align_gaussian(inputs, integrand)
+            lhs = Gaussian(lhs_info_vec, lhs_precision, inputs)
+
+            # Compute the expectation of a non-normalized quadratic form.
+            # See "The Matrix Cookbook" (November 15, 2012) ss. 8.2.2 eq. 380.
+            # http://www.math.uwaterloo.ca/~hwolkowi/matrixcookbook.pdf
+            norm = ops.exp(lhs.log_normalizer.data)
+            lhs_cov = ops.cholesky_inverse(lhs._precision_chol)
+            lhs_loc = ops.cholesky_solve(ops.unsqueeze(lhs.info_vec, -1), lhs._precision_chol).squeeze(-1)
+            vmv_term = _vv(lhs_loc, rhs_info_vec - 0.5 * _mv(rhs_precision, lhs_loc))
+            data = norm * (vmv_term - 0.5 * _trace_mm(rhs_precision, lhs_cov))
+            inputs = OrderedDict((k, d) for k, d in inputs.items() if k not in reduced_vars)
+            result = Tensor(data, inputs)
+            return result.reduce(ops.add, reduced_vars - real_vars)
+
+        raise NotImplementedError('TODO implement partial integration')
+
+    return None  # defer to default implementation
+
+
+__all__ = [
+    'Integrate',
+]
---
+++ packages/funsor/funsor-0.3.0-py3-none-any.whl/funsor/jax/distributions.py
@@ -0,0 +1,230 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import functools
+from typing import Tuple, Union
+
+import numpyro.distributions as dist
+
+from funsor.cnf import Contraction
+from funsor.distribution import (  # noqa: F401
+    Bernoulli,
+    FUNSOR_DIST_NAMES,
+    LogNormal,
+    backenddist_to_funsor,
+    eager_beta,
+    eager_beta_bernoulli,
+    eager_binomial,
+    eager_categorical_funsor,
+    eager_categorical_tensor,
+    eager_delta_funsor_funsor,
+    eager_delta_funsor_variable,
+    eager_delta_tensor,
+    eager_delta_variable_variable,
+    eager_dirichlet_multinomial,
+    eager_dirichlet_posterior,
+    eager_gamma_poisson,
+    eager_multinomial,
+    eager_mvn,
+    eager_normal,
+    indepdist_to_funsor,
+    make_dist,
+    maskeddist_to_funsor,
+    mvndist_to_funsor,
+    transformeddist_to_funsor,
+)
+from funsor.domains import Real, Reals
+import funsor.ops as ops
+from funsor.tensor import Tensor, dummy_numeric_array
+from funsor.terms import Binary, Funsor, Variable, eager, to_funsor
+from funsor.util import methodof
+
+
+################################################################################
+# Distribution Wrappers
+################################################################################
+
+
+class _NumPyroWrapper_Binomial(dist.BinomialProbs):
+    pass
+
+
+class _NumPyroWrapper_Categorical(dist.CategoricalProbs):
+    pass
+
+
+class _NumPyroWrapper_Multinomial(dist.MultinomialProbs):
+    pass
+
+
+class _NumPyroWrapper_NonreparameterizedBeta(dist.Beta):
+    has_rsample = False
+
+
+class _NumPyroWrapper_NonreparameterizedDirichlet(dist.Dirichlet):
+    has_rsample = False
+
+
+class _NumPyroWrapper_NonreparameterizedGamma(dist.Gamma):
+    has_rsample = False
+
+
+class _NumPyroWrapper_NonreparameterizedNormal(dist.Normal):
+    has_rsample = False
+
+
+def _get_numpyro_dist(dist_name):
+    if dist_name in ['Binomial', 'Categorical', 'Multinomial'] or dist_name.startswith('Nonreparameterized'):
+        return globals().get('_NumPyroWrapper_' + dist_name)
+    else:
+        return getattr(dist, dist_name, None)
+
+
+NUMPYRO_DIST_NAMES = FUNSOR_DIST_NAMES
+_HAS_RSAMPLE_DISTS = ['Beta', 'Dirichlet', 'Gamma', 'Normal', 'MultivariateNormal']
+
+
+for dist_name, param_names in NUMPYRO_DIST_NAMES:
+    numpyro_dist = _get_numpyro_dist(dist_name)
+    if numpyro_dist is not None:
+        # resolve numpyro distributions do not have `has_rsample` attributes
+        has_rsample = getattr(numpyro_dist, 'has_rsample',
+                              not getattr(numpyro_dist, "is_discrete", dist_name not in _HAS_RSAMPLE_DISTS))
+        if has_rsample:
+            numpyro_dist.has_rsample = True
+            numpyro_dist.rsample = numpyro_dist.sample
+        locals()[dist_name] = make_dist(numpyro_dist, param_names)
+
+
+# Delta has to be treated specially because of its weird shape inference semantics
+@methodof(Delta)  # noqa: F821
+@staticmethod
+def _infer_value_domain(**kwargs):
+    return kwargs['v']
+
+
+# Multinomial and related dists have dependent Bint dtypes, so we just make them 'real'
+# See issue: https://github.com/pyro-ppl/funsor/issues/322
+@methodof(Binomial)  # noqa: F821
+@methodof(Multinomial)  # noqa: F821
+@classmethod
+@functools.lru_cache(maxsize=5000)
+def _infer_value_domain(cls, **kwargs):
+    instance = cls.dist_class(**{k: dummy_numeric_array(domain) for k, domain in kwargs.items()}, validate_args=False)
+    return Reals[instance.event_shape]
+
+
+# TODO fix Delta.arg_constraints["v"] to be a
+# constraints.independent[constraints.real]
+@methodof(Delta)  # noqa: F821
+@staticmethod
+@functools.lru_cache(maxsize=5000)
+def _infer_param_domain(name, raw_shape):
+    if name == "v":
+        return Reals[raw_shape]
+    elif name == "log_density":
+        return Real
+    else:
+        raise ValueError(name)
+
+
+# TODO fix Dirichlet.arg_constraints["concentration"] to be a
+# constraints.independent[constraints.positive]
+@methodof(Dirichlet)  # noqa: F821
+@methodof(NonreparameterizedDirichlet)  # noqa: F821
+@staticmethod
+@functools.lru_cache(maxsize=5000)
+def _infer_param_domain(name, raw_shape):
+    assert name == "concentration"
+    return Reals[raw_shape[-1]]
+
+
+# TODO: remove this `if` for NumPyro > 0.4.0
+if hasattr(dist, "DirichletMultinomial"):
+    @methodof(DirichletMultinomial)  # noqa: F821
+    @classmethod
+    @functools.lru_cache(maxsize=5000)
+    def _infer_value_domain(cls, **kwargs):
+        instance = cls.dist_class(**{k: dummy_numeric_array(domain) for k, domain in kwargs.items()},
+                                  validate_args=False)
+        return Reals[instance.event_shape]
+
+    # TODO fix DirichletMultinomial.arg_constraints["concentration"] to be a
+    # constraints.independent[constraints.positive]
+    @methodof(DirichletMultinomial)  # noqa: F821
+    @classmethod
+    @functools.lru_cache(maxsize=5000)
+    def _infer_param_domain(cls, name, raw_shape):
+        if name == "concentration":
+            return Reals[raw_shape[-1]]
+        assert name == "total_count"
+        return Real
+
+
+###############################################
+# Converting PyTorch Distributions to funsors
+###############################################
+
+to_funsor.register(dist.Distribution)(backenddist_to_funsor)
+to_funsor.register(dist.Independent)(indepdist_to_funsor)
+if hasattr(dist, "MaskedDistribution"):
+    to_funsor.register(dist.MaskedDistribution)(maskeddist_to_funsor)
+to_funsor.register(dist.TransformedDistribution)(transformeddist_to_funsor)
+to_funsor.register(dist.MultivariateNormal)(mvndist_to_funsor)
+
+
+@to_funsor.register(dist.BinomialProbs)
+@to_funsor.register(dist.BinomialLogits)
+def categorical_to_funsor(numpyro_dist, output=None, dim_to_name=None):
+    new_pyro_dist = _NumPyroWrapper_Binomial(probs=numpyro_dist.probs)
+    return backenddist_to_funsor(new_pyro_dist, output, dim_to_name)
+
+
+@to_funsor.register(dist.CategoricalProbs)
+# XXX: in Pyro backend, we always convert pyro.distributions.Categorical
+# to funsor.torch.distributions.Categorical
+@to_funsor.register(dist.CategoricalLogits)
+def categorical_to_funsor(numpyro_dist, output=None, dim_to_name=None):
+    new_pyro_dist = _NumPyroWrapper_Categorical(probs=numpyro_dist.probs)
+    return backenddist_to_funsor(new_pyro_dist, output, dim_to_name)
+
+
+@to_funsor.register(dist.MultinomialProbs)
+@to_funsor.register(dist.MultinomialLogits)
+def categorical_to_funsor(numpyro_dist, output=None, dim_to_name=None):
+    new_pyro_dist = _NumPyroWrapper_Multinomial(probs=numpyro_dist.probs)
+    return backenddist_to_funsor(new_pyro_dist, output, dim_to_name)
+
+
+JointDirichletMultinomial = Contraction[
+    Union[ops.LogAddExpOp, ops.NullOp],
+    ops.AddOp,
+    frozenset,
+    Tuple[Dirichlet, Multinomial],  # noqa: F821
+]
+
+
+eager.register(Beta, Funsor, Funsor, Funsor)(eager_beta)  # noqa: F821)
+eager.register(Binomial, Funsor, Funsor, Funsor)(eager_binomial)  # noqa: F821
+eager.register(Multinomial, Tensor, Tensor, Tensor)(eager_multinomial)  # noqa: F821)
+eager.register(Categorical, Funsor, Tensor)(eager_categorical_funsor)  # noqa: F821)
+eager.register(Categorical, Tensor, Variable)(eager_categorical_tensor)  # noqa: F821)
+eager.register(Delta, Tensor, Tensor, Tensor)(eager_delta_tensor)  # noqa: F821
+eager.register(Delta, Funsor, Funsor, Variable)(eager_delta_funsor_variable)  # noqa: F821
+eager.register(Delta, Variable, Funsor, Variable)(eager_delta_funsor_variable)  # noqa: F821
+eager.register(Delta, Variable, Funsor, Funsor)(eager_delta_funsor_funsor)  # noqa: F821
+eager.register(Delta, Variable, Variable, Variable)(eager_delta_variable_variable)  # noqa: F821
+eager.register(Normal, Funsor, Tensor, Funsor)(eager_normal)  # noqa: F821
+eager.register(MultivariateNormal, Funsor, Tensor, Funsor)(eager_mvn)  # noqa: F821
+eager.register(Contraction, ops.LogAddExpOp, ops.AddOp, frozenset, Dirichlet, BernoulliProbs)(  # noqa: F821
+    eager_beta_bernoulli)
+eager.register(Contraction, ops.LogAddExpOp, ops.AddOp, frozenset, Dirichlet, Multinomial)(  # noqa: F821
+    eager_dirichlet_multinomial)
+eager.register(Contraction, ops.LogAddExpOp, ops.AddOp, frozenset, Gamma, Poisson)(  # noqa: F821
+    eager_gamma_poisson)
+if hasattr(dist, "DirichletMultinomial"):
+    eager.register(Binary, ops.SubOp, JointDirichletMultinomial, DirichletMultinomial)(  # noqa: F821
+        eager_dirichlet_posterior)
+
+
+__all__ = list(x[0] for x in FUNSOR_DIST_NAMES if _get_numpyro_dist(x[0]) is not None)
