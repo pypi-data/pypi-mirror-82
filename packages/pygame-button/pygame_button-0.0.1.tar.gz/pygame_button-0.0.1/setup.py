f2da440538b05f128a3781ce325c39d0887ecaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:42 +0000

    Add package pygame-button/0.0.1/pygame_button-0.0.1.tar.gz

---
+++ packages/pygame-button/pygame_button-0.0.1.tar.gz/pygame_button-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+VERSION = "0.0.1"
+
+setuptools.setup(
+    name="pygame_button",
+    version=VERSION,
+    py_modules=["pygame_button"],
+    url="",
+    license="GPLv3",
+    author="Layerex",
+    author_email="layerex@dismail.de",
+    description="A very simple button class for pygame",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["pygame"],
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: pygame",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
