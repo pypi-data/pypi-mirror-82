6fff9c1d90f93532a6393b55892c542571c35d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:55 +0000

    Add package smart-integration-utils/2.9.7/smart-integration-utils-2.9.7.tar.gz

---
+++ packages/smart-integration-utils/smart-integration-utils-2.9.7.tar.gz/smart-integration-utils-2.9.7/integration_utils/fields.py
@@ -0,0 +1,25 @@
+import json
+
+from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.forms.jsonb import JSONField as JSONFormField
+
+__all__ = ('UTF8JSONFormField', 'UTF8JSONField')
+
+
+class UTF8JSONFormField(JSONFormField):
+    def prepare_value(self, value):
+        try:
+            return json.dumps(value, ensure_ascii=False)
+        except json.JSONDecodeError:
+            return value
+
+
+class UTF8JSONField(JSONField):
+    """JSONField for postgres databases.
+
+    Displays UTF-8 characters directly in the admin, i.e. äöü instead of
+    unicode escape sequences.
+    """
+
+    def formfield(self, **kwargs):
+        return super().formfield(**{**{'form_class': UTF8JSONFormField}, **kwargs,})
