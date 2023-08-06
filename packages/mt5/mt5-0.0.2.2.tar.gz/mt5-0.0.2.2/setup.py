c07ce7abcf129ab5640f65fa814103d8ff236ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:10 +0000

    Add package mt5/0.0.2.2/mt5-0.0.2.2.tar.gz

---
+++ packages/mt5/mt5-0.0.2.2.tar.gz/mt5-0.0.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mt5", # Replace with your own username
+    version="0.0.2.2",
+    author="Jakub Pawłowski",
+    author_email="jkpawlowski96@gmail.com",
+    description="MetaTrader5 multiplatform python api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jkpawlowski96/mt5",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['mt5']
+)
