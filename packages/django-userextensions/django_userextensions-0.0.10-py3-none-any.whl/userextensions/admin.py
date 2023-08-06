8e3894ddcfd8d7443bcd377e659130c8618c1051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:11 +0000

    Add package django-userextensions/0.0.10/django_userextensions-0.0.10-py3-none-any.whl

---
+++ packages/django-userextensions/django_userextensions-0.0.10-py3-none-any.whl/userextensions/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+# import models
+from userextensions.models import (Theme, UserFavorite, UserPreference, UserRecent, ServiceAccount)
+
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "css_file")
+    search_fields = ["name", "css_file"]
+
+
+class UserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "theme", "recents_count", "page_refresh_time", "start_page", "updated_at")
+    search_fields = ["host", "user__username"]
+
+
+class UserRecentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "url")
+    search_fields = ["user__name", "url"]
+    list_filter = ["user"]
+
+
+class UserFavoriteAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "name", "url")
+    search_fields = ["name", "user__username", "name", "url"]
+    list_filter = ["user"]
+
+
+class ServiceAccountAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "group", "description", "enabled")
+    search_fields = ["user__username", "group__name", "description"]
+    list_filter = ["enabled"]
+
+
+# register models
+admin.site.register(Theme, ThemeAdmin)
+admin.site.register(UserPreference, UserPreferenceAdmin)
+admin.site.register(UserFavorite, UserFavoriteAdmin)
+admin.site.register(UserRecent, UserRecentAdmin)
+admin.site.register(ServiceAccount, ServiceAccountAdmin)
