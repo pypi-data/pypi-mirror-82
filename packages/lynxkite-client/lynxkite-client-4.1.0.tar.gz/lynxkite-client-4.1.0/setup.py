0294df4113764235e565e7852a225f42eda8299d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:06 +0000

    Add package lynxkite-client/4.1.0/lynxkite-client-4.1.0.tar.gz

---
+++ packages/lynxkite-client/lynxkite-client-4.1.0.tar.gz/lynxkite-client-4.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_namespace_packages, setup
+import os
+
+setup(
+    name='lynxkite-client',
+    version=os.environ.get('VERSION', '4.1.0'),
+    install_requires=[
+        'requests',
+        'pandas',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'ruamel.yaml>=0.15',
+            'mypy',
+        ]
+    },
+    python_requires='>=3.6',
+    packages=find_namespace_packages('src'),
+    package_dir={'': 'src'},
+    author='Lynx Analytics',
+    author_email='lynxkite@lynxkite.com',
+    description='Python API for LynxKite',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://lynxkite.com/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+)
