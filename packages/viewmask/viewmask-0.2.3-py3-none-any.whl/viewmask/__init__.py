6fa711d9b4a088481c7a7f701c5589d8e0182783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:28 +0000

    Add package viewmask/0.2.3/viewmask-0.2.3-py3-none-any.whl

---
+++ packages/viewmask/viewmask-0.2.3-py3-none-any.whl/viewmask/__init__.py
@@ -0,0 +1,167 @@
+from collections import UserList
+__version__ = '0.2.3'
+
+from viewmask import utils  # noqa  # pylint: disable=unused-import
+
+
+class Annotations(UserList):
+    def __init__(self, initlist=()):
+        UserList.__init__(self)
+        if isinstance(initlist, Annotations):
+            # Already deduped, copy without duplicate checking
+            self.data[:] = Annotations.data
+        else:
+            self.extend(initlist)
+
+    @classmethod
+    def from_tcga(cls, xml_tree, fit_spline=False):
+        """Extract contours from a TCGA XML annotations file.
+
+        Parameters
+        ----------
+        xml_tree : xml.etree.ElementTree
+            The XML tree of the TCGA annotations file.
+        fit_spline : bool, optional
+            Whether the points in each contour should be fit through a spline.
+
+        Returns
+        -------
+        contours : viewmask.Annotations
+            A list of contours, where each contour is a 1-dimensional NumPy
+            array of coordinate pairs, where each coordinate pair is a list
+            with exactly 2 integers, representing the X and Y coordinates,
+            respectively.
+        """
+        # TODO: add reference to vmu.fit_spline_to_points in docstring
+        # TODO: fit_spline is untested
+
+        from numpy import array as to_numpy_array, int32 as npint32
+
+        def region_to_contour(region_):
+            contour = [[float(vertex.get("X")), float(vertex.get("Y"))]
+                       for vertex in region]
+            if fit_spline:
+                from viewmask.utils import fit_spline_to_points
+                return fit_spline_to_points(contour)
+            else:
+                return contour
+        contours = [
+            to_numpy_array(
+                region_to_contour(region),
+                # np.int32 is necessary for cv2.drawContours
+                dtype=npint32)
+            for region in xml_tree.getroot().iter("Vertices")
+        ]
+        return cls(initlist=contours)
+
+    @classmethod
+    def from_qpdata(cls, filepath):
+        pass
+
+    @classmethod
+    def from_asap(cls, xml_tree):
+        """Extract contours from a TCGA XML annotations file.
+
+        Parameters
+        ----------
+        xml_tree : xml.etree.ElementTree
+            The XML tree of the ASAP annotations file.
+
+        Returns
+        -------
+        contours : viewmask.Annotations
+            A list of contours, where each contour is a 1-dimensional NumPy
+            array of coordinate pairs, where each coordinate pair is a list
+            with exactly 2 integers, representing the X and Y coordinates,
+            respectively.
+        """
+        pass
+
+    def export(self, mode):
+        """Export the annotations to a usable format.
+
+        Parameters
+        ----------
+        mode : {'napari', 'opencv'}
+            If mode is 'napari', the annotations will be transposed over their
+            main diagonal. This is done by reverting the x and y coordinates.
+            If mode is 'opencv', the exported annotations will be unchanged.
+
+        Returns
+        -------
+        contours : list of numpy.ndarray
+            A list of contours, where each contour is a list of coordinates,
+            where each coordinate is a list with exactly 2 integers,
+            representing the X and Y coordinates, respectively.
+
+        Notes
+        -----
+        The main diagonal is defined as the line that connects the top-left
+        corner and the bottom right corner of an image.
+        """
+        if mode == 'napari':
+            from numpy import flip
+            return [flip(coordinate_pair) for coordinate_pair in self.data]
+        elif mode == 'opencv':
+            return self.data
+
+    def fit_spline(self):
+        # TODO: docstring
+        self.data = [region_to_contour(region) for region in self.data]
+        return self
+
+    def as_image(self):
+        """Convert an annotations object to an annotation mask.
+
+        Returns
+        -------
+        rendered_annotations : numpy.ndarray
+            A 3-dimensional NumPy array representing the RGB output image.
+        """
+        import numpy as np
+        from cv2 import drawContours, fillPoly
+        contours = self.export('opencv')
+        x_max = np.amax([x for contour in contours for x, _ in contour])
+        y_max = np.amax([y for contour in contours for _, y in contour])
+        shape = (y_max, x_max, 3)
+        rendered_annotations = np.zeros(shape, dtype=np.uint8)
+        drawContours(rendered_annotations, contours, -1, [0, 255, 0])
+        for contour in contours:
+            fillPoly(
+                rendered_annotations,
+                np.array([contour], dtype=np.int32),
+                [230, 230, 230]
+            )
+        return rendered_annotations
+
+    def check(self, v):
+        from numpy import ndarray
+        if not isinstance(v, ndarray):
+            raise TypeError(f'item is not of type {ndarray}')
+
+    def __add__(self, other):
+        ret = self.__class__(self)  # Alt: self.copy()
+        ret.extend(other)
+        return ret
+
+    def __setitem__(self, i, v):
+        self.check(v)
+        self.list[i] = v
+
+    def insert(self, i, v):
+        self.check(v)
+        super().insert(i, v)
+
+    def extend(self, other):
+        for x in other:
+            self.append(x)
+
+
+__all__ = [
+    'utils.file_to_dask_array',
+    'utils.centers_of_contours',
+    'utils.get_stroke_color',
+    'utils.mask_to_contours',
+    'utils.centers_to_image',
+    'Annotations'
+]
---
+++ packages/viewmask/viewmask-0.2.3-py3-none-any.whl/viewmask/cli.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+import click
+import napari
+import xml.etree.ElementTree as ET
+import numpy as np
+from viewmask.utils import (
+    file_to_dask_array,
+    centers_of_contours,
+    get_stroke_color,
+    mask_to_contours,
+    centers_to_image
+)
+from os.path import splitext
+
+
+INTERACTIVE_OPTION_HELP = 'If passed, the annotations will be rendered as ' + \
+    'napari objects rather than rendered together and displayed as an image.'
+
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command(name='annotations')
+@click.argument(
+    'annotations',
+    type=click.Path(exists=True, dir_okay=False)
+)
+@click.option(
+    '-i',
+    '--interactive',
+    default=False,
+    show_default=True,
+    type=bool,
+    is_flag=True,
+    help=INTERACTIVE_OPTION_HELP
+)
+def view_annotations(annotations, interactive):
+    annotations_ext = splitext(annotations)[1]
+    annotations_are_not_xml = annotations_ext != '.xml'
+    if annotations_are_not_xml and interactive is True:
+        raise ValueError(
+            "The interactive flag is only supported with XML annotations.")
+
+    with napari.gui_qt():
+        viewer = napari.Viewer()
+        if interactive:
+            try:
+                tree = ET.parse(annotations)
+            except ET.ParseError:
+                # TODO: don't just do nothing, some XMLs might actually be
+                # unparseable
+                pass
+            from viewmask import Annotations
+            annotations = Annotations.from_tcga(tree)
+            annotations = regions.fit_spline()
+            regions = annotations.export('napari')
+            line_color = get_stroke_color(tree)
+            viewer.add_shapes(
+                regions,
+                shape_type='path',
+                edge_color=f"#{line_color}"
+            )
+            viewer.add_points(
+                centers_of_contours(regions),
+                name='centers'
+            )
+        else:
+            if annotations_ext == '.npy':
+                rendered_annotations = np.load(annotations)
+            elif annotations_ext == '.png':
+                from dask.array import squeeze
+                rendered_annotations = squeeze(file_to_dask_array(annotations))
+            elif annotations_ext == '.xml':
+                try:
+                    tree = ET.parse(annotations)
+                except ET.ParseError:
+                    # TODO: don't just do nothing, some XMLs might actually be
+                    # unparseable
+                    pass
+                annotations = Annotations.from_tcga(tree)
+                annotations = annotations.fit_spline()
+                rendered_annotations = annotations.as_image()
+            else:
+                # TODO: raise ValueError
+                pass
+            viewer.add_image(
+                rendered_annotations,
+                name='annotations',
+                blending='additive',
+                multiscale=False,
+            )
+            from cv2 import inRange as select_color, countNonZero
+            mask = select_color(  # select pure blue
+                rendered_annotations if isinstance(rendered_annotations,
+                                                   np.ndarray) \
+                else rendered_annotations.compute(),
+                np.array([0, 0, 255]),
+                np.array([0, 0, 255])
+            )
+            if countNonZero(mask) == 0:
+                # TODO: it'd be nice if we could check this before computing
+                # the blue-based nuclei mask. the current flow control seems
+                # unintuitive
+                mask = rendered_annotations
+            centers = centers_of_contours(mask_to_contours(mask))
+            viewer.add_image(
+                centers_to_image(centers, shape=rendered_annotations.shape),
+                name='centers',
+                rgb=True,
+                blending='additive',
+                multiscale=False,
+            )
+
+
+@cli.command(name='image')
+@click.argument('image', type=click.Path(exists=True, dir_okay=False))
+def view_image(image):
+    da_img = file_to_dask_array(image)
+    with napari.gui_qt():
+        napari.view_image(da_img, name='image', multiscale=False)
+
+
+@cli.command(name='overlay')
+@click.argument('image', type=click.Path(exists=True, dir_okay=False))
+@click.argument('annotations', type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    '-i',
+    '--interactive',
+    default=False,
+    show_default=True,
+    type=bool,
+    is_flag=True,
+    help=INTERACTIVE_OPTION_HELP
+)
+def view_overlay(image, annotations, interactive):
+    annotations_ext = splitext(annotations)[1]
+    annotations_are_not_xml = annotations_ext != '.xml'
+    if annotations_are_not_xml and interactive is True:
+        raise ValueError(
+            "The interactive flag is only supported with XML annotations.")
+
+    da_img = file_to_dask_array(image)
+
+    with napari.gui_qt():
+        viewer = napari.Viewer()
+
+        viewer.add_image(
+            da_img,
+            name='image',
+            blending='additive',
+            multiscale=False,
+        )
+
+        if interactive:
+            try:
+                tree = ET.parse(annotations)
+            except ET.ParseError:
+                # TODO: don't just do nothing, some XMLs might actually be
+                # unparseable
+                pass
+            from viewmask import Annotations
+            annotations = Annotations.from_tcga(tree)
+            annotations = annotations.fit_spline()
+            regions = annotations.export('napari')
+            line_color = get_stroke_color(tree)
+            viewer.add_shapes(
+                regions,
+                shape_type='path',
+                edge_color=f"#{line_color}"
+            )
+            viewer.add_points(
+                centers_of_contours(regions),
+                name='centers'
+            )
+        else:
+            if annotations_ext == '.npy':
+                rendered_annotations = np.load(annotations)
+            elif annotations_ext == '.png':
+                from dask.array import squeeze
+                rendered_annotations = squeeze(file_to_dask_array(annotations))
+            elif annotations_ext == '.xml':
+                try:
+                    tree = ET.parse(annotations)
+                except ET.ParseError:
+                    # TODO: don't just do nothing, some XMLs might actually be
+                    # unparseable
+                    pass
+                from viewmask import Annotations
+                annotations = Annotations.from_tcga(tree)
+                annotations = annotations.fit_spline()
+                rendered_annotations = annotations.as_image()
+            else:
+                # TODO: raise ValueError
+                pass
+            viewer.add_image(
+                rendered_annotations,
+                name='annotations',
+                blending='additive',
+                multiscale=False,
+            )
+            from cv2 import inRange as select_color, countNonZero
+            mask = select_color(  # select pure blue
+                rendered_annotations if isinstance(rendered_annotations,
+                                                   np.ndarray) \
+                else rendered_annotations.compute(),
+                np.array([0, 0, 255]),
+                np.array([0, 0, 255])
+            )
+            if countNonZero(mask) == 0:
+                # TODO: it'd be nice if we could check this before computing
+                # the blue-based nuclei mask. the current flow control seems
+                # unintuitive
+                mask = rendered_annotations
+            centers = centers_of_contours(mask_to_contours(mask))
+            viewer.add_image(
+                centers_to_image(centers, shape=rendered_annotations.shape),
+                name='centers',
+                rgb=True,
+                blending='additive',
+                multiscale=False,
+            )
+
+
+if __name__ == '__main__':
+    cli()
