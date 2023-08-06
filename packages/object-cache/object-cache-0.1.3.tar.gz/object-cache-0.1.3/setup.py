3ef4b4293d672593576fa398c4f9c45c7d77334a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:06 +0000

    Add package object-cache/0.1.3/object-cache-0.1.3.tar.gz

---
+++ packages/object-cache/object-cache-0.1.3.tar.gz/object-cache-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['object_cache']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cloudpickle>=1.6.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'object-cache',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'u-masao',
+    'author_email': '4973920+u-masao@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
