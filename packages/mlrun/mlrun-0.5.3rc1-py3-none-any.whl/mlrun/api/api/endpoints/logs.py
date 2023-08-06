8dd23a5693aefb5bf47fddca3823892728e398b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:52 +0000

    Add package mlrun/0.5.3rc1/mlrun-0.5.3rc1-py3-none-any.whl

---
+++ packages/mlrun/mlrun-0.5.3rc1-py3-none-any.whl/mlrun/api/api/endpoints/logs.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, Request, Response
+from fastapi.concurrency import run_in_threadpool
+from sqlalchemy.orm import Session
+
+import mlrun.api.crud as crud
+from mlrun.api.api import deps
+
+router = APIRouter()
+
+
+# curl -d@/path/to/log http://localhost:8080/log/prj/7?append=true
+@router.post("/log/{project}/{uid}")
+async def store_log(request: Request, project: str, uid: str, append: bool = True):
+    body = await request.body()
+    await run_in_threadpool(crud.Logs.store_log, body, project, uid, append)
+    return {}
+
+
+# curl http://localhost:8080/log/prj/7
+@router.get("/log/{project}/{uid}")
+def get_log(
+    project: str,
+    uid: str,
+    size: int = -1,
+    offset: int = 0,
+    db_session: Session = Depends(deps.get_db_session),
+):
+    run_state, log = crud.Logs.get_logs(db_session, project, uid, size, offset)
+    return Response(
+        content=log, media_type="text/plain", headers={"x-mlrun-run-state": run_state}
+    )
