9a17b98afe497c6364eb417179967407cc0ad162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:49 +0000

    Add package helical-thread/0.2.2/helical_thread-0.2.2-py2.py3-none-any.whl

---
+++ packages/helical-thread/helical_thread-0.2.2-py2.py3-none-any.whl/helical_thread/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for helical_thread."""
+
+__author__ = "Wink Saville"
+__email__ = "wink@saville.com"
+__version__ = "0.2.2"
+
+from .helicalthread import HelicalThread, ThreadHelixes, helical_thread
