123de94819e7db02e4b08df4645fbd8ab69cb65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:51 +0000

    Add package doggy/1.0.0/doggy-1.0.0.tar.gz

---
+++ packages/doggy/doggy-1.0.0.tar.gz/doggy-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='doggy',
+    version='1.0.0',
+    author="A.Karthik A.K.A REDMANGOAPPLE",
+    author_email="redmaniac0510@gmail.com",
+    description="A python package that helps you set shortcuts for your project folders or file editors",
+    long_description=long_description,
+    py_modules=['doggy'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'Click',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'doggy=doggy:main_commands',
+        ],
+    },
+)
