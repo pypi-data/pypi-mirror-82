e800eeda1ec99a3d1c6e74677ee7829f7a13e0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package jazz-hands/0.0.2/jazz-hands-0.0.2.tar.gz

---
+++ packages/jazz-hands/jazz-hands-0.0.2.tar.gz/jazz-hands-0.0.2/docs/index.rst
@@ -0,0 +1,11 @@
+Documentation
+=============
+
+This is the documentation for JazzHands.
+
+.. toctree::
+  :maxdepth: 3
+
+  jazzhands/index.rst
+
+
---
+++ packages/jazz-hands/jazz-hands-0.0.2.tar.gz/jazz-hands-0.0.2/jazzhands/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+
+__all__ = []
+from jazzhands.wavelets import *  # noqa
+from jazzhands.utils import *
+# Then you can be explicit to control what ends up in the namespace,
+# or you can keep everything from the subpackage with the following instead
+__all__ += wavelets.__all__
+
+#Version number
+__version__ = '0.0.1'
