7abb039b76b74519ab7c28701b27c18ebb8ad5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:37 +0000

    Add package pip-upgrade-tool/0.3.8/pip-upgrade-tool-0.3.8.tar.gz

---
+++ packages/pip-upgrade-tool/pip-upgrade-tool-0.3.8.tar.gz/pip-upgrade-tool-0.3.8/pip_upgrade/compare_deps.py
@@ -0,0 +1,11 @@
+"""
+    TODO Seperate some of the functions in tools.py
+"""
+
+
+class CompareDependencies:
+    def __init__(self):
+        pass
+
+    def compare(self, deps, latest_version):
+        pass
