dec865e9100cadce8970d56436fdd4c05b97f849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:58 +0000

    Add package python-cthun-client/0.1.3/python_cthun_client-0.1.3-py2.py3-none-any.whl

---
+++ packages/python-cthun-client/python_cthun_client-0.1.3-py2.py3-none-any.whl/cthun_client/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+__author__ = 'jasonxu'
+from cthun_client.exception import AuthErrorException
+
+
+class Config:
+    def __init__(self, api_url=None, access_token=None):
+        self._api_url = api_url
+        self._access_token = access_token
+
+    @property
+    def api_url(self):
+        return self._api_url
+
+    @api_url.setter
+    def api_url(self, value):
+        self._api_url = value
+
+    @property
+    def access_token(self):
+        if not self._access_token:
+            raise AuthErrorException("auth error")
+        return self._access_token
+
+    @access_token.setter
+    def access_token(self, value):
+        self._access_token = value
