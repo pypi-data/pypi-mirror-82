2d8a20d9931b50d3d44f534cd5a52ac0864e00fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:35 +0000

    Add package sensai/0.0.4/sensai-0.0.4.tar.gz

---
+++ packages/sensai/sensai-0.0.4.tar.gz/sensai-0.0.4/src/sensai/__init__.py
@@ -0,0 +1,21 @@
+from . import columngen
+from . import data_transformation
+from . import featuregen
+from . import hyperopt
+from . import local_search
+from . import naive_bayes
+from . import nearest_neighbors
+from . import sklearn
+from . import util
+from .data_ingest import InputOutputData
+from .data_transformation import DataFrameTransformer, RuleBasedDataFrameTransformer
+from .ensemble import AveragingVectorRegressionModel
+from .evaluation.eval_stats import eval_stats_classification, eval_stats_regression
+from .normalisation import NormalisationMode
+from .vector_model import VectorModel, VectorRegressionModel, VectorClassificationModel
+
+# The following submodules are not imported by default to avoid necessarily requiring their dependencies:
+# tensorflow
+# torch
+# lightgbm
+# catboost
