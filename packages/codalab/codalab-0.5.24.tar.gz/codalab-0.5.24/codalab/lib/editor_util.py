961f0cdb4ed5c3246030e39ae5087d2c96e233ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:22 +0000

    Add package codalab/0.5.24/codalab-0.5.24.tar.gz

---
+++ packages/codalab/codalab-0.5.24.tar.gz/codalab-0.5.24/codalab/lib/editor_util.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import sys
+import tempfile
+
+from codalab.lib import path_util
+from codalab.lib.codalab_manager import CodaLabManager
+
+
+def find_default_editor():
+    manager = CodaLabManager()
+    editor = os.getenv('EDITOR')
+    if editor:
+        return editor
+    # If not yet set, use a sane default.
+    if sys.platform == 'win32':
+        editor = 'notepad'
+    else:
+        editor = 'vi'
+    return editor
+
+
+def open_and_edit(suffix, template=''):
+    editor = find_default_editor()
+    tempfile_name = ''
+    with tempfile.NamedTemporaryFile(suffix=suffix, delete=False) as form:
+        form.write(template.encode('utf-8'))
+        form.flush()
+        tempfile_name = form.name
+    lines = ''
+    if os.path.isfile(tempfile_name):
+        subprocess.call([editor, tempfile_name])
+        with open(tempfile_name, 'r') as form:
+            lines = form.readlines()
+        path_util.remove(tempfile_name)
+
+    return lines
