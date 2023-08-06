fd47de78b9f62497f48d489b4f617b3e8e2e35f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:20 +0000

    Add package mara-page/1.5.2/mara-page-1.5.2.tar.gz

---
+++ packages/mara-page/mara-page-1.5.2.tar.gz/mara-page-1.5.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import re
+
+def get_long_description():
+    with open('README.md') as f:
+        return re.sub('!\[(.*?)\]\(docs/(.*?)\)', r'![\1](https://github.com/mara/mara-page/raw/master/docs/\2)', f.read())
+
+setup(
+    name='mara-page',
+    version='1.5.2',
+
+    description='Minimal API for defining pages of Flask-based backends independently from the actual backend infrastructure',
+
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+
+    url = 'https://github.com/mara/mara-page',
+
+    python_requires='>=3.6',
+
+    install_requires=[
+        'flask>=0.12',
+        'pygments>=2.2.0',
+    ],
+
+    extras_require={
+        'test': ['pytest', 'pytest_click'],
+    },
+
+    packages=find_packages(),
+
+    author='Mara contributors',
+    license='MIT',
+
+    entry_points={},
+)
