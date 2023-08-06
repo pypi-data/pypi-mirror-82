e3d5c247f5da85f237252f649faf6fd498b06ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:22 +0000

    Add package botwinick-utils/0.0.2/botwinick_utils-0.0.2-py3-none-any.whl

---
+++ packages/botwinick-utils/botwinick_utils-0.0.2-py3-none-any.whl/botwinick_utils/__init__.py
@@ -0,0 +1,3 @@
+# author: Drew Botwinick, Botwinick Innovations
+# title: miscellaneous utilities for python 2/3 with minimal dependencies and minimal interconnection between modules
+# license: 3-clause BSD
---
+++ packages/botwinick-utils/botwinick_utils-0.0.2-py3-none-any.whl/botwinick_utils/binary/__init__.py
@@ -0,0 +1,6 @@
+# author: Drew Botwinick, Botwinick Innovations
+# license: 3-clause BSD
+
+from .b32 import bytes_size, calcsize, error, pack, pack_sequence, unpack, unpack_sequence
+
+__all__ = 'bytes_size', 'pack_sequence', 'unpack_sequence', 'calcsize', 'pack', 'unpack', 'error'
