622c59a2b5f62c9c0fad1d2af141ce48413886d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:49 +0000

    Add package neurohive-devops-tools/0.0.47/neurohive-devops-tools-0.0.47.tar.gz

---
+++ packages/neurohive-devops-tools/neurohive-devops-tools-0.0.47.tar.gz/neurohive-devops-tools-0.0.47/neurohive/integration/jirawrap.py
@@ -0,0 +1,36 @@
+import os, sys
+import logging
+import argparse
+
+from jira import JIRA
+
+logger = logging.getLogger()
+
+JIRA_USERNAME = os.getenv('JIRA_USERNAME')
+JIRA_TOKEN = os.getenv('JIRA_TOKEN')
+
+if not JIRA_TOKEN or not JIRA_USERNAME:
+    logging.error('Enter credentials JIRA_TOKEN|JIRA_USERNAME')
+    sys.exit(1)
+
+
+def add_comment(server, task_id, comment):
+    jira = JIRA(
+        server=server,
+        basic_auth=(JIRA_USERNAME, JIRA_TOKEN))
+    jira.add_comment(task_id, comment)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', required=True)
+    parser.add_argument('--task-id', required=True)
+    parser.add_argument('--comment', required=True)
+    args = parser.parse_args()
+    try:
+        add_comment(args.server, args.task_id, args.comment)
+    except Exception as e:
+        logger.error("Unable to comment jira task")
+        logger.error(e)
+
+
