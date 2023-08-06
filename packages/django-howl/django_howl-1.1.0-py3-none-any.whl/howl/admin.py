95f036bdbe3926af15d9bdc37f82fb6f7bc18663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:42 +0000

    Add package django-howl/1.1.0/django_howl-1.1.0-py3-none-any.whl

---
+++ packages/django-howl/django_howl-1.1.0-py3-none-any.whl/howl/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Alert, Observer
+
+
+@admin.register(Observer)
+class ObserverAdmin(admin.ModelAdmin):
+    list_display = ("name", "operator", "value")
+
+
+@admin.register(Alert)
+class AlertAdmin(admin.ModelAdmin):
+    list_display = ("identifier", "timestamp", "value", "state")
+    list_filter = ("state",)
---
+++ packages/django-howl/django_howl-1.1.0-py3-none-any.whl/howl/signals.py
@@ -0,0 +1,6 @@
+from django.dispatch import Signal
+
+
+alert_clear = Signal()
+alert_notify = Signal()
+alert_wait = Signal()
