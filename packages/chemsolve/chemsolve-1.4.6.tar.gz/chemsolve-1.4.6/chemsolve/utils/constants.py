5a5ecf652b40af893bdc641f304a62ae4e2ac0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:53 +0000

    Add package chemsolve/1.4.6/chemsolve-1.4.6.tar.gz

---
+++ packages/chemsolve/chemsolve-1.4.6.tar.gz/chemsolve-1.4.6/chemsolve/utils/constants.py
@@ -0,0 +1,27 @@
+'''
+Any prevalent constants in chemistry.
+'''
+
+# BASIC
+
+# Avogadro's Constant
+AVOGADRO = 6.02214e23
+# Elementary Charge
+C = 1.60e-19
+# Atomic Mass Unit
+M = 1.66e-27
+
+# ELECTROCHEMISTRY
+
+# Faraday's Constant
+F = 96485.33289
+
+# GASES
+
+# Molar Gas Constant
+R = 8.3144598
+# Boltzmann's Constant
+K = 1.380649e-23
+
+# CONVENIENCE
+REDOX = "redox"
