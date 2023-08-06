0330bbceead7a6dcc8f0bdc83d09b589c17d9007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:02 +0000

    Add package simulariumio/1.0.0/simulariumio-1.0.0-py2.py3-none-any.whl

---
+++ packages/simulariumio/simulariumio-1.0.0-py2.py3-none-any.whl/simulariumio/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Simularium Conversion."""
+
+__author__ = "Blair Lyons"
+__email__ = "blairl@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "1.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .custom_converter import CustomConverter  # noqa: F401
+from .cytosim_converter import CytosimConverter  # noqa: F401
+from .readdy_converter import ReaddyConverter  # noqa: F401
+from .physicell_converter import PhysicellConverter  # noqa: F401
+
+from .data_objects import (  # noqa: F401
+    CustomData,
+    AgentData,
+    CytosimData,
+    CytosimObjectInfo,
+    CytosimAgentInfo,
+    ReaddyData,
+    PhysicellData,
+    ScatterPlotData,
+    HistogramPlotData,
+)
