419c30b7913d968e29af9560201cdddfc993929f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:54 +0000

    Add package mi/2020.10.20.16.37.34/mi-2020.10.20.16.37.34.tar.gz

---
+++ packages/mi/mi-2020.10.20.16.37.34.tar.gz/mi-2020.10.20.16.37.34/mi/db/Milvus.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : Python.
+# @File         : Milvus
+# @Time         : 2020-03-20 15:11
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
