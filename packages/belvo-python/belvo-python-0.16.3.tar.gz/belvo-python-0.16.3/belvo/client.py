4ff6a50bd49baee8f57d7c58636e5da7d84a31d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:09 +0000

    Add package belvo-python/0.16.3/belvo-python-0.16.3.tar.gz

---
+++ packages/belvo-python/belvo-python-0.16.3.tar.gz/belvo-python-0.16.3/belvo/client.py
@@ -0,0 +1,93 @@
+import os
+
+from .exceptions import BelvoAPIException
+from .http import APISession
+from .resources import (
+    Accounts,
+    Balances,
+    Incomes,
+    Institutions,
+    Invoices,
+    Links,
+    Owners,
+    Statements,
+    TaxReturns,
+    TaxStatus,
+    Transactions,
+    WidgetToken,
+)
+
+
+class Client:
+    def __init__(self, secret_key_id: str, secret_key_password: str, url: str = None) -> None:
+        if url is None:
+            url = os.getenv("BELVO_API_URL")
+
+        if not url:
+            raise BelvoAPIException("You need to provide a URL.")
+
+        self.session = APISession(url)
+
+        if not self.session.login(secret_key_id, secret_key_password):
+            raise BelvoAPIException("Login failed.")
+
+        self._links = Links(self.session)
+        self._accounts = Accounts(self.session)
+        self._transactions = Transactions(self.session)
+        self._balances = Balances(self.session)
+        self._institutions = Institutions(self.session)
+        self._incomes = Incomes(self.session)
+        self._owners = Owners(self.session)
+        self._invoices = Invoices(self.session)
+        self._tax_returns = TaxReturns(self.session)
+        self._tax_status = TaxStatus(self.session)
+        self._statements = Statements(self.session)
+        self._widget_token = WidgetToken(self.session)
+
+    @property
+    def Links(self):
+        return self._links
+
+    @property
+    def Accounts(self):
+        return self._accounts
+
+    @property
+    def Transactions(self):
+        return self._transactions
+
+    @property
+    def Balances(self):
+        return self._balances
+
+    @property
+    def Institutions(self):
+        return self._institutions
+
+    @property
+    def Incomes(self):
+        return self._incomes
+
+    @property
+    def Owners(self):
+        return self._owners
+
+    @property
+    def Invoices(self):
+        return self._invoices
+
+    @property
+    def TaxReturns(self):
+        return self._tax_returns
+
+    @property
+    def TaxStatus(self):
+        return self._tax_status
+
+    @property
+    def Statements(self):
+        return self._statements
+
+    @property
+    def WidgetToken(self):
+        return self._widget_token
