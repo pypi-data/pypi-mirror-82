f3fda416a5c19e743133e7d277d54b171c9d78fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:55 +0000

    Add package launchable/0.0.17/launchable-0.0.17.tar.gz

---
+++ packages/launchable/launchable-0.0.17.tar.gz/launchable-0.0.17/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from launchable.version import __version__
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+def _requirements(file):
+    return open(file).read().splitlines()
+
+setup(
+    name = 'launchable',
+    version=__version__,
+    license = 'MIT',
+    author = 'Satoshi Asano',
+    url = 'https://launchableinc.com/',
+    author_email = 'sasano@launchableinc.com',
+    description = 'Launchable CLI',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=_requirements('requirements.txt'),
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'launchable = launchable.__main__:main',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
