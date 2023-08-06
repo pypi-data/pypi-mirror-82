1e1dc67ac55f298a2aee3063e47c9e59b6e48280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:20 +0000

    Add package tenda/0.0.6/tenda-0.0.6.tar.gz

---
+++ packages/tenda/tenda-0.0.6.tar.gz/tenda-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tenda",
+    version="0.0.6",
+    author="Talha Balaj",
+    author_email="talhabalaj@gmail.com",
+    description="Unofficial Tenda Model N301 API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/talhabalaj/tenda-n301-api-python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
