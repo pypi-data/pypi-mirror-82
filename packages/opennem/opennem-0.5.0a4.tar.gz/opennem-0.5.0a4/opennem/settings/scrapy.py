4edec757de5f6eb1c02d3c89d9f06d508f3a3da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:26 +0000

    Add package opennem/0.5.0a4/opennem-0.5.0a4.tar.gz

---
+++ packages/opennem/opennem-0.5.0a4.tar.gz/opennem-0.5.0a4/opennem/settings/scrapy.py
@@ -0,0 +1,55 @@
+"""
+Scrapy settings for nemweb project
+
+For simplicity, this file contains only settings considered important or
+commonly used. You can find more settings consulting the documentation:
+
+https://docs.scrapy.org/en/latest/topics/settings.html
+"""
+
+BOT_NAME = "opennem"
+SPIDER_MODULES = ["opennem.spiders"]
+NEWSPIDER_MODULE = "opennem.spiders"
+
+
+USER_AGENT = "opennem (+https://opennem.org.au)"
+ROBOTSTXT_OBEY = False
+CONCURRENT_REQUESTS = 16
+COOKIES_ENABLED = True
+TELNETCONSOLE_ENABLED = False
+
+DEFAULT_REQUEST_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en",
+}
+
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_MAX_DELAY = 60
+
+
+# Pipline docs:
+# 1xx series - download handlers
+# 2xx series - unpackers
+# 3xx series -
+# 4xx series - database processes
+ITEM_PIPELINES = {
+    "opennem.pipelines.files.LinkExtract": 101,
+    "opennem.pipelines.nem.ExtractCSV": 301,
+    "opennem.pipelines.nem.DatabaseStore": 401,
+    "opennem.pipelines.nem.facilities.NemStoreFacility": 401,
+    "opennem.pipelines.wem.facilities.WemStoreFacility": 410,
+    "opennem.pipelines.wem.facility_scada.WemStoreFacilityScada": 411,
+    "opennem.pipelines.wem.balancing_summary.WemStoreBalancingSummary": 412,
+    "opennem.pipelines.wem.balancing_summary.WemStoreBalancingSummaryArchive": 413,
+    "opennem.pipelines.wem.pulse.WemStorePulse": 414,
+    "opennem.pipelines.wem.facility_scada.WemStoreLiveFacilityScada": 415,
+    "opennem.pipelines.wem.facilities.WemStoreLiveFacilities": 416,
+    "opennem.pipelines.wem.participant.WemStoreParticipant": 417,
+    "opennem.pipelines.wem.participant.WemStoreLiveParticipant": 418,
+    "opennem.pipelines.bom.StoreBomObservation": 430,
+}
+
+# @TODO if DEBUG
+HTTPCACHE_ENABLED = False
+HTTPCACHE_DIR = ".scrapy"
