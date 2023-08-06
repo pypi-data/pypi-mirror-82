18ebf728ff7f76ec9f3874c049e565158fd3eaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:10 +0000

    Add package stkml/1.0.1/stkml-1.0.1-py3-none-any.whl

---
+++ packages/stkml/stkml-1.0.1-py3-none-any.whl/Stackml/Stackml/LayerElement/NetworklayerElement.py
@@ -0,0 +1,8 @@
+#Copyright STACKEO - INRIA 2020 .
+from Stackml.Stackml.LayerElement import LayerElement
+
+
+class NetworklayerElement(LayerElement):
+
+    def __init__(self, name: str, hub):
+        super().__init__(name, type(self).__name__, hub)
