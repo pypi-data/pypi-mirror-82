ed126e242e9ef80333ff56837915c07894f13097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:33 +0000

    Add package Flask-Mpesa/1.2.5/Flask-Mpesa-1.2.5.tar.gz

---
+++ packages/Flask-Mpesa/Flask-Mpesa-1.2.5.tar.gz/Flask-Mpesa-1.2.5/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='Flask-Mpesa',
+    version='1.2.5',
+    url='https://github.com/allansifuna/flask-mpesa',
+    license='BSD',
+    author='Allan Namasaka Sifuna',
+    author_email='allansifuna324@gmail.com',
+    description='A Safaricom\'s DarajaAPI package for Flask Applications.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
