1561c58f6d835ebb510653dfc8cbd15355130f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:01 +0000

    Add package pymmaster/0.1/pymmaster-0.1.tar.gz

---
+++ packages/pymmaster/pymmaster-0.1.tar.gz/pymmaster-0.1/pymmaster/__init__.py
@@ -0,0 +1,6 @@
+"""
+pymmaster is a python package to remove jitter-related bias in ASTER DEMs using `MicMac ASTER`_.
+
+.. _MicMac ASTER: https://www.mdpi.com/2072-4292/9/7/704
+"""
+import pymmaster.mmaster_tools
