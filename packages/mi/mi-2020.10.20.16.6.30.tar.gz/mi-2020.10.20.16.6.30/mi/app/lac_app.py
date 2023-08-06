13422b5148af654c9add4031fdd09cafbdc495c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:47 +0000

    Add package mi/2020.10.20.16.6.30/mi-2020.10.20.16.6.30.tar.gz

---
+++ packages/mi/mi-2020.10.20.16.6.30.tar.gz/mi-2020.10.20.16.6.30/mi/app/lac_app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : mi.
+# @File         : lac_app
+# @Time         : 2020/10/20 3:55 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
