11cb2cd3c30390c4ef3b82df71b479911a3bc162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:55 +0000

    Add package checkov/1.0.576/checkov-1.0.576.tar.gz

---
+++ packages/checkov/checkov-1.0.576.tar.gz/checkov-1.0.576/checkov/common/util/banner.py
@@ -0,0 +1,10 @@
+from checkov.version import version
+
+banner = r"""
+       _               _              
+   ___| |__   ___  ___| | _______   __
+  / __| '_ \ / _ \/ __| |/ / _ \ \ / /
+ | (__| | | |  __/ (__|   < (_) \ V / 
+  \___|_| |_|\___|\___|_|\_\___/ \_/  
+                                      
+by bridgecrew.io | version: {} """.format(version)
