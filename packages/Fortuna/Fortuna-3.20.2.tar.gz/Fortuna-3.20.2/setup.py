6e9d5489a44a5b99f4af3c581c6496f0d2f3578a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:41 +0000

    Add package Fortuna/3.20.2/Fortuna-3.20.2.tar.gz

---
+++ packages/Fortuna/Fortuna-3.20.2.tar.gz/Fortuna-3.20.2/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+dev_status = {
+    "Alpha": "Development Status :: 3 - Alpha",
+    "Beta": "Development Status :: 4 - Beta",
+    "Pro": "Development Status :: 5 - Production/Stable",
+    "Mature": "Development Status :: 6 - Mature",
+}
+
+setup(
+    name="Fortuna",
+    ext_modules=cythonize(
+        Extension(
+            name="Fortuna",
+            sources=["Fortuna.pyx"],
+            language=["c++"],
+            extra_compile_args=["-std=c++17"],
+        ),
+        compiler_directives={
+            'embedsignature': True,
+            'language_level': 3,
+        },
+    ),
+    author="Robert Sharp",
+    author_email="webmaster@sharpdesigndigital.com",
+    version="3.20.2",
+    description="Random Value Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Free for non-commercial use",
+    platforms=["Darwin", "Linux"],
+    classifiers=[
+        dev_status["Pro"],
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Cython",
+        "Programming Language :: C++",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=[
+        "Fortuna", "Random Patterns", "Data Perturbation", "Game Dice",
+        "WeightedChoice", "Random Value Generator", "Gaussian Distribution",
+        "Linear Distribution", "TruffleShuffle", "FlexCat", "Percent True",
+        "ZeroCool", "QuantumMonty", "Custom Distribution", "Rarity Table",
+        "D20", "Generative Modeling",
+    ],
+    python_requires='>=3.6',
+)
