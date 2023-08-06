2127a6e95876723e68237638f1e2f8fd86e49f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:14 +0000

    Add package PyVisualizer/1.0.6/PyVisualizer-1.0.6.tar.gz

---
+++ packages/PyVisualizer/PyVisualizer-1.0.6.tar.gz/PyVisualizer-1.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="PyVisualizer",
+    version="1.0.6",
+    description="A simple static visualizer.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vishalkashyap95/PyVisualizer_Task2.git",
+    author="Vishal Kashyap",
+    author_email="vkashyap569@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["PyVisualizer"],
+    include_package_data=True,
+    install_requires=["gspread","pandas","requests","matplotlib","seaborn"],
+    entry_points={
+        "console_scripts": [
+            "PyVisualizer=PyVisualizer.Visualizer:__init__",
+        ]
+    },
+)
