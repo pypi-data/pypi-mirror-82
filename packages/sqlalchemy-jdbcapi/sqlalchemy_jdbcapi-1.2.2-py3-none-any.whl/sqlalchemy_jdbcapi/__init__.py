e836639b80966b82860fa5cf2aa39ba12834072a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:03 +0000

    Add package sqlalchemy-jdbcapi/1.2.2/sqlalchemy_jdbcapi-1.2.2-py3-none-any.whl

---
+++ packages/sqlalchemy-jdbcapi/sqlalchemy_jdbcapi-1.2.2-py3-none-any.whl/sqlalchemy_jdbcapi/__init__.py
@@ -0,0 +1,10 @@
+from sqlalchemy.dialects import registry
+
+__version__ = "1.2.2"
+
+registry.register(
+    "jdbcapi.pgjdbc", "sqlalchemy_jdbcapi.pgjdbc", "PGJDBCDialect"
+)
+registry.register(
+    "jdbcapi.oraclejdbc", "sqlalchemy_jdbcapi.oraclejdbc", "OracleJDBCDialect"
+)
