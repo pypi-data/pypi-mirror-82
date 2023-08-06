eb7829b06642a5a79577e6e26d2c3d6f515cdfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:26 +0000

    Add package django-itelegram/1.0.1/Django-iTelegram-1.0.1.tar.gz

---
+++ packages/django-itelegram/Django-iTelegram-1.0.1.tar.gz/Django-iTelegram-1.0.1/README.rst
@@ -0,0 +1,36 @@
+.. image:: https://img.shields.io/github/license/YazdanRa/django-itelegram.svg
+   :target: https://github.com/YazdanRa/django-itelegram/blob/master/LICENSE
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
+
+-------
+
+
+Django iTelegram
+=================
+
+It's a library for building Telegram bot on Django with extra built-in options.
+created from `django-telegrambot`_ with a lot of extra features
+
+
+Compatibility
+=============
+
+- Python: **3.6**, **3.7**, **3.8**
+- Django: **2.2**, **3.0**, **3.1**, **3.2**
+
+
+Usage
+=====
+
+TODO :D
+
+
+License
+=======
+
+`MIT`_
+
+.. _`django-telegrambot`: https://pypi.org/project/django-telegrambot/
+.. _`MIT`: https://github.com/YazdanRa/django-itelegram/blob/master/LICENSE
---
+++ packages/django-itelegram/Django-iTelegram-1.0.1.tar.gz/Django-iTelegram-1.0.1/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import django_itelegram
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = django_itelegram.__version__
+
+if sys.argv[-1] == "publish":
+    try:
+        import wheel
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel")
+    os.system("twine upload dist/*")
+    os.system("git tag v{}".format(version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+
+setup(
+    name="Django-iTelegram",
+    version=version,
+    description="It's a library for building Telegram bot on Django with extra built-in features",
+    long_description=readme + "\n\n" + history,
+    author="Yazdan Ranjbar",
+    author_email="yazdan_ra@icloud.com",
+    url="https://github.com/YazdanRa/django-itelegram",
+    packages=[
+        "django_itelegram",
+    ],
+    include_package_data=True,
+    install_requires=[
+        "django>=2.2",
+        "python-telegram-bot>=6.0.1",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords="django-itelegram",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
