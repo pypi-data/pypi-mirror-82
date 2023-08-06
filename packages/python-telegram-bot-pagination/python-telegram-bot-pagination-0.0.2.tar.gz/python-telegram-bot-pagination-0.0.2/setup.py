5811f98928471f4b7365cb4361538fa9cba92a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:47 +0000

    Add package python-telegram-bot-pagination/0.0.2/python-telegram-bot-pagination-0.0.2.tar.gz

---
+++ packages/python-telegram-bot-pagination/python-telegram-bot-pagination-0.0.2.tar.gz/python-telegram-bot-pagination-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='python-telegram-bot-pagination',
+    version='0.0.2',
+    packages=['telegram_bot_pagination'],
+    url='https://github.com/ksinn/python-telegram-bot-pagination',
+    license='GPL2',
+    author='ksinn',
+    author_email='ksinnd@gmail.com',
+    description='Python inline keyboard pagination for Telegram Bot API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='telegram bot api pagination keyboard inline tools',
+)
