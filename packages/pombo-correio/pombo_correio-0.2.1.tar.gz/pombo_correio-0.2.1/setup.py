13a939ba25a2df2445b74c630d9a8da011f58b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:43 +0000

    Add package pombo-correio/0.2.1/pombo_correio-0.2.1.tar.gz

---
+++ packages/pombo-correio/pombo_correio-0.2.1.tar.gz/pombo_correio-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pombo_correio',
+    version='0.2.1',
+    packages=['pombo_correio'],
+    url='https://github.com/JarbasAl/pombo_correio',
+    license='Apache',
+    author='jarbasAI',
+    include_package_data=True,
+    author_email='jarbasai@mailfence.com',
+    description='simple selenium wrapper'
+)
