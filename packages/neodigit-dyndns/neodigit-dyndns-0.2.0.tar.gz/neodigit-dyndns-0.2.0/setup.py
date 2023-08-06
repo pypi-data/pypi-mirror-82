3a3febb2c8c0fa17aed85542e06a1922b641301a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:29 +0000

    Add package neodigit-dyndns/0.2.0/neodigit-dyndns-0.2.0.tar.gz

---
+++ packages/neodigit-dyndns/neodigit-dyndns-0.2.0.tar.gz/neodigit-dyndns-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['neodigit_dyndns']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'neodigit-dyndns',
+    'version': '0.2.0',
+    'description': "A dynamic DNS client using Neodigit's API",
+    'long_description': None,
+    'author': 'Eduardo Collado',
+    'author_email': 'edu@tecnocratica.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
