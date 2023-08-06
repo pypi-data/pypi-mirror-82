14fc336a6d7b3dc3ce2753aa3b752f76623ee604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:00 +0000

    Add package TkinterExtensions/1.4.0/TkinterExtensions-1.4.0.tar.gz

---
+++ packages/TkinterExtensions/TkinterExtensions-1.4.0.tar.gz/TkinterExtensions-1.4.0/setup.py
@@ -0,0 +1,64 @@
+# ------------------------------------------------------------------------------
+#  Created by Tyler Stegmaier
+#  Copyright (c) 2020.
+#
+# ------------------------------------------------------------------------------
+import os
+
+from setuptools import setup
+
+
+def GetVesion():
+      from src.TkinterExtensions.__version__ import version
+      return version
+
+
+with open(os.path.abspath("README.md"), "r") as f:
+    long_description = f.read()
+
+package = 'TkinterExtensions'
+data_files = [
+        f'{package}/*.py'
+        ]
+
+setup(name=package,
+      version=GetVesion(),
+      packages=[package],
+      url='https://github.com/Jakar510/TkinterExtensions',
+      # download_url=f'https://github.com/Jakar510/PyDebug/TkinterExtensions/releases/tag/{version}',
+      license='MIT License',
+      author='Tyler Stegmaier',
+      author_email='tyler.stegmaier.510@gmail.com',
+      description='Strongly typed widgets and event with multiple helper functions build in to speed up development.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires=[
+              'pillow', 'tk_html_widgets',
+              ],
+      classifiers=[
+              # How mature is this project? Common values are
+              #   3 - Alpha
+              #   4 - Beta
+              #   5 - Production/Stable
+              'Development Status :: 4 - Beta',
+
+              # Indicate who your project is intended for
+              'Intended Audience :: Developers',
+              'Topic :: Software Development :: Build Tools',
+
+              # Pick your license as you wish
+              'License :: Free To Use But Restricted',
+
+              # Support platforms
+              'Operating System :: MacOS',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+
+              'Programming Language :: Python :: 3',
+              ],
+      keywords=f'{package} Tkinter Extensions tk ttk tkinter',
+      package_dir={ package: f'src/{package}' },
+      package_data={
+              package: data_files,
+              },
+      )
---
+++ packages/TkinterExtensions/TkinterExtensions-1.4.0.tar.gz/TkinterExtensions-1.4.0/src/TkinterExtensions/Widgets/__init__.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+#  Created by Tyler Stegmaier
+#  Copyright (c) 2020.
+#
+# ------------------------------------------------------------------------------
+
+from .BaseWidgets import CallWrapper
+from .Custom import *
+from .Frames import *
+from .HTML import *
+from .Root import *
+from .Style import *
+from .Themed import *
+from .Widgets import *
+from .base import *
