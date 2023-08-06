c113ffcfa9afbd37cf31284592c3225bfb3eea24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:20 +0000

    Add package TorchSUL/0.1.22/TorchSUL-0.1.22-py3-none-any.whl

---
+++ packages/TorchSUL/TorchSUL-0.1.22-py3-none-any.whl/TorchSUL/__init__.py
@@ -0,0 +1,13 @@
+from . import DataReader
+from . import Layers 
+from . import Model
+from . import sul_tool
+from . import sulio 
+
+__all__ = [
+	'DataReader',
+	'Layers',
+	'Model',
+	'sul_tool'
+	'sulio'
+]
