c040064e97dbff52a6a1716115efd4f4cc88f4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:50 +0000

    Add package pyre-check/0.0.57/pyre_check-0.0.57-py3-none-macosx_10_11_x86_64.whl

---
+++ packages/pyre-check/pyre_check-0.0.57-py3-none-macosx_10_11_x86_64.whl/pyre_check/tools/sapp/setup.py
@@ -0,0 +1,24 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import json
+
+from setuptools import find_packages, setup
+
+
+with open("requirements.json") as json_file:
+    data = json.load(json_file)
+    requirements = data["sapp"]
+
+setup(
+    name="sapp",
+    version="0.1",
+    install_requires=requirements,
+    entry_points={"console_scripts": ["sapp = sapp.cli:cli"]},
+    packages=find_packages(),
+    url="https://pyre-check.org/",
+    maintainer="Zachary Landau, Dominik Gabi",
+    maintainer_email="zacharyl@fb.com, dominik@fb.com",
+)
