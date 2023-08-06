b87a990ec716c3be8a9ae531bd717bd94b956c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:45 +0000

    Add package whoare/0.1.19/whoare-0.1.19.tar.gz

---
+++ packages/whoare/whoare-0.1.19.tar.gz/whoare-0.1.19/tests/test_mock.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+from whoare.whoare import WhoAre
+
+
+def test_data99():
+
+    wa = WhoAre()    
+    wa.load('data99.com.ar', mock_from_txt_file='whoare/zone_parsers/ar/sample_data99.txt')
+    
+    assert wa.domain.base_name == 'data99'
+    assert wa.domain.zone == 'com.ar'
+    assert not wa.domain.is_free
+    assert wa.domain.registered == datetime(2010, 4, 12, 0, 0, 0)
+    assert wa.domain.changed == datetime(2020, 3, 24, 8, 26, 1, 899786)
+    assert wa.domain.expire == datetime(2021, 4, 12, 0, 0, 0)
+    
+    assert wa.registrant.name == 'gomez pedro'
+    assert wa.registrant.legal_uid == '20xxxxxxxx8'
+    assert wa.registrant.created == datetime(2013, 8, 20, 0, 0, 0)
+    assert wa.registrant.changed == datetime(2020, 5, 4, 19, 34, 57, 928489)
+    
+    assert wa.dnss[0].name == 'ns2.cluster311.com'
+    assert wa.dnss[1].name == 'ns1.cluster311.com'
+
+
+def test_free_01():
+
+    wa = WhoAre()
+    wa.load('domainfree.com.ar', mock_from_txt_file='whoare/zone_parsers/ar/sample_free.txt')
+    assert wa.domain.is_free
+
+def test_fernet():
+
+    wa = WhoAre()
+    wa.load('fernet.com.ar', mock_from_txt_file='whoare/zone_parsers/ar/sample_fernet.txt')
+    
+    assert wa.domain.base_name == 'fernet'
+    assert wa.domain.zone == 'com.ar'
+    assert not wa.domain.is_free
+    assert wa.domain.registered == datetime(2020, 5, 7, 10, 44, 4, 210977)
+    assert wa.domain.changed == datetime(2020, 5, 7, 14, 34, 40, 828423)
+    assert wa.domain.expire == datetime(2021, 5, 7, 0, 0, 0)
+
+    assert wa.registrant.name == 'perez juan'
+    assert wa.registrant.legal_uid == '20xxxxxxxx9'
+    assert wa.registrant.created == datetime(2013, 8, 19, 0, 0, 0)
+    assert wa.registrant.changed == datetime(2020, 10, 11, 17, 37, 54, 831645)
+    
+    assert wa.dnss[0].name == 'ns2.sedoparking.com'
+    assert wa.dnss[1].name == 'ns1.sedoparking.com'
+
+
+def test_nic():
+
+    wa = WhoAre()
+    wa.load('nic.ar', mock_from_txt_file='whoare/zone_parsers/ar/sample_nic.txt')
+    
+    assert wa.domain.base_name == 'nic'
+    assert wa.domain.zone == 'ar'
+    assert not wa.domain.is_free
+    assert wa.domain.registered == datetime(1998, 5, 29, 0, 0, 0)
+    assert wa.domain.changed == datetime(2019, 10, 7, 16, 23, 59, 503535)
+    assert wa.domain.expire == datetime(2020, 5, 1, 0, 0, 0)
+
+    assert wa.registrant.name == 'nic.ar'
+    assert wa.registrant.legal_uid == '99999999994'
+    assert wa.registrant.created == datetime(2016, 9, 26, 12, 4, 48, 869673)
+    assert wa.registrant.changed == datetime(2016, 9, 26, 17, 39, 38, 373610)
+    
+    assert wa.dnss[0].name == 'ns5.rdns.ar'
+    assert wa.dnss[1].name == 'ns6.rdns.ar'
+    assert wa.dnss[2].name == 'ns1.rdns.ar'
+    assert wa.dnss[3].name == 'ns2.rdns.ar'
+    assert wa.dnss[4].name == 'ns3.rdns.ar'
+
+
+def test_fernet():
+
+    wa = WhoAre()
+    wa.load('fernet.com.ar', mock_from_txt_file='whoare/zone_parsers/ar/sample_fernet.txt')
+    
+    expected_dict = {
+        "domain": {
+            "base_name": 'fernet',
+            "zone": 'com.ar',
+            "is_free": False,
+            "registered": wa.domain.registered,
+            "changed": wa.domain.changed,
+            "expire": wa.domain.expire
+            },
+        "registrant": {
+            "name": wa.registrant.name,
+            "legal_uid": wa.registrant.legal_uid,
+            "created": wa.registrant.created,
+            "changed": wa.registrant.changed
+        },
+        "dnss": ['ns2.sedoparking.com', 'ns1.sedoparking.com']
+    }
+    
+    assert wa.as_dict() == expected_dict
