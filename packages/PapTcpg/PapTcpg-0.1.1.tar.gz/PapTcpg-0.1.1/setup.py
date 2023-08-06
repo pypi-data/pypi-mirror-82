452e566403abed2d1e364697288476c4b24eea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:37 +0000

    Add package PapTcpg/0.1.1/PapTcpg-0.1.1.tar.gz

---
+++ packages/PapTcpg/PapTcpg-0.1.1.tar.gz/PapTcpg-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md",'r') as fh:
+    long = fh.read()
+
+attrs = {
+    "name":"PapTcpg",
+    "version":"0.1.1",
+    "author" : "RapperXandSheepW",
+
+
+
+}
+
+setuptools.setup(**attrs)
