d75d90086d55e81b1d2598d9a8d0b2f14cd45aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:28 +0000

    Add package c7n/0.9.7/c7n-0.9.7-py3-none-any.whl

---
+++ packages/c7n/c7n-0.9.7-py3-none-any.whl/c7n/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2015-2017 Capital One Services, LLC
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+#
---
+++ packages/c7n/c7n-0.9.7-py3-none-any.whl/c7n/reports/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2016-2017 Capital One Services, LLC
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+from .csvout import report
