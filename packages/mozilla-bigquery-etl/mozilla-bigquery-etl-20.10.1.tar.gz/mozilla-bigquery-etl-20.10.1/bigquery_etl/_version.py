e7d4b0b4079e8e7ed1c1faa5e5d03379a9215e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:29 +0000

    Add package mozilla-bigquery-etl/20.10.1/mozilla-bigquery-etl-20.10.1.tar.gz

---
+++ packages/mozilla-bigquery-etl/mozilla-bigquery-etl-20.10.1.tar.gz/mozilla-bigquery-etl-20.10.1/bigquery_etl/_version.py
@@ -0,0 +1,3 @@
+"""Provides bigquery-etl version information."""
+
+__version__ = "20.10.1"
---
+++ packages/mozilla-bigquery-etl/mozilla-bigquery-etl-20.10.1.tar.gz/mozilla-bigquery-etl-20.10.1/bigquery_etl/cli/__init__.py
@@ -0,0 +1,44 @@
+"""bigquery-etl CLI."""
+
+import click
+import warnings
+
+
+from ..cli.query import query
+from ..cli.dag import dag
+from ..cli.dryrun import dryrun
+from ..cli.format import format
+from ..cli.routine import routine, mozfun
+from ..glam.cli import glam
+from .._version import __version__
+from ..stripe import stripe_
+
+
+def cli():
+    """Create the bigquery-etl CLI."""
+    commands = {
+        "query": query,
+        "dag": dag,
+        "dryrun": dryrun,
+        "format": format,
+        "routine": routine,
+        "mozfun": mozfun,
+        "stripe": stripe_,
+        "glam": glam,
+    }
+
+    @click.group(commands=commands)
+    @click.version_option(version=__version__)
+    def group():
+        """CLI tools for working with bigquery-etl."""
+        pass
+
+    warnings.filterwarnings(
+        "ignore", "Your application has authenticated using end user credentials"
+    )
+
+    group()
+
+
+if __name__ == "__main__":
+    cli()
