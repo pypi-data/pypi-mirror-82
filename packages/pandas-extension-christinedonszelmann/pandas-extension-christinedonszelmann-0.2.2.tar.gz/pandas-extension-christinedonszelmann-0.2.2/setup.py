ac19385b7e5dbf72801f9595e2d5507d01e0edcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:34 +0000

    Add package pandas-extension-christinedonszelmann/0.2.2/pandas-extension-christinedonszelmann-0.2.2.tar.gz

---
+++ packages/pandas-extension-christinedonszelmann/pandas-extension-christinedonszelmann-0.2.2.tar.gz/pandas-extension-christinedonszelmann-0.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pandas-extension-christinedonszelmann',
+      version='0.2.2',
+      description='makes combinations of all columns of one dataframe, able to group them and do stuff',
+      author='Christine Donszelmann',
+      author_email='christine.donszelmann@nl.ey.com',
+      license='MIT',
+      packages=['pandas_extension_christine'],
+      zip_safe=False)
+
