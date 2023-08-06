ecd601c2e6c3ede197d7a5ccb08da3c4e06501b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:49 +0000

    Add package python-fx/0.1.0a2/python_fx-0.1.0a2-py3-none-any.whl

---
+++ packages/python-fx/python_fx-0.1.0a2-py3-none-any.whl/pyfx/view/components/help_window.py
@@ -0,0 +1,15 @@
+import urwid
+
+
+class HelpWindow(urwid.WidgetWrap):
+    HELP_TEXT = [
+            ('title', "Pyfx"), "    ",
+            ('key', "UP"), ",", ('key', "DOWN"), ",",
+            ('key', "ENTER"), "  ",
+            ('key', "Q"),
+        ]
+
+    def __init__(self, manager):
+        self._manager = manager
+        self._text_widget = urwid.Text(HelpWindow.HELP_TEXT)
+        super().__init__(urwid.AttrWrap(self._text_widget, "foot"))
