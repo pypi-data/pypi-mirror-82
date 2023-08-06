7742bec52e7ebd060e589463c9fd38aef76f28d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:43 +0000

    Add package graphlib-backport/1.0.1/graphlib_backport-1.0.1.tar.gz

---
+++ packages/graphlib-backport/graphlib_backport-1.0.1.tar.gz/graphlib_backport-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['graphlib']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'graphlib-backport',
+    'version': '1.0.1',
+    'description': 'Backport of the Python 3.9 graphlib module for Python 3.6+',
+    'long_description': None,
+    'author': 'Marius Helf',
+    'author_email': 'helfsmarius@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
