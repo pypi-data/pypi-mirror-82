c53ded9183576f05910e5be1398a5c7d48ba2a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:43 +0000

    Add package acrcloud/14.10.2020/acrcloud-14.10.2020.tar.gz

---
+++ packages/acrcloud/acrcloud-14.10.2020.tar.gz/acrcloud-14.10.2020/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = "acrcloud",
+	version = "14.10.2020",
+	description = "Detects the songs from an audio",
+	license = "MIT",
+	author = "ACRCloud",
+	author_email = "support@acrcloud.com",
+	url = "https://github.com/acrcloud/acrcloud_sdk_python",
+	install_requires = ["pyacrcloud"]
+)
