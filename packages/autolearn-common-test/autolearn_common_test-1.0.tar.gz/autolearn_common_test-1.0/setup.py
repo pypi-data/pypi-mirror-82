8c172b85ec06c9ddf718fd274f39f816945488ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:19 +0000

    Add package autolearn-common-test/1.0/autolearn_common_test-1.0.tar.gz

---
+++ packages/autolearn-common-test/autolearn_common_test-1.0.tar.gz/autolearn_common_test-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup #import setuptools python package
+
+setup(name='autolearn_common_test', #Name of the package
+    version='1.0', #Version of the package
+    description='Gaussian distributions and Binomial Distribution', #Package description
+    packages=['Anglogica_probability'],   
+    author = "******",
+    author_email='*****@gmail.com',  #Author e-mail address
+    zip_safe=False)
