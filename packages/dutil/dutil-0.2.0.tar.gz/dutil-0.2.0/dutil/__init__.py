35c4f37aef528453ef9c1c2070999770d67a4aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:59 +0000

    Add package dutil/0.2.0/dutil-0.2.0.tar.gz

---
+++ packages/dutil/dutil-0.2.0.tar.gz/dutil-0.2.0/dutil/__init__.py
@@ -0,0 +1,4 @@
+from dutil.src.persist import cached
+from dutil.src.stats import q_01, q_10, q_25, q_50, q_75, q_90, q_99, mean_lower, mean_upper
+from dutil.src.transform import ht
+from dutil.src.jupyter import dht
