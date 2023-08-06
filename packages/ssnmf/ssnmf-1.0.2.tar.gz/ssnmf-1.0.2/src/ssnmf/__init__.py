1d0ea7424be4eeebee600c8ac6be03b09de9d5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package ssnmf/1.0.2/ssnmf-1.0.2.tar.gz

---
+++ packages/ssnmf/ssnmf-1.0.2.tar.gz/ssnmf-1.0.2/src/ssnmf/__init__.py
@@ -0,0 +1,11 @@
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "1.0.2"
+
+__author__ = "Jamie Haddock"
+__email__ = "jhaddock@math.ucla.edu"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Jamie Haddock"
+
+from .ssnmf import SSNMF
