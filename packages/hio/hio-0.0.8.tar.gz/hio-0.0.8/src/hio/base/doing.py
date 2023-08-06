612e76fd0fe87b90b46a4997072e2476443499e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:11 +0000

    Add package hio/0.0.8/hio-0.0.8.tar.gz

---
+++ packages/hio/hio-0.0.8.tar.gz/hio-0.0.8/src/hio/base/doing.py
@@ -0,0 +1,547 @@
+# -*- encoding: utf-8 -*-
+"""
+hio.core.doing Module
+"""
+from collections import deque, namedtuple
+
+from ..hioing import ValidationError, VersionError
+from .basing import State
+from . import ticking
+from ..core.tcp import serving, clienting
+
+"""
+Also create bare generator functions
+
+whodo
+
+"""
+
+class Doer():
+    """
+    Doer base class for hierarchical structured async coroutine like generators.
+    Doer.__call__ on instance returns generator
+    Doer is generator creator and has extra methods and attributes that plain
+    generator function does not
+
+    Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    """
+
+    def __init__(self, ticker=None, tock=0.0):
+        """
+        Initialize instance.
+        Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        """
+        self.ticker = ticker or ticking.Ticker(tyme=0.0)
+        self.tock = tock  # desired tyme interval between runs, 0.0 means asap
+
+
+    def __call__(self, **kwa):
+        """
+        Returns generator
+        Does not advance to first yield.
+        The advance to first yield effectively invodes the enter or open context
+        on the generator.
+        To enter either call .next or .send(None) on generator
+        """
+        return self.do(**kwa)
+
+
+    @property
+    def tock(self):
+        """
+        tock property getter, get ._tock
+        .tock is float desired .tyme increment in seconds
+        """
+        return self._tock
+
+
+    @tock.setter
+    def tock(self, tock):
+        """
+        desired cycle tyme interval until next run
+        0.0 means run asap,
+        set ._tock to tock
+        """
+        self._tock= abs(float(tock))
+
+
+    def do(self, ticker, tock=0.0):
+        """
+        Generator method to run this doer
+        Calling this method returns generator
+        """
+        if ticker is not None:
+            self.ticker = ticker
+        if tock is not None:
+            self.tock = tock
+
+        try:
+            # enter context
+
+            while (True):  # recur context
+                feed = (yield (self.tock))  # yields tock then waits for next send
+
+
+        except GeneratorExit:  # close context, forced exit due to .close
+            pass
+
+        except Exception:  # abort context, forced exit due to uncaught exception
+            raise
+
+        finally:  # exit context,  unforced exit due to normal exit of try
+            pass
+
+        return True # return value of yield from, or yield ex.value of StopIteration
+
+
+def dog(ticker, tock=0.0):
+    """
+    Generator function example non-class based generator
+    Calling this function returns generator
+    """
+    feed = "Default"
+    count = 0
+
+    try:
+        # enter context
+
+
+        while (True):  # recur context
+            feed = (yield (tock))  # yields tock then waits for next send
+
+    except GeneratorExit:  # close context, forced exit due to .close
+        pass
+
+    except Exception:  # abort context, forced exit due to uncaught exception
+        raise
+
+    finally:  # exit context,  unforced exit due to normal exit of try
+        pass
+
+    return True # return value of yield from, or yield ex.value of StopIteration
+
+
+
+class TestDoer(Doer):
+    """
+    TestDoer supports testing with methods to record sends and yields
+
+    Inherited Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Inherited Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Inherited Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    Attributes:
+       .states is list of State namedtuples (tyme, feed, result)
+
+    """
+
+    def __init__(self, **kwa):
+        """
+        Initialize instance.
+        Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        """
+        super(TestDoer, self).__init__(**kwa)
+        self.states = []
+
+
+    def do(self, ticker, tock=0.0):
+        """
+        Generator method to run this doer, class based generator
+        Calling this method returns generator
+        """
+        feed = "Default"
+        count = 0
+
+        try:
+            # enter context
+
+            self.states.append(State(tyme=ticker.tyme, context="enter", feed=feed, count=count))
+            while (True):  # recur context
+                feed = (yield (count))  # yields tock then waits for next send
+                count += 1
+                self.states.append(State(tyme=ticker.tyme, context="recur", feed=feed, count = count))
+                if count > 3:
+                    break  # normal exit
+
+        except GeneratorExit:  # close context, forced exit due to .close
+            count += 1
+            self.states.append(State(tyme=ticker.tyme, context='close', feed=feed, count=count))
+
+        except Exception:  # abort context, forced exit due to uncaught exception
+            count += 1
+            self.states.append(State(tyme=ticker.tyme, context='abort', feed=feed, count=count))
+            raise
+
+        finally:  # exit context,  unforced exit due to normal exit of try
+            count += 1
+            self.states.append(State(tyme=ticker.tyme, context='exit', feed=feed, count=count))
+
+        return (True)  # return value of yield from, or yield ex.value of StopIteration
+
+
+def testdog(states, ticker, tock=0.0):
+    """
+    Generator function test example non-class based generator.
+    Calling this function returns generator
+    """
+    feed = "Default"
+    count = 0
+
+    try:
+        # enter context
+
+        states.append(State(tyme=ticker.tyme, context="enter", feed=feed, count=count))
+        while (True):  # recur context
+            feed = (yield (count))  # yields tock then waits for next send
+            count += 1
+            states.append(State(tyme=ticker.tyme, context="recur", feed=feed, count = count))
+            if count > 3:
+                break  # normal exit
+
+    except GeneratorExit:  # close context, forced exit due to .close
+        count += 1
+        states.append(State(tyme=ticker.tyme, context='close', feed=feed, count=count))
+
+    except Exception:  # abort context, forced exit due to uncaught exception
+        count += 1
+        states.append(State(tyme=ticker.tyme, context='abort', feed=feed, count=count))
+        raise
+
+    finally:  # exit context,  unforced exit due to normal exit of try
+        count += 1
+        states.append(State(tyme=ticker.tyme, context='exit', feed=feed, count=count))
+
+    return (True)  # return value of yield from, or yield ex.value of StopIteration
+
+
+class WhoDoer(Doer):
+    """
+    WhoDoer supports inspecption with methods to record sends and yields
+
+    Inherited Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Inherited Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Inherited Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    Attributes:
+       .states is list of State namedtuples (tyme, feed, result)
+
+    """
+
+    def __init__(self, **kwa):
+        """
+        Initialize instance.
+        Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        """
+        super(WhoDoer, self).__init__(**kwa)
+        self.states = []
+
+
+    def do(self, ticker, tock=0.0):
+        """
+        Generator method to run this doer, class based generator
+        Calling this method returns generator
+        """
+        feed = "_"
+        count = 0
+
+        try:
+            # enter context
+
+            self.states.append(State(tyme=ticker.tyme, context="enter", feed=feed, count=count))
+            while (True):  # recur context
+                feed = (yield (tock))  # yields tock then waits for next send
+                count += 1
+                self.states.append(State(tyme=ticker.tyme, context="recur", feed=feed, count=count))
+                if count > 3:
+                    break  # normal exit
+
+        except GeneratorExit:  # close context, forced exit due to .close
+            count += 1
+            self.states.append(State(tyme=ticker.tyme, context='close', feed=feed, count=count))
+
+        except Exception:  # abort context, forced exit due to uncaught exception
+            count += 1
+            self.states.append(State(tyme=ticker.tyme, context='abort', feed=feed, count=count))
+            raise
+
+        finally:  # exit context,  unforced exit due to normal exit of try
+            count += 1
+            self.states.append(State(tyme=ticker.tyme, context='exit', feed=feed, count=count))
+
+        return (True)  # return value of yield from, or yield ex.value of StopIteration
+
+
+def whodog(states, ticker, tock=0.0):
+    """
+    Generator function test example non-class based generator.
+    Calling this function returns generator
+    """
+    feed = "Default"
+    count = 0
+
+    try:
+        # enter context
+
+        states.append(State(tyme=ticker.tyme, context="enter", feed=feed, count=count))
+        while (True):  # recur context
+            feed = (yield (tock))  # yields tock then waits for next send
+            count += 1
+            states.append(State(tyme=ticker.tyme, context="recur", feed=feed, count=count))
+            if count > 3:
+                break  # normal exit
+
+    except GeneratorExit:  # close context, forced exit due to .close
+        count += 1
+        states.append(State(tyme=ticker.tyme, context='close', feed=feed, count=count))
+
+    except Exception:  # abort context, forced exit due to uncaught exception
+        count += 1
+        states.append(State(tyme=ticker.tyme, context='abort', feed=feed, count=count))
+        raise
+
+    finally:  # exit context,  unforced exit due to normal exit of try
+        count += 1
+        states.append(State(tyme=ticker.tyme, context='exit', feed=feed, count=count))
+
+    return (True)  # return value of yield from, or yield ex.value of StopIteration
+
+
+class ServerDoer(Doer):
+    """
+    Basic TCP Server
+
+    Inherited Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Inherited Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Inherited Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    Attributes:
+       .server is TCP Server instance
+
+    """
+
+    def __init__(self, server, **kwa):
+        """
+        Inherited Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        Parameters:
+           server is TCP Server instance
+        """
+        super(ServerDoer, self).__init__(**kwa)
+        server.ticker = self.ticker
+        self.server = server
+
+
+    def do(self, ticker=None, tock=None):
+        """
+        Generator method to run this doer, class based generator
+        Calling this method returns generator
+
+        Run Server
+        """
+        if ticker is not None:
+            self.ticker = ticker
+        if tock is not None:
+            self.tock = tock
+
+        try:
+            # enter context
+            self.server.reopen()  #  opens accept socket
+
+            while (True):  # recur context
+                feed = (yield (self.tock))  # yields tock then waits for next send
+                self.server.serviceAll()
+
+        except GeneratorExit:  # close context, forced exit due to .close
+            pass
+
+        except Exception:  # abort context, forced exit due to uncaught exception
+            raise
+
+        finally:  # exit context,  unforced exit due to normal exit of try
+            self.server.close()
+
+        return
+
+
+class EchoServerDoer(ServerDoer):
+    """
+    Echo TCP Server
+    Just echoes back to client whatever it receives from client
+
+    Inherited Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+        .server is TCP Server instance
+
+    Inherited Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Inherited Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    """
+
+    def do(self, ticker=None, tock=None):
+        """
+        Generator method to run this doer, class based generator
+        Calling this method returns generator
+
+        Run Server
+        """
+        if ticker is not None:
+            self.ticker = ticker
+        if tock is not None:
+            self.tock = tock
+
+        try:
+            # enter context
+            self.server.reopen()  #  opens accept socket
+
+            while (True):  # recur context
+                feed = (yield (self.tock))  # yields tock then waits for next send
+                self.server.serviceAll()
+
+                for ca, ix in self.server.ixes.items():  # echo back
+                    if ix.rxbs:
+                        ix.tx(bytes(ix.rxbs))
+                        ix.clearRxbs()
+
+        except GeneratorExit:  # close context, forced exit due to .close
+            pass
+
+        except Exception:  # abort context, forced exit due to uncaught exception
+            raise
+
+        finally:  # exit context,  unforced exit due to normal exit of try
+            self.server.close()
+
+        return
+
+
+class ClientDoer(Doer):
+    """
+    Basic TCP Client
+
+        Inherited Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Inherited Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Inherited Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    Attributes:
+       .client is TCP Client instance
+
+    """
+
+    def __init__(self, client, **kwa):
+        """
+        Initialize instance.
+        Inherited Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        Parameters:
+           client is TCP Client instance
+        """
+        super(ClientDoer, self).__init__(**kwa)
+        client.ticker = self.ticker
+        self.client = client
+
+
+    def do(self, ticker=None, tock=None):
+        """
+        Generator method to run this doer, class based generator
+        Calling this method returns generator
+
+        Run Server
+        """
+        if ticker is not None:
+            self.ticker = ticker
+        if tock is not None:
+            self.tock = tock
+
+        try:
+            # enter context
+            self.client.reopen()  #  opens accept socket
+
+            while (True):  # recur context
+                feed = (yield (self.tock))  # yields tock then waits for next send
+                self.client.serviceAll()
+
+        except GeneratorExit:  # close context, forced exit due to .close
+            pass
+
+        except Exception:  # abort context, forced exit due to uncaught exception
+            raise
+
+        finally:  # exit context,  unforced exit due to normal exit of try
+            self.client.close()
+
+        return
+
---
+++ packages/hio/hio-0.0.8.tar.gz/hio-0.0.8/src/hio/base/ticking.py
@@ -0,0 +1,177 @@
+# -*- encoding: utf-8 -*-
+"""
+hio.core.ticking Module
+"""
+import time
+from collections import deque
+
+from ..hioing import ValidationError, VersionError
+from ..help.timing import MonoTimer
+
+from .basing import Ctl, Stt
+
+
+class Ticker():
+    """
+    Ticker is simulated time cycling object
+    Provides relative cycle time in seconds with .tyme property and advanced
+    by .tock method.
+    .tyme may be artificial time or real time in seconds.
+
+    Attributes:
+
+    Properties:
+        .tyme is float relative cycle time, .tyme is artificial time
+        .tock is float tyme increment of .tick()
+
+    Methods:
+        .tick increments .tyme by one .tock or provided tock
+
+    """
+    def __init__(self, tyme=0.0, tock=1.0):
+        """
+        Initialize instance
+        Parameters:
+            tyme is initial value of float cycle time in seconds
+            tock is float tock time in seconds
+        """
+        self.tyme = float(tyme)
+        self.tock = float(tock)
+
+    @property
+    def tyme(self):
+        """
+        tyme property getter, get ._tyme
+        .tyme is float cycle time in seconds
+        """
+        return self._tyme
+
+    @tyme.setter
+    def tyme(self, tyme):
+        """
+        cycle time property setter, set ._tyme to tyme
+        """
+        self._tyme = float(tyme)
+
+    @property
+    def tock(self):
+        """
+        tock property getter, get ._tock
+        .tock is float cycle time .tyme increment in seconds
+        """
+        return self._tock
+
+    @tock.setter
+    def tock(self, tock):
+        """
+        cycle time increment property setter, set ._tock to tock
+        """
+        self._tock= float(tock)
+
+    def tick(self, tock=None):
+        """
+        Advance cycle time .tyme by tock seconds when provided othewise by .tock
+        and return new .tyme
+        Parameters:
+            tock is float of amount of time in seconds to change .tyme
+        """
+        self.tyme += float(tock if tock is not None else self.tock)
+        return self.tyme
+
+
+class Tymer():
+    """
+    Tymer class to measure cycle time given by .tyme property of Ticker instance.
+    tyme is relative cycle time either artificial or real
+
+    Attributes:
+        ._start is start tyme in seconds
+        ._stop  is stop tyme in seconds
+
+    Properties:
+        .duration = tyme duration of tymer in seconds from ._start to ._stop
+        .elaspsed = tyme elasped in seconds  since ._start
+        .remaining = tyme remaining in seconds  until ._stop
+        .expired = True if expired, False otherwise, i.e. .ticker.tyme >= ._stop
+
+    methods:
+        .start() = start tymer at current .ticker.tyme
+        .restart() = restart tymer at last ._stop so no time lost
+
+    """
+
+    def __init__(self, ticker=None, duration=0.0, start=None):
+        """
+        Initialization method for instance.
+        Parameters:
+            ticker is reference to Ticker instance. Uses .tyme property
+            duration is float tymer duration in seconds (fractional)
+            start is float optional timer start time in seconds. Allows starting
+                before or after current .ticker.tyme
+        """
+        self.ticker = ticker if ticker is not None else Ticker()
+        start = float(start) if start is not None else self.ticker.tyme
+        self._start = start # need for default duration
+        self._stop = self._start + float(duration)  # need for default duration
+        self.start(duration=duration, start=start)
+
+
+    @property
+    def duration(self):
+        """
+        duration property getter,  .duration = ._stop - ._start
+        .duration is float duration tyme
+        """
+        return (self._stop - self._start)
+
+
+    @property
+    def elapsed(self):
+        """
+        elapsed tyme property getter,
+        Returns elapsed tyme in seconds (fractional) since ._start.
+        """
+        return (self.ticker.tyme - self._start)
+
+
+    @property
+    def remaining(self):
+        """
+        remaining tyme property getter,
+        Returns remaining tyme in seconds (fractional) before ._stop.
+        """
+        return (self._stop - self.ticker.tyme)
+
+
+    @property
+    def expired(self):
+        """
+        Returns True if tymer has expired, False otherwise.
+        .ticker.tyme >= ._stop,
+        """
+        return (self.ticker.tyme >= self._stop)
+
+
+    def start(self, duration=None, start=None):
+        """
+        Starts Tymer of duration secs at start time start secs.
+            If duration not provided then uses current duration
+            If start not provided then starts at current .ticker.tyme
+        """
+        # remember current duration when duration not provided
+        duration = float(duration) if duration is not None else self.duration
+        self._start = float(start) if start is not None else self.ticker.tyme
+        self._stop = self._start + duration
+        return self._start
+
+
+    def restart(self, duration=None):
+        """
+        Lossless restart of Tymer at .tyme = ._stop for duration if provided,
+        current duration otherwise
+        No time lost. Useful to extend Tymer so no time lost
+        """
+        return self.start(duration=duration, start=self._stop)
+
+
+
