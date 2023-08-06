1fe50002a3d840ea2a9aeff799d7db56cbd5b37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:41 +0000

    Add package zfs-replicate/1.2.2/zfs-replicate-1.2.2.tar.gz

---
+++ packages/zfs-replicate/zfs-replicate-1.2.2.tar.gz/zfs-replicate-1.2.2/zfs_test/replicate_test/cli_test/main_test.py
@@ -0,0 +1,24 @@
+"""zfs.replicate.cli.main tests"""
+
+from click.testing import CliRunner
+from zfs.replicate.cli.main import main
+
+
+def test_invokes_without_stacktrace() -> None:
+    """zfs-replicate -l alunduil -i mypy.ini example.com bogus bogus => No stacktrace"""
+
+    runner = CliRunner()
+    result = runner.invoke(main, ["-l", "alunduil", "-i", "mypy.ini", "example.com", "bogus", "bogus"])
+    assert isinstance(result.exception, SystemExit) or (
+        isinstance(result.exception, FileNotFoundError) and result.exception.filename == "/usr/bin/env"
+    ), "Expected SystemExit or FileNotFoundError."
+
+
+def test_invokes_without_stacktrace_verbose() -> None:
+    """zfs-replicate --verbose -l alunduil -i mypy.ini example.com bogus bogus => No stacktrace"""
+
+    runner = CliRunner()
+    result = runner.invoke(main, ["--verbose", "-l", "alunduil", "-i", "mypy.ini", "example.com", "bogus", "bogus"])
+    assert isinstance(result.exception, SystemExit) or (
+        isinstance(result.exception, FileNotFoundError) and result.exception.filename == "/usr/bin/env"
+    ), "Expected SystemExit or FileNotFoundError."
