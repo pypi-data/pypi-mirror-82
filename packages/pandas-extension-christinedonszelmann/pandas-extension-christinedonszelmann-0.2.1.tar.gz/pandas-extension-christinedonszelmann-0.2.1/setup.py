dcc32e3ef207dac7a02039918cfcd6fb39856ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:05 +0000

    Add package pandas-extension-christinedonszelmann/0.2.1/pandas-extension-christinedonszelmann-0.2.1.tar.gz

---
+++ packages/pandas-extension-christinedonszelmann/pandas-extension-christinedonszelmann-0.2.1.tar.gz/pandas-extension-christinedonszelmann-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pandas-extension-christinedonszelmann',
+      version='0.2.1',
+      description='makes dataframe combinations',
+      author='Christine Donszelmann',
+      author_email='christine.donszelmann@nl.ey.com',
+      license='MIT',
+      packages=['pandas_extension_christine'],
+      zip_safe=False)
