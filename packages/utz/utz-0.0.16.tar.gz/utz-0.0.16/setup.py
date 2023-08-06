82c6fdc2a5f2c630b340873d4bf5fde2496479fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:03 +0000

    Add package utz/0.0.16/utz-0.0.16.tar.gz

---
+++ packages/utz/utz-0.0.16.tar.gz/utz-0.0.16/setup.py
@@ -0,0 +1,22 @@
+from utz.setup import setup
+
+setup(
+    install_requires=[
+        "GitPython",
+        "joblib",
+        "pandas",
+        "python-dateutil",
+        "pyyaml",
+    ],
+    extras_require={
+        'pdf': [
+            'reportlab==3.5.42',
+            'pyPDF2==1.26.0',
+        ],
+        'setup': [
+            'md2py==0.0.1',
+        ]
+    },
+    scripts=['utz.sh',],
+    url="https://github.com/runsascoded/utz",
+)
