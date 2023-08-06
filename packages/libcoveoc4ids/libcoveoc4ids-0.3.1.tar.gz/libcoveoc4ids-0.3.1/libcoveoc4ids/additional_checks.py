fe1c3d369acc765134abad6b4f305ec4fa4959ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:54 +0000

    Add package libcoveoc4ids/0.3.1/libcoveoc4ids-0.3.1.tar.gz

---
+++ packages/libcoveoc4ids/libcoveoc4ids-0.3.1.tar.gz/libcoveoc4ids-0.3.1/libcoveoc4ids/additional_checks.py
@@ -0,0 +1,121 @@
+import gettext
+import re
+
+from libcoveoc4ids.check_classes import AdditionalCheck
+
+_ = gettext.gettext
+
+
+class CurrencyCheck(AdditionalCheck):
+    def process(self, data, flat_data):
+        amount_path = re.compile(".*amount$")
+
+        expected_currency_paths = []
+        missing = []
+
+        for path in flat_data.keys():
+            if amount_path.match(path):
+                expected_currency_paths.append(path[: -len("amount")] + "currency")
+
+        for expected in expected_currency_paths:
+            if expected not in flat_data:
+                missing.append(expected[: -len("currency")])
+
+        if len(missing) == 0:
+            return True
+
+        return self.result(
+            "missing-currency",
+            _(
+                "There are %(count)d values without a currency. Currencies should be published for all values."
+            )
+            % {"count": len(missing)},
+            missing,
+        )
+
+
+class EmptyValueCheck(AdditionalCheck):
+    def process(self, data, flat_data):
+        missing = []
+
+        # Similar check to libcoveocds EmptyFieldCheck
+        for path, value in flat_data.items():
+            if isinstance(value, str) and len(value.strip()) == 0:
+                missing.append(path)
+            elif isinstance(value, dict) and len(value) == 0:
+                missing.append(path)
+            elif isinstance(value, list) and len(value) == 0:
+                missing.append(path)
+
+        if len(missing) == 0:
+            return True
+
+        return self.result(
+            "missing-values",
+            _("The data includes fields that are empty or contain only whitespaces. "
+                "Fields that are not being used, or that have no value, "
+                "should be excluded in their entirety (key and value) from the data"),
+            missing,
+        )
+
+
+class OrgReferencesExistCheck(AdditionalCheck):
+    project_id_match = re.compile(r"^/projects/\d+")
+
+    def __init__(self, *args, **kwargs):
+        # Cache of project party ids
+        self._cached_project_parties_ids = {}
+
+    def extract_project_from_path(self, path):
+        """ Matches /projects/<int> and returns the int portion """
+
+        return int(self.project_id_match.match(path).group()[len("/projects/"):])
+
+    def project_parties_ids(self, data, project):
+        try:
+            return self._cached_project_parties_ids[project]
+        except KeyError:
+            try:
+                self._cached_project_parties_ids[project] = [
+                    party["id"] for party in data["projects"][project]["parties"]
+                ]
+                return self._cached_project_parties_ids[project]
+            except KeyError:
+                # This happens if the project has no parties defined
+                return []
+
+    def process(self, data, flat_data):
+        org_paths = re.compile(
+            r"(.*)(publicAuthority|budget\/budgetBreakdown\/\d+\/sourceParty|"
+            r"contractingProcesses\/\d+\/summary\/(tender|suppliers\/\d+)(\/)"
+            r"*(tenderers\/\d+|procuringEntity|administrativeEntity)*)\/id$"
+        )
+
+        missing_references_paths = []
+
+        for path, value in flat_data.items():
+            if org_paths.match(path):
+                project = self.extract_project_from_path(path)
+                if value not in self.project_parties_ids(data, project):
+                    missing_references_paths.append(path)
+
+        if len(missing_references_paths) == 0:
+            return True
+
+        return self.result(
+            "missing-org-refs",
+            _(
+                "There are %(count)d organization references with an id that does not match the id of any parties. "
+                "All organization references should have an associated entry in the parties array with a matching id."
+            )
+            % {"count": len(missing_references_paths)},
+            missing_references_paths,
+        )
+
+
+def additional_checks():
+    return [
+        EmptyValueCheck(),
+        CurrencyCheck(),
+        OrgReferencesExistCheck(),
+    ]
---
+++ packages/libcoveoc4ids/libcoveoc4ids-0.3.1.tar.gz/libcoveoc4ids-0.3.1/libcoveoc4ids/common_checks.py
@@ -0,0 +1,71 @@
+from libcove.lib.common import common_checks_context, get_additional_codelist_values
+from libcoveocds.lib.additional_checks import flatten_dict
+from libcoveocds.lib.common_checks import get_releases_aggregates
+
+from libcoveoc4ids.additional_checks import additional_checks
+from libcoveoc4ids.config import LibCoveOC4IDSConfig
+from libcoveoc4ids.conformance_checks import conformance_checks
+
+
+def common_checks_oc4ids(context, upload_dir, json_data, schema_obj, lib_cove_oc4ids_config=None, cache=True):
+    additional_checks_results = []
+
+    if not lib_cove_oc4ids_config:
+        lib_cove_oc4ids_config = LibCoveOC4IDSConfig()
+
+    # Common schema checks
+    common_checks = common_checks_context(upload_dir, json_data, schema_obj,
+                                          'schema.json', context, cache=cache)
+
+    context.update(common_checks["context"])
+
+    flattened_data = dict(flatten_dict(json_data))
+
+    # end Common checks
+
+    # Conformance checks
+    conformance_checks_results = []
+
+    for conformance_check in conformance_checks():
+        check_result = conformance_check.process(json_data, flattened_data)
+        # Check passed, continue
+        if check_result is True:
+            continue
+
+        conformance_checks_results.append(check_result)
+
+    context.update({"conformance_checks": conformance_checks_results})
+    # end conformance checks
+
+    # Additional checks
+
+    for additional_check in additional_checks():
+        check_result = additional_check.process(json_data, flattened_data)
+        # Check passed, continue
+        if check_result is True:
+            continue
+
+        additional_checks_results.append(check_result)
+
+    context.update({"additional_checks": additional_checks_results})
+    # end Additional checks
+
+    # codelist checks
+    validation_errors = common_checks["context"]["validation_errors"]
+
+    additional_codelist_values = get_additional_codelist_values(schema_obj, json_data)
+    closed_codelist_values = {
+        key: value for key, value in additional_codelist_values.items() if not value["isopen"]
+    }
+    open_codelist_values = {key: value for key, value in additional_codelist_values.items() if value["isopen"]}
+
+    context.update(
+        {
+            "releases_aggregates": get_releases_aggregates(json_data, ignore_errors=bool(validation_errors)),
+            "additional_closed_codelist_values": closed_codelist_values,
+            "additional_open_codelist_values": open_codelist_values,
+        }
+    )
+    # end codelist check
+
+    return context
