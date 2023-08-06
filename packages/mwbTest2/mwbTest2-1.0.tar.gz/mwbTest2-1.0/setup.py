5715c00615848c8f54cd94b2d74f494c18491e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:55 +0000

    Add package mwbTest2/1.0/mwbTest2-1.0.tar.gz

---
+++ packages/mwbTest2/mwbTest2-1.0.tar.gz/mwbTest2-1.0/setup.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+from distutils.core import  setup
+setup(
+    name="mwbTest2",#对外模块的名字,要跟你创建的文件夹的名字一致，不然你都不知道怎么调用
+    version='1.0',#版本号
+    description="孟维彬的第一个python发布",#描述
+    author='mwb',#作者
+    author_email='mwb@mwb.com',#作者邮箱
+    py_modules=["mwbTest.demo"]#要发布的模块
+)
