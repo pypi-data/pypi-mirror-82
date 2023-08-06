b2f49f455690f8446e023fa252530018ce00730d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:24 +0000

    Add package alipayfanhua/3.4.0/alipayfanhua-3.4.0.tar.gz

---
+++ packages/alipayfanhua/alipayfanhua-3.4.0.tar.gz/alipayfanhua-3.4.0/README.md
@@ -0,0 +1,16 @@
+# 概括
+
+## 说明
+本包名字为 alipayfanhua
+
+这个安装完包名也是alipay
+pip install python-alipay-sdk==1.5.0
+
+这个安装后包名也是alipay
+pip install alipay-sdk-python==3.3.398
+https://pypi.org/project/alipay-sdk-python/3.3.398/
+
+所以为了区分,把alipay-sdk-python  的安装包名改成了alipayfanhua
+
+### 安装方法
+pip install alipayfanhua
---
+++ packages/alipayfanhua/alipayfanhua-3.4.0.tar.gz/alipayfanhua-3.4.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+from setuptools import setup
+
+setup(
+      name='alipayfanhua',
+      version='3.4.0',
+      description='alipay sdk',
+      url='',
+      author='fanhua',
+      author_email='507303922@qq.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False
+     )
