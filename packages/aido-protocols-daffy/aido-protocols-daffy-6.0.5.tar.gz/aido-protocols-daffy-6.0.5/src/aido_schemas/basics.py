c83514e91b94da721e482d4a1477f0c45824b78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:58 +0000

    Add package aido-protocols-daffy/6.0.5/aido-protocols-daffy-6.0.5.tar.gz

---
+++ packages/aido-protocols-daffy/aido-protocols-daffy-6.0.5.tar.gz/aido-protocols-daffy-6.0.5/src/aido_schemas/basics.py
@@ -0,0 +1,5 @@
+# noinspection PyUnresolvedReferences
+from zuper_nodes import InteractionProtocol, particularize
+
+# noinspection PyUnresolvedReferences
+from zuper_nodes_wrapper import wrap_direct, Context
