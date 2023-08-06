94558e80c743a2d3f6dd5d2801264d18439f7ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:42 +0000

    Add package dagit/0.9.15rc0/dagit-0.9.15rc0-py3-none-any.whl

---
+++ packages/dagit/dagit-0.9.15rc0-py3-none-any.whl/dagit/webapp/build/precache-manifest.f379a125d5816700c7df43e10162c21a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08785a636b57dfa859088a1f65e15a30",
+    "url": "/de64d77f2aefa82a1dd3.worker.js"
+  },
+  {
+    "revision": "b8714132b55319d9985b269752700df3",
+    "url": "/de64d77f2aefa82a1dd3.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "088e78557088b78f6b23d20b2dcdfb26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5001b1860ab88b72db76",
+    "url": "/static/css/2.71cce147.chunk.css"
+  },
+  {
+    "revision": "5001b1860ab88b72db76",
+    "url": "/static/js/2.d2a94d3d.chunk.js"
+  },
+  {
+    "revision": "c77e24cf387bbb30fda7387e57b810d7",
+    "url": "/static/js/2.d2a94d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808268ebc3dda054ae31",
+    "url": "/static/js/main.2a864f59.chunk.js"
+  },
+  {
+    "revision": "613376841ee7f87ed6c0",
+    "url": "/static/js/runtime-main.2edff307.js"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
