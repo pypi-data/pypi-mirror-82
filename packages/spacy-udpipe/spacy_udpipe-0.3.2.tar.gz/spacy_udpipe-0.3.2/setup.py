3e48c246e723a19789294dd00defdffd3e595678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:08 +0000

    Add package spacy-udpipe/0.3.2/spacy_udpipe-0.3.2.tar.gz

---
+++ packages/spacy-udpipe/spacy_udpipe-0.3.2.tar.gz/spacy_udpipe-0.3.2/setup.py
@@ -0,0 +1,77 @@
+import json
+import os
+
+import setuptools
+
+
+def get_version(fname: str) -> str:
+    full_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        "spacy_udpipe",
+        fname
+    )
+    with open(full_path, "r") as fp:
+        for l in fp:
+            if l.startswith("__version__"):
+                delim = '"' if '"' in l else "'"
+                return l.split(delim)[1]
+            else:
+                raise RuntimeError(
+                    "Unable to find version string."
+                )
+
+
+URL = "https://github.com/TakeLab/spacy-udpipe"
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+# Get available languages and models from spacy_udpipe/languages.json
+languages_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    "spacy_udpipe",
+    "languages.json"
+)
+with open(languages_path, "r") as f:
+    languages = json.load(f)
+
+ENTRY_POINTS = {"spacy_languages":
+                set(f"udpipe_{s.split('-')[0]} = "
+                    "spacy_udpipe:UDPipeLanguage"
+                    for s in languages.keys()
+                    )
+                }
+
+setuptools.setup(
+    name="spacy_udpipe",
+    version=get_version("__init__.py"),
+    description="Use fast UDPipe models directly in spaCy",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url=URL,
+    author="TakeLab",
+    author_email="takelab@fer.hr",
+    license="MIT",
+    keywords="nlp udpipe spacy python",
+    packages=setuptools.find_packages(),
+    install_requires=["spacy>=2.1.0,<3.0.0", "ufal.udpipe>=1.2.0"],
+    python_requires=">=3.6",
+    entry_points=ENTRY_POINTS,
+    tests_require=["pytest>=5.0.0"],
+    package_data={"spacy_udpipe": ["./languages.json"], },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "SpaCy": "https://spacy.io/",
+        "TakeLab": "http://takelab.fer.hr/",
+        "UDPipe": "http://ufal.mff.cuni.cz/udpipe",
+        "Source": URL,
+        "Tracker": URL + "/issues",
+    },
+    zip_safe=False
+)
---
+++ packages/spacy-udpipe/spacy_udpipe-0.3.2.tar.gz/spacy_udpipe-0.3.2/spacy_udpipe/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.3.2"
+__all__ = ["UDPipeLanguage", "UDPipeModel",
+           "load", "load_from_path", "download"]
+
+from .language import UDPipeLanguage, UDPipeModel, load, load_from_path
+from .utils import download
