8eca7f98daa9c7f17cf4a81eca76d5b3cb1db764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:01 +0000

    Add package pylioshelpers/0.0.3/pylioshelpers-0.0.3-py3-none-any.whl

---
+++ packages/pylioshelpers/pylioshelpers-0.0.3-py3-none-any.whl/pylioshelpers/logging/test.py
@@ -0,0 +1,11 @@
+from logging import log
+import warnings
+
+log("test - no level")
+
+log("test ok", "ok")
+log("test success", "success")
+log("test info", "info")
+log("test warn", "warn")
+log("test error", "error")
+log("test message", "message")
