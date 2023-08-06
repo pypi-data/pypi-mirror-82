bb5a06002a918b264c970a24ffd8307745e9d3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:45 +0000

    Add package hashstorage/0.1.1/hashstorage-0.1.1.tar.gz

---
+++ packages/hashstorage/hashstorage-0.1.1.tar.gz/hashstorage-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hashstorage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask==1.1.1']
+
+entry_points = \
+{'console_scripts': ['hashstorage-cli = hashstorage.cli:main']}
+
+setup_kwargs = {
+    'name': 'hashstorage',
+    'version': '0.1.1',
+    'description': 'Implementation of Simple-HashStorage.',
+    'long_description': None,
+    'author': 'chike0905',
+    'author_email': 'chike@sfc.wide.ad.jp',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/chike0905/simple-hashstorage',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
