cba1ed225ac8aa1c6fd430c81a016dfadf87b228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:28 +0000

    Add package flashtool/0.0.10/flashtool-0.0.10.tar.gz

---
+++ packages/flashtool/flashtool-0.0.10.tar.gz/flashtool-0.0.10/flashtool/trackjob.py
@@ -0,0 +1,27 @@
+import psutil
+import time
+from .emailutils import checkemail_exists,setup_email,send_email
+import copy
+
+def trackpid(pid, checktime=1, receiver_email=None, sender_email="gtflashauto@gmail.com", msg=""):
+    if not isinstance(pid,list):
+        pid = [pid]
+    for id in pid:
+        if not psutil.pid_exists(id):
+            print("Job {} does not exists".format(str(id)))
+            return False
+    if receiver_email is not None:
+        if not checkemail_exists(sender_email):
+            print("Setup Password for {}".format(sender_email))
+            setup_email(sender_email)
+    while len(pid) > 0:
+        removed_pid = [id for id in pid if not psutil.pid_exists(id)]
+        for id in removed_pid:
+            print("Job {} is done".format(str(id)))
+            if receiver_email is not None:
+                send_email(receiver_email=receiver_email, sender_email=sender_email, \
+                    subject="Job {} is done".format(str(id)), \
+                    message="{}\n Job {} is done".format(msg, str(id)))
+        pid = [id for id in pid if id not in removed_pid]
+        time.sleep(checktime)
+    return True
