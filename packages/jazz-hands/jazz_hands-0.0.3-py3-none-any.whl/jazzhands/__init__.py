26b67c3352109909e959124082ffaa32724c4b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:14 +0000

    Add package jazz-hands/0.0.3/jazz_hands-0.0.3-py3-none-any.whl

---
+++ packages/jazz-hands/jazz_hands-0.0.3-py3-none-any.whl/jazzhands/__init__.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+
+__all__ = []
+from jazzhands.wavelets import *  # noqa
+from jazzhands.utils import *
+from jazzhands.version import version
+# Then you can be explicit to control what ends up in the namespace,
+# or you can keep everything from the subpackage with the following instead
+__all__ += wavelets.__all__
+
+#Version number
+__version__ = version
