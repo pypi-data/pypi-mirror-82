68b9dec7fd9649e98c7e9652933c5a1d9e8e50d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:53 +0000

    Add package cca-zoo/1.0.0/cca_zoo-1.0.0-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-1.0.0-py3-none-any.whl/cca_zoo/__init__.py
@@ -0,0 +1,8 @@
+from .deep import *
+from .generate_data import *
+from .linear import *
+from .alternating_least_squares import *
+from .mnist_utils import *
+from .plot_utils import *
+
+matplotlib.use('agg')
