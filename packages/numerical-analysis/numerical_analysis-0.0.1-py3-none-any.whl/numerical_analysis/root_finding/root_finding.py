9cb3c73805c1ec82bb981c6146fbb8e67bf312f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:22 +0000

    Add package numerical-analysis/0.0.1/numerical_analysis-0.0.1-py3-none-any.whl

---
+++ packages/numerical-analysis/numerical_analysis-0.0.1-py3-none-any.whl/numerical_analysis/root_finding/root_finding.py
@@ -0,0 +1,45 @@
+def secant(f, x0, x1, error=1e-15):
+    fx0 = f(x0)
+    fx1 = f(x1)
+    while abs(fx1) > error:
+        x2 = (x0 * fx1 - x1 * fx0) / (fx1 - fx0)
+        x0, x1 = x1, x2
+        fx0, fx1 = fx1, f(x2)
+    return x1
+
+
+def newton_raphson(f, df_dx, x0, error=1e-15):
+    while abs(f(x0)) > error:
+        x0 -= f(x0) / df_dx(x0)
+    return x0
+
+
+def newton_raphson_multiple_roots(f, df_dx, n, error=1e-15):
+
+    def sigma(x):
+        s = 0
+        for root in roots:
+            s += 1/(x - root)
+        return s
+
+    def nr_step(xold):
+        return xold - f(xold) / (df_dx(xold) - f(xold)*sigma(xold))
+
+    roots = []
+
+    for i in range(n):
+
+        xk_old = 2
+        xk_new = nr_step(xk_old)
+        ek = xk_new - xk_old
+        xk_old = xk_new
+
+        while abs(ek) > error:
+
+            xk_new = nr_step(xk_old)
+            ek = xk_new - xk_old
+            xk_old = xk_new
+
+        roots.append(xk_new)
+
+    return sorted(roots)
