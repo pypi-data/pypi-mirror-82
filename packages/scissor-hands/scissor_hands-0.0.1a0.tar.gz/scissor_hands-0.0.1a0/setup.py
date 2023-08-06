2dc93115d21edcf5eb4a6000c9c6246f6da42ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:35 +0000

    Add package scissor-hands/0.0.1a0/scissor_hands-0.0.1a0.tar.gz

---
+++ packages/scissor-hands/scissor_hands-0.0.1a0.tar.gz/scissor_hands-0.0.1a0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='scissor_hands',  
+     version='v0.0.1-alpha',
+     scripts=[] ,
+     author="Ceyda Cinarel",
+     author_email="snu_ceyda@gmail.com",
+     description="placeholder until made opensource",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/cceyda/scissor-hands",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    python_requires='>=3.6',
+    install_requires=["pyyaml","coloredlogs"]
+ )
