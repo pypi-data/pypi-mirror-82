e54dc758996e50fef66e51049dea2af99c16e611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:36 +0000

    Add package okapi-python-connector/2020.9.3/okapi-python-connector-2020.9.3.tar.gz

---
+++ packages/okapi-python-connector/okapi-python-connector-2020.9.3.tar.gz/okapi-python-connector-2020.9.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="okapi-python-connector",
+    version="v2020.09.03",
+    author="Jonas Radtke",
+    author_email="jonas@okapiorbits.space",
+    description="Package to connect to OKAPI Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OKAPIOrbits/OkapiPythonConnector",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'time',
+        'json'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
