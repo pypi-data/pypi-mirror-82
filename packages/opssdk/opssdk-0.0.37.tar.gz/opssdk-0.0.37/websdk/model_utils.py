0e9a84cfe6e3daccbd7709c3ff54bd063e0486ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:44 +0000

    Add package opssdk/0.0.37/opssdk-0.0.37.tar.gz

---
+++ packages/opssdk/opssdk-0.0.37.tar.gz/opssdk-0.0.37/websdk/model_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+"""
+Author : shenshuo
+Date   : 2019年12月11日
+Desc   : models类
+"""
+
+from datetime import datetime
+from sqlalchemy.orm import class_mapper
+from .utils import get_contain_dict
+
+
+def model_to_dict(model):
+    model_dict = {}
+    for key, column in class_mapper(model.__class__).c.items():
+        if isinstance(getattr(model, key), datetime):
+            model_dict[column.name] = str(getattr(model, key))
+        else:
+            model_dict[column.name] = getattr(model, key, None)
+    return model_dict
+
+
+def queryset_to_list(queryset, **kwargs) -> list:
+    if kwargs:
+        # if kwargs.get('resource_group') in [None, 'all', '所有项目']: kwargs.pop('resource_group')  ### 资源过滤
+        return [model_to_dict(q) for q in queryset if get_contain_dict(kwargs, model_to_dict(q))]
+    return [model_to_dict(q) for q in queryset]
