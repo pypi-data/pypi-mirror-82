419f6f1763eafba78b7619e244f603ddb4ec7cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:56 +0000

    Add package mummichog/2.4.4/mummichog-2.4.4.tar.gz

---
+++ packages/mummichog/mummichog-2.4.4.tar.gz/mummichog-2.4.4/mummichog/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# Copyright (c) 2010-2020 Shuzhao Li.
+# All rights reserved.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+
+'''
+mummichog 2,
+pathway and network analysis for metabolomics
+
+@author: Shuzhao Li
+
+Online documentation: http://mummichog.org
+Major change in data structure from version 1. 
+Only the default metabolic model (human_model_mfn) and a worm model are ported to version 2 so far.
+This is intended to be the branch of command line version. 
+A server version is upcoming, where metabolic models are stored a separate online database.
+'''
+
+
+from .functional_analysis import *
+from .reporting import *
+
+
+def main():
+    
+    print (fishlogo)
+    print ( "mummichog version %s \n" %VERSION )
+    optdict = dispatcher()
+
+    print_and_loginfo("Started @ %s\n" %time.asctime())
+    userData = InputUserData(optdict)
+    
+    #specify which metabolic model 
+    if userData.paradict['network'] in ['human', 'hsa', 'Human', 'human_mfn', 'hsa_mfn', '']:
+        theoreticalModel = metabolicNetwork(metabolicModels[ 'human_model_mfn' ])
+    elif userData.paradict['network'] in ['worm', 'C. elegans', 'icel1273', 'Caenorhabditis elegans']:
+        theoreticalModel = metabolicNetwork(metabolicModels[ 'worm_model_icel1273' ])
+        
+    else:
+        raise KeyError( "Unsupported species/model. Pls contact author." )
+    
+    mixedNetwork = DataMeetModel(theoreticalModel, userData)
+
+    # getting a list of Pathway instances, with p-values, in PA.resultListOfPathways
+    PA = PathwayAnalysis(mixedNetwork.model.metabolic_pathways, mixedNetwork)
+    PA.cpd_enrich_test()
+    
+    # Module analysis, getting a list of Mmodule instances
+    MA = ModularAnalysis(mixedNetwork)
+    MA.dispatch()
+    
+    # do activity network
+    AN = ActivityNetwork( mixedNetwork, set(PA.collect_hit_Trios() + MA.collect_hit_Trios()) )
+    
+    Local = LocalExporting(mixedNetwork, PA, MA, AN)
+    Local.run()
+
+    Web = WebReporting(Local, PA, MA, AN)
+    Web.run()
+    
+    print_and_loginfo("\nFinished @ %s\n" %time.asctime())
+
+
+
+
+#
+# -----------------------------------------------------------------------------
+#
+
+if __name__ == '__main__':
+
+    main()
+
+
---
+++ packages/mummichog/mummichog-2.4.4.tar.gz/mummichog-2.4.4/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name='mummichog',
+  version='2.4.4',
+
+  author='Shuzhao Li, Francisco Castellanos, Andrei Todor',
+  author_email='shuzhao.li@gmail.com',
+  description='Pathway and network analysis for metabolomics data',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/shuzhao-li/mummichog',
+  license='BSD 3-Clause',
+  keywords='metabolomics analysis bioinformatics mass spectrometry systems biology',
+
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Chemistry',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
+
+  packages=find_packages(include=['mummichog', 'mummichog.*']),
+  data_files=[ ('mummichog/resources', ['mummichog/resources/plotly-graphs.js', 'mummichog/resources/plotly-latest.min.js']),
+               ('mummichog/tests', ['mummichog/tests/testdata0710.txt']) ],
+  include_package_data=True,
+  zip_safe=True,
+  entry_points = {
+        'console_scripts': ['mummichog=mummichog.command_line:main'],
+    },
+
+  python_requires='>=3.4',
+  install_requires=[
+    'matplotlib',
+    'networkx>=1,<2',
+    'numpy',
+    'scipy',
+    'xlsxwriter',
+  ],
+
+)
