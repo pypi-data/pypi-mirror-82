c104ebc4ce5801e7349687b3b1f05ddcb2d9b293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:25 +0000

    Add package aiogcd/0.11.8/aiogcd-0.11.8.tar.gz

---
+++ packages/aiogcd/aiogcd-0.11.8.tar.gz/aiogcd-0.11.8/aiogcd/connector/__init__.py
@@ -0,0 +1,8 @@
+"""__init__.py
+
+Created on: May 19, 2017
+    Author: Jeroen van der Heijden <jeroen@transceptor.technology>
+"""
+from .connector import GcdConnector, GcdServiceAccountConnector
+from .client_token import Token
+from .service_account_token import ServiceAccountToken
---
+++ packages/aiogcd/aiogcd-0.11.8.tar.gz/aiogcd-0.11.8/aiogcd/orm/properties/__init__.py
@@ -0,0 +1,15 @@
+"""__init__.py
+
+Created on: May 19, 2017
+    Author: Jeroen van der Heijden <jeroen@transceptor.technology>
+"""
+from .integervalue import IntegerValue
+from .stringvalue import StringValue
+from .booleanvalue import BooleanValue
+from .arrayvalue import ArrayValue
+from .jsonvalue import JsonValue
+from .doublevalue import DoubleValue
+from .keyvalue import KeyValue
+from .anyvalue import AnyValue
+from .datetimevalue import DatetimeValue
+from .entityvalue import EntityValue
