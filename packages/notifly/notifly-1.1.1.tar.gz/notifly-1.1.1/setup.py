c93e36642d989af666cd64fc01c24a842c845afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:46 +0000

    Add package notifly/1.1.1/notifly-1.1.1.tar.gz

---
+++ packages/notifly/notifly-1.1.1.tar.gz/notifly-1.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+import os.path
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+# Get the long description from the README file
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+ 
+setup(
+  name='notifly',
+  version='1.1.1',
+  description='Telegram Bot Notifier',
+  long_description=long_description,
+  long_description_content_type='text/x-rst',
+  url='',  
+  author='Divakar R',
+  author_email='rexdivakar@hotmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Telegram', 
+  packages=['telegram'],
+  install_requires=[''] 
+)
