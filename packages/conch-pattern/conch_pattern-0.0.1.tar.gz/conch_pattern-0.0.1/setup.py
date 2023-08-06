30bb900e898983f618ab511e8ffa03212b91109c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:17 +0000

    Add package conch-pattern/0.0.1/conch_pattern-0.0.1.tar.gz

---
+++ packages/conch-pattern/conch_pattern-0.0.1.tar.gz/conch_pattern-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="conch_pattern",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=["click", "numpy", "scipy"],
+    extras_require={
+        "dev": ["pytest"],
+        "examples": ["matplotlib"],
+        "docs": ["matplotlib", "sphinx"],
+    },
+)
