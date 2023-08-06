7778dff9d6bbb945d99c4cfff7974b19131865be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:25 +0000

    Add package niceprint/1.0.0/niceprint-1.0.0.tar.gz

---
+++ packages/niceprint/niceprint-1.0.0.tar.gz/niceprint-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="niceprint",
+    version='1.0.0',
+    author="AstralDev",
+    author_email="ekureedem480@gmail.com",
+    description="A simple module for print.",
+    license="MIT",
+    dependencies=["color"],
+    packages=["niceprint"]
+)
