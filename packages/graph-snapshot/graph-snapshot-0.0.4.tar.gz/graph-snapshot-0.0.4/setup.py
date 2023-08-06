3f1a133722fa4dd9b806c8297b283ddc7f4c1966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package graph-snapshot/0.0.4/graph-snapshot-0.0.4.tar.gz

---
+++ packages/graph-snapshot/graph-snapshot-0.0.4.tar.gz/graph-snapshot-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graph-snapshot",
+    version="0.0.4",
+    author="Josua Kugler, Nico Haaf",
+    author_email="josua.kugler@stud.uni-heidelberg.de, nico.haaf@stud.uni-heidelberg.de",
+    description="A small package for creating snapshots of graphs with networkx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NicoHaaf/graph_snapshot_pip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.8',
+)
