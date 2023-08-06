49633c4c949ea51dad032a3502a4fce3905f5a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:28 +0000

    Add package examtool/2.0.11/examtool-2.0.11.tar.gz

---
+++ packages/examtool/examtool-2.0.11.tar.gz/examtool-2.0.11/examtool/api/server_delegate.py
@@ -0,0 +1,36 @@
+from functools import wraps
+from os import getenv
+
+import requests
+
+from examtool.api.auth import get_token, refresh_token
+
+
+def server_only(func):
+    @wraps(func)
+    def wrapped(**kwargs):
+        if getenv("ENV") == "SERVER":
+            return func(**kwargs)
+        else:
+            method = func.__name__
+            try:
+                return call_server(method, kwargs)
+            except PermissionError:
+                refresh_token()
+            return call_server(method, kwargs)
+
+    return wrapped
+
+
+def call_server(method, kwargs):
+    token = get_token()
+    resp = requests.post(
+        "https://exam.cs61a.org/admin/api/{method}".format(method=method),
+        json={**kwargs, "token": token},
+    )
+    if resp.status_code == 200:
+        return resp.json()
+    elif resp.status_code == 401:
+        raise PermissionError
+    else:
+        raise Exception(resp.text)
