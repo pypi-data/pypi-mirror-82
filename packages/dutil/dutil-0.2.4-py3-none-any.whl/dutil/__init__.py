2b9ce1aca955c2f090c7d114e8c14e771e92ded7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package dutil/0.2.4/dutil-0.2.4-py3-none-any.whl

---
+++ packages/dutil/dutil-0.2.4-py3-none-any.whl/dutil/__init__.py
@@ -0,0 +1,5 @@
+from dutil.src.persist import cached, cached2, clear_cache
+from dutil.src.dask_pipeline import cached_delayed, DelayedParameters, dask_compute
+from dutil.src.stats import q_01, q_10, q_25, q_50, q_75, q_90, q_99, mean_lower, mean_upper
+from dutil.src.transform import ht
+from dutil.src.jupyter import dht
