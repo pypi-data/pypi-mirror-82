205be0e5a59b181a5a0f4fce9af40cea1f31ea99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:47 +0000

    Add package dazl/7.3.1/dazl-7.3.1.tar.gz

---
+++ packages/dazl/dazl-7.3.1.tar.gz/dazl-7.3.1/dazl/damleval/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2017-2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from .expr_eval import ExpressionEvaluator
+from .type_eval import TypeComputer
