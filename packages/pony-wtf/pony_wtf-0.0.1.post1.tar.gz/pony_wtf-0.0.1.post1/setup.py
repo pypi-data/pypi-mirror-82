03e9a116aec7acb2f8b3be1b6196e772578da317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:56 +0000

    Add package pony-wtf/0.0.1.post1/pony_wtf-0.0.1.post1.tar.gz

---
+++ packages/pony-wtf/pony_wtf-0.0.1.post1.tar.gz/pony_wtf-0.0.1.post1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def readme():
+    with open('pony_wtf/README.md') as f:
+        return f.read()
+
+setup(name='pony_wtf',
+      version='0.0.1-1',
+      description='Create WTF-Form from your pony database entity',
+      long_description=readme(),      
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 2.7 :: Python :: 3.8',
+        #'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='pony ponyorm wtf wtf-form ',
+      license='MIT',
+      url='https://github.com/tabebqena/Pony-WTF',
+      author='Ahmad Yahia',
+      author_email='qenadev@gmail.com',
+      packages=["pony_wtf"],
+      install_requires=[
+          'pony',
+          'pony_wtf',
+          'Flask_WTF',
+          'WTForms',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      zip_ok=False,
+      )
