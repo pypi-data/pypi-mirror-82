34a5169d8fb987876cd19bacff1a8a61b7101f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:37 +0000

    Add package easy-celery/1.0.1/easy_celery-1.0.1.tar.gz

---
+++ packages/easy-celery/easy_celery-1.0.1.tar.gz/easy_celery-1.0.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/ python3
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: kzl_knight
+# Mail: 164645621@qq.com
+# Created Time: 2020-10-13 00:00:00
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="easy_celery",
+    version="1.0.1",
+    keywords=("celery", "redis", "easy", "job"),
+    description="Make multi-computer distributed tasks easier",
+    long_description="Based on Redis, support task scheduling between master and slave",
+    license="MIT Licence",
+    url="https://github.com/kzl_knight/easy_celery",
+    author="kzl_knight",
+    author_email="164645621@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        'redis>=3.5.0',
+    ]
+)
+
+
+
