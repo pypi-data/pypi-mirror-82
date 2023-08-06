2c04f61f449e89179806329e2536523dc8c5ea24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package notedata/0.4.7/notedata-0.4.7.tar.gz

---
+++ packages/notedata/notedata-0.4.7.tar.gz/notedata-0.4.7/notedata/dataset/__init__.py
@@ -0,0 +1,7 @@
+from .core import ElectronicsData
+from .core import get_adult_data
+from .core import get_bitly_usagov_data
+from .core import get_electronics
+from .core import get_movielens
+from .core import get_movielens
+from .core import get_porto_seguro_data
