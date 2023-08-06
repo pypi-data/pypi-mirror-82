433cbdd6dede8fbcbcce9b9f7af2050046f75db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:04 +0000

    Add package ZenoMapper/0.45/ZenoMapper-0.45.tar.gz

---
+++ packages/ZenoMapper/ZenoMapper-0.45.tar.gz/ZenoMapper-0.45/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = ''
+with open('ZenoMapper/__version__', 'r+') as fh:
+    set_version = fh.readline()
+    build_number = os.getenv('TRAVIS_BUILD_NUMBER', '0.0')
+
+    if not set_version:
+        VERSION = '0.' + build_number
+        fh.write(VERSION)
+    else:
+        VERSION = set_version
+
+
+setup(
+    name='ZenoMapper',
+    license='Apache license 2.0',
+    description='An Object Config Mapper (OCM)',
+    version=VERSION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Joseph Procopio',
+    author_email='josephp27@live.com',
+    packages=find_packages(exclude=['test', 'tests']),
+    options={'bdist_wheel' : {'universal': True}},
+    url='https://github.com/josephp27/Zeno',
+    keywords=['Zeno', 'OCM', 'Config', 'Configuration', 'Mapper', 'Object'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
