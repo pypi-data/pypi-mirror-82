f1a93629a45ebd5935017231881075b7c52d88e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package alfa-sdk/0.1.26/alfa-sdk-0.1.26.tar.gz

---
+++ packages/alfa-sdk/alfa-sdk-0.1.26.tar.gz/alfa-sdk-0.1.26/alfa_sdk/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.26"
+
+from alfa_sdk.algorithm import AlgorithmClient
+from alfa_sdk.integration import IntegrationClient
+from alfa_sdk.data import DataClient
+from alfa_sdk.secrets import SecretsClient
