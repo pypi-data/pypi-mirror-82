987aa0585e599239ba0b8c87adf7eed49d9e6b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package pdfToAudioBookConverter/0.0.2/pdfToAudioBookConverter-0.0.2.tar.gz

---
+++ packages/pdfToAudioBookConverter/pdfToAudioBookConverter-0.0.2.tar.gz/pdfToAudioBookConverter-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+	README = readme_file.read()
+
+setup_args = dict(
+	name='pdfToAudioBookConverter',
+	version='0.0.2',
+	description='This package converts a pdf file to audiobooks',
+	long_description_content_type="text/markdown",
+	long_description=README,
+	license='MIT',
+	packages=find_packages(),
+	author='Rahul Mishra',
+	author_email='rahulmishra.psy@gmail.com',
+	keywords=['Audiobooks', 'pdf', 'audiobookconverter'],
+	url='https://github.com/seigfried1/pdftoaudiobook',
+)
+
+install_requires = [
+	'gTTS>=2.1.1',
+	'PyPDF2>=1.26.0'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
