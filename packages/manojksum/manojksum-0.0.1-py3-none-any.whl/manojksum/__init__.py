00b0636d92c7d1922668ec3c535c6b4442802c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:27 +0000

    Add package manojksum/0.0.1/manojksum-0.0.1-py3-none-any.whl

---
+++ packages/manojksum/manojksum-0.0.1-py3-none-any.whl/manojksum/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=input("Enter 1st number= ")
+
+
+# In[3]:
+
+
+b=input("Enter 2nd number= ")
+
+
+# In[4]:
+
+
+c=a+b
+
+
+# In[7]:
+
+
+print("The sum=",c)
+
+
+# In[ ]:
+
+
+
+
