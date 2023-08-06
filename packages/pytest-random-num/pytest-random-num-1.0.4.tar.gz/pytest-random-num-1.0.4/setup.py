47f7c1ab33cddd5563afe5fad693b53009531f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:21 +0000

    Add package pytest-random-num/1.0.4/pytest-random-num-1.0.4.tar.gz

---
+++ packages/pytest-random-num/pytest-random-num-1.0.4.tar.gz/pytest-random-num-1.0.4/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-random-num',
+    version='1.0.4',
+    author='Jazeps Basko',
+    author_email='jazeps.basko@gmail.com',
+    maintainer='Jazeps Basko',
+    maintainer_email='jazeps.basko@gmail.com',
+    license='MIT',
+    url='https://github.com/jbasko/pytest-random-num',
+    description='Randomise the order in which pytest tests are run with some control over the randomness',
+    long_description=read('README.rst'),
+    packages=[
+        'random_order',
+    ],
+    include_package_data=True,
+    python_requires=">=3.5.0",
+    install_requires=[
+        'pytest>=3.0.0',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='pytest random test order shuffle',
+    entry_points={
+        'pytest11': [
+            'random_order = random_order.plugin',  # >=1.0.0
+        ],
+    },
+)
---
+++ packages/pytest-random-num/pytest-random-num-1.0.4.tar.gz/pytest-random-num-1.0.4/tests/conftest.py
@@ -0,0 +1,48 @@
+import collections
+
+import pytest
+
+pytest_plugins = 'pytester'
+
+
+Call = collections.namedtuple('Call', field_names=('package', 'module', 'cls', 'name'))
+
+
+def _get_test_calls(result):
+    """
+    Returns a tuple of test calls in the order they were made.
+    """
+    calls = []
+
+    for c in result.reprec.getcalls('pytest_runtest_call'):
+        calls.append(Call(
+            package=c.item.module.__package__,
+            module=c.item.module.__name__,
+            cls=(c.item.module.__name__, c.item.cls.__name__) if c.item.cls else None,
+            name=c.item.name,
+        ))
+    return tuple(calls)
+
+
+@pytest.fixture
+def get_test_calls():
+    """
+    Returns a function to get runtest calls out from testdir.pytestrun result object.
+    """
+    return _get_test_calls
+
+
+@pytest.fixture
+def twenty_tests():
+    code = []
+    for i in range(20):
+        code.append('def test_a{0}(): assert True\n'.format(str(i).zfill(2)))
+    return ''.join(code)
+
+
+@pytest.fixture
+def twenty_cls_tests():
+    code = []
+    for i in range(20):
+        code.append('\tdef test_b{0}(self): self.assertTrue\n'.format(str(i).zfill(2)))
+    return ''.join(code)
