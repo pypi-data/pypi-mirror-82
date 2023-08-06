5f923c746fe784b71c354c48506344a82821e0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:09 +0000

    Add package dagit/0.9.15/dagit-0.9.15.tar.gz

---
+++ packages/dagit/dagit-0.9.15.tar.gz/dagit-0.9.15/dagit/webapp/build/precache-manifest.82fb44d83910c47fba1133e4778385eb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf914a1a74e5a2e9b2c7257b875e1a0",
+    "url": "/320e4bbb2a3ef493cd6f.worker.js"
+  },
+  {
+    "revision": "b8714132b55319d9985b269752700df3",
+    "url": "/320e4bbb2a3ef493cd6f.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7c8094835a3d6142db7f2b23914cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c99c51fb94149c406f4",
+    "url": "/static/css/2.df74e261.chunk.css"
+  },
+  {
+    "revision": "7c99c51fb94149c406f4",
+    "url": "/static/js/2.7eb4e6b0.chunk.js"
+  },
+  {
+    "revision": "c77e24cf387bbb30fda7387e57b810d7",
+    "url": "/static/js/2.7eb4e6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1570ad43018fbaa36ff",
+    "url": "/static/js/main.eb390723.chunk.js"
+  },
+  {
+    "revision": "613376841ee7f87ed6c0",
+    "url": "/static/js/runtime-main.2edff307.js"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
