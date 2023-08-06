fe14b6f74fabd2e2febe51ac08e79599192ab7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:26 +0000

    Add package eelbrain/0.33.1/eelbrain-0.33.1.tar.gz

---
+++ packages/eelbrain/eelbrain-0.33.1.tar.gz/eelbrain-0.33.1/eelbrain/_utils/__init__.py
@@ -0,0 +1,8 @@
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+from .basic import (
+    PickleableDataClass,
+    as_list, as_sequence, ask, deprecated, _deprecated_alias, deprecated_attribute, intervals,
+    LazyProperty, keydefaultdict, n_decimals, natsorted,
+    log_level, set_log_level, ScreenHandler,
+)
+from .system import IS_OSX, IS_WINDOWS, user_activity, restore_main_spec
