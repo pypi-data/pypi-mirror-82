97f978cf70e4498de7d50794052c56c4b0dbaf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:35 +0000

    Add package pycoMeth/0.4.25/pycoMeth-0.4.25.tar.gz

---
+++ packages/pycoMeth/pycoMeth-0.4.25.tar.gz/pycoMeth-0.4.25/pycoMeth/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Define self package variable
+__version__ = "0.4.25"
+__description__ = 'DNA methylation analysis downstream to Nanopolish for Oxford Nanopore DNA sequencing datasets'
