d676c2c01ef1270b31ff142d1bcb7229226f1c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:41 +0000

    Add package AutoChatbot/0.0.2/AutoChatbot-0.0.2.tar.gz

---
+++ packages/AutoChatbot/AutoChatbot-0.0.2.tar.gz/AutoChatbot-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AutoChatbot", # Replace with your own username
+    version="0.0.2",
+    author="Kritik Seth",
+    author_email="sethkritik@gmail.com",
+    description="Package that makes chatbots automatically",
+    py_modules=['chat'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kritikseth",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
