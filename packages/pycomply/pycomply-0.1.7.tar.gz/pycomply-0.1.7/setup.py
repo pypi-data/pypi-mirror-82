e7b8ec1b05651f5ee55e8f794be599741dd1f8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:28 +0000

    Add package pycomply/0.1.7/pycomply-0.1.7.tar.gz

---
+++ packages/pycomply/pycomply-0.1.7.tar.gz/pycomply-0.1.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pycomply']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pycomply',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'Ashutosh Bandiwdekar',
+    'author_email': 'ashutoshb@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
