48c00d0d3cc42a00f235c560e5b0aa9658d0c77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:32 +0000

    Add package azureml-train-automl-runtime/1.16.0/azureml_train_automl_runtime-1.16.0-py3-none-any.whl

---
+++ packages/azureml-train-automl-runtime/azureml_train_automl_runtime-1.16.0-py3-none-any.whl/azureml/train/automl/runtime/_automl_job_phases/__init__.py
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+from azureml.train.automl.runtime._automl_job_phases.featurization_phase import FeaturizationPhase
+from azureml.train.automl.runtime._automl_job_phases.setup_phase import SetupPhase
+from azureml.train.automl.runtime._automl_job_phases.training_iteration import TrainingIteration
+from azureml.train.automl.runtime._automl_job_phases.utilities import PhaseUtil
