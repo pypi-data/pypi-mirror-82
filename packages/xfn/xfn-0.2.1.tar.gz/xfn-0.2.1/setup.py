09c78e343f63d0d59395bb81db79fe78207ae9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:23 +0000

    Add package xfn/0.2.1/xfn-0.2.1.tar.gz

---
+++ packages/xfn/xfn-0.2.1.tar.gz/xfn-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xfn',
+    version='0.2.1',
+    packages=['xfn'],
+    url='https://github.com/jjtolton/pyxfn',
+    license='MIT',
+    author='James J. Tolton',
+    author_email='jjtolton@gmail.com',
+    description='Clojure\'s transducer functionality and API in Python'
+)
