5824fc14ac165b24788a9e25d195488afcad80e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:54 +0000

    Add package zoop-wrapper/0.4.5/zoop_wrapper-0.4.5-py3-none-any.whl

---
+++ packages/zoop-wrapper/zoop_wrapper-0.4.5-py3-none-any.whl/zoop_wrapper/response.py
@@ -0,0 +1,17 @@
+from requests.models import Response
+
+
+class ZoopResponse(Response):
+    """
+    Uma :class:`requests.Response` recebida da API da Zoop.
+
+    .. danger:: Essa classe NÃO é utilizada no código (não é instanciada).
+
+        Está na biblioteca apenas para ter o type hinting.
+        Esses atributos são adicionados ao objeto :class:`requests.Response`
+
+    Attributes:
+        data (dict): json da resposta
+        instance (:class:`.ResourceModel`): instância do recurso
+        instances (lista de :class:`.ResourceModel`): lista de instâncias do recurso
+    """
