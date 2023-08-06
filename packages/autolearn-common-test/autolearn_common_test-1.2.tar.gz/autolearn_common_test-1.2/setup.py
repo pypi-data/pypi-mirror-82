5630ee1dd161dfc7ae0c7a9f01e87b0f120c8a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:30 +0000

    Add package autolearn-common-test/1.2/autolearn_common_test-1.2.tar.gz

---
+++ packages/autolearn-common-test/autolearn_common_test-1.2.tar.gz/autolearn_common_test-1.2/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup #import setuptools python package
+
+setup(name='autolearn_common_test', #Name of the package
+    version='1.2', #Version of the package
+    packages=['autolearn_common_test'])
