4623eeb381347650cbfb9970f4fd5b95b81b8b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:37 +0000

    Add package citation-langserver/0.1.1/citation_langserver-0.1.1-py3-none-any.whl

---
+++ packages/citation-langserver/citation_langserver-0.1.1-py3-none-any.whl/citation_langserver/completion.py
@@ -0,0 +1,25 @@
+"""Citation Language Server Completion Utilities"""
+import re
+from typing import Generator
+
+from bibparse import Biblio
+from pygls.types import CompletionItem
+from pygls.types import CompletionItemKind
+
+from .format import info
+
+
+def generate_list(
+    bibliographies: Biblio, search_key: str
+) -> Generator[CompletionItem, None, None]:
+    """Given a bibliography and a search string, find all completion items
+    that might match the entry."""
+    key_regex = re.compile("^{}.*".format(search_key))
+    for key in list(filter(key_regex.match, bibliographies.keys())):
+        entry = bibliographies[key]
+        yield CompletionItem(
+            label="{}".format(key),
+            kind=CompletionItemKind.Text,
+            documentation=info(entry),
+            insert_text=key,
+        )
