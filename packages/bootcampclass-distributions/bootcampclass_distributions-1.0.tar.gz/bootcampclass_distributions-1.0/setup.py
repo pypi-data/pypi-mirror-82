223a60e7e78010a5001f0ba0875c41220e0f20d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package bootcampclass-distributions/1.0/bootcampclass_distributions-1.0.tar.gz

---
+++ packages/bootcampclass-distributions/bootcampclass_distributions-1.0.tar.gz/bootcampclass_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bootcampclass_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['bootcampclass_distributions'],
+      author = 'Uzoma Uzosike',
+      author_email = 'uzoma.uzosike@gmail.com',
+      zip_safe=False)
