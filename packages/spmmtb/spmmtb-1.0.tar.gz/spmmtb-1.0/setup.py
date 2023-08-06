83084852c3a0d6e99e6f786799d2b6f733801169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:21 +0000

    Add package spmmtb/1.0/spmmtb-1.0.tar.gz

---
+++ packages/spmmtb/spmmtb-1.0.tar.gz/spmmtb-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='spmmtb',
+	  version='1.0',
+	  description='Simple math module with + - * / only',
+	  packages=['spmmtb'],
+	  author='ReGamer45',
+	  author_email='t.belf2020@gmail.com',
+	  url='https://github.com/ReGamer45/spmmtb',
+	  zip_safe=False)
---
+++ packages/spmmtb/spmmtb-1.0.tar.gz/spmmtb-1.0/spmmtb/__init__.py
@@ -0,0 +1,14 @@
+first = int()
+second = int()
+
+def add(first, second):
+	return first + second
+	
+def multiply(first, second):
+	return first * second
+	
+def divide(first, second):
+	return first - second
+
+def sustract(first, second):
+	return first / second
