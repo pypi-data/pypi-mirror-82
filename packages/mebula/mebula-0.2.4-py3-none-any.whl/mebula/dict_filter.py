7fe37d67bb5cec4c78dac03044846a7bbb80c0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package mebula/0.2.4/mebula-0.2.4-py3-none-any.whl

---
+++ packages/mebula/mebula-0.2.4-py3-none-any.whl/mebula/dict_filter.py
@@ -0,0 +1,223 @@
+# SPDX-FileCopyrightText: © 2020 Matt Williams <matt@milliams.com>
+# SPDX-License-Identifier: MIT
+
+import operator
+import re
+from typing import Iterable, List, Mapping
+
+import lark  # type: ignore
+
+
+__ALL__ = ["match_dict", "filter_dicts"]
+
+
+def create_parser() -> lark.Lark:
+    """
+    This function implements a the filter language used by Google Cloud as described at
+    https://cloud.google.com/sdk/gcloud/reference/topic/filters
+
+    Returns: the parser object
+    """
+    grammar = """
+    ?start: _expression
+
+    _expression: term
+               | parenthesised
+               | logical_binary
+               | logical_unary
+
+    logical_unary: unary_logical_operator _expression
+    ?logical_binary: _expression binary_logical_operator _expression (binary_logical_operator _expression)*
+
+    ?parenthesised: "(" _expression ")"
+
+    unary_logical_operator: "NOT"      -> not
+    binary_logical_operator: "AND"     -> and
+                           | "OR"      -> or
+                           | WS_INLINE -> and
+
+    LIST_COMPARATOR: ":("
+                   | "=("
+    VALUE_COMPARATOR: ":"
+                    | "="
+                    | "!="
+                    | "<"
+                    | "<="
+                    | ">"
+                    | ">="
+                    | "~"
+                    | "!~"
+
+    term: "- " KEY ":" "*"                   -> not_defined
+        | KEY ":" "*"                        -> is_defined
+        | KEY VALUE_COMPARATOR _value        -> compare
+        | KEY LIST_COMPARATOR list_items ")" -> compare_list
+
+    _LIST_SEPARATOR: (WS | ",")
+
+    list_items: _value (_LIST_SEPARATOR _value)*
+
+    KEY: CNAME("."CNAME)*
+    _value: NUMBER
+         | CHARACTER_SEQUENCE
+         | STRING
+
+    STRING : /("(?!"").*?(?<!\\\\)(\\\\\\\\)*?"|'(?!'').*?(?<!\\\\)(\\\\\\\\)*?')/i
+
+    CHARACTER_SEQUENCE: (LETTER | NUMBER | "-" | "^" | ":" | "[" | "]" | "@" | "."
+                      | "*" | "!" | "£" | "$" | "%" | "*" | "|" | "\\\\" | "/" | "_"
+                      | "+" | "=" | "{" | "}" | ":" | ";" | "~" | "#" | "<" | ">" | "?")+
+
+    %import common.LETTER
+    %import common.NUMBER
+    %import common.CNAME
+    %import common.WS_INLINE
+    %import common.WS
+    %ignore WS_INLINE
+    """
+    return lark.Lark(grammar)
+
+
+def parse_filter(filter_text: str) -> lark.Tree:
+    """
+    Args:
+        filter_text: the filter string to parse
+
+    Returns: the parse tree
+    """
+    return PARSER.parse(filter_text)
+
+
+@lark.v_args(inline=True)
+class FilterDict(lark.Transformer):
+    def __init__(self, instance: Mapping):
+        super().__init__()
+        self.instance = instance
+
+    @staticmethod
+    def _pattern_match(true_value: str, check_value: str):
+        if check_value.endswith("*"):
+            # TODO implement wildcard * prefix matches
+            raise NotImplementedError("Pattern prefix matching not implemented")
+        return check_value in true_value.split()
+
+    def _key_value(self, key: str):
+        true_value = self.instance
+        for key in key.split("."):
+            true_value = true_value[str(key)]
+        return true_value
+
+    def compare_list(
+        self, key: lark.Token, operator_name: lark.Token, list_items: lark.Tree
+    ):
+        try:
+            true_value = self._key_value(key)
+        except KeyError:
+            # If a dotted name does not exist on the instance, return false
+            # Perhaps this should be smarter and e.g. return True on ``not_equals``
+            return False
+
+        operator_f = {":(": self._pattern_match, "=(": operator.eq}[operator_name]
+
+        check_values = [str(v) for v in list_items.children]
+
+        return any(operator_f(true_value, v) for v in check_values)
+
+    def compare(self, key: lark.Token, operator_name: lark.Token, value: lark.Token):
+        try:
+            true_value = self._key_value(key)
+        except KeyError:
+            # If a dotted name does not exist on the instance, return false
+            # Perhaps this should be smarter and e.g. return True on ``not_equals``
+            return False
+
+        operator_f = {
+            ":": self._pattern_match,
+            "<": operator.lt,
+            "<=": operator.le,
+            "=": operator.eq,
+            "!=": operator.ne,
+            ">=": operator.ge,
+            ">": operator.gt,
+            "~": lambda s, p: bool(re.match(p, s)),
+            "!~": lambda s, p: not bool(re.match(p, s)),
+        }[operator_name]
+
+        # Strip the start and end `"` or `'`
+        if value.type == "STRING":
+            value = value.update("CHARACTER_SEQUENCE", value[1:-1])
+
+        return operator_f(true_value, value)
+
+    def is_defined(self, dotted_key_name: lark.Token):
+        d = self.instance
+        try:
+            for key in dotted_key_name.split("."):
+                d = d[key]
+        except KeyError:
+            return False
+        else:
+            return True
+
+    def not_defined(self, dotted_key_name: lark.Token):
+        return not self.is_defined(dotted_key_name)
+
+    def logical_unary(self, unary_operator: lark.Tree, data: bool):
+        if unary_operator.data == "not":
+            return not data
+        else:
+            raise NotImplementedError(
+                f"Unary operator {unary_operator.data} not implemented"
+            )
+
+    @lark.v_args(inline=False)
+    def logical_binary(self, tree):
+        data: List[bool] = tree[0::2]
+        operators: List[lark.Tree] = tree[1::2]
+        all_and = all(t.data == "and" for t in operators)
+        all_or = all(t.data == "or" for t in operators)
+        if not (all_and or all_or):
+            raise SyntaxError("Ambiguous binary operators")
+        if all_and:
+            return all(data)
+        if all_or:
+            return any(data)
+
+        raise NotImplementedError("Boolean operator not implmented")
+
+
+PARSER = create_parser()
+
+
+def match_dict(pattern: str, dictionary: dict) -> bool:
+    """
+    Given a filter pattern and a dictionary, does the dictionary match the filter
+
+    Args:
+        pattern: a https://cloud.google.com/sdk/gcloud/reference/topic/filters compatible filter string
+        dictionary: a (nested) dictionary you wish to filter
+
+    Returns: True if the pattern matches and False otherwise
+    """
+    p = parse_filter(pattern)
+    try:
+        return FilterDict(dictionary).transform(p)
+    except lark.exceptions.VisitError as e:
+        raise e.orig_exc
+
+
+def filter_dicts(pattern: str, dictionaries: Iterable[dict]) -> Iterable[dict]:
+    """
+    Given a filter pattern and an iterable of dictionaries, return an iterable containing the matched entries.
+
+    Args:
+        pattern:a https://cloud.google.com/sdk/gcloud/reference/topic/filters compatible filter string
+        dictionaries: an iterable of (nested) dictionaries you wish to filter
+
+    Returns: an iterable of matched dictionaries
+    """
+    p = parse_filter(pattern)
+    try:
+        return filter(lambda d: FilterDict(d).transform(p), dictionaries)
+    except lark.exceptions.VisitError as e:
+        raise e.orig_exc
---
+++ packages/mebula/mebula-0.2.4-py3-none-any.whl/mebula/oracle.py
@@ -0,0 +1,147 @@
+# SPDX-FileCopyrightText: © 2020 Matt Williams <matt@milliams.com>
+# SPDX-License-Identifier: MIT
+
+import contextlib
+import datetime
+import functools
+import ipaddress
+import random
+import string
+import unittest.mock
+from collections import defaultdict
+from typing import Dict, List
+
+try:
+    import oci  # type: ignore
+except ImportError:
+    raise ImportError(
+        "The mebula 'oracle' module requires the pip package ``mebula[oracle]``"
+    )
+
+
+class OracleState:
+    def __init__(self):
+        self.instances: Dict[str, List[oci.core.models.Instance]] = defaultdict(list)
+        self.vnic_attachments: List[oci.core.models.VnicAttachment] = []
+        self.vnics: List[oci.core.models.Vnic] = []
+
+
+def oracle_arg_check(f):
+    """
+    A decorator which calls a mocked version of an OCI function to use
+    it to check the arguments passed.
+    """
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        getattr(args[0].client, f.__name__)(unittest.mock.Mock(), *args[1:], **kwargs)
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+class OracleComputeClient:
+    """
+    A mocked version of oci.core.ComputeClient
+    """
+
+    def __init__(self, config: dict, state: OracleState, **kwargs):
+        self._state = state
+        self.client = oci.core.compute_client.ComputeClient
+
+    @oracle_arg_check
+    def list_instances(self, compartment_id: str, **kwargs) -> oci.response.Response:
+        ins = self._state.instances[compartment_id]
+        filters = {"availability_domain", "display_name", "lifecycle_state"}
+        for f in filters:
+            if f in kwargs:
+                ins = [i for i in ins if getattr(i, f) == kwargs[f]]
+
+        if "sort_by" in kwargs:
+            if "sort_order" in kwargs:
+                reverse = kwargs["sort_order"] == "DESC"
+            else:
+                reverse = None
+            if kwargs["sort_by"] == "TIMECREATED":
+                reverse = reverse if reverse is not None else True
+                ins = sorted(ins, key=lambda i: i.time_created, reverse=reverse)
+            elif kwargs["sort_by"] == "DISPLAYNAME":
+                reverse = reverse if reverse is not None else False
+                ins = sorted(ins, key=lambda i: i.display_name, reverse=reverse)
+
+        return oci.response.Response(200, None, ins, None)
+
+    @oracle_arg_check
+    def launch_instance(
+        self, launch_instance_details: oci.core.models.LaunchInstanceDetails, **kwargs
+    ):
+        instance = oci.core.models.Instance(
+            availability_domain=launch_instance_details.availability_domain,
+            display_name=launch_instance_details.display_name,
+            freeform_tags=launch_instance_details.freeform_tags,
+            lifecycle_state="RUNNING",
+            time_created=datetime.datetime.now(),
+        )
+        self._state.instances[launch_instance_details.compartment_id].append(instance)
+
+        fake_network = ipaddress.IPv4Network("10.0.0.0/24")
+        ip = ipaddress.IPv4Address(
+            random.randrange(
+                int(fake_network.network_address + 1),
+                int(fake_network.broadcast_address - 1),
+            ),
+        )
+        vnic = oci.core.models.Vnic(
+            compartment_id=launch_instance_details.compartment_id,
+            id="ocid1.vnic.oc1.."
+            + "".join(random.choices(string.ascii_lowercase, k=10)),
+            private_ip=str(ip),
+        )
+
+        self._state.vnics.append(vnic)
+
+        vnic_attachment = oci.core.models.VnicAttachment(
+            compartment_id=launch_instance_details.compartment_id,
+            instance_id=instance.id,
+            vnic_id=vnic.id,
+        )
+
+        self._state.vnic_attachments.append(vnic_attachment)
+
+    @oracle_arg_check
+    def list_vnic_attachments(self, compartment_id: str, **kwargs):
+        attachments = [
+            a
+            for a in self._state.vnic_attachments
+            if a.compartment_id == compartment_id
+            and a.instance_id == kwargs["instance_id"]
+        ]
+        return oci.response.Response(200, None, attachments, None)
+
+
+class OracleVirtualNetworkClient:
+    """
+    A mocked version of oci.core.VirtualNetworkClient
+    """
+
+    def __init__(self, config: dict, state: OracleState, **kwargs):
+        self._state = state
+        self.client = oci.core.virtual_network_client.VirtualNetworkClient
+
+    @oracle_arg_check
+    def get_vnic(self, vnic_id: str, **kwargs):
+        vnics = [v for v in self._state.vnics if v.id == vnic_id]
+        return oci.response.Response(200, None, vnics[0], None)
+
+
+@contextlib.contextmanager
+def mock_oracle():
+    state = OracleState()
+    with unittest.mock.patch(
+        "oci.core.ComputeClient",
+        new=functools.partial(OracleComputeClient, state=state),
+    ), unittest.mock.patch(
+        "oci.core.VirtualNetworkClient",
+        new=functools.partial(OracleVirtualNetworkClient, state=state),
+    ):
+        yield
