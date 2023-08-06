a334677d8359b5eefb73a9024439443111e0a484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:09 +0000

    Add package configure-with-sudo/0.1.0.dev5/configure_with_sudo-0.1.0.dev5-py3-none-any.whl

---
+++ packages/configure-with-sudo/configure_with_sudo-0.1.0.dev5-py3-none-any.whl/configure_with_sudo/__init__.py
@@ -0,0 +1,14 @@
+from .__about__ import (
+    __version__,
+    __title__,
+    __summary__
+)
+
+from .configure_with_sudo import GenericConfigure
+
+__all__ = [
+    "__version__",
+    "__title__",
+    "__summary__",
+    "GenericConfigure"
+]
