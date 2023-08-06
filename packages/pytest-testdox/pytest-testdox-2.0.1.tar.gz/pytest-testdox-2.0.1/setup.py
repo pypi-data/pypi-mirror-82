1a42f0a7278ce66cb064d976f3821e3b07493f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:02 +0000

    Add package pytest-testdox/2.0.1/pytest-testdox-2.0.1.tar.gz

---
+++ packages/pytest-testdox/pytest-testdox-2.0.1.tar.gz/pytest-testdox-2.0.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    with open(path) as f:
+        return f.read()
+
+
+setup(
+    name='pytest-testdox',
+    version='2.0.1',
+    description='A testdox format reporter for pytest',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Renan Ivo',
+    author_email='renanivom@gmail.com',
+    url='https://github.com/renanivo/pytest-testdox',
+    keywords='pytest testdox test report bdd',
+    install_requires=[
+        'pytest>=3.7.0',
+    ],
+    packages=['pytest_testdox'],
+    python_requires=">=3.5",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+    entry_points={
+        'pytest11': [
+            'testdox = pytest_testdox.plugin',
+        ],
+    },
+)
