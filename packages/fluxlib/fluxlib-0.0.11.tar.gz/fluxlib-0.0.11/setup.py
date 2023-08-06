c8b71410ae4d6191bd01c2dfbe70cff8dd25f3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:41 +0000

    Add package fluxlib/0.0.11/fluxlib-0.0.11.tar.gz

---
+++ packages/fluxlib/fluxlib-0.0.11.tar.gz/fluxlib-0.0.11/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Songyan Zhu
+# Mail: sz394@exeter.ac.uk
+# Created Time:  2020-05-08
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "fluxlib",
+	version = "0.0.11",
+	keywords = ("eddy covariance postprocessing, gapfilling and partitioning", "flux"),
+	description = "left blank",
+	long_description = "left blank",
+	license = "MIT Licence",
+
+	url = "https://github.com/soonyenju/fluxlib",
+	author = "Songyan Zhu",
+	author_email = "sz394@exeter.ac.uk",
+
+	packages = find_packages(),
+	include_package_data = True,
+	platforms = "any",
+	install_requires=[
+
+	]
+)
