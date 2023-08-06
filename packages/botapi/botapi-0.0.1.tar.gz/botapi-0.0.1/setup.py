913b35f5cdbd8063a5db2fa53095fb9a72ee5753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:00 +0000

    Add package botapi/0.0.1/botapi-0.0.1.tar.gz

---
+++ packages/botapi/botapi-0.0.1.tar.gz/botapi-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='botapi',
+    version='v0.0.1',
+    packages=['botapi'],
+    url='https://github.com/EdiBoba/botapi',
+    license='Apache 2.0',
+    author='Vyacheslav Rineisky',
+    author_email='rineisky@gmail.com',
+    description='Provides python api to sn and messengers'
+)
