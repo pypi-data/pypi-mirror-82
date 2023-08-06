9f2f3a1dfdac04a260c6bdb587c0b11bddfb5095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:54 +0000

    Add package thonny/3.3.0b4/thonny-3.3.0b4.tar.gz

---
+++ packages/thonny/thonny-3.3.0b4.tar.gz/thonny-3.3.0b4/thonny/plugins/esp/__init__.py
@@ -0,0 +1,344 @@
+import os
+import subprocess
+import time
+import tkinter as tk
+from collections import OrderedDict
+from tkinter import messagebox, ttk
+
+from thonny import get_runner, get_workbench, ui_utils
+from thonny.misc_utils import construct_cmd_line, running_on_mac_os
+from thonny.plugins.micropython import (
+    BareMetalMicroPythonConfigPage,
+    BareMetalMicroPythonProxy,
+    add_micropython_backend,
+    list_serial_ports_with_descriptions,
+)
+from thonny.plugins.micropython.serial_connection import SerialConnection
+from thonny.running import get_interpreter_for_subprocess
+from thonny.ui_utils import (
+    CommonDialogEx,
+    SubprocessDialog,
+    ems_to_pixels,
+    show_dialog,
+)
+
+
+class ESPProxy(BareMetalMicroPythonProxy):
+    @classmethod
+    def _is_potential_port(cls, p):
+        # They have UART adapter
+        return (
+            (p.vid, p.pid) in cls.get_known_usb_vids_pids()
+            or (p.vid, None) in cls.get_known_usb_vids_pids()
+            or p.description in cls.get_known_port_descriptions()
+            or cls.should_consider_unknown_devices()
+            and (p.vid, p.pid) not in cls.get_vids_pids_to_avoid()
+            and (
+                ("USB" in p.description and "serial" in p.description.lower())
+                or "UART" in p.description
+                or "DAPLink" in p.description
+                or "STLink" in p.description
+            )
+            and getattr(p, "manufacturer", "") != "MicroPython"  # adapter can't have this?
+            and "python" not in p.description.lower()
+        )
+
+    @classmethod
+    def get_vids_pids_to_avoid(self):
+        # micro:bit
+        return {(0x0D28, 0x0204)}
+
+
+class ESP8266Proxy(ESPProxy):
+    description = "MicroPython on ESP8266"
+    config_page_constructor = "ESP8266"
+
+    @classmethod
+    def get_known_usb_vids_pids(cls):
+        return cls.get_uart_adapter_vids_pids()
+
+    def _get_api_stubs_path(self):
+        return os.path.join(os.path.dirname(__file__), "esp8266_api_stubs")
+
+
+class ESP32Proxy(ESPProxy):
+    @classmethod
+    def get_known_usb_vids_pids(cls):
+        return cls.get_uart_adapter_vids_pids()
+
+    def _get_api_stubs_path(self):
+        return os.path.join(os.path.dirname(__file__), "esp32_api_stubs")
+
+
+class ESPConfigPage(BareMetalMicroPythonConfigPage):
+    def __init__(self, master, chip, firmware_start_address):
+        self._chip = chip
+        self._firmware_start_address = firmware_start_address
+        super().__init__(master)
+
+    def _has_flashing_dialog(self):
+        return True
+
+    def _open_flashing_dialog(self):
+        dlg = ESPFlashingDialog(self.winfo_toplevel(), self._chip, self._firmware_start_address)
+        ui_utils.show_dialog(dlg)
+
+    @property
+    def allow_webrepl(self):
+        return True
+
+
+class ESP8266ConfigPage(ESPConfigPage):
+    def __init__(self, master):
+        super().__init__(master, "esp8266", "0x0")
+
+
+class ESP32ConfigPage(ESPConfigPage):
+    def __init__(self, master):
+        super().__init__(master, "esp32", "0x1000")
+
+
+class ESPFlashingDialog(CommonDialogEx):
+    def __init__(self, master, chip, start_address, initial_port_desc=""):
+        super().__init__(master)
+
+        self.title("Install %s firmware with esptool" % chip.upper())
+
+        self._chip = chip
+        self._start_address = start_address
+        self._esptool_command = self._get_esptool_command()
+        if not self._esptool_command:
+            messagebox.showerror(
+                "Can't find esptool",
+                "esptool not found.\n"
+                + "Install it via 'Tools => Manage plug-ins'\n"
+                + "or using your OP-system package manager.",
+                master=self,
+            )
+            self._close()
+            return
+
+        self.main_frame.columnconfigure(2, weight=1)
+
+        epadx = ems_to_pixels(2)
+        ipadx = ems_to_pixels(0.5)
+        epady = epadx
+        ipady = ipadx
+
+        # Port
+        port_label = ttk.Label(self.main_frame, text="Port")
+        port_label.grid(row=1, column=1, sticky="w", padx=(epadx, 0), pady=(epady, 0))
+
+        self._port_desc_variable = tk.StringVar(value=initial_port_desc)
+        self._port_combo = ttk.Combobox(
+            self.main_frame, exportselection=False, textvariable=self._port_desc_variable, values=[]
+        )
+        self._port_combo.state(["!disabled", "readonly"])
+        self._port_combo.grid(row=1, column=2, sticky="nsew", padx=ipadx, pady=(epady, 0))
+
+        port_reload_button = ttk.Button(self.main_frame, text="Reload", command=self._reload_ports)
+        port_reload_button.grid(row=1, column=3, sticky="ew", padx=(0, epadx), pady=(epady, 0))
+
+        # Firmware
+        firmware_label = ttk.Label(self.main_frame, text="Firmware")
+        firmware_label.grid(row=2, column=1, sticky="w", padx=(epadx, 0), pady=(ipady, 0))
+
+        self._firmware_entry = ttk.Entry(self.main_frame, width=65)
+        self._firmware_entry.grid(row=2, column=2, sticky="nsew", padx=ipadx, pady=(ipady, 0))
+
+        browse_button = ttk.Button(self.main_frame, text="Browse...", command=self._browse)
+        browse_button.grid(row=2, column=3, sticky="we", padx=(0, epadx), pady=(ipady, 0))
+
+        # FLASH_MODE
+        self._flashmode = tk.StringVar(None, "keep")
+        flashmode_group = ttk.Labelframe(self.main_frame, text="Flash mode")
+        flashmode_group.grid(
+            row=4, column=1, columnspan=2, sticky="w", padx=(epadx, 0), pady=(ipady * 2, 0)
+        )
+
+        self._flashmode_keep_radiobutton = ttk.Radiobutton(
+            flashmode_group, text="From image file (keep)", variable=self._flashmode, value="keep"
+        )
+        self._flashmode_keep_radiobutton.grid(row=0, column=0, sticky="w")
+
+        # self._flashmode_variable.value=False
+        self._flashmode_qio_radiobutton = ttk.Radiobutton(
+            flashmode_group, text="Quad I/O (qio)", variable=self._flashmode, value="qio"
+        )
+        self._flashmode_qio_radiobutton.grid(row=0, column=1, sticky="w")
+
+        self._flashmode_dio_radiobutton = ttk.Radiobutton(
+            flashmode_group, text="Dual I/O (dio)", variable=self._flashmode, value="dio"
+        )
+        self._flashmode_dio_radiobutton.grid(row=1, column=0, sticky="w")
+
+        self._flashmode_dout_radiobutton = ttk.Radiobutton(
+            flashmode_group, text="Dual Output (dout)", variable=self._flashmode, value="dout"
+        )
+        self._flashmode_dout_radiobutton.grid(row=1, column=1, sticky="w")
+
+        # Erase
+        self._erase_variable = tk.BooleanVar(value=True)
+        self._erase_checkbutton = ttk.Checkbutton(
+            self.main_frame, text="Erase flash before installing", variable=self._erase_variable
+        )
+        self._erase_checkbutton.grid(
+            row=6, column=1, columnspan=2, sticky="w", padx=(epadx, 0), pady=(ipady, epady)
+        )
+
+        # Buttons
+        install_button = ttk.Button(self.main_frame, text="Install", command=self._install)
+        install_button.grid(row=6, column=2, columnspan=1, sticky="e", padx=ipadx, pady=(0, epady))
+
+        cancel_button = ttk.Button(self.main_frame, text="Close", command=self._close)
+        cancel_button.grid(
+            row=6, column=3, columnspan=1, sticky="e", padx=(0, epadx), pady=(0, epady)
+        )
+
+        self._reload_ports()
+
+    def _get_esptool_command(self):
+        try:
+            import esptool
+
+            return [get_interpreter_for_subprocess(), "-u", "-m", "esptool"]
+        except ImportError:
+            import shutil
+
+            result = shutil.which("esptool")
+            if result:
+                return [result]
+            else:
+                result = shutil.which("esptool.py")
+                if result:
+                    return [result]
+                else:
+                    return None
+
+    def _reload_ports(self):
+        pairs = list_serial_ports_with_descriptions()
+        self._ports_by_desc = OrderedDict(pairs)
+        self._port_combo.configure(values=list(self._ports_by_desc.keys()))
+
+    def _browse(self):
+        path = ui_utils.askopenfilename(
+            filetypes=[("bin-files", ".bin"), ("all files", ".*")], parent=self.winfo_toplevel()
+        )
+        if path:
+            self._firmware_entry.delete(0, "end")
+            self._firmware_entry.insert(0, path)
+
+    def _check_connection(self, port):
+        proxy = get_runner().get_backend_proxy()
+        if isinstance(proxy, BareMetalMicroPythonProxy):
+            # Most likely it is using the same port
+            proxy.disconnect()
+            time.sleep(1.5)
+
+        # Maybe another program is connected
+        # or the user doesn't have sufficient permissions?
+        try:
+            conn = SerialConnection(port, 115200, skip_reader=True)
+            conn.close()
+
+            return True
+        except Exception as e:
+            messagebox.showerror("Can't connect", str(e), master=self)
+            return False
+
+    def _install(self):
+        if not self._port_desc_variable.get():
+            messagebox.showerror("Select port", "Please select port", parent=self)
+            return
+
+        port = self._ports_by_desc[self._port_desc_variable.get()]
+
+        firmware_path = self._firmware_entry.get()
+        if not os.path.exists(firmware_path):
+            messagebox.showerror(
+                "Bad firmware path", "Can't find firmware, please check path", master=self
+            )
+            return
+
+        erase_command = self._esptool_command + [
+            "--chip",
+            self._chip,
+            "--port",
+            port,
+            "erase_flash",
+        ]
+
+        write_command = self._esptool_command + [
+            "--chip",
+            self._chip,
+            "--port",
+            port,
+            "write_flash",
+            "--flash_mode",
+            self._flashmode.get(),
+            self._start_address,
+            firmware_path,
+        ]
+
+        if not self._check_connection(port):
+            return
+
+        # unknown problem with first dialog appearing at 0,0
+        # self.update_idletasks()
+        if self.winfo_screenwidth() >= 1024:
+            min_left = ems_to_pixels(15)
+            min_top = ems_to_pixels(5)
+        else:
+            min_left = 0
+            min_top = 0
+
+        def long_desc(cmd):
+            return "Command:\n%s\n\nOutput:\n" % construct_cmd_line(cmd)
+
+        self.update_idletasks()
+        if self._erase_variable.get():
+            proc = self._create_subprocess(erase_command)
+            dlg = SubprocessDialog(
+                self,
+                proc,
+                "Erasing flash",
+                long_description=long_desc(erase_command),
+                autoclose=True,
+            )
+            show_dialog(dlg, master=self, min_left=min_left, min_top=min_top)
+            if dlg.cancelled or dlg.returncode:
+                return
+
+        proc = self._create_subprocess(write_command)
+        dlg = SubprocessDialog(
+            self,
+            proc,
+            "Installing firmware",
+            long_description=long_desc(write_command),
+            autoclose=False,
+        )
+        show_dialog(dlg, master=self, min_left=min_left, min_top=min_top)
+
+    def _create_subprocess(self, cmd):
+        return subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True
+        )
+
+    def _close(self):
+        self.closed = True
+        self.destroy()
+
+
+def load_plugin():
+    add_micropython_backend(
+        "ESP32",
+        ESP32Proxy,
+        "MicroPython (ESP32)",
+        ESP32ConfigPage,
+        sort_key="35",
+        write_block_size=255,
+        write_block_delay=0.5,
+    )
+    add_micropython_backend(
+        "ESP8266", ESP8266Proxy, "MicroPython (ESP8266)", ESP8266ConfigPage, sort_key="36"
+    )
---
+++ packages/thonny/thonny-3.3.0b4.tar.gz/thonny-3.3.0b4/thonny/plugins/micropython/webrepl_connection.py
@@ -0,0 +1,168 @@
+import sys
+import threading
+import time
+from queue import Queue
+
+from thonny.plugins.micropython.connection import MicroPythonConnection
+
+DEBUG = False
+
+
+class WebReplConnection(MicroPythonConnection):
+    """
+    Problem with block size:
+    https://github.com/micropython/micropython/issues/2497
+    Start with conservative delay.
+    Client may later reduce it for better efficiency
+    """
+
+    def __init__(self, url, password):
+
+        self.num_bytes_received = 0
+        super().__init__()
+
+        try:
+            import websockets  # @UnusedImport
+        except:
+            print(
+                "Can't import `websockets`. You can install it via 'Tools => Manage plug-ins'.",
+                file=sys.stderr,
+            )
+            sys.exit(-1)
+        self._url = url
+        self._password = password
+        self._write_block_size = 255
+        self._write_block_delay = 0.5
+        self._write_responses = Queue()
+
+        # Some tricks are needed to use async library in a sync program.
+        # Using thread-safe queues to communicate with async world in another thread
+        self._write_queue = Queue()
+        self._connection_result = Queue()
+        self._ws_thread = threading.Thread(target=self._wrap_ws_main, daemon=True)
+        self._ws_thread.start()
+
+        # Wait until connection was made
+        res = self._connection_result.get()
+        if res != "OK":
+            raise res
+
+    def _wrap_ws_main(self):
+        import asyncio
+
+        loop = asyncio.new_event_loop()
+        loop.set_debug(DEBUG)
+        loop.run_until_complete(self._ws_main())
+
+    async def _ws_main(self):
+        import asyncio
+
+        try:
+            await self._ws_connect()
+        except Exception as e:
+            self._connection_result.put_nowait(e)
+            return
+
+        self._connection_result.put_nowait("OK")
+        await asyncio.gather(self._ws_keep_reading(), self._ws_keep_writing())
+
+    async def _ws_connect(self):
+        import websockets
+
+        try:
+            self._ws = await websockets.connect(self._url, ping_interval=None)
+        except websockets.exceptions.InvalidMessage:
+            # try once more
+            self._ws = await websockets.connect(self._url, ping_interval=None)
+
+        debug("GOT WS", self._ws)
+
+        # read password prompt and send password
+        read_chars = ""
+        while read_chars != "Password: ":
+            debug("prelude", read_chars)
+            ch = await self._ws.recv()
+            debug("GOT", ch)
+            read_chars += ch
+
+        debug("sending password")
+        await self._ws.send(self._password + "\n")
+        debug("sent password")
+
+    async def _ws_keep_reading(self):
+        import websockets.exceptions
+
+        while True:
+            try:
+                data = (await self._ws.recv()).encode("UTF-8")
+                if len(data) == 0:
+                    self._error = "EOF"
+                    break
+            except websockets.exceptions.ConnectionClosedError:
+                # TODO: try to reconnect in case of Ctrl+D
+                self._error = "EOF"
+                break
+
+            self.num_bytes_received += len(data)
+            self._make_output_available(data, block=False)
+
+    async def _ws_keep_writing(self):
+        import asyncio
+
+        while True:
+            while not self._write_queue.empty():
+                data = self._write_queue.get(block=False)
+                debug(
+                    "To be written:",
+                    len(data),
+                    self._write_block_size,
+                    self._write_block_delay,
+                    repr(data),
+                )
+
+                # chunk without breaking utf-8 chars
+                start_pos = 0
+                while start_pos < len(data):
+                    if start_pos > 0:
+                        await asyncio.sleep(self._write_block_delay)
+
+                    end_pos = start_pos + self._write_block_size
+                    # make sure next block doesn't start with a continuation char
+                    while end_pos < len(data) and data[end_pos] >= 0x80 and data[end_pos] < 0xC0:
+                        end_pos -= 1
+
+                    block = data[start_pos:end_pos]
+                    str_block = block.decode("UTF-8")
+                    await self._ws.send(str_block)
+                    debug("Wrote chars", len(str_block))
+
+                    start_pos = end_pos
+
+                debug("Wrote bytes", len(data))
+                self._write_responses.put(len(data))
+
+            # Allow reading loop to progress
+            await asyncio.sleep(0.01)
+
+    def write(self, data):
+        self._write_queue.put_nowait(data)
+        return self._write_responses.get()
+
+    async def _async_close(self):
+        await self._ws.close()
+
+    def close_and_return_new_connection(self):
+        self.close()
+        return WebReplConnection(self._url, self._password)
+
+    def close(self):
+        """
+        # TODO:
+        import asyncio
+        asyncio.get_event_loop().run_until_complete(self.async_close())
+        """
+
+
+def debug(*args):
+    if DEBUG:
+        print(*args, file=sys.stderr)
