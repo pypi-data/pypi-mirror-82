0c438698de9d88f5f6f352683e2c488b767d5f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:42 +0000

    Add package earn-newlibrary/1.0/earn-newlibrary-1.0.tar.gz

---
+++ packages/earn-newlibrary/earn-newlibrary-1.0.tar.gz/earn-newlibrary-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="earn-newlibrary",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Arunkamon',
+    author_email='arunkamon106@gmail.com',
+)
