c671ed669450e5256352ba1c679a24d9131268f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:04 +0000

    Add package pptx2md/0.7.6/pptx2md-0.7.6.tar.gz

---
+++ packages/pptx2md/pptx2md-0.7.6.tar.gz/pptx2md-0.7.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = 'pptx2md',
+    version = '0.7.6',
+    keywords = ["pip", "pptx2md"],
+    description = "convert pptx to markdown",
+    long_description = "keeps the titles, formats including color, bold and italic, extracts the images",
+    license = "MIT Licence",
+    url = "https://github.com/ssine/pptx2md",
+    author = "Liu Siyao",
+    author_email = "liu.siyao@qq.com",
+    packages = ['pptx2md'],
+    entry_points = {
+        'console_scripts': [
+            'pptx2md = pptx2md.__main__:main'
+        ]
+    },
+    install_requires = [
+        'python-pptx',
+        'rapidfuzz',
+        'pillow'
+    ]
+)
