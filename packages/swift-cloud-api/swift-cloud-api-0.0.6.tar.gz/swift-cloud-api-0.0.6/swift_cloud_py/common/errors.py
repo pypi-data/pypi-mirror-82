920ccf22e1aeab07bf066fdfb7cbf6ebb5931e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package swift-cloud-api/0.0.6/swift-cloud-api-0.0.6.tar.gz

---
+++ packages/swift-cloud-api/swift-cloud-api-0.0.6.tar.gz/swift-cloud-api-0.0.6/swift_cloud_py/common/errors.py
@@ -0,0 +1,34 @@
+class NoInternetConnectionException(Exception):
+    """
+    Exception indicating no internet connection is present
+    """
+    pass
+
+
+class UnauthorizedException(Exception):
+    """
+    Exception to indicate that access is denied to the cloud api.
+    """
+    pass
+
+
+class UnknownAuthenticationException(Exception):
+    """
+    Exception to indicate that something went wrong in the cloud during authentication;
+    possibly an unexpected error was raised in the cloud.
+    """
+    pass
+
+
+class UnknownCloudException(Exception):
+    """
+    Exception to indicate that something went wrong in the cloud; possibly an unexpected error was raised in the cloud.
+    """
+    pass
+
+
+class BadRequestException(Exception):
+    """
+    Exception to indicate that the input to the rest-api was incorrect.
+    """
+    pass
