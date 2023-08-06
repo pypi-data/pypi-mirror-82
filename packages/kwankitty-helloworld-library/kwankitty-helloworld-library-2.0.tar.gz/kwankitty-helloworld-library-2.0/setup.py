badf34a1c1eaadf48a9a9c54696e1a4a21d55718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:47 +0000

    Add package kwankitty-helloworld-library/2.0/kwankitty-helloworld-library-2.0.tar.gz

---
+++ packages/kwankitty-helloworld-library/kwankitty-helloworld-library-2.0.tar.gz/kwankitty-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kwankitty-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Kwankitty',
+    author_email='kwankitty_55555@hotmail.com',
+)
