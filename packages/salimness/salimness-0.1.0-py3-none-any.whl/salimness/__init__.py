c1ae7219057b08738c9f555cd91658731752e028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:05 +0000

    Add package salimness/0.1.0/salimness-0.1.0-py3-none-any.whl

---
+++ packages/salimness/salimness-0.1.0-py3-none-any.whl/salimness/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""A karma-based measurement to gauge salimness in your social footprint"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
