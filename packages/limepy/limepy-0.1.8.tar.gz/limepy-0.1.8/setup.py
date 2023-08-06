f24bf82de6454d887080124c0ab2beb8d3a61752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:10 +0000

    Add package limepy/0.1.8/limepy-0.1.8.tar.gz

---
+++ packages/limepy/limepy-0.1.8.tar.gz/limepy-0.1.8/setup.py
@@ -0,0 +1,19 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(name='limepy',
+    version='0.1.8',
+    description='Download, summarise and process LimeSurvey data',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='dirkmjk',
+    author_email='info@dirkmjk.nl',
+    url='https://github.com/DIRKMJK/limepy',
+    license="MIT",
+    packages=['limepy'],
+    install_requires=['pandas', 'numpy', 'requests', 'xmltodict'],
+    zip_safe=False)
