297461af10a6b336c7dc5e1b356e07ab20aff76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:08 +0000

    Add package Baygon/0.1.1/baygon-0.1.1.tar.gz

---
+++ packages/Baygon/baygon-0.1.1.tar.gz/baygon-0.1.1/baygon/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.1'
+
+__author__ = 'Yves Chevallier'
+__description__ = 'Functional tests for teaching activity'
+__email__ = 'yves.chevallier@heig-vd.ch'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 HEIG-VD'
+
+from .validation import Test, Tests
+from .test import TestCase, TestSuite
+from .executable import Executable
