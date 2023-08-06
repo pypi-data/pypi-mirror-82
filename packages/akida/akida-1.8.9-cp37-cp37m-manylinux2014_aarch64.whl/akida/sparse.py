69a3920093d6f4932e1c0b948f0a5273d01a6341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:19 +0000

    Add package akida/1.8.9/akida-1.8.9-cp37-cp37m-manylinux2014_aarch64.whl

---
+++ packages/akida/akida-1.8.9-cp37-cp37m-manylinux2014_aarch64.whl/akida/sparse.py
@@ -0,0 +1,9 @@
+from akida.core import Sparse
+
+
+def sparse_repr(self):
+    data = "<shape=" + str(self.shape) + ", nnz=" + str(self.nnz)
+    data += ", sparsity=" + format(self.sparsity, "0.2f") + ",\n"
+    data += "coords=" + str(self.coords) + ",\n"
+    data += "data=" + str(self.data) + ">"
+    return data
