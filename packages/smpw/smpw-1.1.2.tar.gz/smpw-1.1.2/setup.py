030e7681e55e58763e0bf172de41b44c85998234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:27 +0000

    Add package smpw/1.1.2/smpw-1.1.2.tar.gz

---
+++ packages/smpw/smpw-1.1.2.tar.gz/smpw-1.1.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['smpw']
+setup_kwargs = {
+    'name': 'smpw',
+    'version': '1.1.2',
+    'description': 'hi my name Azarov Nick and i programist modileJohn Smith',
+    'long_description': None,
+    'author': 'John Smith',
+    'author_email': 'azarovnick@bk.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
