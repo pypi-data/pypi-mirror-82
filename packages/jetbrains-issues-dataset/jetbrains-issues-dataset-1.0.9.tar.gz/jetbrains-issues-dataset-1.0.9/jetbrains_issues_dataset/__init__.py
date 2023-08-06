53f0b79fab344f245127d2bcb9462364b2eae306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:32 +0000

    Add package jetbrains-issues-dataset/1.0.9/jetbrains-issues-dataset-1.0.9.tar.gz

---
+++ packages/jetbrains-issues-dataset/jetbrains-issues-dataset-1.0.9.tar.gz/jetbrains-issues-dataset-1.0.9/jetbrains_issues_dataset/__init__.py
@@ -0,0 +1,1 @@
+from jetbrains_issues_dataset.idea.idea_data_set import idea_2019_03_20_to_idea_2020_03_20
---
+++ packages/jetbrains-issues-dataset/jetbrains-issues-dataset-1.0.9.tar.gz/jetbrains-issues-dataset-1.0.9/test/test_issue_created_snapshot_manager.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from jetbrains_issues_dataset.idea.idea_activity_manager import IdeaActivityManager
+from jetbrains_issues_dataset.idea.issue_created_snapshot_strategy import IssueCreatedSnapshotStrategy
+
+
+class TestIssueCreatedSnapshotStrategy(TestCase):
+    def test_fields_missed_in_activities(self):
+        snapshot_strategy = IssueCreatedSnapshotStrategy()
+        activity_manager = IdeaActivityManager(snapshot_strategy)
+        activity_manager.load_issues_from_activities_file('data/missed_activities.json')
+
+        issue = list(snapshot_strategy.issues.values())[0]
+        self.assertEqual(issue['fixed_by'], 'assignee_developer')
+
+    def test_all_main_field_presence(self):
+        snapshot_strategy = IssueCreatedSnapshotStrategy()
+        activity_manager = IdeaActivityManager(snapshot_strategy)
+        activity_manager.load_issues_from_activities_file('data/missed_activities.json')
+
+        issue = list(snapshot_strategy.issues.values())[0]
+        self.assertTrue('summary' in issue)
+        self.assertTrue('description' in issue)
+        self.assertTrue('state' in issue)
+        self.assertTrue('reporter' in issue)
+
+    def test_read_all(self):
+        snapshot_strategy = IssueCreatedSnapshotStrategy()
+        activity_manager = IdeaActivityManager(snapshot_strategy)
+        activity_manager.load_issues_from_activities_file('data/snapshot.json')
+
+        self.assertEqual(145, len(snapshot_strategy.issues.values()))
+
