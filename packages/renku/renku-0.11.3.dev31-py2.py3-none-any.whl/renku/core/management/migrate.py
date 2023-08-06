a87c6865389373d48e84c8115ad15a6220fc4425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package renku/0.11.3.dev31/renku-0.11.3.dev31-py2.py3-none-any.whl

---
+++ packages/renku/renku-0.11.3.dev31-py2.py3-none-any.whl/renku/core/management/migrate.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017-2020 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Renku migrations management.
+
+Migrations files are put in renku/core/management/migrations directory. Name
+of these files has m_1234__name.py format where 1234 is the migration version
+and name can be any alphanumeric and underscore combination. Migration files
+are sorted based on their lowercase name. Each migration file must define a
+public "migrate" function that accepts a client as its argument.
+
+When executing a migration, the migration file is imported as a module and the
+"migrate" function is executed. Migration version is checked against the Renku
+project version (in .renku/metadata.yml) and any migration which has a higher
+version is applied to the project.
+"""
+import importlib
+import re
+from pathlib import Path
+
+import pkg_resources
+
+from renku.core.errors import MigrationRequired, ProjectNotSupported
+from renku.core.utils.migrate import read_project_version
+
+SUPPORTED_PROJECT_VERSION = 8
+
+
+def check_for_migration(client):
+    """Checks if migration is required."""
+    if is_migration_required(client):
+        raise MigrationRequired
+    elif is_project_unsupported(client):
+        raise ProjectNotSupported
+
+
+def is_migration_required(client):
+    """Check if project requires migration."""
+    return _is_renku_project(client) and _get_project_version(client) < SUPPORTED_PROJECT_VERSION
+
+
+def is_project_unsupported(client):
+    """Check if this version of Renku cannot work with the project."""
+    return _is_renku_project(client) and _get_project_version(client) > SUPPORTED_PROJECT_VERSION
+
+
+def migrate(client, progress_callback=None):
+    """Apply all migration files to the project."""
+    if not _is_renku_project(client):
+        return
+
+    project_version = _get_project_version(client)
+    n_migrations_executed = 0
+
+    for version, path in get_migrations():
+        if version > project_version:
+            module = importlib.import_module(path)
+            if progress_callback:
+                module_name = module.__name__.split(".")[-1]
+                progress_callback(f"Applying migration {module_name}...")
+            module.migrate(client)
+            n_migrations_executed += 1
+    if n_migrations_executed > 0:
+        client._project = None  # NOTE: force reloading of project metadata
+        client.project.version = str(version)
+        client.project.to_yaml()
+
+        if progress_callback:
+            progress_callback(f"Successfully applied {n_migrations_executed} migrations.")
+
+    return n_migrations_executed != 0
+
+
+def _get_project_version(client):
+    try:
+        return int(read_project_version(client))
+    except ValueError:
+        return 1
+
+
+def _is_renku_project(client):
+    try:
+        return client.project is not None
+    except ValueError:  # Error in loading due to an older schema
+        return client.renku_metadata_path.exists()
+
+
+def get_migrations():
+    """Return a sorted list of versions and migration modules."""
+    migrations = []
+    for file_ in pkg_resources.resource_listdir("renku.core.management", "migrations"):
+        match = re.search(r"m_([0-9]{4})__[a-zA-Z0-9_-]*.py", file_)
+
+        if match is None:  # migration files match m_0000__[name].py format
+            continue
+
+        version = int(match.groups()[0])
+        path = "renku.core.management.migrations.{}".format(Path(file_).stem)
+        migrations.append((version, path))
+
+    migrations = sorted(migrations, key=lambda v: v[1].lower())
+    return migrations
---
+++ packages/renku/renku-0.11.3.dev31-py2.py3-none-any.whl/renku/service/views/templates.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Renku service templates view."""
+
+import shutil
+
+from flask import Blueprint, request
+from flask_apispec import marshal_with, use_kwargs
+from marshmallow import EXCLUDE
+
+from renku.core.commands.init import create_from_template_local, read_template_manifest
+from renku.core.utils.contexts import chdir
+from renku.service.config import INVALID_PARAMS_ERROR_CODE, SERVICE_PREFIX
+from renku.service.serializers.templates import (
+    ManifestTemplatesRequest,
+    ManifestTemplatesResponseRPC,
+    ProjectTemplateRequest,
+    ProjectTemplateResponseRPC,
+)
+from renku.service.utils import make_new_project_path, new_repo_push
+from renku.service.views import error_response, result_response
+from renku.service.views.cache import _project_clone
+from renku.service.views.decorators import (
+    accepts_json,
+    handle_base_except,
+    handle_git_except,
+    handle_renku_except,
+    handle_schema_except,
+    handle_validation_except,
+    header_doc,
+    requires_cache,
+    requires_identity,
+)
+
+TEMPLATES_BLUEPRINT_TAG = "templates"
+templates_blueprint = Blueprint(TEMPLATES_BLUEPRINT_TAG, __name__, url_prefix=SERVICE_PREFIX)
+
+
+@use_kwargs(ManifestTemplatesRequest, location="query")
+@marshal_with(ManifestTemplatesResponseRPC)
+@header_doc("Clone a remote template repository and read the templates.", tags=(TEMPLATES_BLUEPRINT_TAG,))
+@templates_blueprint.route(
+    "/templates.read_manifest", methods=["GET"], provide_automatic_options=False,
+)
+@handle_base_except
+@handle_git_except
+@handle_renku_except
+@handle_validation_except
+@handle_schema_except
+@accepts_json
+@requires_cache
+@requires_identity
+def read_manifest_from_template(user, cache):
+    """Read templates from the manifest file of a template repository."""
+    project_data = ManifestTemplatesRequest().load({**user, **request.args,}, unknown=EXCLUDE)
+    project = _project_clone(user, project_data)
+    manifest = read_template_manifest(project.abs_path)
+
+    return result_response(ManifestTemplatesResponseRPC(), {"templates": manifest})
+
+
+@use_kwargs(ProjectTemplateRequest)
+@marshal_with(ProjectTemplateRequest)
+@header_doc(
+    "Create a new project starting from a target template available in a " "remote repositpry.",
+    tags=(TEMPLATES_BLUEPRINT_TAG,),
+)
+@templates_blueprint.route(
+    "/templates.create_project", methods=["POST"], provide_automatic_options=False,
+)
+@handle_base_except
+@handle_git_except
+@handle_renku_except
+@handle_validation_except
+@handle_schema_except
+@accepts_json
+@requires_cache
+@requires_identity
+def create_project_from_template(user, cache):
+    """Create a new project starting form target template."""
+    ctx = ProjectTemplateRequest().load({**user, **request.json,}, unknown=EXCLUDE)
+
+    # Clone project and find target template
+    template_project = _project_clone(user, ctx)
+    templates = read_template_manifest(template_project.abs_path)
+    template = next((template for template in templates if template["folder"] == ctx["identifier"]), None)
+    if template is None:
+        return error_response(INVALID_PARAMS_ERROR_CODE, "invalid identifier for target repository")
+
+    # Verify missing parameters
+    template_parameters = template.get("variables", {})
+    provided_parameters = {p["key"]: p["value"] for p in ctx["parameters"]}
+    missing_keys = list(template_parameters.keys() - provided_parameters.keys())
+    if len(missing_keys) > 0:
+        return error_response(INVALID_PARAMS_ERROR_CODE, f"missing parameter: {missing_keys[0]}")
+
+    # Create new path
+    new_project_path = make_new_project_path(user, ctx)
+    if new_project_path.exists():
+        shutil.rmtree(str(new_project_path))
+    new_project_path.mkdir(parents=True, exist_ok=True)
+
+    default_metadata = {
+        "__template_source__": ctx["git_url"],
+        "__template_ref__": ctx["ref"],
+        "__template_id__": ctx["identifier"],
+        "__namespace__": ctx["project_namespace"],
+        "__repository__": ctx["project_repository"],
+        "__sanitized_project_name__": ctx["project_name_stripped"],
+        "__project_slug__": ctx["project_slug"],
+    }
+
+    # prepare data and init new project
+    source_path = template_project.abs_path / ctx["identifier"]
+    git_user = {"email": user["email"], "name": user["fullname"]}
+    with chdir(new_project_path):
+        create_from_template_local(
+            source_path,
+            ctx["project_name"],
+            provided_parameters,
+            default_metadata,
+            git_user,
+            ctx["url"],
+            ctx["ref"],
+            "service",
+        )
+    new_repo_push(new_project_path, ctx["new_project_url_with_auth"])
+
+    resp = {
+        "url": ctx["new_project_url"],
+        "namespace": ctx["project_namespace"],
+        "name": ctx["project_name_stripped"],
+    }
+    return result_response(ProjectTemplateResponseRPC(), resp)
