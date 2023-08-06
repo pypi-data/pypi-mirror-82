c813c257b0789db1c8d1d459840e864f559d05a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:51 +0000

    Add package yoottana-helloworld-library/3.0/yoottana-helloworld-library-3.0.tar.gz

---
+++ packages/yoottana-helloworld-library/yoottana-helloworld-library-3.0.tar.gz/yoottana-helloworld-library-3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yoottana-helloworld-library",
+    version='3.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Somkiat',
+    author_email='somkiat.p@gmail.com',
+)
