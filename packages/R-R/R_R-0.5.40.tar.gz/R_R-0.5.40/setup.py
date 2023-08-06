6508fc59fdc990ac25624804130c5b522dc682d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:54 +0000

    Add package R-R/0.5.40/R_R-0.5.40.tar.gz

---
+++ packages/R-R/R_R-0.5.40.tar.gz/R_R-0.5.40/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='R_R',
+      version='0.5.4',
+      description='It is programming language in russian! Main project - robin_pl!',
+      packages=['R_R'],
+      author_email='foton-pc@inbox.ru',
+      zip_safe=False,
+      home_page='https://ppbe.ru',
+      author='FotonPC',
+      platform=['win32','linux'])
