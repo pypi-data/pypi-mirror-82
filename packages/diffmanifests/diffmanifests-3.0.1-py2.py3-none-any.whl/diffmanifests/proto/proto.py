3a8a1bcf08595bad4c52cd3b35a3a982c11d008b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package diffmanifests/3.0.1/diffmanifests-3.0.1-py2.py3-none-any.whl

---
+++ packages/diffmanifests/diffmanifests-3.0.1-py2.py3-none-any.whl/diffmanifests/proto/proto.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+
+"""Prototype
+{
+    "add repo": {
+        "REPO": [
+            {},
+            {
+                "branch": BRANCH,
+                "commit": COMMIT,
+                "diff": "add commit"
+            }
+        ]
+    },
+    "remove repo": {
+        "REPO": [
+            {
+                "branch": BRANCH,
+                "commit": COMMIT,
+                "diff": "remove commit"
+            },
+            {}
+        ]
+    },
+    "update repo": {
+        "REPO": [
+            {
+                "branch": BRANCH,
+                "commit": COMMIT,
+                "diff": "add commit"
+            },
+            {
+                "branch": BRANCH,
+                "commit": COMMIT,
+                "diff": "remove commit"
+            }
+        ]
+    }
+}
+"""
+
+
+class Commit:
+    AUTHOR = 'author'
+    BRANCH = 'branch'
+    COMMIT = 'commit'
+    DATE = 'date'
+    DIFF = 'diff'
+    MESSAGE = 'message'
+    REPO = 'repo'
+    URL = 'url'
+
+
+class Label:
+    ADD_COMMIT = 'add commit'
+    ADD_REPO = 'add repo'
+    REMOVE_COMMIT = 'remove commit'
+    REMOVE_REPO = 'remove repo'
+    UPDATE_REPO = 'update repo'
+
+
+class Repo:
+    BRANCH = 'branch'
+    COMMIT = 'commit'
+    DIFF = 'diff'
