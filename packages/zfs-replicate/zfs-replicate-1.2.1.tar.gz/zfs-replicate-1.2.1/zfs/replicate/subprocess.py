8cbdbf19c6a505b766fcb7c7db6d5d9d08143f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:38 +0000

    Add package zfs-replicate/1.2.1/zfs-replicate-1.2.1.tar.gz

---
+++ packages/zfs-replicate/zfs-replicate-1.2.1.tar.gz/zfs-replicate-1.2.1/zfs/replicate/subprocess.py
@@ -0,0 +1,19 @@
+"""subprocess wrapper."""
+
+import shlex
+import subprocess
+from typing import IO, Union
+
+STDOUT = subprocess.STDOUT
+
+
+def open(
+    command: str,
+    shell: bool = False,
+    stdin: Union[IO[str], int] = subprocess.PIPE,
+    stdout: Union[IO[str], int] = subprocess.PIPE,
+    stderr: Union[IO[str], int] = subprocess.PIPE,
+):  # type: (...) -> subprocess.Popen[bytes]
+    """Wrapper around subprocess.Popen."""
+
+    return subprocess.Popen(shlex.split(command), stdin=stdin, stdout=stdout, stderr=stderr, shell=shell)
---
+++ packages/zfs-replicate/zfs-replicate-1.2.1.tar.gz/zfs-replicate-1.2.1/zfs_test/replicate_test/optional_test.py
@@ -0,0 +1,24 @@
+"""Tests for zfs.replicate.optional."""
+
+from hypothesis import given
+from hypothesis.strategies import integers
+from zfs.replicate import optional
+
+
+def test_value_none() -> None:
+    """optional.value(None) → RuntimeError"""
+
+    # Use try except due to lack of typing on pytest module.
+    try:
+        optional.value(None)
+    except RuntimeError:
+        pass
+    except:  # noqa: E722
+        assert False, "Expected RuntimeError"
+
+
+@given(integers())
+def test_value_not_none(value: int) -> None:
+    """optional.value(value) == value"""
+
+    assert optional.value(value) == value
