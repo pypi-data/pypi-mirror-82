ea0e847b27150f450f4d236283fb875d2c7d4f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:20 +0000

    Add package getadvicetest/0.0.10/getadvicetest-0.0.10.tar.gz

---
+++ packages/getadvicetest/getadvicetest-0.0.10.tar.gz/getadvicetest-0.0.10/getadvice/__init__.py
@@ -0,0 +1,7 @@
+from .functions import MyClass
+from .functions import Mykonos
+import requests 
+
+__version__ = "0.0.10"
+
+__url__ = "https://api.adviceslip.com/advice"
---
+++ packages/getadvicetest/getadvicetest-0.0.10.tar.gz/getadvicetest-0.0.10/getadvice/functions.py
@@ -0,0 +1,22 @@
+import requests
+
+class MyClass:
+
+    def __init__(self):
+        self.url = "https://api.adviceslip.com/advice"
+
+    def getURL(self):
+        return self.url
+
+class Mykonos:
+
+    def __init__(self):
+        self.name = "RichismakingpartyinMikonos"
+
+    def advice(self, name):
+        json_result = requests.get(self.url).json()
+        advice_result = json_result["slip"]["advice"]
+        return f"{name}, {advice_result}"
+
+    def getName(self):
+        return self.name
