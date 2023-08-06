b4828b38a87bda7c2b35e8de018704c12bfbdb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:35 +0000

    Add package adcc-backup/1.0.3/adcc_backup-1.0.3-py3-none-any.whl

---
+++ packages/adcc-backup/adcc_backup-1.0.3-py3-none-any.whl/adcc_backup/scripts/adcc_backup_client.py
@@ -0,0 +1,20 @@
+
+from markipy.basic import Channel
+
+from adcc_backup import Configuration
+from adcc_backup import EthEventProducer, EthEventConsumer
+
+
+def adcc_backup_client_main():
+    print('Main-adcc_backup')
+    cfg = Configuration('default')
+    if cfg.loaded:
+        upload_channel = Channel()
+        event_producer = EthEventProducer(eth_interface=cfg.get_eth_client(), channel=upload_channel, console=True)
+        event_producer.start()
+
+        event_consumer = EthEventConsumer(cfg=cfg, channel=upload_channel)
+        event_consumer.start()
+
+        event_producer.join()
+        event_consumer.join()
