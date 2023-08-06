e00921a3a850413509cd307cb8d8c1ecfd872d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:59 +0000

    Add package skill-test/0.1.0/skill-test-0.1.0.tar.gz

---
+++ packages/skill-test/skill-test-0.1.0.tar.gz/skill-test-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+modules = \
+['skill_test']
+install_requires = \
+['dynabuffers==0.23.0',
+ 'gaia-sdk==2.0.1',
+ 'interstate-py==1.1.6',
+ 'pyyaml>=5.3.1,<6.0.0']
+
+setup_kwargs = {
+    'name': 'skill-test',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'fridayy',
+    'author_email': 'benjamin.krenn@leftshift.one',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
