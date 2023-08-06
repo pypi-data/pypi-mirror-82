67d20bc1e4a7417fa02e033aeab40f94c7d69acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:14 +0000

    Add package quesadiya/0.2.1/quesadiya-0.2.1.tar.gz

---
+++ packages/quesadiya/quesadiya-0.2.1.tar.gz/quesadiya-0.2.1/quesadiya/commands/run.py
@@ -0,0 +1,24 @@
+import click
+
+from quesadiya.errors import ProjectNotExistError
+from quesadiya.errors import ProjectRunningError
+
+from quesadiya.db import factory
+from quesadiya import utils
+import quesadiya
+
+import os
+import subprocess
+
+
+def operator(port_number):
+    admin_interface = factory.get_admindb_interface()
+    run_path = os.path.join(quesadiya.get_base_path(), 'django_tool', 'manage.py')
+    subprocess.call(
+        [
+            run_path,
+            "runserver",
+            "localhost:{}".format(port_number),
+            "--insecure"
+        ], shell=True
+    )
