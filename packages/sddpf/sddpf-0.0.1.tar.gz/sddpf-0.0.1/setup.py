fc91f0285b93394687b089ea6d043638968596ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:53 +0000

    Add package sddpf/0.0.1/sddpf-0.0.1.tar.gz

---
+++ packages/sddpf/sddpf-0.0.1.tar.gz/sddpf-0.0.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sddpf",
+    version='0.0.1',
+    author='ansheng',
+    author_email='ianshengme@gmail.com',
+    url='https://github.com/anshengme/sentry-dingding',
+    description='A Sentry extension which send errors stats to DingDing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry dingding',
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
+    entry_points={
+        'sentry.plugins': [
+            'sddpf = sddpf.plugin:DingDingPlugin'
+        ]
+    },
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+
+    ]
+)
