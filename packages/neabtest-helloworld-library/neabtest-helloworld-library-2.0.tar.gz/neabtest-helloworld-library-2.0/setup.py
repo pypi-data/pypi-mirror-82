ddf0cf03393d374a4ea78b35a02f7f963653b563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:51 +0000

    Add package neabtest-helloworld-library/2.0/neabtest-helloworld-library-2.0.tar.gz

---
+++ packages/neabtest-helloworld-library/neabtest-helloworld-library-2.0.tar.gz/neabtest-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="neabtest-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/pornchanok/demo-helloworld-library',
+    author='Somkiat',
+    author_email='somkiat.p@gmail.com',
+)
