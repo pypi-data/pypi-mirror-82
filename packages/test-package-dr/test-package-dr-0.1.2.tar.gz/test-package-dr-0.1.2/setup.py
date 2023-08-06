2dd845387eb73f0121267abae5a8b515bc4be7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:01 +0000

    Add package test-package-dr/0.1.2/test-package-dr-0.1.2.tar.gz

---
+++ packages/test-package-dr/test-package-dr-0.1.2.tar.gz/test-package-dr-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_package_dr', 'test_package_dr.print']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['my-script = test_package_dr.load:print_method']}
+
+setup_kwargs = {
+    'name': 'test-package-dr',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'drazum',
+    'author_email': 'domagoj.razum1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
