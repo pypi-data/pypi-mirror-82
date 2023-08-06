04b9eb6f5a0971af9a4bc1bbbad036ca28f43ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package tdxapi/0.4.0/tdxapi-0.4.0-py3-none-any.whl

---
+++ packages/tdxapi/tdxapi-0.4.0-py3-none-any.whl/tdxapi/managers/impact.py
@@ -0,0 +1,23 @@
+from typing import List
+
+import attr
+
+from tdxapi.managers.bases import TdxManager, tdx_method
+from tdxapi.managers.mixins import TdxAppMixin
+from tdxapi.models.impact import Impact
+
+
+@attr.s
+class ImpactManager(TdxManager, TdxAppMixin):
+    __tdx_section__ = "Impacts"
+
+    @tdx_method("GET", "/api/{appId}/tickets/impacts")
+    def all(self) -> List[Impact]:
+        """Gets all active ticket impacts."""
+        return self.dispatcher.send(
+            self.all.method,
+            self.all.url.format(appId=self.app_id),
+            rclass=Impact,
+            rlist=True,
+            rpartial=False,
+        )
