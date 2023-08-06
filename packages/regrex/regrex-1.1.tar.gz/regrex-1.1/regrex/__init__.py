6a1241fd15da3237f58ebed923b7dcf09c26134f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:24 +0000

    Add package regrex/1.1/regrex-1.1.tar.gz

---
+++ packages/regrex/regrex-1.1.tar.gz/regrex-1.1/regrex/__init__.py
@@ -0,0 +1,9 @@
+from regrex.linear_model import Linear
+from regrex.polynomial_model import Polynomial
+from regrex.preprocess import preprocess
+from regrex.transform import Transformer
+from regrex.dataset import LoadData, SaveData, TestTrainSplit
+from regrex.statistics import MeanSquaredError, abstract, arraylog, square
+from regrex.validation import Difference, ScoreValidation
+from regrex.log import log, error, nnewline
+from regrex.SingleLineFunction import SLF
