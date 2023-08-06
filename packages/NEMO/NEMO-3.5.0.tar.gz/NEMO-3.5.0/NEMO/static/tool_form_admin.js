59bf4fd87a07274bfda16983e4cb11c8a1bd6606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:23 +0000

    Add package NEMO/3.5.0/NEMO-3.5.0.tar.gz

---
+++ packages/NEMO/NEMO-3.5.0.tar.gz/NEMO-3.5.0/NEMO/static/tool_form_admin.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", function() {
+    (function ($) {
+        var selector = $('#id_parent_tool');
+
+        function hide_fields(){
+            if(selector.val()) {
+                /* hide everything but name and parent_tool */
+                $(".form-row:not(.field-name,.field-parent_tool)").hide();
+                $(".form-row:not(.field-name,.field-parent_tool)").parent().hide();
+                $(".form-row.field-name,.form-row.field-parent_tool").parent().show();
+            } else {
+                $(".form-row:not(.field-name,.field-parent_tool)").show();
+                $(".form-row:not(.field-name,.field-parent_tool)").parent().show();
+            }
+        }
+        selector.change(hide_fields);
+        hide_fields()
+
+        /* set a separate form for testing post_usage data */
+        $('#tool_form').after('<form id="post_usage_preview_form"></form>')
+        $('.post_usage_preview input').each(function(index, element){
+            $(element).attr('form', 'post_usage_preview_form')
+        })
+        function update_validation_button() {
+            var valid_message = $("#form_validity")
+            if (valid_message) {
+                if(document.querySelector('#post_usage_preview_form').checkValidity()) {
+                    valid_message.removeClass("invalid");
+                    valid_message.text("The form is valid!");
+                }
+                else {
+                    valid_message.addClass("invalid");
+                    valid_message.text("The form is invalid");
+                }
+            }
+        }
+        update_validation_button();
+        $("input[required][form='post_usage_preview_form']").on('change keyup', update_validation_button);
+    })(django.jQuery);
+});
---
+++ packages/NEMO/NEMO-3.5.0.tar.gz/NEMO-3.5.0/NEMO/widgets/dynamic_form.py
@@ -0,0 +1,87 @@
+from json import dumps, loads
+from logging import getLogger
+
+from django.utils.safestring import mark_safe
+
+from NEMO.models import Consumable
+from NEMO.views.consumables import make_withdrawal
+
+
+dynamic_form_logger = getLogger(__name__)
+
+
+class DynamicForm:
+	def __init__(self, questions):
+		self.questions = loads(questions) if questions else None
+
+	def render(self):
+		if not self.questions:
+			return ''
+
+		result = ''
+		for question in self.questions:
+			if question['type'] == "radio":
+				result += f'<div class="form-group">{question["title"]}'
+				for choice in question['choices']:
+					result += '<div class="radio">'
+					required = 'required' if 'required' in question and question['required'] is True else ''
+					is_default_choice = 'checked' if 'default_choice' in question and question['default_choice'] == choice else ''
+					result += f'<label><input type="radio" name="{question["name"]}" value="{choice}" {required} {is_default_choice}>{choice}</label>'
+					result += '</div>'
+				result += '</div>'
+			elif question['type'] == "textbox" or question['type'] == "number":
+				result += '<div class="form-group">'
+				result += f'<label for="{question["name"]}">{question["title"]}</label>'
+				input_group_required = True if 'prefix' in question or 'suffix' in question else False
+				if input_group_required:
+					result += f'<div class="input-group" style="max-width:{question["max-width"]}px">'
+				if 'prefix' in question:
+					result += f'<span class="input-group-addon">{question["prefix"]}</span>'
+				required = 'required' if 'required' in question and question['required'] is True else ''
+				pattern = f'pattern="{question["pattern"]}"' if 'pattern' in question else ''
+				placeholder = f'placeholder="{question["placeholder"]}"' if 'placeholder' in question else ''
+				if question['type'] == "textbox":
+					result += f'<input type="text" class="form-control" name="{question["name"]}" id="{question["name"]}" {placeholder} {pattern} {required} style="max-width:{question["max-width"]}px" spellcheck="false" autocapitalize="off" autocomplete="off" autocorrect="off">'
+				elif question['type'] == "number":
+					minimum = f'min="{question["min"]}"' if 'min' in question else ''
+					maximum = f'max="{question["max"]}"' if 'max' in question else ''
+					result += f'<input type="number" class="form-control" name="{question["name"]}" id="{question["name"]}" {placeholder} {pattern} {minimum} {maximum} {required} style="max-width:{question["max-width"]}px" spellcheck="false" autocapitalize="off" autocomplete="off" autocorrect="off">'
+				if 'suffix' in question:
+					result += f'<span class="input-group-addon">{question["suffix"]}</span>'
+				if input_group_required:
+					result += '</div>'
+				result += '</div>'
+
+		return mark_safe(result)
+
+	def extract(self, request):
+		if not self.questions:
+			return ''
+
+		results = {}
+		for question in self.questions:
+			# Only record the answer when the question was answered. Discard questions that were left blank
+			if request.POST.get(question['name']):
+				results[question['name']] = request.POST[question['name']]
+		return dumps(results, indent='\t', sort_keys=True) if len(results) else ''
+
+	def charge_for_consumables(self, customer, merchant, project, run_data):
+		try:
+			run_data = loads(run_data)
+		except Exception as error:
+			dynamic_form_logger.debug(error)
+			return
+		for question in self.questions:
+			if 'consumable' in question:
+				try:
+					consumable = Consumable.objects.get(name=question['consumable'])
+					quantity = 0
+					if question['type'] == 'number':
+						if question['name'] in run_data:
+							quantity = int(run_data[question['name']])
+
+					if quantity > 0:
+						make_withdrawal(consumable=consumable, customer=customer, merchant=merchant, quantity=quantity, project=project)
+				except Exception as e:
+					dynamic_form_logger.warning(f"Could not withdraw consumable: '{question['consumable']}' with quantity: '{run_data[question['name']]}' for customer: '{customer}' by merchant: '{merchant}' for project: '{project}'", e)
+					pass
