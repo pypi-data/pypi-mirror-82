2653f1f6d61e9e31f13707b809465148e960723b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:32 +0000

    Add package composable/0.2.4/composable-0.2.4.tar.gz

---
+++ packages/composable/composable-0.2.4.tar.gz/composable-0.2.4/composable/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.0'
+from composable.pipeable import (
+
+    pipeable, 
+    )
+
+from composable.utility import pipeable_all_in_module
+import toolz.curried.operator as operator
+import toolz.curried as from_toolz
+
+pipeable_all_in_module(operator)
+pipeable_all_in_module(from_toolz)
