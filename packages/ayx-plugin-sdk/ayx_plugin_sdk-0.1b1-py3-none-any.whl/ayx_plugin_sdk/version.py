bcd9603a2105e031299063b00044f55bc646ec27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:39 +0000

    Add package ayx-plugin-sdk/0.1b1/ayx_plugin_sdk-0.1b1-py3-none-any.whl

---
+++ packages/ayx-plugin-sdk/ayx_plugin_sdk-0.1b1-py3-none-any.whl/ayx_plugin_sdk/version.py
@@ -0,0 +1,9 @@
+"""FILE GENERATED FROM SETUP.PY."""
+short_version = "0.1b1"
+version = "0.1b1"
+full_version = "0.1b1"
+git_revision = "757ae72ba4b530842abd4525b8634726376b18ed"
+release = True
+
+if not release:
+    version = full_version
