cef1ccdd59137288bfaeb78ef918c916b2bd14e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:48 +0000

    Add package dbs-uda-distributions/0.1/dbs_uda_distributions-0.1.tar.gz

---
+++ packages/dbs-uda-distributions/dbs_uda_distributions-0.1.tar.gz/dbs_uda_distributions-0.1/README.md
@@ -0,0 +1,1 @@
+# dbs_uda_distributions package
---
+++ packages/dbs-uda-distributions/dbs_uda_distributions-0.1.tar.gz/dbs_uda_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dbs_uda_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dbs_uda_distributions'],
+      zip_safe=False)
