d4131d9beecf5111937908586a083811b4d2e87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:56 +0000

    Add package infosystem/0.2.22/infosystem-0.2.22.tar.gz

---
+++ packages/infosystem/infosystem-0.2.22.tar.gz/infosystem-0.2.22/infosystem/subsystem/capability/manager.py
@@ -0,0 +1,19 @@
+from infosystem.common.subsystem import operation, manager
+from infosystem.common import exception
+
+
+class Delete(operation.Delete):
+
+    def pre(self, session, id, **kwargs):
+        super().pre(session, id=id)
+        policies = self.manager.api.policies.list(capability_id=id)
+        if policies:
+            message = 'You can\'t remove this capability because' + \
+                ' there are policies associated'
+            raise exception.BadRequest(message)
+
+
+class Manager(manager.Manager):
+    def __init__(self, driver) -> None:
+        super().__init__(driver)
+        self.delete = Delete(self)
