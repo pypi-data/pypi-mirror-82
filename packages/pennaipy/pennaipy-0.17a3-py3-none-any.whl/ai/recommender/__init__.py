c980da046c462ec51385d0a908cc02fa31162370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:21 +0000

    Add package pennaipy/0.17a3/pennaipy-0.17a3-py3-none-any.whl

---
+++ packages/pennaipy/pennaipy-0.17a3-py3-none-any.whl/ai/recommender/__init__.py
@@ -0,0 +1,10 @@
+from .average_recommender import AverageRecommender
+from .random_recommender import RandomRecommender
+from .knn_meta_recommender import KNNMetaRecommender
+from .surprise_recommenders import (
+    CoClusteringRecommender,
+    KNNWithMeansRecommender,
+    KNNDatasetRecommender,
+    KNNMLRecommender,
+    SlopeOneRecommender,
+    SVDRecommender)
