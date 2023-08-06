8f66a6908da9846ba171a3289fdc38066b1f4164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:08 +0000

    Add package simplest-logger/1.1/simplest_logger-1.1.tar.gz

---
+++ packages/simplest-logger/simplest_logger-1.1.tar.gz/simplest_logger-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="simplest_logger",
+    version="1.1",
+    packages=["simplest_logger"],
+    url="https://github.com/megahomyak/simplest_logger",
+    download_url=(
+        "https://github.com/megahomyak/simplest_logger/archive/v1.1.tar.gz"
+    ),
+    license="GPL-3.0",
+    author="megahomyak",
+    author_email="g.megahomyak@gmail.com",
+    description="Simplest logger you've ever seen (probably)"
+)
