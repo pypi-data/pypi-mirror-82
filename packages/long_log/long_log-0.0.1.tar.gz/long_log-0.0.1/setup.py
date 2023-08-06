59125a83c92c9ea2f534b2cfdeee11e6da0b6179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:05 +0000

    Add package long_log/0.0.1/long_log-0.0.1.tar.gz

---
+++ packages/long_log/long_log-0.0.1.tar.gz/long_log-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='long_log',
+    version='0.0.1',
+    author='zhenzi',
+    author_email='82131529@qq.com',
+    url='https://gitee.com/zhenzi0322_admin/long_log',
+    long_description='long_image',
+    description='eee',
+    packages=['long_log'],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux'
+    ]
+)
