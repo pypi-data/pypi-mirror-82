cafc72e5d0303528fd9d98dd38d37a3cf0bbc98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:04 +0000

    Add package gsheetsplus/0.3.3/gsheetsplus-0.3.3-py3-none-any.whl

---
+++ packages/gsheetsplus/gsheetsplus-0.3.3-py3-none-any.whl/gsheetsplus/api.py
@@ -0,0 +1,21 @@
+from gsheets import Sheets
+import matplotlib.pyplot as plt
+
+
+class SheetsPlus(Sheets):
+    """
+    Extends Sheets for added functionalities.
+    """
+
+    def plot(self, df, x, y):
+        """Plots graph between x and y axis and saves the image on local disk.
+
+        Args:
+            x (str): X-axis for the graph
+            y (str): Y-axis for the graph
+            df (pandas.DataFrame) : Pandas DataFrame containing sheets data
+        """
+        plt.scatter(df[x], df[y])
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.savefig("graph.jpg")
