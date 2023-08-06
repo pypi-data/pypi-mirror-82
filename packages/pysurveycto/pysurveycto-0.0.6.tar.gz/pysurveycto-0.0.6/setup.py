941816e4991d5259d45066858b8524e6420c1066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:07 +0000

    Add package pysurveycto/0.0.6/pysurveycto-0.0.6.tar.gz

---
+++ packages/pysurveycto/pysurveycto-0.0.6.tar.gz/pysurveycto-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysurveycto",
+    version="0.0.6",
+    author="Eric Dodge, Jeenu Thomas",
+    author_email="it@idinsight.org, Eric.Dodge@idinsight.org, Jeenu.Thomas@idinsight.org",
+    description="Interacting with SurveyCTO using Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IDinsight/surveycto-python/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests>=2.0.0',
+          'urllib3>=1.21.1',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
