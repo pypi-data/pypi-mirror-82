e6a357efda3ac274ec8cdff3ff9924aed75ec11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:25 +0000

    Add package aiodata/2.1.2/aiodata-2.1.2.tar.gz

---
+++ packages/aiodata/aiodata-2.1.2.tar.gz/aiodata-2.1.2/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open('README.rst') as file:
+
+    readme = file.read()
+
+name = 'aiodata'
+
+version = '2.1.2'
+
+author = 'Exahilosys'
+
+url = f'https://github.com/{author}/{name}'
+
+setuptools.setup(
+    name = name,
+    version = version,
+    author = author,
+    url = url,
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    description = 'PostgREST proxy and client.',
+    long_description = readme,
+    package_data = {
+        '': ['*.psql']
+    },
+    install_requires = [
+        'asyncpg',
+        'aiohttp',
+        'yarl',
+        'aiofiles',
+        'pyjwt',
+        'docopt',
+        'vessel'
+    ],
+    entry_points = {
+        'console_scripts': [
+            f'{name}={name}.server:serve'
+        ]
+    }
+)
