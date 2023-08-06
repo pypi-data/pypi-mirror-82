66bac6f401cb7f99da01453034fa2a92592be17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:21 +0000

    Add package wostools/3.0.0/wostools-3.0.0.tar.gz

---
+++ packages/wostools/wostools-3.0.0.tar.gz/wostools-3.0.0/wostools/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Python WoS tools."""
+
+__author__ = """Core of Science"""
+__email__ = "dev@coreofscience.com"
+__version__ = "3.0.0"
+
+from wostools.article import Article
+from wostools.cached import CachedCollection
+from wostools.cached import CachedCollection as Collection
+
+__all__ = ["CachedCollection", "Collection", "Article"]
