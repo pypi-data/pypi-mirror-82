d894a33ac38812af137df739d664338b58607676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package jaxboard/0.0.1/jaxboard-0.0.1.tar.gz

---
+++ packages/jaxboard/jaxboard-0.0.1.tar.gz/jaxboard-0.0.1/README.md
@@ -0,0 +1,3 @@
+# [WIP] jaxboard
+
+This is a standalone, modified version of the [trax jaxboard](https://github.com/google/trax/blob/master/trax/jaxboard.py)
---
+++ packages/jaxboard/jaxboard-0.0.1.tar.gz/jaxboard-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jaxboard",
+    version="0.0.1",
+    description="A tensorboard logger for jax",
+    long_description=long_description,
+    author="Eduardo Pignatelli",
+    author_email="edu.pignatelli@gmail.com",
+    url="https://github.com/epignatelli/jaxboard",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "tensorboard",
+        "tensorflow>=1.15.0",
+        "tensorflow-gpu>=1.15.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires=">=3.7",
+)
