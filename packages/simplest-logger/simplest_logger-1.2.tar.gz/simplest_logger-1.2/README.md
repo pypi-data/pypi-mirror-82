91fe4a589cdd186812e0a53420e1de0218b60a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:55 +0000

    Add package simplest-logger/1.2/simplest_logger-1.2.tar.gz

---
+++ packages/simplest-logger/simplest_logger-1.2.tar.gz/simplest_logger-1.2/README.md
@@ -0,0 +1,47 @@
+## simplest_logger
+
+simplest_logger is a very simple library for logging for Python 3.5+
+
+
+# Usage examples
+
+### Creating a Logger object
+
+    from simplest_logger import Logger
+
+    logger = Logger("my_logging_file.log")
+
+You can also log something only in the console:
+
+    logger = Logger()  # print_to_console is True by default
+
+Or you can specify the encoding of your records:
+
+    logger = Logger("my_logging_file.log", encoding="cp1251")  # Cyrillic encoding
+
+### Logging something
+
+    logger.log("MY LOG TYPE", "My message")  # Writes or/and prints a record
+
+Record will look like `[CURRENT DATE AND TIME] [LOG TYPE] - MESSAGE`
+
+There is also shortcuts to some frequently used log types.
+
+    # Record type is DEBUG
+    logger.debug(f"Variable {a=}")
+
+    # Record type is INFO
+    logger.info(f"Received a message from user {user.id}: {message.text}")
+
+    # Record type is WARNING
+    logger.warning(
+        f"Cached info about user {user.id} not found, it will be downloaded!"
+    )
+
+    # Record type is ERROR
+    logger.error(f"Error while serving the user {user.id}:\n{get_traceback(exc)}")
+
+    # Record type is CRITICAL
+    logger.error(f"Database file {db_file_path} not found!")
+
+You can inherit from the Logger class and add more shortcuts if you want.
---
+++ packages/simplest-logger/simplest_logger-1.2.tar.gz/simplest_logger-1.2/simplest_logger/logger.py
@@ -0,0 +1,99 @@
+import datetime
+import os
+from typing import Optional
+
+
+class Logger:
+
+    def __init__(
+            self, file_path: Optional[str] = None,
+            encoding: str = "utf-8",
+            print_to_console: bool = True) -> None:
+        """
+        Initialises the Logger, also creates the file if file_path is provided.
+
+        Args:
+            file_path:
+                absolute or relative path
+            encoding:
+                name of encoding of the logger file as string (not used, if
+                file_path is not provided)
+            print_to_console:
+                a flag that determines whether to output messages to the console
+                or not
+        """
+        if file_path and not os.path.exists(file_path):
+            open(file_path, "w")
+        self.file_path = file_path
+        self.encoding = encoding
+        self.print_to_console = print_to_console
+
+    def log(self, log_type: str, message: str) -> None:
+        """
+        Appends the message to a file (if file_path is provided) and prints it
+        in the console (if print_to_console is True).
+
+        Args:
+            log_type:
+                type of the record. Does not affect anything except it is stated
+                in the record after the date and time when the record were made
+            message:
+                a string to be written to the file or/and printed to the console
+        """
+        record = f"[{datetime.datetime.now()}] [{log_type}] - {message}"
+        if self.file_path:
+            with open(self.file_path, "a", encoding=self.encoding) as f:
+                f.write(f"{record}\n")
+        if self.print_to_console:
+            print(record)
+
+    def debug(self, message: str) -> None:
+        """
+        A shortcut for the log method of this class. log_type is set to DEBUG.
+
+        Args:
+            message:
+                a string to be written to the file or/and printed to the console
+        """
+        self.log("DEBUG", message)
+
+    def info(self, message: str) -> None:
+        """
+        A shortcut for the log method of this class. log_type is set to INFO.
+
+        Args:
+            message:
+                a string to be written to the file or/and printed to the console
+        """
+        self.log("INFO", message)
+
+    def warning(self, message: str) -> None:
+        """
+        A shortcut for the log method of this class. log_type is set to WARNING.
+
+        Args:
+            message:
+                a string to be written to the file or/and printed to the console
+        """
+        self.log("WARNING", message)
+
+    def error(self, message: str) -> None:
+        """
+        A shortcut for the log method of this class. log_type is set to ERROR.
+
+        Args:
+            message:
+                a string to be written to the file or/and printed to the console
+        """
+        self.log("ERROR", message)
+
+    def critical(self, message: str) -> None:
+        """
+        A shortcut for the log method of this class. log_type is set to
+        CRITICAL.
+
+        Args:
+            message:
+                a string to be written to the file or/and printed to the console
+        """
+        self.log("CRITICAL", message)
