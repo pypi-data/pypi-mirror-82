17a4452efb409b64402e5ca388a408295ba2cb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:43 +0000

    Add package rpextractsink/1.0.1/rpextractsink-1.0.1-py3-none-any.whl

---
+++ packages/rpextractsink/rpextractsink-1.0.1-py3-none-any.whl/rpextractsink/__init__.py
@@ -0,0 +1,11 @@
+"""
+Created on Oct 5 2020
+
+@author: Joan Hérisson
+
+"""
+
+
+from rpextractsink.rpextractsink import rpExtractSink, build_args_parser
+
+__all__ = ["rpExtractSink", "build_args_parser"]
