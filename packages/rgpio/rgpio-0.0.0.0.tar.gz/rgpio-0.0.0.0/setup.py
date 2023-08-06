d31fe5e31d9a6f7c2ce63a7a163d11f3b6ec381b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:18 +0000

    Add package rgpio/0.0.0.0/rgpio-0.0.0.0.tar.gz

---
+++ packages/rgpio/rgpio-0.0.0.0.tar.gz/rgpio-0.0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rgpio',
+    version='0.0.0.0',
+    description='',
+    long_description='',
+    url='http://www.abyz.me.uk/lg',
+    author='me',
+    author_email='joan@abyz.me.uk',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
