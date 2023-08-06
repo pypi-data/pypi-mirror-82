a4757b6553b145f3e77a1f7b12b474940d69ad32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package pydamo-test/0.0.4/pydamo-test-0.0.4.tar.gz

---
+++ packages/pydamo-test/pydamo-test-0.0.4.tar.gz/pydamo-test-0.0.4/README.md
@@ -0,0 +1,4 @@
+# bode's time model.
+> 测试版本
+
+需要```pywin32```和```bode-time```作为依赖项！
---
+++ packages/pydamo-test/pydamo-test-0.0.4.tar.gz/pydamo-test-0.0.4/pydamo_0/__init__.py
@@ -0,0 +1,5 @@
+name = "pydamo"
+
+# from bd_time.my_time import tt, Time
+from pydamo_0.damo import Time, vk, DM, Key, Mouse, run_in_bat
+from pydamo_0.enum_wind import get_class_winds
