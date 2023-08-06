a1c3605e67f0b990ae879b90ad9805af03dd6fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:05 +0000

    Add package tfinance/0.1.2/tfinance-0.1.2-py3-none-any.whl

---
+++ packages/tfinance/tfinance-0.1.2-py3-none-any.whl/tfinance/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.2"
+__author__ = "Sadiq Rahmati"
+import logging
+
+from .ticker import Ticker
+from .tse import TSE
+from .tse_scrapper import TSEScrapper
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
