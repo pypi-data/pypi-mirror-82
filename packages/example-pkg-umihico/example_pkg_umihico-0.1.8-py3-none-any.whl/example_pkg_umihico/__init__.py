d8f178f332706cd26f93679700b5d0ceb2a1562c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:06 +0000

    Add package example-pkg-umihico/0.1.8/example_pkg_umihico-0.1.8-py3-none-any.whl

---
+++ packages/example-pkg-umihico/example_pkg_umihico-0.1.8-py3-none-any.whl/example_pkg_umihico/__init__.py
@@ -0,0 +1,13 @@
+from .funcs import testfunc
+from .chunks import testfunc
+from .__funcs__ import testfunc
+from .funcs import testfunc
+from .funcs import testfunc
+from . import *
+# try:
+#     from version import version
+#     from __version__ import version
+# except Exception as e:
+#     from .funcs import testfunc
+#     from .version import version
+#     from .__version__ import version
