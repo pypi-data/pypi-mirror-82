30bab5dc27e52cfa7be7a8f1e5a261adae92f3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:12 +0000

    Add package geometricus/0.2.0/geometricus-0.2.0.tar.gz

---
+++ packages/geometricus/geometricus-0.2.0.tar.gz/geometricus-0.2.0/README.md
@@ -0,0 +1,17 @@
+<p align="center"><img src="geometricus_logo.png" width="300" title="Geometricus Logo"></p>
+
+[![PyPI version](https://badge.fury.io/py/geometricus.svg)](https://badge.fury.io/py/geometricus)
+
+# Geometricus Represents Protein Structures as Shape-mers derived from Moment Invariants
+
+A structure-based, alignment-free embedding approach for proteins. Can be used as input to machine learning algorithms.
+
+See the [documentation](https://turtletools.github.io/geometricus/) or read the [preprint](https://biorxiv.org/cgi/content/short/2020.09.07.285569v1).
+
+## Installation
+Geometricus is a Python (3.7+) package with NumPy, SciPy, Numba and ProDy as dependencies. 
+
+Install with `pip install geometricus`
+
+## Usage
+See the [Getting Started](https://turtletools.github.io/geometricus/getting_started) page for example usage.
---
+++ packages/geometricus/geometricus-0.2.0.tar.gz/geometricus-0.2.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+    
+DISTNAME = "geometricus"
+DESCRIPTION = "Fast, structure-based, alignment-free protein embedding"
+LONG_DESCRIPTION = long_description
+MAINTAINER = "Janani Durairaj, Mehmet Akdel"
+MAINTAINER_EMAIL = "janani.durairaj@wur.nl"
+URL = "https://github.com/TurtleTools/geometricus"
+LICENSE = "MIT License"
+DOWNLOAD_URL = "https://github.com/TurtleTools/geometricus"
+VERSION = "0.2.0"
+PYTHON_VERSION = (3, 7)
+INST_DEPENDENCIES = ["scipy", "numba", "prody"]
+
+if __name__ == "__main__":
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        url=URL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        license=LICENSE,
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+        ],
+        packages=["geometricus"],
+        package_data={},
+        install_requires=INST_DEPENDENCIES,
+        long_description_content_type='text/markdown',
+    )
