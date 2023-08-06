861772b0c570f230aab0cfeecd5d741bf57cebb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package aurflux/3.2.30/aurflux-3.2.30.tar.gz

---
+++ packages/aurflux/aurflux-3.2.30.tar.gz/aurflux-3.2.30/aurflux/__init__.py
@@ -0,0 +1,11 @@
+__package__ = "aurflux"
+
+from . import cog, command, context, errors, types_ as ty, utils
+from .cog import FluxCog
+from .config import Config
+from .context import CommandCtx
+from .errors import CommandError
+from .flux import CommandEvent, FluxClient, FluxEvent
+from .types_ import GuildCommandCtx
+
+__all__ = ["FluxClient", "FluxEvent", "CommandEvent", "Config", "errors", "utils", "context", "cog", "command", "ty", "FluxCog","CommandCtx","GuildCommandCtx","CommandError"]
