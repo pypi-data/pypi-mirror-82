0de88d3526138712eb58bbbb774cdeccc4cf4426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:56 +0000

    Add package papyru/0.0.1.dev54/papyru-0.0.1.dev54.tar.gz

---
+++ packages/papyru/papyru-0.0.1.dev54.tar.gz/papyru-0.0.1.dev54/papyru/resource.py
@@ -0,0 +1,32 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from papyru.logger import SequenceGuard, log_root
+from papyru.problem import Problem
+
+# defined in RFC 7231
+HTTP_METHODS = set(['get', 'head', 'post', 'put', 'delete', 'connect',
+                    'options', 'trace'])
+
+
+class Resource:
+    @log_root()
+    def __call__(self, request, *args):
+        try:
+            with SequenceGuard('%s %s' % (request.method, request.path)):
+                try:
+                    method = getattr(self.__class__, request.method.lower())
+                except AttributeError:
+                    allowed = set(dir(self.__class__)) & HTTP_METHODS
+                    raise Problem.method_not_allowed(allowed)
+
+                return method(self, request, *args)
+
+        except Problem as problem:
+            return problem.to_response()
+
+        except ObjectDoesNotExist:
+            return Problem.not_found().to_response()
+
+        except Exception as exc:
+            return Problem.internal_error(
+                'unexpected error: %s' % exc).to_response()
