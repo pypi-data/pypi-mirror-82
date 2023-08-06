1909c1feaf8c18ba16d13f1f2b6c8c0fd93eed5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:06 +0000

    Add package fancylog/0.2.1/fancylog-0.2.1-py3-none-any.whl

---
+++ packages/fancylog/fancylog-0.2.1-py3-none-any.whl/fancylog/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.2.1"
+__author__ = "Adam Tyson"
+__license__ = "MIT"
+__name__ = "fancylog"
+
+from . import *
---
+++ packages/fancylog/fancylog-0.2.1-py3-none-any.whl/tests/tests/test_DUMMY.py
@@ -0,0 +1,4 @@
+def test_import():
+    import fancylog
+
+    print(fancylog.__version__)
