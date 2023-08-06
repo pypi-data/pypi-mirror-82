809bca6c3ef002851f45d7d579032e51dbf2d646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:25 +0000

    Add package hi2020/1.0.1/hi2020-1.0.1.tar.gz

---
+++ packages/hi2020/hi2020-1.0.1.tar.gz/hi2020-1.0.1/README.md
@@ -0,0 +1,56 @@
+#### 开源pypi包制作
+
+##### 1.注册账号
+```
+https://pypi.org
+```
+##### 2.添加许可证
+```
+https://choosealicense.com/licenses/
+```
+##### 3.定义项目包结构
+```
+hi2020
+|-- LICENSE 	
+|-- README.md	
+|-- examples			
+|-- hi2020	     
+|     |-- __init__.py        	   
+|__ setup.py	 
+```
+##### 4.安装打包工具
+```
+# 安装依赖
+pip install setuptools
+pip install wheel
+
+# 更新依赖
+python -m pip install --upgrade setuptools wheel
+```
+##### 5.打包代码
+```
+python setup.py sdist bdist_wheel
+```
+##### 6.安装上传工具
+```
+python -m pip install --upgrade twine
+或
+pip install --upgrade twine
+```
+##### 7.发布模块
+```
+python -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+或
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+```
+##### 8.使用模块
+```
+# 安装模块
+pip install hi2020
+
+# 导入模块
+from hi2020 import say
+
+# 执行函数
+say.hi()
+```
---
+++ packages/hi2020/hi2020-1.0.1.tar.gz/hi2020-1.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+@File  : setup.py
+@Author: HappyLay
+@Date  : 2020/10/15 13:32
+@Desc  : 安徽理工大学
+"""
+__author__ = 'HappyLay'
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hi2020",  # 模块名称
+    version="1.0.1",  # 当前版本
+    author="happylay",  # 作者
+    # author_email="",  # 作者邮箱
+    description="一个hi问候包",  # 模块介绍
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+
+    # 模块相关的元数据（更多描述信息）
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+        'click',
+    ],
+    python_requires='>=3'
+)
