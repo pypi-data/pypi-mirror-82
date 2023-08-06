f230e7ca6044ebd743e1d8c58b3a684d1372fa2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:56 +0000

    Add package Python-EasyGraph/0.2a8/Python-EasyGraph-0.2a8.tar.gz

---
+++ packages/Python-EasyGraph/Python-EasyGraph-0.2a8.tar.gz/Python-EasyGraph-0.2a8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import io
+
+def parse_requirements(filename):
+    lineiter=(line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Python-EasyGraph",                                     
+    version="0.2a8",                                        
+    author="Fudan MSN Group",                                       
+    author_email="easygraph@163.com",                      
+    description="Easy Graph",                            
+    long_description=long_description,                      
+    long_description_content_type="text/x-rst",          
+    url="https://github.com/easy-graph/Easy-Graph",                              
+    packages=setuptools.find_packages(),                    
+    classifiers=[                                           
+        "Programming Language :: Python :: 3",              
+        "License :: OSI Approved :: BSD License",           
+        "Operating System :: OS Independent",               
+    ],
+    iinstall_requires=parse_requirements("requirements.txt"),
+)
+
