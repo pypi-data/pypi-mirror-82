feab872ec919420d178abeacb36c4b91374973f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:05 +0000

    Add package visaplan.plone.tools/1.2.0/visaplan.plone.tools-1.2.0.tar.gz

---
+++ packages/visaplan.plone.tools/visaplan.plone.tools-1.2.0.tar.gz/visaplan.plone.tools-1.2.0/src/visaplan/plone/tools/decorators.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+
+try:
+    from simplejson import dumps
+except ImportError:
+    from json import dumps
+
+
+def returns_json(raw):
+    """
+    Decorate the given function to ...
+    - convert the result to JSON, and
+    - add the appropriate HTTP headers
+    """
+    @wraps(raw)
+    def wrapped(self, **kwargs):
+        dic = raw(self, **kwargs)
+        txt = dumps(dic)
+        context = self.context
+        setHeader = context.REQUEST.RESPONSE.setHeader
+        setHeader('Content-Type', 'application/json; charset=utf-8')
+        setHeader('Content-Length', len(txt))
+        return txt
+    return wrapped
