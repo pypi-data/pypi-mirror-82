4947ebf4c1c095a34569a479473cdc27780a64e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:31 +0000

    Add package eagleapi/1.0.0/eagleapi-1.0.0.tar.gz

---
+++ packages/eagleapi/eagleapi-1.0.0.tar.gz/eagleapi-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'eagleapi',
+    packages = ['eagleapi'],
+    version = '1.0.0',
+    license='MIT',
+    description = 'official Eagle bot Api',
+    author = 'Macéo Tuloup | Axel Denis',
+    author_email = 'eagle.discordbot@gmail.com',
+    url = 'https://eagle-bot.ml/',
+    download_url = 'https://github.com/eagle-discordbot/Eagle-Api/archive/master.tar.gz',
+    keywords = ['discord', 'eagle', 'api'],
+    classifiers=[
+        'Development Status :: 3 - Alpha', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
