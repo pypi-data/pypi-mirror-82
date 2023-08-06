9f58e74b98b415a32c9e4500b88ce669f02728ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:44 +0000

    Add package insta-scrape/0.6.0/insta-scrape-0.6.0.tar.gz

---
+++ packages/insta-scrape/insta-scrape-0.6.0.tar.gz/insta-scrape-0.6.0/instascrape/instascrape.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+# pylint: disable=unused-wildcard-import
+
+from typing import Dict
+
+from instascrape.scrapers import *
+
+class InstaScrape:
+    def load_profile(self, username: str) -> Dict[str, str]:
+        profile = Profile.from_username(username)
+        profile.load()
+        return profile
+
+    def load_post(self, url: str) -> Dict[str, str]:
+        post = Post(url)
+        post.load()
+        return post
+
+    def load_hashtag(self, hashtag: str) -> Dict[str, str]:
+        hashtag = Hashtag.from_hashtag(hashtag)
+        hashtag.load()
+        return hashtag
