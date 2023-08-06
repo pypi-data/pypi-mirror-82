0d1ff1eb2c41c8492e4a535721f44c0e21df0e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:39 +0000

    Add package pyclone-module/1.53.4/pyclone-module-1.53.4.tar.gz

---
+++ packages/pyclone-module/pyclone-module-1.53.4.tar.gz/pyclone-module-1.53.4/pyclone/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+"""Top-level package for simple-mega."""
+from .pyclone import *
+
+__author__ = """Mwila Nyirongo"""
+__email__ = 'mpnyirongo@gmail.com'
+__version__ = '1.0.0'
