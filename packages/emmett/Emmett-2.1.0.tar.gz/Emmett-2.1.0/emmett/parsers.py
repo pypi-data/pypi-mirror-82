22d7dc697bef79e6788563636b42ed123bb65245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package emmett/2.1.0/Emmett-2.1.0.tar.gz

---
+++ packages/emmett/Emmett-2.1.0.tar.gz/Emmett-2.1.0/emmett/parsers.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    emmett.parsers
+    --------------
+
+    :copyright: 2014 Giovanni Barillari
+    :license: BSD-3-Clause
+"""
+
+from functools import partial
+from typing import Any, Callable, Dict
+
+from rapidjson import (
+    DM_ISO8601, DM_NAIVE_IS_UTC,
+    NM_NATIVE,
+    loads as _json_loads
+)
+
+
+class Parsers(object):
+    _registry_: Dict[str, Callable[[str], Dict[str, Any]]] = {}
+
+    @classmethod
+    def register_for(cls, target):
+        def wrap(f):
+            cls._registry_[target] = f
+            return f
+        return wrap
+
+    @classmethod
+    def get_for(cls, target):
+        return cls._registry_[target]
+
+
+json = partial(
+    _json_loads,
+    datetime_mode=DM_ISO8601 | DM_NAIVE_IS_UTC,
+    number_mode=NM_NATIVE
+)
+
+Parsers.register_for('json')(json)
