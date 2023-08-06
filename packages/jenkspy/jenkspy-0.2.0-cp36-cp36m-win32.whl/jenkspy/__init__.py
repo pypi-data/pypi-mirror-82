7e1833c56434cb728e6114a00262c7e33008942f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:25 +0000

    Add package jenkspy/0.2.0/jenkspy-0.2.0-cp36-cp36m-win32.whl

---
+++ packages/jenkspy/jenkspy-0.2.0-cp36-cp36m-win32.whl/jenkspy/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__version__ = "0.2.0"
+
+from .core import jenks_breaks
+from .core import JenksNaturalBreaks
+
+
+__all__ = ['jenks_breaks', 'JenksNaturalBreaks']
