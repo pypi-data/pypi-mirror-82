79ce8a5129b9c1078d883b97faf2590e20a95859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:15 +0000

    Add package kaaengine/0.9/kaaengine-0.9-cp36-cp36m-win_amd64.whl

---
+++ packages/kaaengine/kaaengine-0.9-cp36-cp36m-win_amd64.whl/kaa/transitions.py
@@ -0,0 +1,8 @@
+from ._kaa import (
+    NodeTransition, NodeTransitionsSequence, NodeTransitionsParallel,
+    NodeCustomTransition, AttributeTransitionMethod,
+    NodePositionTransition, NodeRotationTransition, NodeScaleTransition,
+    NodeColorTransition, BodyNodeVelocityTransition,
+    BodyNodeAngularVelocityTransition, NodeTransitionDelay,
+    NodeTransitionCallback, NodeSpriteTransition
+)
