c0347c53f25e5fd5135fd9bae29b63dc81fa9090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:31 +0000

    Add package pypcode/0.0.2/pypcode-0.0.2.tar.gz

---
+++ packages/pypcode/pypcode-0.0.2.tar.gz/pypcode-0.0.2/pypcode/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Based on sleighexample.cc
+
+import cppyy
+import sys
+import logging
+import os.path
+
+log = logging.getLogger(__name__)
+
+PYPCODENATIVE_PATH=os.path.join(os.path.dirname(__file__), 'pypcode-native')
+SLEIGH_PATH=os.path.join(PYPCODENATIVE_PATH, 'sleigh-2.1.0')
+SLEIGH_SRC_PATH=os.path.join(SLEIGH_PATH, 'src')
+SLEIGH_SPECFILES_PATH=os.path.join(SLEIGH_PATH, 'specfiles')
+
+log.debug('Loading Library')
+cppyy.include(os.path.join(PYPCODENATIVE_PATH, 'pypcode-native.h'))
+cppyy.include(os.path.join(SLEIGH_SRC_PATH, 'translate.hh'))
+cppyy.include(os.path.join(SLEIGH_SRC_PATH, 'error.hh'))
+cppyy.load_library(os.path.join(PYPCODENATIVE_PATH, 'pypcode-native.so'))
+
+class AssemblyEmitCacher(cppyy.gbl.AssemblyEmit):
+  def dump(self, addr, mnem, body):
+    self.addr = addr
+    self.mnem = mnem
+    self.body = body
+
+# Import names into this namespace
+from cppyy.gbl import Address
+from cppyy.gbl import AddrSpace
+from cppyy.gbl import ContextInternal
+from cppyy.gbl import DocumentStorage
+from cppyy.gbl import Element
+from cppyy.gbl import get_opname
+from cppyy.gbl import PcodeRawOutHelper
+from cppyy.gbl import SimpleLoadImage
+from cppyy.gbl import Sleigh
+from cppyy.gbl import OpCode
+from cppyy.gbl import VarnodeData
+from cppyy.gbl import PcodeOpRaw
---
+++ packages/pypcode/pypcode-0.0.2.tar.gz/pypcode-0.0.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import os
+import os.path
+import platform
+import shutil
+import subprocess
+from setuptools import setup
+from setuptools.command.build_py import build_py
+
+SOURCE_DIR = os.path.abspath(os.path.dirname(__file__))
+LIB_SOURCE_DIR = os.path.join(SOURCE_DIR, 'pypcode', 'pypcode-native')
+OUTPUT_DIR = os.path.join(SOURCE_DIR, 'pypcode')
+
+class custom_build(build_py):
+	def run(self):
+		if not os.path.exists(OUTPUT_DIR):
+			os.makedirs(OUTPUT_DIR)
+		subprocess.check_call(['make'], cwd=LIB_SOURCE_DIR)
+		shutil.copyfile(os.path.join(LIB_SOURCE_DIR, 'pypcode-native.so'),
+		                os.path.join(OUTPUT_DIR,     'pypcode-native.so'))
+		return super().run()
+
+with open('README.md') as f:
+	long_description = f.read()
+
+setup(name='pypcode',
+	version='0.0.2',
+	description='Python bindings to Ghidra\'s SLEIGH library',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	author='Matt Borgerson',
+	author_email='contact@mborgerson.com',
+	url='https://github.com/angr/pypcode',
+	license='GPL',
+	cmdclass=dict(build_py=custom_build),
+	include_package_data=True,
+	packages=['pypcode'],
+	package_data={
+		'pypcode': ['pypcode-native.*']
+		},
+	install_requires=[
+		'cppyy'
+		]
+	)
