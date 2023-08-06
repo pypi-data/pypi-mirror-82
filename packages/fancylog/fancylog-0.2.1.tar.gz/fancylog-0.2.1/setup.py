6bd23c6fecf4ada680f909b9f166c5d0d96a8b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:06 +0000

    Add package fancylog/0.2.1/fancylog-0.2.1.tar.gz

---
+++ packages/fancylog/fancylog-0.2.1.tar.gz/fancylog-0.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fancylog",
+    version="0.2.1",
+    description="Fancier logging in Python",
+    install_requires=["packaging"],
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "coverage",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+        ]
+    },
+    python_requires=">=3.6",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/adamltyson/fancylog",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+    ],
+)
