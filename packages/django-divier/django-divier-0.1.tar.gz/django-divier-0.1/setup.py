76d9e01da3848430e819d8897d52c0240750391e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:24 +0000

    Add package django-divier/0.1/django-divier-0.1.tar.gz

---
+++ packages/django-divier/django-divier-0.1.tar.gz/django-divier-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='django-divier',
+      version='0.1',
+      description='Django - divier admin theme',
+      packages=['django-divier'],
+      author_email='belinsky.dev@gmail.com',
+      zip_safe=False)
