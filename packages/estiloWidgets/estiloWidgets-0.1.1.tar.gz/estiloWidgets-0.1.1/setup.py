4823c70dacc76780f4d3308557733972cd8339ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:35 +0000

    Add package estiloWidgets/0.1.1/estiloWidgets-0.1.1.tar.gz

---
+++ packages/estiloWidgets/estiloWidgets-0.1.1.tar.gz/estiloWidgets-0.1.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import *
+
+setup(
+    name='estiloWidgets',
+    version='0.1.1',
+    url='https://github.com/rafael-rfzorzi/estiloWidgets',
+    license='MIT License',
+    author='Rafael De Leon Serafim',
+    author_email='rafaelserafim.rfzorzi@gmail.com',
+    keywords='autocomplety buttonStyle entryPlaceHolder gradienteFrame labelStyle',
+    description=u'Modulo que adiciona recursos extras ao tkinter',
+    packages=['estiloWidgets'],
+    install_requires=[],
+)
