a854a349ddb0d8298765ff196481c52bd6af715a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:19 +0000

    Add package Sanic-Session-SPF/0.5.1.post1/Sanic-Session-SPF-0.5.1.post1.tar.gz

---
+++ packages/Sanic-Session-SPF/Sanic-Session-SPF-0.5.1.post1.tar.gz/Sanic-Session-SPF-0.5.1.post1/sanic_session_spf/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+from sanic_session.memcache import MemcacheSessionInterface
+from sanic_session.redis import RedisSessionInterface
+from sanic_session.memory import InMemorySessionInterface
+from sanic_session.mongodb import MongoDBSessionInterface
+from sanic_session.aioredis import AIORedisSessionInterface
+
+from sanic_session_spf.plugin import Session, session
+
+__all__ = ('MemcacheSessionInterface', 'RedisSessionInterface',
+           'InMemorySessionInterface', 'MongoDBSessionInterface',
+           'AIORedisSessionInterface', 'Session', 'session')
+
+__version__ = '0.5.1.post1'
