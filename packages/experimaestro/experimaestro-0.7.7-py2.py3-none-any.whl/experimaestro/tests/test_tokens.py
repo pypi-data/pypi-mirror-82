1a5a6f80380dfa815c2f0a0769eb2246bcc8b416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:11 +0000

    Add package experimaestro/0.7.7/experimaestro-0.7.7-py2.py3-none-any.whl

---
+++ packages/experimaestro/experimaestro-0.7.7-py2.py3-none-any.whl/experimaestro/tests/test_tokens.py
@@ -0,0 +1,190 @@
+import sys
+import pytest
+import logging
+import time
+from pathlib import Path
+
+import subprocess
+from experimaestro.tokens import CounterToken
+from experimaestro.scheduler import JobState
+from .utils import TemporaryExperiment, TemporaryDirectory, timeout
+from .task_tokens import TokenTask
+from experimaestro.ipc import IPCom
+from . import restart
+
+
+class TimeInterval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __lt__(self, value):
+        return self.start > value.end
+
+    def __str__(self):
+        return "%.4f - %.4f" % (self.start, self.end)
+
+    def __repr__(self):
+        return str(self)
+
+
+def get_times(task):
+    return TimeInterval(
+        *(float(t) for t in task.stdout().read_text().strip().split("\n"))
+    )
+
+
+def get_times_frompath(path):
+    s = path.read_text().strip().split("\n")
+    logging.info("Read times: %s", s)
+    return TimeInterval(*(float(t) for t in s))
+
+
+def token_experiment(xp, token):
+    path = xp.workspace.path / "test_token.file"
+    task1 = TokenTask(path=path, x=1)
+    task2 = TokenTask(path=path, x=2)
+    for task in [task1, task2]:
+        if token:
+            task.add_dependencies(token.dependency(1))
+        task.submit()
+
+    # Wait that both tasks are scheduled
+    for task in [task1, task2]:
+        while task.job.state == JobState.UNSCHEDULED:
+            time.sleep(0.01)
+
+    # Waiting to ensure that both jobs are launched
+    path.write_text("Hello world")
+
+    xp.wait()
+
+    time1 = get_times(task1)
+    time2 = get_times(task2)
+
+    assert time1 > time2 or time2 > time1
+
+
+@pytest.mark.xfail(strict=True)
+def test_token_fail():
+    """Simple token test: should fail without token"""
+    with TemporaryExperiment("tokens", maxwait=10) as xp:
+        token_experiment(xp, token)
+
+
+def test_token():
+    """Simple token test: should succeed without token"""
+    with TemporaryExperiment("tokens", maxwait=10) as xp:
+        token = xp.workspace.connector.createtoken("test-token", 1)
+        token_experiment(xp, token)
+
+
+def test_token_monitor():
+    """Two different schedulers (within the same process)
+
+    Test the ability of the token to monitor the filesystem
+    """
+
+    def run(xp, x, path):
+        token = xp.workspace.connector.createtoken("test-token-monitor", 1)
+        task = TokenTask(path=path, x=x).add_dependencies(token.dependency(1)).submit()
+        return task
+
+    with TemporaryExperiment("tokens1", maxwait=10) as xp1, TemporaryExperiment(
+        "tokens2", maxwait=10
+    ) as xp2:
+        path = xp1.workspace.path / "test_token.file"
+        task1 = run(xp1, 1, path)
+        task2 = run(xp2, 2, path)
+
+        time.sleep(0.5)
+        path.write_text("Hello world")
+
+        xp1.wait()
+        xp2.wait()
+        time1 = get_times(task1)
+        time2 = get_times(task2)
+
+        logging.info("%s vs %s", time1, time2)
+        assert time1 > time2 or time2 > time1
+
+
+import multiprocessing
+
+
+def test_token_reschedule():
+    """Test whether a job can be re-submitted if it failed to acquire a token due to multiple schedulers concurrency
+
+    - task 1 and 2 are started in two different processes, using the token
+    - we wait for both to be scheduled
+    - we write a file so that both can finish
+    """
+    queue1 = multiprocessing.Queue(3)
+    queue2 = multiprocessing.Queue(3)
+
+    with TemporaryDirectory("reschedule") as workdir:
+        lockingpath = workdir / "lockingpath"
+
+        command = [
+            sys.executable,
+            Path(__file__).parent / "token_reschedule.py",
+            workdir,
+        ]
+
+        ready1 = workdir / "ready.1"
+        time1 = workdir / "time.1"
+        p1 = subprocess.Popen(command + ["1", lockingpath, str(ready1), str(time1)])
+
+        ready2 = workdir / "ready.1"
+        time2 = workdir / "time.2"
+        p2 = subprocess.Popen(command + ["2", lockingpath, str(ready2), str(time2)])
+
+        try:
+            with timeout(10):
+                # Wait that both processes are ready
+                while not ready1.is_file():
+                    time.sleep(0.01)
+                while not ready2.is_file():
+                    time.sleep(0.01)
+                logging.info("Both processes are ready: allowing tasks to finish")
+                lockingpath.write_text("Let's go")
+
+                # Waiting for the output
+                while not time1.is_file():
+                    time.sleep(0.01)
+                while not time2.is_file():
+                    time.sleep(0.01)
+
+                time1 = get_times_frompath(time1)
+                time2 = get_times_frompath(time2)
+
+                logging.info("%s vs %s", time1, time2)
+                assert time1 > time2 or time2 > time1
+        except TimeoutError as e:
+            p1.terminate()
+            p2.terminate()
+            pytest.fail("Timeout")
+
+        except Exception as e:
+            logging.warning("Other exception: killing processes (just in case)")
+            p1.terminate()
+            p2.terminate()
+            pytest.fail("Other exception")
+
+
+@pytest.mark.skip("TODO: not implemented")
+def test_token_process():
+    """Test tokens with two processes"""
+    raise NotImplementedError()
+
+
+def restart_function(xp):
+    token = CounterToken(xp.workdir / "token", 1)
+    token(1, restart.Restart()).submit()
+
+
+@pytest.mark.skip("TODO: not implemented")
+@pytest.mark.parametrize("terminate", restart.TERMINATES_FUNC)
+def test_restart(terminate):
+    """Restarting the experiment should take back running tasks"""
+    restart.restart(terminate, restart_function)
---
+++ packages/experimaestro/experimaestro-0.7.7-py2.py3-none-any.whl/experimaestro/tokens.py
@@ -0,0 +1,237 @@
+"""Tokens are special types of dependency controlling the access to 
+a computational resource (e.g. number of launched jobs, etc.)
+"""
+
+import sys
+from pathlib import Path
+from experimaestro.core.objects import Task
+import fasteners
+import threading
+import struct
+import time
+import os.path
+from watchdog.events import FileSystemEventHandler
+from typing import Dict
+
+from .ipc import ipcom
+from .locking import Lock, LockError
+from .dependencies import Dependency, DependencyStatus, Resource
+import logging
+
+
+logger = logging.getLogger("xpm.tokens")
+
+
+class Token(Resource):
+    """Base class for all token-based resources"""
+
+    pass
+
+
+class CounterTokenLock(Lock):
+    def __init__(self, dependency: "CounterTokenDependency"):
+        super().__init__()
+        self.dependency = dependency
+
+    def _acquire(self):
+        self.dependency.token.acquire(self.dependency.count)
+
+    def _release(self):
+        self.dependency.token.release(self.dependency.count)
+
+    def __str__(self):
+        return "Lock(%s)" % self.dependency
+
+
+class CounterTokenDependency(Dependency):
+    def __init__(self, token: "CounterToken", count: int):
+        super().__init__(token)
+        self._token = token
+        self.count = count
+
+    def status(self) -> DependencyStatus:
+        if self.count <= self.token.available:
+            return DependencyStatus.OK
+        return DependencyStatus.WAIT
+
+    def lock(self) -> "Lock":
+        return CounterTokenLock(self)
+
+    @property
+    def token(self):
+        return self._token
+
+
+# FIXME:
+# to haver recovery, we need:
+# - a folder for the token
+# - each dependency is a file containing:
+#   - the job URI
+#   - number of tokens
+# - automatic removal when possible / periodic checking if not our scheduler / checking at startup
+class CounterToken(Token, FileSystemEventHandler):
+    """File-based counter token"""
+
+    TOKENS: Dict[str, "CounterToken"] = {}
+
+    """Maps paths to instances"""
+    VALUES = struct.Struct("<LL")
+
+    @staticmethod
+    def forkhandler():
+        CounterToken.TOKENS = {}
+
+    @staticmethod
+    def create(name: str, path: Path, count: int):
+        created = CounterToken.TOKENS.get(name, None)
+        if created:
+            logger.warning("Re-using token for path %s", path)
+        else:
+            created = CounterToken(name, path, count)
+            CounterToken.TOKENS[name] = created
+        return created
+
+    def __init__(self, name: str, path: Path, count: int):
+        """[summary]
+
+        Arguments:
+            path {Path} -- The file path of the token file
+            count {int} -- Number of tokens (overrides previous definitions)
+        """
+        super().__init__()
+        self.path = path
+        self.ipc_lock = fasteners.InterProcessLock(
+            path.with_suffix(path.suffix + ".lock")
+        )
+        self.lock = threading.Lock()
+
+        self.name = name
+
+        # Watched path
+        self.watchedpath = str(path.absolute())
+        self.watcher = ipcom().fswatch(self, str(path.parent.absolute()))
+        logger.info("Watching %s", self.watchedpath)
+        # When writing, to avoid re-reading the file we just wrote
+        self.timestamp = 0
+
+        # Set the new number of tokens
+        with self.lock, self.ipc_lock:
+            bytes = self.path.read_bytes() if self.path.is_file() else None
+
+            if bytes:
+                total, taken = CounterToken.VALUES.unpack(bytes)
+                logger.info("Read token from %s: %d/%d", self.path, total, taken)
+            else:
+                taken = 0
+                total = count
+
+            if total != count:
+                logger.warning("Changing number of tokens from %d to %d", total, count)
+                total = count
+
+            self._write(total, taken)
+
+        # Set the number of available tokens
+        self.available = total - taken
+
+    def __str__(self):
+        return "token[{}]".format(self.name)
+
+    def on_modified(self, event):
+        logger.debug(
+            "Watched path notification %s [watched %s]",
+            event.src_path,
+            self.watchedpath,
+        )
+        if event.src_path == self.watchedpath:
+            logger.debug("Watched path modified [%s]", self.path)
+            timestamp = os.path.getmtime(self.path)
+            if timestamp <= self.timestamp:
+                logger.debug(
+                    "Not reading token file [%f <= %f]", timestamp, self.timestamp
+                )
+            else:
+                logger.debug("Trying to read...")
+                with self.lock, self.ipc_lock:
+                    logger.debug("Got lock...")
+                    total, taken = CounterToken.VALUES.unpack(self.path.read_bytes())
+                    available = total - taken
+
+                if available != self.available:
+                    logger.info(
+                        "Counter token changed: %d to %d", self.available, available
+                    )
+                    self.available = available
+                    # Notify jobs
+                    with self.dependents as dependents:
+                        for dependency in dependents:
+                            dependency.check()
+
+    def dependency(self, count):
+        return CounterTokenDependency(self, count)
+
+    def __call__(self, count, task: Task):
+        return task.add_dependencies(self.dependency(count))
+
+    def _write(self, total, taken):
+        # Should only be called when locked
+        self.path.write_bytes(CounterToken.VALUES.pack(total, taken))
+        self.timestamp = os.path.getmtime(self.path)
+
+        logger.debug(
+            "Token: wrote %d/%d to %s [%d]", total, taken, self.path, self.timestamp
+        )
+
+    def _read(self):
+        # Should only be called when locked
+        self.timestamp = os.path.getmtime(self.path)
+        total, taken = CounterToken.VALUES.unpack(self.path.read_bytes())
+        self.available = total - taken
+        logger.debug("Read token information from %s: %d/%d", self.path, total, taken)
+        return total, taken
+
+    def acquire(self, count):
+        """Acquire"""
+        with self.lock, self.ipc_lock:
+            total, taken = self._read()
+            logger.debug(
+                "Token state [acquire %d]: count %d, taken %d", count, total, taken
+            )
+            if count + taken > total:
+                logger.warning("No more token available - cannot lock")
+                raise LockError("No token")
+
+            taken += count
+
+            self._write(total, taken)
+            logger.debug(
+                "Token state [acquired %d]: count %d, taken %d", count, total, taken
+            )
+
+    def release(self, count):
+        """Release"""
+        with self.lock, self.ipc_lock:
+            logger.debug("Reading token information from %s", self.path)
+            total, taken = CounterToken.VALUES.unpack(self.path.read_bytes())
+            logger.debug(
+                "Token state [release %d]: count %d, taken %d", count, total, taken
+            )
+            taken -= count
+            if taken < 0:
+                taken = 0
+                logger.error("More tokens released that taken")
+            self._write(total, taken)
+            logger.debug(
+                "Token state [released %d]: count %d, taken %d", count, total, taken
+            )
+            self.available = total - taken
+
+        # Now, check
+        with self.dependents as dependents:
+            dependents = list(dependents)
+        for dependency in dependents:
+            dependency.check()
+
+
+if sys.platform != "win32":
+    os.register_at_fork(after_in_child=CounterToken.forkhandler)
