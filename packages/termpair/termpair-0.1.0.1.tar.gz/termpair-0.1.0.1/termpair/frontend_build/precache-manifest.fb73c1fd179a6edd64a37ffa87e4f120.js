e03b1e8db75ba0dc5e1c1c08cb8fe49b88b4a9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package termpair/0.1.0.1/termpair-0.1.0.1.tar.gz

---
+++ packages/termpair/termpair-0.1.0.1.tar.gz/termpair-0.1.0.1/termpair/frontend_build/precache-manifest.fb73c1fd179a6edd64a37ffa87e4f120.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf1bb69616000a770e7",
+    "url": "./static/css/main.95ffe78b.chunk.css"
+  },
+  {
+    "revision": "cdf1bb69616000a770e7",
+    "url": "./static/js/main.e12d8782.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "082027f6019b7a96bfcf",
+    "url": "./static/css/2.4b811dd8.chunk.css"
+  },
+  {
+    "revision": "082027f6019b7a96bfcf",
+    "url": "./static/js/2.3723caf5.chunk.js"
+  },
+  {
+    "revision": "74cdedb8050c1198d3c477e62d8bd849",
+    "url": "./static/media/logo.74cdedb8.png"
+  },
+  {
+    "revision": "588f6970a9445507e409207f97b54b90",
+    "url": "./index.html"
+  }
+];
