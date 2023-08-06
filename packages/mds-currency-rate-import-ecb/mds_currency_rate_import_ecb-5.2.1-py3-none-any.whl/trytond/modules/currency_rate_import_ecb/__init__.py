8fb17bf7059cd82cecdb15f37b34557503903b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:28 +0000

    Add package mds-currency-rate-import-ecb/5.2.1/mds_currency_rate_import_ecb-5.2.1-py3-none-any.whl

---
+++ packages/mds-currency-rate-import-ecb/mds_currency_rate_import_ecb-5.2.1-py3-none-any.whl/trytond/modules/currency_rate_import_ecb/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from .configuration import Configuration
+from .wizard import UpdateNowWizard
+from .currency import Currency
+from .cron import Cron
+
+
+def register():
+    Pool.register(
+        Cron,
+        Configuration,
+        Currency,
+        module='currency_rate_import_ecb', type_='model')
+    Pool.register(
+        UpdateNowWizard,
+        module='currency_rate_import_ecb', type_='wizard')
