ee06a0308b188ed2861386552e599b1e0142c566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package ynab-moka/1.0.1/ynab_moka-1.0.1-py3-none-any.whl

---
+++ packages/ynab-moka/ynab_moka-1.0.1-py3-none-any.whl/ynab_moka/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ynab_moka.api.accounts_api import AccountsApi
+from ynab_moka.api.budgets_api import BudgetsApi
+from ynab_moka.api.categories_api import CategoriesApi
+from ynab_moka.api.deprecated_api import DeprecatedApi
+from ynab_moka.api.months_api import MonthsApi
+from ynab_moka.api.payee_locations_api import PayeeLocationsApi
+from ynab_moka.api.payees_api import PayeesApi
+from ynab_moka.api.scheduled_transactions_api import ScheduledTransactionsApi
+from ynab_moka.api.transactions_api import TransactionsApi
+from ynab_moka.api.user_api import UserApi
