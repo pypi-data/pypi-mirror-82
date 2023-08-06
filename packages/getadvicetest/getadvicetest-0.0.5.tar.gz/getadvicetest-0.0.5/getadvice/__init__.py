ccbcf944e1631adbebec1682b08af9631d7c4877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:14 +0000

    Add package getadvicetest/0.0.5/getadvicetest-0.0.5.tar.gz

---
+++ packages/getadvicetest/getadvicetest-0.0.5.tar.gz/getadvicetest-0.0.5/getadvice/__init__.py
@@ -0,0 +1,10 @@
+import requests 
+
+__version__ = "0.0.5"
+
+__url__ = "https://api.adviceslip.com/advice"
+
+def advice(name):
+    json_result = requests.get(__url__).json()
+    advice_result = json_result["slip"]["advice"]
+    return f"{name}, {advice_result}"
