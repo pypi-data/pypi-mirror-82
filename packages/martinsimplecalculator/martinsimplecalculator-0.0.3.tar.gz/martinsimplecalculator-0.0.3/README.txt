8831df077ae901acf8740aff5a744fdf3a763f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:09 +0000

    Add package martinsimplecalculator/0.0.3/martinsimplecalculator-0.0.3.tar.gz

---
+++ packages/martinsimplecalculator/martinsimplecalculator-0.0.3.tar.gz/martinsimplecalculator-0.0.3/README.txt
@@ -0,0 +1,18 @@
+This is a verry simple calculator.
+
+This is the difrent things you can do: (thiese commands are examples and after 'returns' there is what comes out after doing that command)
+
+add_num(1,1)  
+returns 2
+
+subtract_num(2,1)
+returns 1
+
+multiply_num(5,2)
+returns 10
+
+divide_num(10,2)
+returns 5
+
+exponent_num(2,10)
+returns 1024
---
+++ packages/martinsimplecalculator/martinsimplecalculator-0.0.3.tar.gz/martinsimplecalculator-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+'Development Status :: 5 - Production/Stable',
+'Intended Audience :: Education',
+'Operating System :: Microsoft :: Windows :: Windows 10',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3'
+]
+
+setup(
+	name='martinsimplecalculator',
+	version='0.0.3',
+	description='a basic calculator',
+	long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+	long_description_content_type='text/plain',
+	url='',
+	author='Martin Husby',
+	author_email='martin.husby.dev@gmail.com',
+	license='MIT',
+	classifiers=classifiers,
+	keywords='calculator',
+	packages=find_packages(),
+	install_requires=['']
+
+)
