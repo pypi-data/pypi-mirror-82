8d8782759786cf51d05ac35068ee38bd772b901c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:48 +0000

    Add package Database-by-ike-welborn/1.0.0/Database-by-ike-welborn-1.0.0.tar.gz

---
+++ packages/Database-by-ike-welborn/Database-by-ike-welborn-1.0.0.tar.gz/Database-by-ike-welborn-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+__project__ = "Database-by-ike-welborn"
+__version__ = "1.0.0"
+__description__ = "a Python for making data programming esier"
+__packages__ = ["data"]
+__author__ = "Ike Welborn"
+__requires__ = ["random"]
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    requires = __requires__,
+)
