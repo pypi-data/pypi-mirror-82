5ac66d9422db4812be5d870b390c56ebd91e4aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:28 +0000

    Add package cuenca/0.3.4/cuenca-0.3.4-py3-none-any.whl

---
+++ packages/cuenca/cuenca-0.3.4-py3-none-any.whl/cuenca/resources/__init__.py
@@ -0,0 +1,34 @@
+__all__ = [
+    'ApiKey',
+    'Account',
+    'BalanceEntry',
+    'Card',
+    'Commission',
+    'Deposit',
+    'Transfer',
+    'WhatsappTransfer',
+]
+
+from .accounts import Account
+from .api_keys import ApiKey
+from .balance_entries import BalanceEntry
+from .cards import Card
+from .commissions import Commission
+from .deposits import Deposit
+from .resources import RESOURCES
+from .transfers import Transfer
+from .whatsapp_transfers import WhatsappTransfer
+
+# avoid circular imports
+resource_classes = [
+    ApiKey,
+    Account,
+    BalanceEntry,
+    Card,
+    Commission,
+    Deposit,
+    Transfer,
+    WhatsappTransfer,
+]
+for resource_cls in resource_classes:
+    RESOURCES[resource_cls._resource] = resource_cls  # type: ignore
