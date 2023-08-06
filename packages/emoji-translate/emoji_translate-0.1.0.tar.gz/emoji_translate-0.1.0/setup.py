05c5a167e749ca22f8256bb7929118480f449696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:30 +0000

    Add package emoji-translate/0.1.0/emoji_translate-0.1.0.tar.gz

---
+++ packages/emoji-translate/emoji_translate-0.1.0.tar.gz/emoji_translate-0.1.0/setup.py
@@ -0,0 +1,62 @@
+"""
+Setup script for emoji-translate
+"""
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+author = email = source = version = None
+with open(path.join(this_directory, 'emoji_translate', '__init__.py'), encoding='utf-8') as f:
+    for line in f:
+        if line.strip().startswith('__version__'):
+            version = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__author__'):
+            author = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__email__'):
+            email = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif line.strip().startswith('__source__'):
+            source = line.split('=')[1].strip().replace('"', '').replace("'", '')
+        elif None not in (version, author, email, source):
+            break
+
+setuptools.setup(
+    name='emoji_translate',
+    author=author,
+    author_email=email,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    description="Emoji translation for Python",
+    keywords=['emoji', 'translate', 'emoji_translate'],
+    include_package_data=True,
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_namespace_packages(),
+    install_requires=['emoji', 'pandas', 'numpy'],
+    url=source,
+    version=version,
+    zip_safe=True,
+    # data_files=[('emoji_translate/data', ['emoji_translate/data/emoji_utf8_lexicon.txt', 
+    #                                       'emoji_translate/data/emojis.json', 
+    #                                       'emoji_translate/data/merged_emojis.json'])]
+)
