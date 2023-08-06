71eacba6e80a50a743cb5ce07244bc3a48cc2751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:34 +0000

    Add package argufy/0.1.1.dev13/argufy-0.1.1.dev13.tar.gz

---
+++ packages/argufy/argufy-0.1.1.dev13.tar.gz/argufy-0.1.1.dev13/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['argufy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argparse_color_formatter>=1.2.2,<2.0.0', 'docstring-parser>=0.7.2,<0.8.0']
+
+setup_kwargs = {
+    'name': 'argufy',
+    'version': '0.1.1.dev13',
+    'description': 'Inspection based parser based on argparse.',
+    'long_description': None,
+    'author': 'Jesse P. Johnson',
+    'author_email': 'jpj6652@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
