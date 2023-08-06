164ce89f6ac995c6ac5aaef448ca45c281a503a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:39 +0000

    Add package Theano-PyMC/1.0.7/Theano-PyMC-1.0.7.tar.gz

---
+++ packages/Theano-PyMC/Theano-PyMC-1.0.7.tar.gz/Theano-PyMC-1.0.7/theano/sandbox/linalg/__init__.py
@@ -0,0 +1,12 @@
+from theano.tensor.slinalg import cholesky, solve, eigvalsh
+from theano.tensor.nlinalg import (
+    matrix_inverse,
+    diag,
+    extract_diag,
+    alloc_diag,
+    det,
+    eig,
+    eigh,
+    trace,
+)
+from theano.sandbox.linalg.ops import psd, spectral_radius_bound
---
+++ packages/Theano-PyMC/Theano-PyMC-1.0.7.tar.gz/Theano-PyMC-1.0.7/theano/scalar/__init__.py
@@ -0,0 +1,3 @@
+from .basic import *
+
+from .basic_scipy import *
