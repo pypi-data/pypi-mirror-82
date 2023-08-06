10da299b9342b0da78ab3468f47010a4031af6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:16 +0000

    Add package camus-chat/0.2.dev5/camus_chat-0.2.dev5-py3-none-any.whl

---
+++ packages/camus-chat/camus_chat-0.2.dev5-py3-none-any.whl/camus/config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '420sixtyn9ne'
+    STUN_HOST = os.environ.get('STUN_HOST') or 'stun.l.google.com'
+    STUN_PORT = os.environ.get('STUN_PORT') or 19302
+    TURN_HOST = os.environ.get('TURN_HOST')
+    TURN_PORT = os.environ.get('TURN_PORT')
+    TURN_STATIC_AUTH_SECRET = os.environ.get('TURN_STATIC_AUTH_SECRET')
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_KEY_SID = os.environ.get('TWILIO_KEY_SID')
