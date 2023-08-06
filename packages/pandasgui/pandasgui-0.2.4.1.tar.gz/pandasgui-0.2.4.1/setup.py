0d57e0c9c46f0b0e0262fe0800f67b691de2dd03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:08 +0000

    Add package pandasgui/0.2.4.1/pandasgui-0.2.4.1.tar.gz

---
+++ packages/pandasgui/pandasgui-0.2.4.1.tar.gz/pandasgui-0.2.4.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pandasgui",
+    version="0.2.4.1",
+    description="A GUI for Pandas DataFrames.",
+    author="Adam Rose",
+    author_email="adam.e.rose@hotmail.com",
+    url="https://github.com/adamerose/pandasgui",
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "pandas",
+        "PyQt5>=5.15.0",  # Using QtCore.Qt.MatchRegularExpression
+        "PyQtWebEngine",
+        "plotly>=4.4.1",  # Using plotly.express.pie
+        "setuptools",
+        "appdirs",
+        "dacite",
+        "pynput",
+        "IPython"
+    ],
+)
