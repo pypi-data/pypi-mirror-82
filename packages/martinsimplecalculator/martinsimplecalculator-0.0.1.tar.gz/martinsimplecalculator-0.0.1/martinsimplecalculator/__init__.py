7774a926e146141a1997bf51adc85eeda905aa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:06 +0000

    Add package martinsimplecalculator/0.0.1/martinsimplecalculator-0.0.1.tar.gz

---
+++ packages/martinsimplecalculator/martinsimplecalculator-0.0.1.tar.gz/martinsimplecalculator-0.0.1/martinsimplecalculator/__init__.py
@@ -0,0 +1,14 @@
+def add_num(num1, num2):
+    return num1 + num2
+
+def subtract_num(num1, num2):
+    return num1 - num2
+
+def multiply_num(num1, num2):
+    return num1 * num2
+
+def divide_num(num1, num2):
+    return num1 / num2
+
+def exponent_num(num1, num2):
+    return num1 ** num2
