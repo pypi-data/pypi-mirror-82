5a8a691c4ad3f8f14716954e2434de38ef6245c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:54 +0000

    Add package simplemonads/1.0.1/simplemonads-1.0.1.tar.gz

---
+++ packages/simplemonads/simplemonads-1.0.1.tar.gz/simplemonads-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['simplemonads']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'simplemonads',
+    'version': '1.0.1',
+    'description': 'Simple Monads for Python. Use Just to end checking for None, Success to end unhandled exceptions, and Reader for dependency injection at runtime.',
+    'long_description': None,
+    'author': 'Stephen Daves',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
