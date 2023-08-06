fec56f7cd69cabd12aea6f01c4a7b1d34f708a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:21 +0000

    Add package ckan-editor-utils/0.1.0/ckan-editor-utils-0.1.0.tar.gz

---
+++ packages/ckan-editor-utils/ckan-editor-utils-0.1.0.tar.gz/ckan-editor-utils-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ckan_editor_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.15.1,<2.0.0', 'requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'ckan-editor-utils',
+    'version': '0.1.0',
+    'description': 'Utilities for editors using the CKAN API',
+    'long_description': None,
+    'author': 'Eric McCowan',
+    'author_email': 'eric.mccowan@servian.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
