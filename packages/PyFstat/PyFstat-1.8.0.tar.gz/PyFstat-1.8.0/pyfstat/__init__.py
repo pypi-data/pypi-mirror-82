6ddefc20808f2c78c60537f92f7a35add74f7772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:10 +0000

    Add package PyFstat/1.8.0/PyFstat-1.8.0.tar.gz

---
+++ packages/PyFstat/PyFstat-1.8.0.tar.gz/PyFstat-1.8.0/pyfstat/__init__.py
@@ -0,0 +1,35 @@
+from .core import (
+    BaseSearchClass,
+    ComputeFstat,
+    SearchForSignalWithJumps,
+    SemiCoherentSearch,
+    SemiCoherentGlitchSearch,
+)
+from .make_sfts import (
+    Writer,
+    BinaryModulatedWriter,
+    GlitchWriter,
+    FrequencyModulatedArtifactWriter,
+    FrequencyAmplitudeModulatedArtifactWriter,
+)
+from .mcmc_based_searches import (
+    MCMCSearch,
+    MCMCGlitchSearch,
+    MCMCSemiCoherentSearch,
+    MCMCFollowUpSearch,
+    MCMCTransientSearch,
+)
+from .grid_based_searches import (
+    GridSearch,
+    GridUniformPriorSearch,
+    GridGlitchSearch,
+    FrequencySlidingWindow,
+    DMoff_NO_SPIN,
+    SliceGridSearch,
+    TransientGridSearch,
+)
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
