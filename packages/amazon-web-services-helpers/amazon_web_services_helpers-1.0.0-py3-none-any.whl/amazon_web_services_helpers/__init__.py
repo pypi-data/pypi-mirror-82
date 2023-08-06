38daf80e2937412be92708d661470ff3f3c09f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:27 +0000

    Add package amazon-web-services-helpers/1.0.0/amazon_web_services_helpers-1.0.0-py3-none-any.whl

---
+++ packages/amazon-web-services-helpers/amazon_web_services_helpers-1.0.0-py3-none-any.whl/amazon_web_services_helpers/__init__.py
@@ -0,0 +1,8 @@
+from amazon_web_services_helpers.document_store import DocumentStore
+from amazon_web_services_helpers.s3_helper import S3Helper
+
+__all__ = (
+    '__version__',
+    'DocumentStore',
+    'S3Helper',
+)
