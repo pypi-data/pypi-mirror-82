a7167806d58d70c63e22ec86b1a4ddde0b661edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:27 +0000

    Add package chimelems1/1.0/chimelems1-1.0.tar.gz

---
+++ packages/chimelems1/chimelems1-1.0.tar.gz/chimelems1-1.0/chimelems1/chimelems1.py
@@ -0,0 +1,9 @@
+
+
+H, He, Li, Be, B, C, N, O, F, Ne, Na, Mg, Al, Si, P, S, Cl, Ar, K, Ca, Sc, Ti, V, Cr, Mn, Fe, Co, Ni, Cu, Zn, Ga, Ge, As, Se, Br, Kr, Rb, Sr, Y, Zr, Nb, Mo, Tc, Ru, Rh, Rd, Ag, Cd, In, Sn, Sb, Te, I, Xe, Cs, Ba, Hf, Ta, W, Re, Os, Ir, Pt, Au, Hg, Tl, Pb, Bi, Po, At, Rn, Fr, Ra, Rf, Db, Sg, Bh, Hs, Mt, Ds = 1, 4, 7, 9, 11, 12, 14, 16, 19, 20, 23, 24, 27, 28, 31, 32, 35, 40, 39, 40, 45, 48, 51, 52, 55, 56, 59, 59, 64, 66, 70, 73, 75, 79, 80, 84, 85, 88, 89, 91, 93, 96, 98, 101, 103, 106, 108, 112, 115, 119, 122, 128, 127, 131, 133, 137, 178, 181, 184, 186, 190, 192, 195, 197, 201, 204, 207, 209, 209, 210, 222, 223, 226, 261, 262, 266, 267, 269, 268, 271
+print(H)
+print(Fe)
+print(Au)
+print(Li)
+print(Fr)
+print(Ti)
---
+++ packages/chimelems1/chimelems1-1.0.tar.gz/chimelems1-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='chimelems1',
+      version='1.0',
+      description='Chim_elems',
+      packages=['chimelems1'],
+      author_email='ya.9637@yandex.ru',
+      zip_safe=False
+      )
+
