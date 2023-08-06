b1afb38ac2918d516d5955a5a407d6b0989aaafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:13 +0000

    Add package gillespy2/1.5.6/gillespy2-1.5.6.tar.gz

---
+++ packages/gillespy2/gillespy2-1.5.6.tar.gz/gillespy2-1.5.6/gillespy2/solvers/cpp/__init__.py
@@ -0,0 +1,6 @@
+from gillespy2.solvers.cpp.ssa_c_solver import SSACSolver
+from gillespy2.solvers.cpp.variable_ssa_c_solver import VariableSSACSolver
+from gillespy2.core import log
+
+
+__all__ = ['SSACSolver', 'VariableSSACSolver']
---
+++ packages/gillespy2/gillespy2-1.5.6.tar.gz/gillespy2-1.5.6/test/test_SBML.py
@@ -0,0 +1,33 @@
+import unittest
+import tempfile
+import os
+from gillespy2.core.model import import_SBML
+from gillespy2 import ODESolver
+
+
+class TestSBML(unittest.TestCase):
+
+    def test_sbml_conversion(self):
+        try:
+            import libsbml
+        except ImportError:
+            return
+
+        try:
+            from urllib2 import urlopen
+
+        except ImportError:
+            from urllib.request import urlopen
+
+        sbml_file = 'https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000028.2?filename=BIOMD0000000028_url.xml'
+        response = urlopen(sbml_file)
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+        tmp.write(response.read())
+        tmp.close()
+        sbml_model, errors = import_SBML(tmp.name)
+        os.remove(tmp.name)
+        sbml_results = sbml_model.run(solver=ODESolver)
+
+
+if __name__ == '__main__':
+    unittest.main()
