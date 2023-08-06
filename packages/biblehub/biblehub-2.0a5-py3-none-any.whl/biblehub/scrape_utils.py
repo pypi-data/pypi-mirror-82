b269ad107a58314931ac8cadcb75f998506f8367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:13 +0000

    Add package biblehub/2.0a5/biblehub-2.0a5-py3-none-any.whl

---
+++ packages/biblehub/biblehub-2.0a5-py3-none-any.whl/biblehub/scrape_utils.py
@@ -0,0 +1,17 @@
+import re
+
+
+def parse_str(reference: str) -> dict:
+    response = {}
+    parsed = re.search('([1-3]?[^\\s]+[A-Za-z ]+[^\\d ]) ?(\\d+)?:?(\\d+)?-?(\\d+)?', reference)
+    response['book'] = parsed.group(1).lower()
+    response['chapter'] = int(parsed.group(2))
+    response['start_verse'] = 1
+    response['end_verse'] = None
+    print(parsed.groups())
+    if parsed.group(3) is not None:
+        response['start_verse'] = int(parsed.group(3))
+    if parsed.group(4) is not None:
+        response['end_verse'] = int(parsed.group(4))
+    return response
+
