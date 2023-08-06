5b7e6c371555b1ab343add45f250047e7507c62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:05 +0000

    Add package kora/0.8.1/kora-0.8.1.tar.gz

---
+++ packages/kora/kora-0.8.1.tar.gz/kora-0.8.1/kora/install/influxdb.py
@@ -0,0 +1,18 @@
+"""
+InfluxDB is a time-series database
+"""
+import os
+
+# install server
+os.system("curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -")
+with open('/etc/apt/sources.list.d/influxdb.list', 'w') as f:
+    f.write("deb https://repos.influxdata.com/ubuntu bionic stable")
+os.system('apt update')
+os.system('apt install influxdb')
+os.system('influxd -config /etc/influxdb/influxdb.conf &')
+
+# install client
+os.system("pip install influxdb")
+import influxdb
+# default client, just call "from influxdb import db"
+influxdb.db = influxdb.InfluxDBClient(host='localhost', port=8086, database='_internal')
