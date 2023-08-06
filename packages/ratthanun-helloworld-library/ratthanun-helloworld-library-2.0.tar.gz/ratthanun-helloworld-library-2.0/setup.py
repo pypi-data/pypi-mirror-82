2680ae9aba338aca5be742b751446de590a88bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:50 +0000

    Add package ratthanun-helloworld-library/2.0/ratthanun-helloworld-library-2.0.tar.gz

---
+++ packages/ratthanun-helloworld-library/ratthanun-helloworld-library-2.0.tar.gz/ratthanun-helloworld-library-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ratthanun-helloworld-library",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Ratthanun',
+    author_email='ratthanun.p@gmail.com',
+)
