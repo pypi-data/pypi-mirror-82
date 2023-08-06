48593fe7a0cd8eaaa0f212ac3f58db7fee75e581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package queenbee-pollination/0.6.1/queenbee_pollination-0.6.1-py2.py3-none-any.whl

---
+++ packages/queenbee-pollination/queenbee_pollination-0.6.1-py2.py3-none-any.whl/queenbee_pollination/client.py
@@ -0,0 +1,38 @@
+import pollination_sdk as sdk
+
+
+class Client(object):
+    """A Pollination client designed to interact with Workflow and Simulation objects."""
+
+    def __init__(self, api_token=None, access_token=None, host='https://api.pollination.cloud'):
+        config = sdk.Configuration()
+        config.host = host
+
+        auth = sdk.UserApi(sdk.ApiClient(config))
+
+        if access_token is None:
+            api_token = sdk.Login(
+                api_token=api_token
+            )
+
+            try:
+                auth_response = auth.login(api_token)
+            except sdk.rest.ApiException as error:
+                if error.status == 403:
+                    raise ValueError("Failed to log in... </3")
+                raise error
+
+            config.access_token = auth_response.access_token
+        else:
+            config.access_token = access_token
+
+        self.config = config
+        self.auth = sdk.UserApi(sdk.ApiClient(config))
+        self.recipes = sdk.RecipesApi(sdk.ApiClient(config))
+        self.operators = sdk.OperatorsApi(sdk.ApiClient(config))
+        self.simulations = sdk.SimulationsApi(sdk.ApiClient(config))
+        self.artifacts = sdk.ArtifactsApi(sdk.ApiClient(config))
+        self.projects = sdk.ProjectsApi(sdk.ApiClient(config))
+
+    def get_account(self) -> sdk.models.UserPrivate:
+        return self.auth.get_me()
