4148f388f6939433bfd6fcbbfc9035861b3522ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:52 +0000

    Add package ExtractandPlot/0.0.2/ExtractandPlot-0.0.2.tar.gz

---
+++ packages/ExtractandPlot/ExtractandPlot-0.0.2.tar.gz/ExtractandPlot-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+    ]
+setup(
+      name='ExtractandPlot',
+      version='0.0.2',
+      description='Download file from google drive and Plot it using scatter plot',
+      long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+      url='',  
+      author='Hardik Choudhary',
+      author_email='hardikchoudhary2698@gmail.com',
+      license='MIT', 
+      classifiers=classifiers,
+      keywords='', 
+      packages=find_packages(),
+      install_requires=['Python>=3.0',
+                        'Pandas',
+                        'Matplotlib',
+                        'google_drive_downloader'
+                        ] 
+)
