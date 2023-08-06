bbb87b2dff7b4027fa486fd467130d1acf2173ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:20 +0000

    Add package linestickerdata/0.0.12/linestickerdata-0.0.12.tar.gz

---
+++ packages/linestickerdata/linestickerdata-0.0.12.tar.gz/linestickerdata-0.0.12/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='linestickerdata',
+    version='0.0.12',
+    description='repo to get line sticker data',
+    url='https://github.com/steerapi/line-sticker-data',
+    author='Surat Teerapittayanon',
+    author_email='steerapi@gmail.com',
+    license='unlicense',
+    packages=['linestickerdata'],
+    zip_safe=True
+)
