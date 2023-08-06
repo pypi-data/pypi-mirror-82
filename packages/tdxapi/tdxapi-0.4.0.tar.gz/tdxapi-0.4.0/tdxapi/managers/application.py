24a07d9ca0bffe7fb35ca4913734cf81c3daae38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package tdxapi/0.4.0/tdxapi-0.4.0.tar.gz

---
+++ packages/tdxapi/tdxapi-0.4.0.tar.gz/tdxapi-0.4.0/tdxapi/managers/application.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import attr
+
+from tdxapi.managers.bases import TdxManager, tdx_method
+from tdxapi.models.application import Application
+
+
+@attr.s
+class ApplicationManager(TdxManager):
+    __tdx_section__ = "Applications"
+
+    def get(self, app_id: int) -> Application:
+        """Gets an Application."""
+        for app in self.all():
+            if app.id == app_id:
+                return app
+
+    @tdx_method("GET", "/api/applications")
+    def all(self) -> List[Application]:
+        """Gets all the applications for an organization."""
+        return self.dispatcher.send(
+            self.all.method,
+            self.all.url,
+            rclass=Application,
+            rlist=True,
+            rpartial=True,
+        )
---
+++ packages/tdxapi/tdxapi-0.4.0.tar.gz/tdxapi-0.4.0/tdxapi/managers/ticket_type.py
@@ -0,0 +1,23 @@
+from typing import List
+
+import attr
+
+from tdxapi.managers.bases import TdxManager, tdx_method
+from tdxapi.managers.mixins import TdxAppMixin
+from tdxapi.models.ticket_type import TicketType
+
+
+@attr.s
+class TicketTypeManager(TdxManager, TdxAppMixin):
+    __tdx_section__ = "Ticket Types"
+
+    @tdx_method("GET", "/api/{appId}/tickets/types")
+    def all(self) -> List[TicketType]:
+        """Gets all active ticket types."""
+        return self.dispatcher.send(
+            self.all.method,
+            self.all.url.format(appId=self.app_id),
+            rclass=TicketType,
+            rlist=True,
+            rpartial=False,
+        )
