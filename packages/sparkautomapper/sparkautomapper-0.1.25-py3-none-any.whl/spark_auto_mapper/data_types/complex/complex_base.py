d299675f64c460fb59aadfa64ac98f43567d95a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:05 +0000

    Add package sparkautomapper/0.1.25/sparkautomapper-0.1.25-py3-none-any.whl

---
+++ packages/sparkautomapper/sparkautomapper-0.1.25-py3-none-any.whl/spark_auto_mapper/data_types/complex/complex_base.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+from pyspark.sql import Column
+from pyspark.sql.functions import struct
+
+from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
+
+
+class AutoMapperDataTypeComplexBase(AutoMapperDataTypeBase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.value: Dict[str, AutoMapperDataTypeBase] = {}
+
+    def get_column_spec(self) -> Column:
+        return struct(
+            [
+                self.get_value(value).alias(key)
+                for key, value in self.value.items()
+            ]
+        )
