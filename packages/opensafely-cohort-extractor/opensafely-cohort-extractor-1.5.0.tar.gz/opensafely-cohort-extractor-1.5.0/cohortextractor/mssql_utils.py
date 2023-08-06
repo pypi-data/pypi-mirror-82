df63150a9db17d37a02fe0710f473352c146d2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:04 +0000

    Add package opensafely-cohort-extractor/1.5.0/opensafely-cohort-extractor-1.5.0.tar.gz

---
+++ packages/opensafely-cohort-extractor/opensafely-cohort-extractor-1.5.0.tar.gz/opensafely-cohort-extractor-1.5.0/cohortextractor/mssql_utils.py
@@ -0,0 +1,113 @@
+import csv
+import re
+from urllib.parse import urlparse, unquote
+import warnings
+
+import sqlalchemy
+from sqlalchemy.engine.url import URL
+
+
+# Some drivers warn about the use of features marked "optional" in the DB-ABI
+# spec, using a standardised set of warnings. See:
+# https://www.python.org/dev/peps/pep-0249/#optional-db-api-extensions
+warnings.filterwarnings("ignore", re.escape("DB-API extension cursor.__iter__() used"))
+
+
+def mssql_connection_params_from_url(url):
+    parsed = urlparse(url)
+    if parsed.scheme != "mssql" and not parsed.scheme.startswith("mssql+"):
+        raise ValueError(f"Wrong scheme for MS-SQL URL: {url}")
+    return {
+        "host": parsed.hostname,
+        "port": parsed.port or 1433,
+        "database": parsed.path.lstrip("/"),
+        "username": unquote(parsed.username),
+        "password": unquote(parsed.password),
+    }
+
+
+def mssql_dbapi_connection_from_url(url):
+    # We avoid importing this immediately because we're using the TPPBackend
+    # code for error checking (as a hopefully temporary shortcut) and so want
+    # to be able to create a TPPBackend instance without needing a MSSQL
+    # database driver installed (which complicates local installing).
+    params = mssql_connection_params_from_url(url)
+
+    # For more background on why we use cTDS and why we support multiple
+    # database drivers see:
+    # https://github.com/opensafely/cohort-extractor/pull/286
+    try:
+        import ctds
+    except ImportError:
+        pass
+    else:
+        return _ctds_connect(ctds, params)
+
+    try:
+        import pyodbc
+    except ImportError:
+        pass
+    else:
+        return _pyodbc_connect(pyodbc, params)
+
+    raise ImportError(
+        "Unable to import database driver, tried `ctds` and `pyodbc`\n"
+        "\n"
+        "We use `ctds` in production. If you are on Linux the correct version is "
+        "specified in the `requirements.txt` file.\n"
+        "\n"
+        "Installation instructions for other platforms can be found at:\n"
+        "https://zillow.github.io/ctds/install.html"
+    )
+
+
+def _pyodbc_connect(pyodbc, params):
+    connection_str_template = (
+        "DRIVER={{ODBC Driver 17 for SQL Server}};"
+        "SERVER={host},{port};"
+        "DATABASE={database};"
+        "UID={username};"
+        "PWD={password}"
+    )
+    connection_str = connection_str_template.format(**params)
+    return pyodbc.connect(connection_str)
+
+
+def _ctds_connect(ctds, params):
+    params = params.copy()
+    params["server"] = params.pop("host")
+    params["user"] = params.pop("username")
+    # Default timeout is 5 seconds. We don't want queries to timeout at all so
+    # set to one week
+    params["timeout"] = 7 * 24 * 60 * 60
+    params["autocommit"] = True
+    return ctds.connect(**params)
+
+
+def mssql_sqlalchemy_engine_from_url(url):
+    params = mssql_connection_params_from_url(url)
+    params["drivername"] = "mssql+pyodbc"
+    params["query"] = {"driver": "ODBC Driver 17 for SQL Server"}
+    return sqlalchemy.create_engine(URL(**params))
+
+
+def dbapi_cursor_to_csv_file(cursor, filename, batch_size=None, row_callback=None):
+    """
+    Takes a DB-API comptabile cursor object with a result set and writes those
+    results to a CSV file, calling `row_callback` (if defined) on each row as
+    it does so
+    """
+    if batch_size is None:
+        batch_size = cursor.arraysize
+    if row_callback is None:
+        row_callback = lambda x: None
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([x[0] for x in cursor.description])
+        while True:
+            result_batch = cursor.fetchmany(batch_size)
+            if not result_batch:
+                break
+            for row in result_batch:
+                writer.writerow(row)
+                row_callback(row)
