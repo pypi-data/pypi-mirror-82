2b933ae5dff9904b577e8ecbba4e9f79e4fd1d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:47 +0000

    Add package us-service-config/0.0.4/us_service_config-0.0.4.tar.gz

---
+++ packages/us-service-config/us_service_config-0.0.4.tar.gz/us_service_config-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+def get_version():
+    with open('version') as version_file:
+        return version_file.readline()
+
+requirement_file = open('requirements.txt')
+requirements = [l for l in requirement_file.readlines() if not l.startswith('-')]
+requirement_file.close()
+
+setuptools.setup(
+    name="us_service_config",
+    version=get_version(),
+    author="Ukuspeed",
+    author_email="info@ukuspeed.gmail.com",
+    description="A service config for us services",
+    url="https://github.com/ukuspeed",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
