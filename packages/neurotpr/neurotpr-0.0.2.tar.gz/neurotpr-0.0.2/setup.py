bcbb576a053404d69715137bcd73b7cee14992f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:06 +0000

    Add package neurotpr/0.0.2/neurotpr-0.0.2.tar.gz

---
+++ packages/neurotpr/neurotpr-0.0.2.tar.gz/neurotpr-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neurotpr", 
+    version="0.0.2",
+    author="Jimin Wang and Yingjie Hu",
+    author_email="yhu42@buffalo.edu",
+    description="A Neuro-net ToPonym Recognition model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geoai-lab/NeuroTPR",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'tensorflow',
+          'keras',
+          'tensorflow-hub',
+          'emoji',
+          'nltk'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
