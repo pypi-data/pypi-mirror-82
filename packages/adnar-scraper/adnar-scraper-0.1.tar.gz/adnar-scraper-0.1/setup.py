cb2dfad357529631a605917e80424604c8ec6149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:41 +0000

    Add package adnar-scraper/0.1/adnar-scraper-0.1.tar.gz

---
+++ packages/adnar-scraper/adnar-scraper-0.1.tar.gz/adnar-scraper-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adnar-scraper',
+    version='0.1',
+    description='adnar-scraper',
+    author              = 'Mabus Kang',
+    author_email        = 'discoverious@naver.com',
+    url                 = 'https://github.com/discoverious/adnar-scraper',
+    download_url        = 'https://github.com/discoverious/adnar-scraper/archive/0.0.tar.gz',
+    install_requires    =  [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['AdnarScraper'],
+    python_requires     = '>=3.6',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
