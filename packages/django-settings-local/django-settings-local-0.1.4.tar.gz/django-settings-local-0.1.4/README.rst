ab84cb253261421762b5d189b47d882b2e5bdc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:42 +0000

    Add package django-settings-local/0.1.4/django-settings-local-0.1.4.tar.gz

---
+++ packages/django-settings-local/django-settings-local-0.1.4.tar.gz/django-settings-local-0.1.4/README.rst
@@ -0,0 +1,40 @@
+.. role:: python(code)
+   :language: python
+
+####################################################
+django-settings-local - Manage Django Local Settings
+####################################################
+
+|travis| |nbsp| |codecov| |nbsp| |rtd-badge| |nbsp| |pypi-version|
+
+*****
+Usage
+*****
+
+0. Installation
+===============
+
+The preferred instalation method is directly from pypi:
+
+.. code:: console
+
+   pip install -U django-settings-local
+
+
+1. Quickstart
+=============
+
+In your Django project app directory:
+
+.. code:: console
+
+   dsl
+   
+2. Quickhelp
+=============
+
+Quick help from simple console script:
+
+.. code:: console
+
+   dsl -h
---
+++ packages/django-settings-local/django-settings-local-0.1.4.tar.gz/django-settings-local-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+  name = 'django-settings-local',
+  packages = ['django_settings_local'],
+  version = '0.1.4',
+  description = 'Manage Django Local Settings',
+  author = 'Fasih Ahmad Fakhri',
+  author_email = 'fasih@email.com',
+  url = 'https://github.com/fasih/django-settings-local',
+  download_url='https://github.com/fasih/django-settings-local/archive/0.1.4.tar.gz',
+  keywords = 'django local settings',
+  entry_points={
+    'console_scripts': [
+      'dsl = django_settings_local.__main__:main',
+    ],
+  },
+  license='GPL-3.0',
+  classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    python_requires='>=3.6',
+)
