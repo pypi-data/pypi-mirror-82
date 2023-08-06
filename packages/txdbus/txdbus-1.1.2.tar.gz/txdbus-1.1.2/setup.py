c1a43f16aff18c875ed5403973fd95506ce86d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:44 +0000

    Add package txdbus/1.1.2/txdbus-1.1.2.tar.gz

---
+++ packages/txdbus/txdbus-1.1.2.tar.gz/txdbus-1.1.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+VERSION = '1.1.2'
+DESCRIPTION = (
+    'A native Python implementation of the DBus protocol for Twisted '
+    'applications.'
+)
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='txdbus',
+    version=VERSION,
+    description=DESCRIPTION,
+    license='MIT',
+    long_description=open('README.rst').read(),
+    url='https://github.com/cocagne/txdbus',
+    author='Tom Cocagne',
+    author_email='tom.cocagne@gmail.com',
+    install_requires=['twisted>=10.1', 'six'],
+    provides=['txdbus'],
+    packages=['txdbus'],
+    keywords=['dbus', 'twisted'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking',
+    ],
+)
