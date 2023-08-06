2253333eb4df2a505cb877b53c3b7d3cd67156c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:49 +0000

    Add package dingpy/1.0.6/dingpy-1.0.6.tar.gz

---
+++ packages/dingpy/dingpy-1.0.6.tar.gz/dingpy-1.0.6/dingpy/__init__.py
@@ -0,0 +1,8 @@
+"""
+DingPy: An alarm package for Python.
+================================================
+Documentation is available in the docstrings and
+in the README.
+"""
+
+from dingpy.Alarm import Alarm, ding, list_alarms, upload_alarm, delete_alarm
