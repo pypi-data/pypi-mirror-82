b61ce1aac2c57226ce73a8efb06e8a3a1e722ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:06 +0000

    Add package libertyrpa/0.0.1/libertyrpa-0.0.1.tar.gz

---
+++ packages/libertyrpa/libertyrpa-0.0.1.tar.gz/libertyrpa-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libertyrpa',
+    version='0.0.1',
+    url='https://www.netcall.com',
+    author='Netcall',
+    author_email='info@netcall.com',
+    description='Liberty RPA',
+    packages=find_packages(),    
+)
