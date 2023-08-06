49f56e942b1881e9a5c627d7f09654e77a9f1f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:32 +0000

    Add package lethai/0.0.3/lethai-0.0.3.tar.gz

---
+++ packages/lethai/lethai-0.0.3.tar.gz/lethai-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lethai']
+
+package_data = \
+{'': ['*'], 'lethai': ['public/*']}
+
+install_requires = \
+['requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'lethai',
+    'version': '0.0.3',
+    'description': 'Lethical AI python library.',
+    'long_description': None,
+    'author': 'Amanl04',
+    'author_email': 'amanlodha423@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
