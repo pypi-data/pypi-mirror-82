57ca777d380a30c28e4644d1f8e8559bc41f0412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:09 +0000

    Add package djangoldp/1.1.8/djangoldp-1.1.8.tar.gz

---
+++ packages/djangoldp/djangoldp-1.1.8.tar.gz/djangoldp-1.1.8/djangoldp/admin.py
@@ -0,0 +1,33 @@
+from guardian.admin import GuardedModelAdmin
+from django.contrib.auth.admin import UserAdmin
+
+
+class DjangoLDPAdmin(GuardedModelAdmin):
+    '''
+    An admin model representing a federated object. Inherits from GuardedModelAdmin to provide Django-Guardian
+    object-level permissions
+    '''
+    pass
+
+
+class DjangoLDPUserAdmin(UserAdmin, GuardedModelAdmin):
+    '''An extension of UserAdmin providing the functionality of DjangoLDPAdmin'''
+
+    list_display = ('urlid', 'email', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_staff')
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        
+        federated_fields = ['urlid', 'allow_create_backlink']
+        if self.exclude is not None:
+            federated_fields = list(set(federated_fields) - set(self.exclude))
+
+        for fieldset in fieldsets:
+            federated_fields = list(set(federated_fields) - set(fieldset[1]['fields']))
+
+        if len(federated_fields) == 0:
+            return fieldsets
+
+        fieldsets = [('Federation', {'fields': federated_fields})] + list(fieldsets)
+
+        return fieldsets
