de18acadb4b963cd1361016562e8da89b4cfb814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:11 +0000

    Add package django-mp-barcode/0.4/django-mp-barcode-0.4.tar.gz

---
+++ packages/django-mp-barcode/django-mp-barcode-0.4.tar.gz/django-mp-barcode-0.4/product_barcode/views.py
@@ -0,0 +1,25 @@
+
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.admin.views.decorators import staff_member_required
+
+from product_barcode.models import BarCode
+
+
+@staff_member_required
+def get_code(request, code):
+
+    img = BarCode.get_image(code)
+
+    response = HttpResponse(content_type='image/png')
+
+    img.save(response, 'png')
+
+    return response
+
+
+@staff_member_required
+def print_codes(request):
+    codes_str = request.GET.get('codes', '')
+    codes = codes_str.split(',') if codes_str else ''
+    return render(request, 'barcode/print.html', {'codes': codes})
