90f04b04c40f71b593b97d00fe3f053a6302b53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:10 +0000

    Add package donphan/2.6.0/donphan-2.6.0-py3-none-any.whl

---
+++ packages/donphan/donphan-2.6.0-py3-none-any.whl/donphan/connection.py
@@ -0,0 +1,53 @@
+import json
+
+from asyncpg import create_pool as apg_create_pool, Connection  # noqa: F401
+from asyncpg.pool import Pool
+
+
+_pool: Pool = None  # type: ignore
+
+
+async def create_pool(dsn: str, **kwargs) -> Pool:
+    """Creates the database connection pool."""
+    global _pool
+
+    def _encode_json(value):
+        return json.dumps(value)
+
+    def _decode_json(value):
+        return json.loads(value)
+
+    async def init(connection: Connection):
+        await connection.set_type_codec('json', schema='pg_catalog', encoder=_encode_json, decoder=_decode_json, format='text')
+        await connection.set_type_codec('jsonb', schema='pg_catalog', encoder=_encode_json, decoder=_decode_json, format='text')
+
+    _pool = p = await apg_create_pool(dsn, init=init, **kwargs)
+    return p
+
+
+class MaybeAcquire:
+    """Async helper for acquiring a connection to the database.
+
+    Args:
+        connection (asyncpg.Connection, optional): A database connection to use
+                If none is supplied a connection will be acquired from the pool.
+    Kwargs:
+        pool (asyncpg.pool.Pool, optional): A connection pool to use.
+            If none is supplied the default pool will be used.
+    """
+
+    def __init__(self, connection: Connection = None, *, pool=None):
+        self.connection = connection
+        self.pool = pool or _pool
+        self._cleanup = False
+
+    async def __aenter__(self) -> Connection:
+        if self.connection is None:
+            self._cleanup = True
+            self._connection = c = await self.pool.acquire()
+            return c
+        return self.connection
+
+    async def __aexit__(self, *args):
+        if self._cleanup:
+            await self.pool.release(self._connection)
