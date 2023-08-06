fa5782e8aa7c0f097849f6987c48cdf6297af244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package utz/0.0.14/utz-0.0.14.tar.gz

---
+++ packages/utz/utz-0.0.14.tar.gz/utz-0.0.14/setup.py
@@ -0,0 +1,18 @@
+from utz.setup import setup
+
+setup(
+    install_requires=[
+        "GitPython",
+        "joblib",
+        "pandas",
+        "pyyaml",
+    ],
+    extras_require={
+        'pdf': [
+            'reportlab==3.5.42',
+            'pyPDF2==1.26.0',
+        ],
+    },
+    scripts=['utz.sh',],
+    url="https://github.com/runsascoded/utz",
+)
