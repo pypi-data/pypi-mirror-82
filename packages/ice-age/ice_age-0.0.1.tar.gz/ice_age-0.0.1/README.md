c8b1e8ee50b918eb339f3bfb6aa8a1bc5fac553c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:46 +0000

    Add package ice-age/0.0.1/ice_age-0.0.1.tar.gz

---
+++ packages/ice-age/ice_age-0.0.1.tar.gz/ice_age-0.0.1/README.md
@@ -0,0 +1,19 @@
+ice_age
+======
+Freeze your ENVironment during testing. Just in case you need to change ENV variables during tests, this library ensures that everything is reset before the next test runs.
+
+
+### Install
+```pip install ice_age```
+
+
+### Usage
+```python
+import ice_age
+
+```
+
+----
+[![Build Status](https://travis-ci.org/dpep/py_ice_age.svg?branch=master)](https://travis-ci.org/dpep/py_rekey)
+[![Coverage Status](https://coveralls.io/repos/github/dpep/py_ice_age/badge.svg?branch=master)](https://coveralls.io/github/dpep/py_ice_age?branch=master)
+[![installs](https://img.shields.io/pypi/dm/ice_age.svg?label=installs)](https://pypi.org/project/ice_age)
---
+++ packages/ice-age/ice_age-0.0.1.tar.gz/ice_age-0.0.1/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+    IceAge - freeze your ENVironment
+
+    Copyright (c) 2020 Daniel Pepper
+
+    The MIT License (MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
+
+__author__ = 'dpepper'
+__version__ = '0.0.1'
+
+import setuptools
+import unittest
+
+
+def discover_tests():
+    return unittest.TestLoader().discover(
+        'tests',
+        pattern='*Test.py',
+    )
+
+
+if __name__ == '__main__':
+
+    setuptools.setup(
+        name='ice_age',
+        version=__version__,
+        url='https://github.com/dpep/ice_age_py',
+        license='MIT',
+        author='Daniel Pepper',
+        description='freeze your ENVironment',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        platforms='any',
+
+        packages=[
+            'ice_age',
+        ],
+
+        test_suite = 'setup.discover_tests',
+
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Topic :: Utilities',
+        ],
+    )
