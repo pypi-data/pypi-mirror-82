938e2089468a8c753c16033b6249b0c5909ded62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:13 +0000

    Add package sensitivity/0.2.4/sensitivity-0.2.4-py3-none-any.whl

---
+++ packages/sensitivity/sensitivity-0.2.4-py3-none-any.whl/sensitivity/__init__.py
@@ -0,0 +1,5 @@
+"""
+Python sensitivity analysis - run models with varying inputs to produce 
+visualizations including gradient DataFrames and hex-bin plots
+"""
+from sensitivity.main import SensitivityAnalyzer
