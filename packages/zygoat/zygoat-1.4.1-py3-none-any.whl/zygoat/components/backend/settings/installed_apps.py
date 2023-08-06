2b04053240c8643e6ba7c4bc9849af2ae78dbdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:38 +0000

    Add package zygoat/1.4.1/zygoat-1.4.1-py3-none-any.whl

---
+++ packages/zygoat/zygoat-1.4.1-py3-none-any.whl/zygoat/components/backend/settings/installed_apps.py
@@ -0,0 +1,31 @@
+import logging
+
+from zygoat.components import SettingsComponent
+
+log = logging.getLogger()
+
+
+class InstalledApps(SettingsComponent):
+    def create(self):
+        red = self.parse()
+        apps_list = red.find("name", value="INSTALLED_APPS").parent.value
+
+        log.info("Adding DRF to installed apps")
+        apps_list.append("'rest_framework'")
+
+        log.info("Adding backend app to installed apps")
+        apps_list.append("'zygoat_django'")
+
+        log.info("Dumping installed apps node")
+        self.dump(red)
+
+    @property
+    def installed(self):
+        red = self.parse()
+        return (
+            "zygoat_django"
+            in red.find("name", value="INSTALLED_APPS").parent.value.to_python()
+        )
+
+
+installed_apps = InstalledApps()
