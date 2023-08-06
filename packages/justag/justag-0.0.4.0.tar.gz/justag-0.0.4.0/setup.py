1c98d768d09a68d9adc6ccf00804b9431740fd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:10 +0000

    Add package justag/0.0.4.0/justag-0.0.4.0.tar.gz

---
+++ packages/justag/justag-0.0.4.0.tar.gz/justag-0.0.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='justag',
+    version='0.0.4.0',
+    description='Generating JTAG debug hardware from Markdown files.',
+    url='https://github.com/StanfordVLSI/JusTAG',
+    author='Stanford University',
+    packages=['justag'],
+    entry_points = {
+        'console_scripts': [
+            'justag=justag.JusTAG:main',
+            'justag_dir=justag.JusTAG:jtag_directory'
+        ]
+    },
+    install_requires=[
+        'mistune',
+        'pandas',
+        'lxml'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
