79c7ef43543371e724a12c89a7dd6c7fd9e11a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:42 +0000

    Add package solum-dashboard/5.0.0/solum-dashboard-5.0.0.tar.gz

---
+++ packages/solum-dashboard/solum-dashboard-5.0.0.tar.gz/solum-dashboard-5.0.0/HACKING.rst
@@ -0,0 +1,4 @@
+Solum Style Commandments
+========================
+
+Read the OpenStack Style Commandments https://docs.openstack.org/hacking/latest/
---
+++ packages/solum-dashboard/solum-dashboard-5.0.0.tar.gz/solum-dashboard-5.0.0/solumdashboard/applications/urls.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2014 Rackspace Hosting.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.conf.urls import url
+
+import solumdashboard.applications.views as views
+
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='applications'),
+    url(r'^create$', views.CreateView.as_view(), name='create'),
+    url(r'^launch/(?P<application_id>[^/]+)$', views.LaunchView.as_view(),
+        name='launch'),
+    url(r'^detail/(?P<application_id>[^/]+)$',
+        views.DetailView.as_view(), name='detail'),
+    url(r'^scale/(?P<application_id>[^/]+)$', views.ScaleView.as_view(),
+        name='scale'),
+    url(r'^update/(?P<application_id>[^/]+)$', views.UpdateView.as_view(),
+        name='update')
+]
