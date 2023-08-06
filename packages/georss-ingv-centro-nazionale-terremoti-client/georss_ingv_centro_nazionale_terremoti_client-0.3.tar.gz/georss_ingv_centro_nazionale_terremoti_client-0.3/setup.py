7ae955d218025dfe61307f027f46ee35b76db38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:49 +0000

    Add package georss-ingv-centro-nazionale-terremoti-client/0.3/georss_ingv_centro_nazionale_terremoti_client-0.3.tar.gz

---
+++ packages/georss-ingv-centro-nazionale-terremoti-client/georss_ingv_centro_nazionale_terremoti_client-0.3.tar.gz/georss_ingv_centro_nazionale_terremoti_client-0.3/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup, find_packages
+
+NAME = "georss_ingv_centro_nazionale_terremoti_client"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = "A GeoRSS client library for the INGV Centro Nazionale Terremoti (Earthquakes) feed."
+URL = "https://github.com/exxamalte/python-georss-ingv-centro-nazionale-terremoti-client"
+
+REQUIRES = [
+    'georss_client>=0.10',
+]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+VERSION = {}
+with open(os.path.join(HERE, NAME, "__version__.py")) as f:
+    exec(f.read(), VERSION)  # pylint: disable=exec-used
+
+setup(
+    name=NAME,
+    version=VERSION["__version__"],
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=find_packages(exclude=("tests*",)),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
