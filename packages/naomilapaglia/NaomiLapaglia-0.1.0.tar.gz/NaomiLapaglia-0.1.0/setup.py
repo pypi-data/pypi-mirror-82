a7ae3290e8f6f86850e1127f8e1efdc8de72bd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package naomilapaglia/0.1.0/NaomiLapaglia-0.1.0.tar.gz

---
+++ packages/naomilapaglia/NaomiLapaglia-0.1.0.tar.gz/NaomiLapaglia-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['naomilapaglia']
+
+package_data = \
+{'': ['*'], 'naomilapaglia': ['static/images/*', 'templates/*']}
+
+entry_points = \
+{'console_scripts': ['customize = naomilapaglia.overrides:admin_override']}
+
+setup_kwargs = {
+    'name': 'naomilapaglia',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'drazum',
+    'author_email': 'domagoj.razum1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
