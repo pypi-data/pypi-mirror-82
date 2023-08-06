8c0fa7564279b86a3c689bd349468bcfdac43f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:23 +0000

    Add package odbc-niceties/0.1.1/odbc-niceties-0.1.1.tar.gz

---
+++ packages/odbc-niceties/odbc-niceties-0.1.1.tar.gz/odbc-niceties-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['odbc_niceties']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cytoolz', 'pyodbc', 'tabulate']
+
+setup_kwargs = {
+    'name': 'odbc-niceties',
+    'version': '0.1.1',
+    'description': 'Some ODBC nice things.',
+    'long_description': None,
+    'author': 'joefromct',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
