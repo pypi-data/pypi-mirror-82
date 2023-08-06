e49ca8221dbdd1555cf61f836721a85b5e27626d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package qumranica/0.7.0/qumranica-0.7.0.tar.gz

---
+++ packages/qumranica/qumranica-0.7.0.tar.gz/qumranica-0.7.0/qumranica/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from qumranica.api.artefact_api import ArtefactApi
+from qumranica.api.catalogue_api import CatalogueApi
+from qumranica.api.edition_api import EditionApi
+from qumranica.api.imaged_object_api import ImagedObjectApi
+from qumranica.api.roi_api import RoiApi
+from qumranica.api.sign_interpretation_api import SignInterpretationApi
+from qumranica.api.text_api import TextApi
+from qumranica.api.user_api import UserApi
+from qumranica.api.util_api import UtilApi
