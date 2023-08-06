0476102657f4f6fe19577a0a9c255b0ac1222ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:45 +0000

    Add package testproject-python-sdk/0.63.16/testproject-python-sdk-0.63.16.tar.gz

---
+++ packages/testproject-python-sdk/testproject-python-sdk-0.63.16.tar.gz/testproject-python-sdk-0.63.16/src/testproject/sdk/drivers/webdriver/__init__.py
@@ -0,0 +1,8 @@
+from .chrome import Chrome
+from .firefox import Firefox
+from .edge import Edge
+from .safari import Safari
+from .ie import Ie
+from .remote import Remote
+
+__all__ = ["Chrome", "Firefox", "Edge", "Safari", "Ie", "Remote"]
