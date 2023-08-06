6a13c869a97f370e63f963a6746ac0df105cb010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:14 +0000

    Add package opendp-smartnoise-core/0.0.1/opendp-smartnoise-core-0.0.1.tar.gz

---
+++ packages/opendp-smartnoise-core/opendp-smartnoise-core-0.0.1.tar.gz/opendp-smartnoise-core-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#from setuptools import setup
+#import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opendp-smartnoise-core",
+    version="0.0.1",
+    author="OpenDP",
+    author_email="contact@opendp.io",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/opendifferentialprivacy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
