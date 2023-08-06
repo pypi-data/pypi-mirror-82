b1ef09074215825a0409dcb802df4b0e3d76d595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package binaryai/0.2.7/binaryai-0.2.7.tar.gz

---
+++ packages/binaryai/binaryai-0.2.7.tar.gz/binaryai-0.2.7/README.md
@@ -0,0 +1,38 @@
+# BinaryAI Python SDK
+
+![ida](https://img.shields.io/badge/IDA->%3D7.3-brightgreen.svg)
+![PUBLISH](https://github.com/binaryai/sdk/workflows/PUBLISH/badge.svg)
+![Download](https://img.shields.io/pypi/dm/binaryai)
+[![Gitter](https://badges.gitter.im/binaryai/community.svg)](https://gitter.im/binaryai/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
+## Introduction
+
+BinaryAI is a Neural Search Engine for binaries developed by Tencent Security KEEN Lab, aiming to help security researchers match the binary functions with the most possible source codes.
+
+BinaryAI provides three ways to access core APIs of BinaryAI Search Engine framework.
+
+1. Python SDK
+
+   BinaryAI provides users with the ability to write custom tools to analyze binaries. Please refer to [BinaryAI SDK Reference](https://binaryai.readthedocs.io/en/latest/binaryai.html#)
+
+2. IDA plugin
+
+   BinaryAI provides an IDA plugin to assist reverse engineering analysis with the IDA Pro user interface. The IDA Pro plugin enables users to apply BinaryAI retrieval results from source codes of millions of functions in the cloud to the file loaded in IDA Pro with a few clicks.
+
+3. Command line tool
+
+   The command line tool now facilitates users with easy access to manage their own private function sets,  but will be broadened to support more features in the future. 
+
+Please see the [Documentation]( https://binaryai.readthedocs.io/ ) for more details.
+
+## Installation
+
+```shell
+pip install --upgrade binaryai
+# then you can add the binaryai plugin into $IDAUSR
+binaryai install_ida_plugin
+```
+
+## Token Registration
+
+A token is all you need to join BinaryAI Community, please [apply for it here](https://binaryai.tencent.com/apply-token).
---
+++ packages/binaryai/binaryai-0.2.7.tar.gz/binaryai-0.2.7/binaryai/function.py
@@ -0,0 +1,137 @@
+from .graphql.function import q_create_function, q_query_function, q_create_function_set
+from .graphql.function import q_query_function_set, q_search_func_similarity
+from .client import Client
+from .error import BinaryAIException
+
+
+def upload_function(
+        client,
+        name,
+        feature,
+        source_code=None,
+        source_file=None,
+        source_line=None,
+        language=None,
+        funcset_id=None
+):
+    '''
+    upload function to BinaryAI server
+
+    Args:
+        client(binaryai.client.Client): Client instance
+        name(string): name of the function
+        feature(string): Feature of the function. Genertaed by feature extraction library. Encoding in base64.
+        source_code(string): Source code of the function
+        source_file(string): Source file of the function
+        source_line(int): line number of the function
+        language(string): Programming language of the function
+        funcset_id(string): If functionSetID specified, it would be added into that set when adding function;
+                            if not, it would not be added into any set
+
+    Returns:
+        * **id** (string) -- id of this function
+    '''
+    if not isinstance(client, Client):
+        raise BinaryAIException("SDK_ERROR", "Invalid client argument", None, None)
+    var = {
+        'name': name,
+        'feature': feature,
+        'sourceCode': source_code,
+        'sourceFile': source_file,
+        'sourceLine': source_line,
+        'language': language,
+        'functionSetId': funcset_id
+    }
+    r = client.execute(q_create_function, var)
+    return r['createFunction']['function']['id']
+
+
+def query_function(client, function_id):
+    '''
+    get function information by id
+
+    Args:
+        client(binaryai.client.Client): Client instance
+        function_id(string): ID of the function
+
+    Returns:
+        * **function** (dict) -- Function's information
+    '''
+    if not isinstance(client, Client):
+        raise BinaryAIException("SDK_ERROR", "Invalid client argument", None, None)
+    var = {
+        'funcId': function_id
+    }
+    r = client.execute(q_query_function, var)
+    if r['function'] and function_id != r['function']['id']:
+        raise BinaryAIException("SDK_ERROR", "Response function id not equal to the function_id", r, None)
+    return r['function']
+
+
+def create_function_set(client, function_ids=None):
+    '''
+    Create a new function set and add functions if needed
+
+    Args:
+        client(binaryai.client.Client): Client instance
+        function_ids(list): Functions to be inserted into the new function set.
+                            Can be null so no functions will be added into the set.
+
+    Returns:
+        * **id** (string) -- id of the function set
+    '''
+    if not isinstance(client, Client):
+        raise BinaryAIException("SDK_ERROR", "Invalid client argument", None, None)
+    var = {
+        'functionIds': function_ids
+    }
+    r = client.execute(q_create_function_set, var)
+    return r['createFunctionSet']['functionSet']['id']
+
+
+def query_function_set(client, funcset_id):
+    '''
+    get function set information by id
+
+    Args:
+        client(binaryai.client.Client): Client instance
+        funcset_id(string): id of the function set
+
+    Returns:
+        * **functionSet** (dict) -- functionSet's information
+    '''
+    if not isinstance(client, Client):
+        raise BinaryAIException("SDK_ERROR", "Invalid client argument", None, None)
+    var = {
+        'funcSetId': funcset_id
+    }
+    r = client.execute(q_query_function_set, var)
+    if 'functionSet' not in r or r['functionSet'] is None:
+        raise BinaryAIException("SDK_ERROR", "Invalid function set id")
+    if funcset_id != r['functionSet']['id']:
+        raise BinaryAIException("SDK_ERROR", "Response function set id not equal to the funcset_id", r, None)
+    return r['functionSet']
+
+
+def search_sim_funcs(client, function_id, funcset_ids=None, topk=1):
+    '''
+    search top similar functions of the function
+
+    Args:
+        client(binaryai.client.Client): Client instance
+        function_id(string): id of the function
+        funcset_ids(list): ids of the function set to be compared, None means BinaryAI official sets.
+        topk(int): return first topk results, default value is 1.
+
+    Returns:
+        * **similarity** (list): list of the top similarity functions
+    '''
+    if not isinstance(client, Client):
+        raise BinaryAIException("SDK_ERROR", "Invalid client argument", None, None)
+    var = {
+        'funcId': function_id,
+        'setId': funcset_ids,
+        'topk': topk
+    }
+    r = client.execute(q_search_func_similarity, var)
+    return r['function']['similarity']
