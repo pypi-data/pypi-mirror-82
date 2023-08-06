13a78f4394ca0277b8eca94e657a77f2055e9705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:20 +0000

    Add package discordhelp/1.0.4/discordhelp-1.0.4.tar.gz

---
+++ packages/discordhelp/discordhelp-1.0.4.tar.gz/discordhelp-1.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discordhelp",
+    version="1.0.4",
+    author="Ashvin Ranjan",
+    author_email="me@ash.vin",
+    description="A library to help with discord.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ashvin-Ranjan/Discord-Helper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'discord.py'
+    ],
+    python_requires='>=3.6',
+)
