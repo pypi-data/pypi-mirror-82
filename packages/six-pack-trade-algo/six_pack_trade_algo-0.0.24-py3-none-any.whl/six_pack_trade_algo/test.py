30658042ffce7c0dc5c3ea041a6efd5764219998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:42 +0000

    Add package six-pack-trade-algo/0.0.24/six_pack_trade_algo-0.0.24-py3-none-any.whl

---
+++ packages/six-pack-trade-algo/six_pack_trade_algo-0.0.24-py3-none-any.whl/six_pack_trade_algo/test.py
@@ -0,0 +1,43 @@
+# import pandas as pd
+# from ta.utils import dropna
+# from datetime import datetime, timedelta
+# import ta
+# import time
+# import argparse
+# from multiprocessing import Process, Manager
+# from openpyxl import load_workbook
+
+
+# from AlgoTrader.algos.AlgoRsi import AlgoRsi as Algo
+# from AlgoTrader.util.AlgoLogger import log
+# from algotrader.util import DataSource
+
+print("TEST 0")
+
+# from algos import *
+# from util import *
+
+
+# import six_pack_trade_algo
+
+print("TEST .5")
+# from six_pack_trade_algo.algos.AlgoRsi import AlgoRsi as Algo
+
+# import six_pack_trade_algo.util
+
+# from six_pack_trade_algo import log
+
+print("TEST 1")
+
+
+import os
+os.mkdir("$HOME/six_pack_cache/algo_mem")
+
+
+# log = AlgoLogger()
+log.info("test")
+log.output()
+print("TEST 2")
+
+# print(api_paper.get_last_trade("BA"))
+# print(float(api_paper.get_last_trade("BA").price))
