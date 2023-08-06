0c3a92bcb6299fc8c35b0ef0aa7976b300d2a99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:28 +0000

    Add package bentoml/0.9.2/BentoML-0.9.2-py3-none-any.whl

---
+++ packages/bentoml/BentoML-0.9.2-py3-none-any.whl/bentoml/yatai/deployment/utils.py
@@ -0,0 +1,129 @@
+# Copyright 2019 Atalaya Tech, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import re
+import subprocess
+
+import boto3
+from botocore.exceptions import ClientError
+
+from bentoml.exceptions import (
+    BentoMLException,
+    MissingDependencyException,
+    InvalidArgument,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def process_docker_api_line(payload):
+    """ Process the output from API stream, throw an Exception if there is an error """
+    # Sometimes Docker sends to "{}\n" blocks together...
+    errors = []
+    for segment in payload.decode("utf-8").strip().split("\n"):
+        line = segment.strip()
+        if line:
+            try:
+                line_payload = json.loads(line)
+            except ValueError as e:
+                logger.warning("Could not decipher payload from Docker API: %s", str(e))
+            if line_payload:
+                if "errorDetail" in line_payload:
+                    error = line_payload["errorDetail"]
+                    error_msg = 'Error running docker command: {}: {}'.format(
+                        error["code"], error['message']
+                    )
+                    logger.error(error_msg)
+                    errors.append(error_msg)
+                elif "stream" in line_payload:
+                    logger.info(line_payload['stream'])
+
+    if errors:
+        error_msg = ";".join(errors)
+        raise BentoMLException("Error running docker command: {}".format(error_msg))
+
+
+def ensure_docker_available_or_raise():
+    try:
+        subprocess.check_output(['docker', 'info'])
+    except subprocess.CalledProcessError as error:
+        raise BentoMLException(
+            'Error executing docker command: {}'.format(error.output.decode())
+        )
+    except FileNotFoundError:
+        raise MissingDependencyException(
+            'Docker is required for this deployment. Please visit '
+            'www.docker.com for instructions'
+        )
+
+
+def raise_if_api_names_not_found_in_bento_service_metadata(metadata, api_names):
+    all_api_names = [api.name for api in metadata.apis]
+
+    if not set(api_names).issubset(all_api_names):
+        raise InvalidArgument(
+            "Expect api names {api_names} to be "
+            "subset of {all_api_names}".format(
+                api_names=api_names, all_api_names=all_api_names
+            )
+        )
+
+
+def generate_aws_compatible_string(*items, max_length=63):
+    """
+    Generate a AWS resource name that is composed from list of string items. This
+    function replaces all invalid characters in the given items into '-', and allow user
+    to specify the max_length for each part separately by passing the item and its max
+    length in a tuple, e.g.:
+
+    >> generate_aws_compatible_string("abc", "def")
+    >> 'abc-def'  # concatenate multiple parts
+
+    >> generate_aws_compatible_string("abc_def")
+    >> 'abc-def'  # replace invalid chars to '-'
+
+    >> generate_aws_compatible_string(("ab", 1), ("bcd", 2), max_length=4)
+    >> 'a-bc'  # trim based on max_length of each part
+    """
+    trimmed_items = [
+        item[0][: item[1]] if type(item) == tuple else item for item in items
+    ]
+    items = [item[0] if type(item) == tuple else item for item in items]
+
+    for i in range(len(trimmed_items)):
+        if len('-'.join(items)) <= max_length:
+            break
+        else:
+            items[i] = trimmed_items[i]
+
+    name = '-'.join(items)
+    if len(name) > max_length:
+        raise BentoMLException(
+            'AWS resource name {} exceeds maximum length of {}'.format(name, max_length)
+        )
+    invalid_chars = re.compile("[^a-zA-Z0-9-]|_")
+    name = re.sub(invalid_chars, "-", name)
+    return name
+
+
+def get_default_aws_region():
+    try:
+        aws_session = boto3.session.Session()
+        return aws_session.region_name
+    except ClientError as e:
+        # We will do nothing, if there isn't a default region
+        logger.error('Encounter error when getting default region for AWS: %s', str(e))
+        return ''
---
+++ packages/bentoml/BentoML-0.9.2-py3-none-any.whl/bentoml/yatai/web/dist/client/precache-manifest.361110784070059b37e46b4c5a8a3bba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc3a3da99950f1108b6089fc61a8c42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9ffb52d3e16a7d69bc1",
+    "url": "/static/css/2.f079c8ee.chunk.css"
+  },
+  {
+    "revision": "67fa25cf6070e73bd79c",
+    "url": "/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "d9ffb52d3e16a7d69bc1",
+    "url": "/static/js/2.0a95bfd0.chunk.js"
+  },
+  {
+    "revision": "08c255e5272a7d1d7642f1ee410793de",
+    "url": "/static/js/2.0a95bfd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fa25cf6070e73bd79c",
+    "url": "/static/js/main.d50d590a.chunk.js"
+  },
+  {
+    "revision": "41c06a2d8a10d8c9d301",
+    "url": "/static/js/runtime-main.05c49fbe.js"
+  },
+  {
+    "revision": "21d67caf31c9a92087a54e4754d15b97",
+    "url": "/static/media/bentoml-logo.21d67caf.png"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
