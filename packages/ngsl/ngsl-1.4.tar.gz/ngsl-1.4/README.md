f12c9ad54257ee8129291401af2ff8b20f9abf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:52 +0000

    Add package ngsl/1.4/ngsl-1.4.tar.gz

---
+++ packages/ngsl/ngsl-1.4.tar.gz/ngsl-1.4/README.md
@@ -0,0 +1,18 @@
+# How to use
+
+```bash
+pip install ngsl
+```
+
+```python
+import ngsl
+
+# return True
+ngsl.include('smile')
+
+# return True
+ngsl.include('smiles')
+
+# return "smile"
+ngsl.get_infinitiv('smiles')
+```
---
+++ packages/ngsl/ngsl-1.4.tar.gz/ngsl-1.4/tests/test_ngsl.py
@@ -0,0 +1,36 @@
+import ngsl
+from ngsl.dictionary import DICTIONARY
+
+
+def test_include():
+    assert ngsl.include('smile')
+    assert ngsl.include('smiles')
+
+
+def test_get_infinitiv():
+    assert ngsl.get_infinitiv('smiles') == 'smile'
+
+
+def test_get_infinitiv_list():
+    assert ngsl.get_infinitiv_list(['smiles', 'quarterback']) == ['smile']
+
+
+def test_all_infinitiv():
+    assert ngsl.all_infinitiv() == DICTIONARY.keys()
+
+
+def test_get_rank():
+    assert ngsl.get_rank("and") == 3
+
+
+def test_classify():
+    words = ["smiles", "and", "the", "snapback", "1st"]
+    result = ngsl.classify(words=words)
+    assert result.ngsl_words == ["the", "and", "smile"]
+    assert result.not_ngsl_words == ["snapback"]
+
+
+def test_has_number():
+    assert ngsl._has_number('1st')
+    assert not ngsl._has_number('first')
+    assert ngsl._has_number('catch22')
