bcf72bbf270a80a32b46c5477de7cc6e926e403a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:43 +0000

    Add package linode-api4/3.0.1/linode_api4-3.0.1-py3-none-any.whl

---
+++ packages/linode-api4/linode_api4-3.0.1-py3-none-any.whl/linode_api4/objects/region.py
@@ -0,0 +1,10 @@
+from linode_api4.objects import Base, Property
+
+
+class Region(Base):
+    api_endpoint = "/regions/{id}"
+    properties = {
+        'id': Property(identifier=True),
+        'country': Property(filterable=True),
+        'capabilities': Property(),
+    }
