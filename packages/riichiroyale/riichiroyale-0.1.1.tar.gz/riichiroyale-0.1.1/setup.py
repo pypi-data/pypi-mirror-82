22c2f9d95310dcffcf4a5ac232c8bc5a48b8d0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:49 +0000

    Add package riichiroyale/0.1.1/riichiroyale-0.1.1.tar.gz

---
+++ packages/riichiroyale/riichiroyale-0.1.1.tar.gz/riichiroyale-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="riichiroyale",
+    version="0.1.1",
+    author="Christopher Miller",
+    author_email="cmiller548@gmail.com",
+    description="Singleplayer Riichi Mahjong Game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HartleyAHartley/RiichiRoyale",
+    packages=['libmahjong', 'riichiroyale', 'riichiroyale.audio', 'riichiroyale.sprites', 'riichiroyale.utils', 'riichiroyale.test', 'riichiroyale.game', 'riichiroyale.tutorial', 'riichiroyale.views'],
+    package_dir={'libmahjong': '.'},
+    package_data={'libmahjong': ['libmahjong.pyd']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        'console_scripts':['riichiroyale=riichiroyale.__main__:main']
+    },
+    include_package_data=True,
+    python_requires='>=3.6',
+)
