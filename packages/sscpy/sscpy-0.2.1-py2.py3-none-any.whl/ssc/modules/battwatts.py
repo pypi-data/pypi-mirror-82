dd7af56c8bf6fa72fb3e875cbd493acf1196ecdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:59 +0000

    Add package sscpy/0.2.1/sscpy-0.2.1-py2.py3-none-any.whl

---
+++ packages/sscpy/sscpy-0.2.1-py2.py3-none-any.whl/ssc/modules/battwatts.py
@@ -0,0 +1,17 @@
+from ssc.core import Data
+from ssc.modules import SSCModule
+
+
+class BattWatts(SSCModule):
+    """
+    SSCModule implementation of the BattWatts interface
+    """
+
+    def __init__(self):
+        super().__init__("battwatts")
+
+    def run(self, **inputs):
+        d = Data()
+        d.update(inputs)
+        self.execute(d)
+        return d
