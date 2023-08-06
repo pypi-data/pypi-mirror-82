bf1a32db1276f3e01f655229b09206ffff84c8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:56 +0000

    Add package gym-derk/0.12.4/gym_derk-0.12.4-py3-none-any.whl

---
+++ packages/gym-derk/gym_derk-0.12.4-py3-none-any.whl/gym_derk/app_build/precache-manifest.9a8356aceb6ecacdf78f0a799e889c59.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c6fe466c9148d50551911dafafdc90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51b1557a7cdc8dde7e35",
+    "url": "./static/css/2.95878150.chunk.css"
+  },
+  {
+    "revision": "56dc4cdcb655a8323318",
+    "url": "./static/css/3.f73af1c3.chunk.css"
+  },
+  {
+    "revision": "d4dc067b75170da69890",
+    "url": "./static/css/4.d86d17fe.chunk.css"
+  },
+  {
+    "revision": "f51f368df0fbd66f6723",
+    "url": "./static/css/main.05e1bb6b.chunk.css"
+  },
+  {
+    "revision": "51b1557a7cdc8dde7e35",
+    "url": "./static/js/2.e54f6a6e.chunk.js"
+  },
+  {
+    "revision": "23e3afda946b1c2fe6f32b37bfff25ba",
+    "url": "./static/js/2.e54f6a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dc4cdcb655a8323318",
+    "url": "./static/js/3.c1e8a9df.chunk.js"
+  },
+  {
+    "revision": "d4dc067b75170da69890",
+    "url": "./static/js/4.d323149b.chunk.js"
+  },
+  {
+    "revision": "053588ab0d56fc4648bb9bc554dc104f",
+    "url": "./static/js/4.d323149b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51f368df0fbd66f6723",
+    "url": "./static/js/main.75d3fb7d.chunk.js"
+  },
+  {
+    "revision": "822fa58d2ecec1b70dd9",
+    "url": "./static/js/runtime-main.fb98c9fb.js"
+  },
+  {
+    "revision": "547f669e7f65f2025b084992e2404d2e",
+    "url": "./static/media/ArenaMap.547f669e.glb"
+  },
+  {
+    "revision": "5add8e120a9ebd5efa1a61c68862ba9c",
+    "url": "./static/media/BountyPoster.5add8e12.svg"
+  },
+  {
+    "revision": "ceda5beabf747510de7d93c04afe6891",
+    "url": "./static/media/Bullet.ceda5bea.glb"
+  },
+  {
+    "revision": "025f6b836c6daf84c9148ecf64312fa9",
+    "url": "./static/media/Changelog.025f6b83.md"
+  },
+  {
+    "revision": "284e7cfa1d736388288f66602188a88f",
+    "url": "./static/media/Crab.284e7cfa.glb"
+  },
+  {
+    "revision": "43c359aa274e8edc12f7dde322223c3e",
+    "url": "./static/media/Dart.43c359aa.glb"
+  },
+  {
+    "revision": "abedfa95b494c2e982c974f0fc994b74",
+    "url": "./static/media/Duck.abedfa95.glb"
+  },
+  {
+    "revision": "29eeb6680e6dd8ea2681864a62b2816b",
+    "url": "./static/media/EnergyModel05.29eeb668.png"
+  },
+  {
+    "revision": "42cbcde578c7038757f69ed1b9e5ddf9",
+    "url": "./static/media/EnergyModel052.42cbcde5.png"
+  },
+  {
+    "revision": "02b875b3002299491c6f9f75c6794009",
+    "url": "./static/media/Gold.02b875b3.png"
+  },
+  {
+    "revision": "89394eb6f33c9197e6a09f619b690d8d",
+    "url": "./static/media/Image0000.89394eb6.exr"
+  },
+  {
+    "revision": "c745c48132966ce74237c0c4fccc770c",
+    "url": "./static/media/Image0000.c745c481.exr"
+  },
+  {
+    "revision": "6f2bf24b20d3081d9f9ef029095615b1",
+    "url": "./static/media/Impact.6f2bf24b.glb"
+  },
+  {
+    "revision": "d13440099872c39a01ae3a11fb172f6d",
+    "url": "./static/media/Items.d1344009.png"
+  },
+  {
+    "revision": "b45be274b8b65fc9c72b038d72486edd",
+    "url": "./static/media/Karla-Bold.b45be274.ttf"
+  },
+  {
+    "revision": "17f8a645015756768592e12b12219220",
+    "url": "./static/media/Karla-BoldItalic.17f8a645.ttf"
+  },
+  {
+    "revision": "363d3c4ec8cb40d863cdacfc0049eb12",
+    "url": "./static/media/Karla-Italic.363d3c4e.ttf"
+  },
+  {
+    "revision": "fd3f938a23d57da8894c8aa0e85adf3f",
+    "url": "./static/media/Karla-Regular.fd3f938a.ttf"
+  },
+  {
+    "revision": "14bc61bddd3fe859d8368b6d27642ac9",
+    "url": "./static/media/Logo.14bc61bd.png"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "79787eff578adeb74d59bed4b02fc3e5",
+    "url": "./static/media/MountRoukeLogo.79787eff.svg"
+  },
+  {
+    "revision": "45c21cad7db78fe9fa940f9186989b29",
+    "url": "./static/media/PlayerUnit.45c21cad.glb"
+  },
+  {
+    "revision": "87a3e571b4939880bc4fff8d20538dee",
+    "url": "./static/media/PortraitBackground.87a3e571.png"
+  },
+  {
+    "revision": "490cb0e45f39580c7ba1b856f2f1f4a7",
+    "url": "./static/media/Righteous-Regular.490cb0e4.ttf"
+  },
+  {
+    "revision": "df3844078500b0d7b0ef99ce08674f30",
+    "url": "./static/media/Scarecrow.df384407.glb"
+  },
+  {
+    "revision": "21001bb47a5ea4cc4435f543d2cdba8b",
+    "url": "./static/media/Statue.21001bb4.glb"
+  },
+  {
+    "revision": "ac244f86962b6eff4ada5e318fa0aefd",
+    "url": "./static/media/Training.ac244f86.md"
+  },
+  {
+    "revision": "1eeb4868a75271576ffec45a41472e6f",
+    "url": "./static/media/Turtle.1eeb4868.glb"
+  },
+  {
+    "revision": "0400c54a6f41c5e0bec5022e64af3df4",
+    "url": "./static/media/UnitMarker.0400c54a.glb"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "./static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "./static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "./static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "./static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "./static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "./static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "./static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "./static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "./static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "./static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "./static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "./static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "./static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "./static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "./static/media/fa-solid-900.ec763292.svg"
+  }
+]);
