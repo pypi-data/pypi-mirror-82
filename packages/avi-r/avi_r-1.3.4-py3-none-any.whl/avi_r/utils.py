7093f8e73c3e5d144015608a7fb8aeb4de0c83e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:53 +0000

    Add package avi-r/1.3.4/avi_r-1.3.4-py3-none-any.whl

---
+++ packages/avi-r/avi_r-1.3.4-py3-none-any.whl/avi_r/utils.py
@@ -0,0 +1,17 @@
+import logging
+from functools import partial
+
+
+def get_logger(name, level=logging.INFO, log_file=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)s %(levelname)s %(message)s')
+    handlers = [logging.StreamHandler()]
+    if log_file is not None:
+        handlers.append(logging.FileHandler(log_file))
+    for handler in handlers:
+        handler.setLevel(level)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
