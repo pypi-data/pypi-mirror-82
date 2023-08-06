2009969350162c8bf59609b3a4c0172c47785c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:49 +0000

    Add package trashpy/1.0.5/trashpy-1.0.5.tar.gz

---
+++ packages/trashpy/trashpy-1.0.5.tar.gz/trashpy-1.0.5/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trashpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-api-python-client>=1.12.3,<2.0.0',
+ 'google-auth-oauthlib>=0.4.1,<0.5.0',
+ 'google-auth>=1.22.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['trashpy = trashpy.main:main']}
+
+setup_kwargs = {
+    'name': 'trashpy',
+    'version': '1.0.5',
+    'description': 'Download your Google Drive Trash folder',
+    'long_description': '# TrashPy\n\nDownload your google drive trash folder\n\n## Install\n\n*Make sure to use Python 3*\n\n```\nsudo pip3 install trashpy\n```\n\n## Usage\n\n```\ntrashpy\n```\n\nIt will ask you to authenicate then it will download all your trash to: `gtrash`\n',
+    'author': 'Paul Bailey',
+    'author_email': 'paul.m.bailey@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/pizzapanther/pydrive-trash-backup',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
