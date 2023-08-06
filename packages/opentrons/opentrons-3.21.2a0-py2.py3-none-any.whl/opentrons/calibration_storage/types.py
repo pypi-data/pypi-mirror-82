4e4160e67cbba7255e69fe35d88671b18db3f0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package opentrons/3.21.2a0/opentrons-3.21.2a0-py2.py3-none-any.whl

---
+++ packages/opentrons/opentrons-3.21.2a0-py2.py3-none-any.whl/opentrons/calibration_storage/types.py
@@ -0,0 +1,91 @@
+import typing
+
+from dataclasses import dataclass
+from datetime import datetime
+from os import PathLike
+
+
+CalibrationID = typing.NewType('CalibrationID', str)
+StrPath = typing.Union[str, PathLike]
+AttitudeMatrix = typing.List[typing.List[float]]
+PipetteOffset = typing.List[float]
+
+
+class TipLengthCalNotFound(Exception):
+    pass
+
+
+@dataclass
+class UriDetails:
+    namespace: str
+    load_name: str
+    version: int
+
+
+@dataclass
+class OffsetData:
+    """
+    Class to categorize the shape of a
+    given calibration data.
+    """
+    value: typing.List[float]
+    last_modified: typing.Optional[datetime]
+
+
+@dataclass
+class TipLengthData:
+    """
+    Class to categorize the shape of a
+    given calibration data.
+    """
+    value: typing.Optional[float] = None
+    last_modified: typing.Optional[datetime] = None
+
+
+@dataclass
+class ParentOptions:
+    """
+    Class to store whether a labware calibration has
+    a module, as well the original parent (slot).
+    As of now, the slot is not saved in association
+    with labware calibrations.
+
+    The slot value will be the empty string.
+    """
+    slot: str
+    module: str = ''
+
+
+@dataclass
+class CalibrationTypes:
+    """
+    Class to categorize what calibration
+    data might be stored for a labware.
+    """
+    offset: OffsetData
+    tip_length: TipLengthData
+
+
+@dataclass
+class CalibrationInformation:
+    """
+    Class to store important calibration
+    info for labware.
+    """
+    calibration: CalibrationTypes
+    parent: ParentOptions
+    labware_id: str
+    uri: str
+
+
+@dataclass
+class PipetteOffsetCalibration:
+    """
+    Class to store pipette offset calibration
+    """
+    pipette: str
+    mount: str
+    offset: PipetteOffset
+    tiprack: str
+    uri: str
+    last_modified: datetime
