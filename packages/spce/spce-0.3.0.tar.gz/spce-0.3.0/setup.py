804efcf90e001d6c9e3accf71dc7ee252ffdf54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:36 +0000

    Add package spce/0.3.0/spce-0.3.0.tar.gz

---
+++ packages/spce/spce-0.3.0.tar.gz/spce-0.3.0/setup.py
@@ -0,0 +1,46 @@
+# Copyright 2020 Scale Plan Yazılım A.Ş.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='spce',
+    version='0.3.0',
+    packages=['spce'],
+    url='https://github.com/scaleplandev/spce-python',
+    license='Apache 2.0',
+    author='ScalePlan Engineering',
+    author_email='dev@scaleplan.io',
+    description="ScalePlan's CloudEvents implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    keywords='cloudevents,scaleplan,cloud,events,event',
+    tests_require=['pytest', 'coverage', 'pytest-cov', 'avro'],
+    extras_require={
+        'avro': ['avro>=1.10']
+    }
+)
---
+++ packages/spce/spce-0.3.0.tar.gz/spce-0.3.0/spce/cloudevents.py
@@ -0,0 +1,98 @@
+# Copyright 2020 Scale Plan Yazılım A.Ş.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import builtins
+from datetime import datetime
+from typing import Union, Dict, Any
+
+__all__ = "CloudEvent",
+
+
+class CloudEvent:
+
+    __slots__ = "_attributes", "_data", "_has_binary_data"
+
+    NOW = "now"
+
+    def __init__(self, *,
+                 type: str,
+                 source: str,
+                 id: str,
+                 specversion="1.0",
+                 subject = "",
+                 data: Union[str, bytes, Dict[str, Any], list, str, int, float, bool, None] = None,
+                 datacontenttype = "",
+                 dataschema = "",
+                 time: Union[str, datetime] = "",
+                 **attributes
+                 ):
+        attrs = {
+            "type": type,
+            "source": source,
+            "id": id,
+            "specversion": specversion,
+        }
+
+        if isinstance(time, datetime):
+            # if the time has timezone information, convert it directly to isoformat
+            if time.tzinfo is not None and time.tzinfo.utcoffset(time) is not None:
+                time = time.isoformat()
+            else:
+                # time is naive, assume it is UTC
+                time = "%sZ" % time.isoformat()
+        elif isinstance(time, str):
+            pass
+        elif time:
+            raise TypeError("time must be either a string or a datetime.datetime, but it is: %s"
+                            % builtins.type(time))
+
+        if subject: attrs["subject"] = subject
+        if datacontenttype: attrs["datacontenttype"] = datacontenttype
+        if dataschema: attrs["dataschema"] = dataschema
+        if time: attrs["time"] = time
+
+        # TODO: validation
+
+        self._attributes = attrs
+        self._data = data
+        self._attributes.update(attributes)
+        self._has_binary_data = isinstance(data, bytes)
+
+    type = property(lambda self: self._attributes.get("type"))
+    source = property(lambda self: self._attributes.get("source"))
+    id = property(lambda self: self._attributes.get("id"))
+    specversion = property(lambda self: self._attributes.get("specversion"))
+    data = property(lambda self: self._data)
+    datacontenttype = property(lambda self: self._attributes.get("datacontenttype"))
+    dataschema = property(lambda self: self._attributes.get("dataschema"))
+    subject = property(lambda self: self._attributes.get("subject"))
+    time = property(lambda self: self._attributes.get("time"))
+
+    def attribute(self, name):
+        return self._attributes.get(name)
+
+    def __str__(self):
+        return str(self._attributes)
+
+    def __repr__(self):
+        return repr(self._attributes)
+
+    def __eq__(self, other):
+        if not isinstance(other, CloudEvent):
+            return False
+        return self._attributes == other._attributes \
+            and self._data == other._data
+
+    def __hash__(self):
+        return hash(self._attributes, self._data)
