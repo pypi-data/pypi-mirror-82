04be701f5cc74699e94886046019b26108061f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:58 +0000

    Add package insecure-but-secure-enough/0.1.1/insecure_but_secure_enough-0.1.1.tar.gz

---
+++ packages/insecure-but-secure-enough/insecure_but_secure_enough-0.1.1.tar.gz/insecure_but_secure_enough-0.1.1/setup.py
@@ -0,0 +1,50 @@
+"""insecure_but_secure_enough installation script.
+"""
+import os
+import re
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.md")).read()
+README = README.split("\n\n", 1)[0] + "\n"
+
+# store version in the init.py
+with open(
+    os.path.join(os.path.dirname(__file__), "insecure_but_secure_enough", "__init__.py")
+) as v_file:
+    VERSION = re.compile(r'.*__VERSION__ = "(.*?)"', re.S).match(v_file.read()).group(1)
+
+requires = ["pycryptodomex", "simplejson"]
+tests_require = []
+testing_extras = tests_require + [
+    "pytest",
+]
+
+setup(
+    name="insecure_but_secure_enough",
+    version=VERSION,
+    description="Lightweight tools for signing and encrypting cookies, urls and stuff. This package isn't really secure, but it is secure enough for most needs.",
+    long_description=README,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="web pylons pyramid",
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    url="https://github.com/jvanasco/insecure_but_secure_enough",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=tests_require,
+    extras_require={
+        "testing": testing_extras,
+    },
+    test_suite="tests",
+)
