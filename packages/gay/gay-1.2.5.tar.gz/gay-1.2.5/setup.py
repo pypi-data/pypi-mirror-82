b8a3b0c2a50a5612a944ff7e8dcab33e905bf059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:00 +0000

    Add package gay/1.2.5/gay-1.2.5.tar.gz

---
+++ packages/gay/gay-1.2.5.tar.gz/gay-1.2.5/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def slurp(path: str) -> str:
+    with open(path) as fd:
+        return fd.read()
+
+
+setup(
+    name="gay",
+    version="1.2.5",
+    description="Colour your text / terminal to be more gay. 🏳️‍🌈",
+    long_description=slurp("README.md"),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/gay",
+    scripts=["gay"],
+)
