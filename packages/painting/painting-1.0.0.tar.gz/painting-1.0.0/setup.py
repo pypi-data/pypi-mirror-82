fa0668766b543083c5755b851e18bba09e880454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:38 +0000

    Add package painting/1.0.0/painting-1.0.0.tar.gz

---
+++ packages/painting/painting-1.0.0.tar.gz/painting-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+import io
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+BASE_DIR = os.path.join(os.path.dirname(__file__))
+
+with io.open(os.path.join(BASE_DIR, "requirements.txt"), encoding="utf-8") as fh:
+    REQUIREMENTS = fh.read()
+
+setup(
+    name="painting",
+    version="1.0.0",
+    description="Beautiful output on the terminal",
+    author="Isclub Studio",
+    url="https://github.com/isclub/painting",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    install_requires=REQUIREMENTS,
+    long_description_content_type="text/markdown",
+    license="MIT License",
+)
