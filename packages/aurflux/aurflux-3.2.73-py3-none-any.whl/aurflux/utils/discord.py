6f6c9d8ced4e8899e5132a4f5a50c8607cbaa823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package aurflux/3.2.73/aurflux-3.2.73-py3-none-any.whl

---
+++ packages/aurflux/aurflux-3.2.73-py3-none-any.whl/aurflux/utils/discord.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+import typing as ty
+from ..errors import BotMissingPermissions
+
+if ty.TYPE_CHECKING:
+   import discord
+
+
+def perm_check(c: discord.TextChannel, need: discord.Permissions):
+   print(f"Checking that {need} <= {c.permissions_for(c.guild.me)}")
+   if not need <= (p := c.permissions_for(c.guild.me)):
+      raise BotMissingPermissions(need, p)
