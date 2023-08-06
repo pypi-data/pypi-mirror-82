70041bde8e79e36d175e92d5a9f7fa6319daeef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:30 +0000

    Add package mkdocs-codeinclude-plugin/0.1.0/mkdocs-codeinclude-plugin-0.1.0.tar.gz

---
+++ packages/mkdocs-codeinclude-plugin/mkdocs-codeinclude-plugin-0.1.0.tar.gz/mkdocs-codeinclude-plugin-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from os.path import dirname
+from os.path import join
+from setuptools import setup
+
+
+def read_file(fname):
+    "Read a local file"
+    return open(join(dirname(__file__), fname)).read()
+
+
+setup(
+    name='mkdocs-codeinclude-plugin',
+    version='0.1.0',
+    description="A plugin to include code snippets into mkdocs pages",
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs python markdown codeinclude',
+    url='https://github.com/rnorth/mkdocs-codeinclude-plugin',
+    author='Richard North',
+    author_email='rich.north@gmail.com',
+    license='MIT',
+    python_requires='>=3.7',
+    install_requires=[
+        'mkdocs>=0.17',
+        'pygments'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['codeinclude'],
+    include_package_data=True,
+    entry_points={
+        'mkdocs.plugins': [
+            'codeinclude = codeinclude.plugin:CodeIncludePlugin'
+        ]
+    }
+)
