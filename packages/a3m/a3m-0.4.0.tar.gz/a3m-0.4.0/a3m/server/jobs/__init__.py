7d2af6408c57fabc3afcb658ce4564362970bd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:37 +0000

    Add package a3m/0.4.0/a3m-0.4.0.tar.gz

---
+++ packages/a3m/a3m-0.4.0.tar.gz/a3m-0.4.0/a3m/server/jobs/__init__.py
@@ -0,0 +1,35 @@
+"""
+A job corresponds to a microservice, a link in the workflow, and the `Jobs`
+table in the database.
+
+Initialization of `Job` objects is typically done via a `JobChain`,
+corresponding to a chain in the workflow. The `JobChain` object handles
+determining the next job to be run, and passing data between jobs.
+
+The `Job` class is a base class for other job types. There are various
+concrete types of jobs, handled by subclasses:
+    * `ClientScriptJob`, handling Jobs to be execute on MCPClient
+    * `DecisionJob`, handling workflow decision points
+    * `LocalJob`, handling work done directly on MCPServer
+"""
+from a3m.server.jobs.base import Job
+from a3m.server.jobs.chain import JobChain
+from a3m.server.jobs.client import ClientScriptJob
+from a3m.server.jobs.client import DirectoryClientScriptJob
+from a3m.server.jobs.client import FilesClientScriptJob
+from a3m.server.jobs.decisions import DecisionJob
+from a3m.server.jobs.decisions import NextChainDecisionJob
+from a3m.server.jobs.decisions import UpdateContextDecisionJob
+from a3m.server.jobs.local import LocalJob
+
+__all__ = (
+    "ClientScriptJob",
+    "DecisionJob",
+    "DirectoryClientScriptJob",
+    "FilesClientScriptJob",
+    "Job",
+    "JobChain",
+    "LocalJob",
+    "NextChainDecisionJob",
+    "UpdateContextDecisionJob",
+)
