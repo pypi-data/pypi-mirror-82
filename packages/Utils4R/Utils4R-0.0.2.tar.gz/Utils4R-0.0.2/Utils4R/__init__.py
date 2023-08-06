651b0a5928215563a5cf1049d387a37e581d4fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:39 +0000

    Add package Utils4R/0.0.2/Utils4R-0.0.2.tar.gz

---
+++ packages/Utils4R/Utils4R-0.0.2.tar.gz/Utils4R-0.0.2/Utils4R/__init__.py
@@ -0,0 +1,5 @@
+from . import io
+from . import db
+
+from .basic import console
+from .basic import get_scope_random as random
---
+++ packages/Utils4R/Utils4R-0.0.2.tar.gz/Utils4R-0.0.2/Utils4R/io/__init__.py
@@ -0,0 +1,4 @@
+from .file import load_string
+from .file import write_string
+from .json import load_json
+from .json import write_json
