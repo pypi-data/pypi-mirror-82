b23565abcd2323760f3c0021883e430d642f0c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:21 +0000

    Add package schulich-ignite/0.0.3/schulich-ignite-0.0.3.tar.gz

---
+++ packages/schulich-ignite/schulich-ignite-0.0.3.tar.gz/schulich-ignite-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="schulich-ignite",
+    version="0.0.3",
+    author="Schulich Ignite",
+    author_email="info@shulichignite.com",
+    description="Spark library for Shulich Ignite sessions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Schulich-Ignite/spark",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        'ipywidgets',
+        'ipycanvas',
+        'ipyevents'
+    ]
+)
