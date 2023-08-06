9cb5356c3d5ee418833aafe217783ce702925dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:03 +0000

    Add package wofrywiser/0.1.0/wofrywiser-0.1.0.tar.gz

---
+++ packages/wofrywiser/wofrywiser-0.1.0.tar.gz/wofrywiser-0.1.0/wofrywiser/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofrywise SETUP.PY
+short_version = '0.1.0'
+version = '0.1.0'
+full_version = '0.1.0'
+git_revision = '26616ce5b92895f3886f2da440ed0d9036516c77'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
