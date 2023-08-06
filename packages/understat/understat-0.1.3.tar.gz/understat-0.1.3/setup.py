16f8ef2d4a84230afb5a3415d1aa3d97b544f997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:01 +0000

    Add package understat/0.1.3/understat-0.1.3.tar.gz

---
+++ packages/understat/understat-0.1.3.tar.gz/understat-0.1.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="understat",
+    version="0.1.3",
+    packages=find_packages(),
+    description="A Python wrapper for https://understat.com/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amosbastian/understat",
+    author="amosbastian",
+    author_email="amosbastian@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ],
+    keywords="fpl fantasy premier league understat football",
+    project_urls={
+        "Documentation": "http://fpl.readthedocs.io/en/latest/",
+        "Source": "https://github.com/amosbastian/fpl"
+    },
+    install_requires=[
+        "aiohttp",
+        "beautifulsoup4",
+        "codecov",
+        "pytest-aiohttp",
+        "pytest-cov",
+        "pytest-mock",
+        "pytest",
+    ],
+)
