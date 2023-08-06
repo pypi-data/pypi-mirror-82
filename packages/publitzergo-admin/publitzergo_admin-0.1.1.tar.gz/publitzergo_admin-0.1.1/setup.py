aca02a38143791aa734db9251e23e11012338b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:45 +0000

    Add package publitzergo-admin/0.1.1/publitzergo_admin-0.1.1.tar.gz

---
+++ packages/publitzergo-admin/publitzergo_admin-0.1.1.tar.gz/publitzergo_admin-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['publitzergo_admin']
+
+package_data = \
+{'': ['*'], 'publitzergo_admin': ['static/images/*', 'templates/*']}
+
+entry_points = \
+{'console_scripts': ['customize = publitzergo_admin.overrides:admin_override']}
+
+setup_kwargs = {
+    'name': 'publitzergo-admin',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'drazum',
+    'author_email': 'domagoj.razum1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
