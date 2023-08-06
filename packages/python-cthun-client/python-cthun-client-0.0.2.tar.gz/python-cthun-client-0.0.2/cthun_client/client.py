2561edba10b407ea3c8b4d66838962935bf5d947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:20 +0000

    Add package python-cthun-client/0.0.2/python-cthun-client-0.0.2.tar.gz

---
+++ packages/python-cthun-client/python-cthun-client-0.0.2.tar.gz/python-cthun-client-0.0.2/cthun_client/client.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+__author__ = 'jasonxu'
+from cthun_client.config import Config
+from cthun_client.device.ios import Ios
+from cthun_client.device.android import Android
+from cthun_client.connection import Connection
+
+
+class Client:
+    def __init__(self, config, connection=None):
+        self._config = config if config else Config()
+        self._connection = connection if connection else Connection(self._config)
+        self._ios = Ios
+        self._android = Android
+
+    @property
+    def connection(self):
+        return self._connection
+
+    @property
+    def config(self):
+        return self._config
+
+    def ios(self, device_id):
+        return self._ios(device_id)
+
+    def android(self, device_id):
+        return self._android(device_id)
+
---
+++ packages/python-cthun-client/python-cthun-client-0.0.2.tar.gz/python-cthun-client-0.0.2/cthun_client/config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = 'jasonxu'
+from exception import AuthErrorException
+
+
+class Config:
+    def __init__(self, api_url=None, access_token=None):
+        self._api_url = api_url
+        self._access_token = access_token
+
+    @property
+    def api_url(self):
+        return self._api_url
+
+    @api_url.setter
+    def api_url(self, value):
+        self._api_url = value
+
+    @property
+    def access_token(self):
+        if not self._access_token:
+            pass
+            # raise AuthErrorException("auth error")
+        return self._access_token
+
+    @access_token.setter
+    def access_token(self, value):
+        self._access_token = value
