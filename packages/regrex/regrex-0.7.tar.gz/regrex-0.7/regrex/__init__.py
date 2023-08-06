590a3a569730366109f2c4d4c3f94b7e5a987cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:10 +0000

    Add package regrex/0.7/regrex-0.7.tar.gz

---
+++ packages/regrex/regrex-0.7.tar.gz/regrex-0.7/regrex/__init__.py
@@ -0,0 +1,7 @@
+from regrex import linear_model
+from regrex import polynomial_model
+from regrex import preprocess
+from regrex import transform
+from regrex import dataset
+from regrex import statistics
+from regrex import validation
