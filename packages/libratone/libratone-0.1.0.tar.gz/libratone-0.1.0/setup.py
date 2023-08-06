572c4178434346e1987570d2d6babfb8cad3c361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package libratone/0.1.0/libratone-0.1.0.tar.gz

---
+++ packages/libratone/libratone-0.1.0.tar.gz/libratone-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from libratone import __version__
+from distutils.core import setup
+
+setup(
+    name='libratone',
+    version=__version__,
+    description='A Libratone client library',
+    author='Malachi Soord',
+    license='MIT',
+    url='https://github.com/inverse/python-libratone',
+    py_modules=['libratone'],
+)
