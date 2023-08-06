8444ef74ca015c861fc550f539cb998f877044a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:40 +0000

    Add package lighthive/0.2.6/lighthive-0.2.6.tar.gz

---
+++ packages/lighthive/lighthive-0.2.6.tar.gz/lighthive-0.2.6/lighthive/broadcast/chains.py
@@ -0,0 +1,9 @@
+known_chains = {
+    "HIVE": {
+        "chain_id": "beeab0de" + ("0" * 56),
+    },
+    "TEST": {
+        "chain_id":
+            "9afbce9f2416520733bacb370315d32b6b2c43d6097576df1c1222859d91eecc",
+    },
+}
