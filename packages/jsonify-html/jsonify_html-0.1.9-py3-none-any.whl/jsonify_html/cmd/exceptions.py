7e4ec1a3505ce65d263d52e47bf5f34f09595693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:18 +0000

    Add package jsonify-html/0.1.9/jsonify_html-0.1.9-py3-none-any.whl

---
+++ packages/jsonify-html/jsonify_html-0.1.9-py3-none-any.whl/jsonify_html/cmd/exceptions.py
@@ -0,0 +1,18 @@
+class ArgumentError(Exception):
+    pass
+
+
+class RunTimeError(Exception):
+    pass
+
+
+class ValidatorError(Exception):
+    pass
+
+
+class CommandTypeError(Exception):
+    pass
+
+
+class CommandNotFound(Exception):
+    pass
