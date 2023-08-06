beb0fe2606c79a6eabef24a6854cce7fdf2b1e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:03 +0000

    Add package jex/0.1.3/jex-0.1.3.tar.gz

---
+++ packages/jex/jex-0.1.3.tar.gz/jex-0.1.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="jex",
+    version="0.1.3",
+    description="JSON/YAML interactive explorer - no JQ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Viet Hung Nguyen",
+    author_email="hvn@familug.org",
+    url="https://github.com/hvnsweeting/jex",
+    license="MIT",
+    classifiers=["Environment :: Console"],
+    packages=find_packages(include=["jex"]),
+    install_requires=['PyYAML>5'],
+    entry_points={"console_scripts": ["jex=jex.cli:main"]},
+)
