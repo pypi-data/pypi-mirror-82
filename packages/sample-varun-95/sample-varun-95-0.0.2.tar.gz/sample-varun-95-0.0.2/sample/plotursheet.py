af0ee90b3e292b23deb954ef4732b3dd3cfc2bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:26 +0000

    Add package sample-varun-95/0.0.2/sample-varun-95-0.0.2.tar.gz

---
+++ packages/sample-varun-95/sample-varun-95-0.0.2.tar.gz/sample-varun-95-0.0.2/sample/plotursheet.py
@@ -0,0 +1,16 @@
+#__main__.py
+import json
+
+def main():
+    preson = json.loads(client_secrets)    
+    print(preson)
+if __name__ == "main":
+    main()
+def rom(a):
+    print("Enter 1 or 2")
+    preson = json.loads(client_secrets)    
+    if a==1:
+        print(preson)
+    else:
+        print("2")
+
---
+++ packages/sample-varun-95/sample-varun-95-0.0.2.tar.gz/sample-varun-95-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sample-varun-95",
+    version="0.0.2",
+    author="varun_95",
+    author_email="varunjay7@gmail.com",
+    description="A small example package",
+    long_description="NOpe",
+    long_description_content_type="text/markdown",
+    url="https://github.com/fat-a-lity/plotursheet",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
