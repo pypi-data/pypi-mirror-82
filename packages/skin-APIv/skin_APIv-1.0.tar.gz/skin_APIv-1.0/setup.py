e6f76e7228ce02a90b08d5ffdee262a83433a3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:12 +0000

    Add package skin-APIv/1.0/skin_APIv-1.0.tar.gz

---
+++ packages/skin-APIv/skin_APIv-1.0.tar.gz/skin_APIv-1.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'skin_APIv',
+    version = '1.0',
+    description="API",
+    packages= find_packages(),
+    py_modules=['skinAPI'],
+    author="Claire",
+    license="LGPL"
+)
