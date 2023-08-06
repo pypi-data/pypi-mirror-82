9af7de713d8f2ffc3093689a9d0aa84e643143c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:11 +0000

    Add package bioprov/0.1.9/bioprov-0.1.9-py3-none-any.whl

---
+++ packages/bioprov/bioprov-0.1.9-py3-none-any.whl/bioprov/tests/test_src_prov.py
@@ -0,0 +1,42 @@
+__author__ = "Vini Salazar"
+__license__ = "MIT"
+__maintainer__ = "Vini Salazar"
+__url__ = "https://github.com/vinisalazar/bioprov"
+__version__ = "0.1.9"
+
+
+"""
+Testing for prov module.
+    - BioProvDocument class
+"""
+
+from os import environ
+from bioprov import read_csv
+from bioprov.data import picocyano_dataset
+from bioprov.src.prov import BioProvDocument
+from bioprov.src.config import EnvProv
+
+project = read_csv(
+    picocyano_dataset, sequencefile_cols="assembly", tag="picocyanobacteria"
+)
+
+
+def test_EnvProv():
+    """
+    Tests the construction of an instance of EnvProv.
+    :return:
+    """
+    env = EnvProv()
+    for statement in (
+        env.env_set == frozenset(environ.items()),
+        env.env_hash == hash(env.env_set),
+    ):
+        assert statement
+
+
+def test_BioProvDocument():
+    """
+    Tests the construction of an instance of BioProvDocument.
+    :return:
+    """
+    _ = BioProvDocument(project)
