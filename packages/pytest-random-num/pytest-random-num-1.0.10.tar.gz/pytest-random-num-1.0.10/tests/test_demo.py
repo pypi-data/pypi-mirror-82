097046a19619fcf090ce4c6f52526b8c87fe6f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:54 +0000

    Add package pytest-random-num/1.0.10/pytest-random-num-1.0.10.tar.gz

---
+++ packages/pytest-random-num/pytest-random-num-1.0.10.tar.gz/pytest-random-num-1.0.10/tests/test_demo.py
@@ -0,0 +1,19 @@
+import pytest
+
+class TestDemo:
+
+    def test_demo_1(self):
+        print('tst')
+        pass
+
+    def test_demo_2(self):
+        print('tst')
+        pass
+
+    def test_demo_3(self):
+        print('tst')
+        pass
+
+    def test_demo_4(self):
+        print('tst')
+        pass
