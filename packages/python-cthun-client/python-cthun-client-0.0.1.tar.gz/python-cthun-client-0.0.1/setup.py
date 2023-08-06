0eb0d095d9e6254051f0a3426f05f5040dc692e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:12 +0000

    Add package python-cthun-client/0.0.1/python-cthun-client-0.0.1.tar.gz

---
+++ packages/python-cthun-client/python-cthun-client-0.0.1.tar.gz/python-cthun-client-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='python-cthun-client',
+    url='https://github.com/Jason916/python-cthun-client',
+    version='0.0.1',
+    description='test',
+    author='Jason916',
+    author_email='Jason1989Xu@gmail.com',
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+    include_package_data=True
+)
