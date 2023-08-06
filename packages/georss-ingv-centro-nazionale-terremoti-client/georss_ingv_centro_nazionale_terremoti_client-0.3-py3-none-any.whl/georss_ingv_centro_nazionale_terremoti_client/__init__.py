aaa03a3a8377c3f6e2cc1e18d36db9f5353f79ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:49 +0000

    Add package georss-ingv-centro-nazionale-terremoti-client/0.3/georss_ingv_centro_nazionale_terremoti_client-0.3-py3-none-any.whl

---
+++ packages/georss-ingv-centro-nazionale-terremoti-client/georss_ingv_centro_nazionale_terremoti_client-0.3-py3-none-any.whl/georss_ingv_centro_nazionale_terremoti_client/__init__.py
@@ -0,0 +1,3 @@
+"""INGV Centro Nazionale Terremoti (Earthquakes) library."""
+from .feed import IngvCentroNazionaleTerremotiFeed  # noqa
+from .feed_manager import IngvCentroNazionaleTerremotiFeedManager  # noqa
