2617eaaefe4b25f2afd079002e9d82a2f0672dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:54 +0000

    Add package raise/1.1.3/raise-1.1.3-py3-none-any.whl

---
+++ packages/raise/raise-1.1.3-py3-none-any.whl/raise_.py
@@ -0,0 +1,38 @@
+# Copyright 2018 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+"""Raise exceptions with a function instead of a statement.
+
+Provides a minimal, clean and portable interface for raising exceptions
+with all the advantages of functions over syntax.
+"""
+
+
+__all__ = ('raise_',)
+__version__ = '1.1.3'
+
+
+def raise_(exception, traceback=None):
+    """Raise an exception, optionally with a custom traceback.
+
+    Arguments:
+        exception: The exception instance or type to raise.
+        traceback (optional): Traceback to raise the exception with.
+    """
+    if isinstance(exception, type) and issubclass(exception, BaseException):
+        exception = exception()
+    try:
+        raise exception.with_traceback(traceback)
+    finally:
+        exception = traceback = None
