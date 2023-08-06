c73c7483f9be7f186c04e362fae91584051e7a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:37 +0000

    Add package allianceauth/2.8.0/allianceauth-2.8.0.tar.gz

---
+++ packages/allianceauth/allianceauth-2.8.0.tar.gz/allianceauth-2.8.0/allianceauth/groupmanagement/urls.py
@@ -0,0 +1,29 @@
+from . import views
+
+from django.conf.urls import url
+app_name = 'groupmanagement'
+
+urlpatterns = [
+    url(r'^groups/', views.groups_view, name='groups'),    
+    url(r'^management/', views.group_management,
+        name='management'),
+    url(r'^membership/$', views.group_membership,
+        name='membership'),
+    url(r'^membership/(\w+)/$', views.group_membership_list,
+        name='membership_list'),
+    url(r'^membership/(\w+)/audit/$', views.group_membership_audit, name="audit_log"),
+    url(r'^membership/(\w+)/remove/(\w+)/$', views.group_membership_remove,
+        name='membership_remove'),
+    url(r'^request_add/(\w+)', views.group_request_add,
+        name='request_add'),
+    url(r'^request/accept/(\w+)', views.group_accept_request,
+        name='accept_request'),
+    url(r'^request/reject/(\w+)', views.group_reject_request,
+        name='reject_request'),
+    url(r'^request_leave/(\w+)', views.group_request_leave,
+        name='request_leave'),
+    url(r'leave_request/accept/(\w+)', views.group_leave_accept_request,
+        name='leave_accept_request'),
+    url(r'^leave_request/reject/(\w+)', views.group_leave_reject_request,
+        name='leave_reject_request'),
+]
