1c345b6c065386d67f57c27e13c2225a850398e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:17 +0000

    Add package globus-search-cli/0.5.2/globus-search-cli-0.5.2.tar.gz

---
+++ packages/globus-search-cli/globus-search-cli-0.5.2.tar.gz/globus-search-cli-0.5.2/globus_search_cli/commands/__init__.py
@@ -0,0 +1,25 @@
+from globus_search_cli.commands.entry import entry_cmd
+from globus_search_cli.commands.ingest import ingest_func
+from globus_search_cli.commands.login import login_command
+from globus_search_cli.commands.logout import logout_command
+from globus_search_cli.commands.query import query_func
+from globus_search_cli.commands.query_template import query_template_cmd
+from globus_search_cli.commands.show_index import show_index_cmd
+from globus_search_cli.commands.structured_query import structured_query_func
+from globus_search_cli.commands.subject import subject_cmd
+from globus_search_cli.commands.task import task_cmd
+from globus_search_cli.commands.delete_by_query import delete_by_query_cmd
+
+__all__ = (
+    "ingest_func",
+    "query_func",
+    "structured_query_func",
+    "subject_cmd",
+    "entry_cmd",
+    "task_cmd",
+    "query_template_cmd",
+    "login_command",
+    "logout_command",
+    "show_index_cmd",
+    "delete_by_query_cmd",
+)
