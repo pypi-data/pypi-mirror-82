9313f29fd4207bd76e88e90a5c3cce46f07578fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:15 +0000

    Add package vquery/0.34/vquery-0.34.tar.gz

---
+++ packages/vquery/vquery-0.34.tar.gz/vquery-0.34/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name = "vquery",
+	version = "0.34",
+	author = "Jakob Svanholm",
+	author_email = "rymdlego@gmail.com",
+	url = "https://github.com/rymdlego/vquery",
+        download_url = "https://github.com/rymdlego/vquery/archive/v0.34.tar.gz",
+	description = "A tool for querying vCenter for various information.",
+	keywords = "vSphere VMware vCenter",
+	scripts=['vq'],
+	packages=['vquery'],
+	install_requires=['docopt', 'pyVmomi', 'future', 'six']
+)
