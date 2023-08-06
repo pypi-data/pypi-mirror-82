96325f8ceefe25f4ed2f4734eb6eaaed3cd8db4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package ewah/0.1.33/ewah-0.1.33.tar.gz

---
+++ packages/ewah/ewah-0.1.33.tar.gz/ewah-0.1.33/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ewah", # Replace with your own username
+    version="0.1.33",
+    author="Bijan Soltani",
+    author_email="bijan.soltani+ewah@gemmaanalytics.com",
+    description="An ELT with airflow helper module: Ewah",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gemmaanalytics.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    install_requires=[            # I get to this in a second
+          'pyyaml',
+          'psycopg2',
+          'gspread',
+          'pytz',
+          'yahoofinancials',
+          'google-api-python-client',
+          'oauth2client',
+          'cx_Oracle',
+          'facebook_business',
+          'mysql-connector-python',
+          'snowflake-connector-python==2.0.2',
+          'pymongo',
+          'apache-airflow-backport-providers-google',
+          'googlemaps',
+      ],
+)
