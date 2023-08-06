ef758fe1a19db2ee07c615710ec1bdbf1bad279e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:12 +0000

    Add package ai-university-bio/1.1.4/ai_university_bio-1.1.4.tar.gz

---
+++ packages/ai-university-bio/ai_university_bio-1.1.4.tar.gz/ai_university_bio-1.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ai_university_bio",    
+    version="1.1.4",
+    author="AI University",
+    author_email="nejronki@gmail.compile",
+    description="Genetics model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],    
+    python_requires='>=3.6',
+)
