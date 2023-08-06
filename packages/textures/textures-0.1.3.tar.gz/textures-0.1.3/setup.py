df7fcd121031d72cd30bf8dd5d4718022e7728cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:58 +0000

    Add package textures/0.1.3/textures-0.1.3.tar.gz

---
+++ packages/textures/textures-0.1.3.tar.gz/textures-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import setuptools
+setup(name='textures',
+      version='0.1.3',
+      url="https://github.com/mcnakhaee/textures",
+      description="A python package to extract features from text data",
+      long_description=open('DESCRIPTION.rst').read(),
+      author='Muhammad Chenariyan Nakhaee',
+      author_emai='mcnakhaee@gmail.com',
+      packages = ['textures'],
+      install_requires=['pandas', 'spacy','pyenchant','textblob','nltk','langdetect','textstat','pyenchant'],
+      include_package_data=True,
+      package_data={'': ['data/*.csv']},
+      )
