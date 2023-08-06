351c845f6b31b1625e66d49a830b3069ffe3301c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:42 +0000

    Add package BAproducetracker/0.0.3/BAproducetracker-0.0.3.tar.gz

---
+++ packages/BAproducetracker/BAproducetracker-0.0.3.tar.gz/BAproducetracker-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import os
+import platform
+
+if platform.system == 'Windows':
+
+    folder = os.path.dirname(os.path.realpath(__file__))
+    req_path = folder + '/requirements.txt'
+    install_requires = []
+
+    if os.path.isfile(req_path):
+        with open(req_path) as file:
+            install_requires = file.read().splitlines()
+else:
+    folder = os.path.dirname(os.path.realpath(__file__))
+    req_path = folder + '/linuxrequirements.txt'
+    install_requires = []
+
+    if os.path.isfile(req_path):
+        with open(req_path) as file:
+            install_requires = file.read().splitlines()
+
+setuptools.setup(
+    name="BAproducetracker",
+    version="0.0.3",
+    author="Brody, Joseph // Dillon, John // Estrada, Pablo // Todd, Alexis // Garcia, Marissa",
+    url="https://github.com/jd3builds/BadApplesProject",
+    description="Produce expiration tracker",
+    python_requires='>=3.8',
+    install_requires=install_requires
+)
