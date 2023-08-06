c7696d6a7da52f11d12daaefbc6f39a9739a524f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:09 +0000

    Add package eddington/0.0.20.dev0/eddington-0.0.20.dev0.tar.gz

---
+++ packages/eddington/eddington-0.0.20.dev0.tar.gz/eddington-0.0.20.dev0/src/eddington/print_util.py
@@ -0,0 +1,38 @@
+"""Printing functions for displaying numbers with given precision."""
+import math
+from typing import Tuple
+
+from eddington.consts import DEFAULT_PRECISION
+
+
+def to_precise_string(decimal: float, precision: int = DEFAULT_PRECISION) -> str:
+    """
+    Returns a decimal as string with desired precision.
+
+    :param decimal: a floating point number.
+    :param precision: the desired precision to return the number.
+    :return: a string representing the decimal with given precision.
+    """
+    new_decimal, relevant_precision = __to_relevant_precision(decimal)
+    if relevant_precision < 3:
+        return f"{decimal:.{precision + relevant_precision}f}"
+    return f"{new_decimal:.{precision}f}e-0{relevant_precision}"
+
+
+def __to_relevant_precision(decimal: float) -> Tuple[float, int]:
+    """
+    Get relevant precision of a decimal number.
+
+    :param decimal: a floating point number.
+    :return: a tuple of a and n such that: decimal = a * 10^(-b).
+    """
+    if decimal == 0:
+        return 0, 0
+    precision = 0
+    abs_a = math.fabs(decimal)
+    while abs_a < 1.0:
+        abs_a *= 10
+        precision += 1
+    if decimal < 0:
+        return -abs_a, precision
+    return abs_a, precision
