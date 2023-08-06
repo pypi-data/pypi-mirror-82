c84b486d4c10d26e02614a5e5fc1bb6a5f4b8fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package dNote/0.3.7/dNote-0.3.7.tar.gz

---
+++ packages/dNote/dNote-0.3.7.tar.gz/dNote-0.3.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+setup(
+    name='dNote',
+    version='0.3.7',
+    description='Simple CLI utility for managing notes with AWS',
+    url='https://github.com/yetisir/dnote',
+    author='M. Yetisir',
+    author_email='yetisir@gmail.com',
+    install_requires=[
+        'boto3>=1.13',
+        'click>=7.1',
+        'configurator[yaml]>=2.0',
+        'dateparser>=0.7',
+        'nltk>=3.5',
+    ],
+    extras_require={
+        'dev': [
+            'coveralls>=2.0',
+            'mkdocs>=1.1',
+            'pip-tools>=5.1',
+            'pytest>=5.4',
+            'pytest-cov>=2.8',
+            'pytest-flake8>=1.0',
+        ],
+    },
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'dnote = dnote.__main__:main',
+        ],
+    },
+)
