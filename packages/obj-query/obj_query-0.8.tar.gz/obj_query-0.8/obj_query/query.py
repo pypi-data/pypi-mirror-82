050aa94c92bdebdf1f4c5348abe921a019d9d963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:54 +0000

    Add package obj-query/0.8/obj_query-0.8.tar.gz

---
+++ packages/obj-query/obj_query-0.8.tar.gz/obj_query-0.8/obj_query/query.py
@@ -0,0 +1,74 @@
+import datetime
+from typing import List, Callable, Any
+
+
+class Retriever:
+    def __init__(self, operations: List):
+        self.Operations = operations
+
+    def get_from(self, obj):
+        value = obj
+        for operation in self.Operations:
+            value = operation(value)
+        return value
+
+
+class Query:
+    def __init__(self):
+        self.Operations = []
+
+    def get(self, attr_or_key: str):
+        def _get(obj):
+            if obj is None:
+                return None
+            if hasattr(obj, attr_or_key):
+                return getattr(obj, attr_or_key)
+            try:
+                return obj[attr_or_key]
+            except:
+                return None
+        self.Operations.append(_get)
+        return self
+
+    def index(self, index: int):
+        def _index(items):
+            try:
+                return items[index]
+            except:
+                return None
+        self.Operations.append(_index)
+        return self
+
+    def first(self):
+        def _first(items):
+            try:
+                return items[0]
+            except:
+                return None
+        self.Operations.append(_first)
+        return self
+
+    def last(self):
+        def _last(items):
+            try:
+                return items[-1]
+            except:
+                return None
+        self.Operations.append(_last)
+        return self
+
+    def to_datetime(self, date_format: str):
+        def _to_datetime(obj):
+            try:
+                return datetime.datetime.strptime(obj, date_format)
+            except:
+                return None
+        self.Operations.append(_to_datetime)
+        return self
+
+    def custom(self, func: Callable[[Any], Any]):
+        self.Operations.append(func)
+        return self
+
+    def finalize(self) -> Retriever:
+        return Retriever(self.Operations)
---
+++ packages/obj-query/obj_query-0.8.tar.gz/obj_query-0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='obj_query',
+   version='0.8',
+   description='translate JSON and Python objects to Alteryx RecordInfos',
+   license="MIT",
+   author='Thomas Larsen',
+   author_email='larsenthomasj@gmail.com',
+   packages=['obj_query'],
+   install_requires=[],
+   url='https://github.com/tlarsen7572/obj_query',
+)
