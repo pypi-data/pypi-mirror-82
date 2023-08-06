120945258cde1d5f9aa02b8678321ac6ac29b745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package yautil/0.0.1/yautil-0.0.1.tar.gz

---
+++ packages/yautil/yautil-0.0.1.tar.gz/yautil-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yautil", # Replace with your own username
+    version="0.0.1",
+    author="Donghwi Kim",
+    author_email="dhkim09@kaist.ac.kr",
+    description="Yet Another Python util.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dhkim09a/pyutil",
+    packages=setuptools.find_packages(),
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+    python_requires='>=3.8',
+)
