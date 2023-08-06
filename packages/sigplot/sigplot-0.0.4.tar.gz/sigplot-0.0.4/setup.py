5a5d3e8660ac413afd626184243078d7596c4fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:43 +0000

    Add package sigplot/0.0.4/sigplot-0.0.4.tar.gz

---
+++ packages/sigplot/sigplot-0.0.4.tar.gz/sigplot-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+setuptools.setup(
+  name='sigplot',
+  packages=['sigplot'],
+  version='0.0.4',
+  license='MIT',
+  description='Graphical library for signal processing',
+  author='Hari',
+  author_email='harikalyanraman@gmail.com',
+  keywords=['signals', 'systems', 'convolution', 'fourier'],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'tk',
+          'matplotlib'
+      ],
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',
+  ],
+)
+
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
