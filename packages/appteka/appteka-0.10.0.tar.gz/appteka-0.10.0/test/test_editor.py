f4aae4338d7696a16f4640a928c03d4abdb6a2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:19 +0000

    Add package appteka/0.10.0/appteka-0.10.0.tar.gz

---
+++ packages/appteka/appteka-0.10.0.tar.gz/appteka-0.10.0/test/test_editor.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import unittest
+
+sys.path.insert(0, os.path.abspath("."))
+from appteka.pyqt import testing
+from appteka.pyqt.code_text_edit import CodeTextEdit
+
+
+class TestCodeTextEdit(unittest.TestCase):
+    """Tests for CodeTextEdit"""
+
+    def test_text(self):
+        app = testing.TestApp(self)
+
+        e = CodeTextEdit()
+
+        code = ''
+        code += '{\n'
+        code += '  "a": 1,\n'
+        code += '  "b": 2\n'
+        code += '}'
+        e.set_text(code)
+
+        app(e, [
+            "Some text is printed",
+            "Lines numbered",
+            "Current line is highlighted",
+            "Font is monospace",
+        ])
