9b7d58793cce45ee56ed5b7b939c64edd0f73167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:08 +0000

    Add package examtool/2.0.17/examtool-2.0.17.tar.gz

---
+++ packages/examtool/examtool-2.0.17.tar.gz/examtool-2.0.17/examtool/api/utils.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+
+def as_list(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        return list(func(*args, **kwargs))
+    return wrapped
+
+
+def sanitize_email(email):
+    return email.replace("_", r"\_")
+
+
