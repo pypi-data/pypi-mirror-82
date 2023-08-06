47bf7477f78ddee25e8becfce0b5a4118ea71f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:28 +0000

    Add package sbdscommons/0.0.2/sbdscommons-0.0.2.tar.gz

---
+++ packages/sbdscommons/sbdscommons-0.0.2.tar.gz/sbdscommons-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="sbdscommons",
+    version="0.0.2",
+    author="Olof Nilsson",
+    author_email="olof.nilsson@snusbolaget.se",
+    description="Packages shared between sb data science projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Snusbolaget/data-science",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
