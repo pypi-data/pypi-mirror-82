1df2b345d228dcba1a3dce9912276f27ac765d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:31 +0000

    Add package extras-require/0.2.2/extras_require-0.2.2.tar.gz

---
+++ packages/extras-require/extras_require-0.2.2.tar.gz/extras_require-0.2.2/__pkginfo__.py
@@ -0,0 +1,69 @@
+#  This file is managed by 'repo_helper'. Don't edit it directly.
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This file is distributed under the same license terms as the program it came with.
+#  There will probably be a file called LICEN[S/C]E in the same directory as this file.
+#
+#  In any case, this program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+#
+
+# stdlib
+import pathlib
+
+__all__ = [
+		"__copyright__",
+		"__version__",
+		"modname",
+		"pypi_name",
+		"__license__",
+		"__author__",
+		"short_desc",
+		"author",
+		"author_email",
+		"github_username",
+		"web",
+		"github_url",
+		"repo_root",
+		"install_requires",
+		"extras_require",
+		"project_urls",
+
+		"import_name",
+		]
+
+__copyright__ = """
+2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+"""
+
+__version__ = "0.2.2"
+modname = "extras_require"
+pypi_name = "extras_require"
+import_name = "sphinxcontrib.extras_require"
+__license__ = "BSD License"
+short_desc = 'Display a warning at the top of module documentation that it has additional requirements.'
+__author__ = author = 'Dominic Davis-Foster'
+author_email = 'dominic@davis-foster.co.uk'
+github_username = "domdfcoding"
+web = github_url = "https://github.com/domdfcoding/extras_require"
+repo_root = pathlib.Path(__file__).parent
+install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
+extras_require = {'all': []}
+
+
+
+conda_description = """Display a warning at the top of module documentation that it has additional requirements.
+
+
+Before installing please ensure you have added the following channels: domdfcoding, conda-forge"""
+__all__.append("conda_description")
+
+
+project_urls = {
+		"Documentation": "https://extras_require.readthedocs.io",
+		"Issue Tracker": f"{github_url}/issues",
+		"Source Code": github_url,
+		}
