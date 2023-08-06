a76c37c99ffa32b2aea6ca44a8c3906e07c0c609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:47 +0000

    Add package onnxruntime-tools/1.5.2/onnxruntime_tools-1.5.2.tar.gz

---
+++ packages/onnxruntime-tools/onnxruntime_tools-1.5.2.tar.gz/onnxruntime_tools-1.5.2/onnxruntime_tools/__init__.py
@@ -0,0 +1,26 @@
+###############################################################################
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+###############################################################################
+"""
+onnxruntime_tools
+this package includes transformers model optimization tools
+"""
+__version__ = "1.5.2"
+__git_version__ = "186f0668b0ef5557db1827f90e3ad777265afd1e"
+__author__ = "Microsoft Corporation"
+__producer__ = "onnxruntime_tools"
+
+import os
+import sys
+import types
+
+from . import transformers
+
+_transformers_path = os.path.join(os.path.dirname(__file__), 'transformers')
+sys.path.append(_transformers_path)
+
+from .transformers import optimizer  # noqa
+
+sys.modules[__name__ + '.optimizer'] = types.ModuleType('optimizer')
