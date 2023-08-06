171273289d729ba90d2293760ba3fbfe62b81c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package continuum/1.0.3/continuum-1.0.3-py3-none-any.whl

---
+++ packages/continuum/continuum-1.0.3-py3-none-any.whl/continuum/scenarios/data_incremental.py
@@ -0,0 +1,9 @@
+from continuum.scenarios import _BaseCLLoader
+
+
+class DataIncremental(_BaseCLLoader):
+    """[Not Implemented Yet] Continual Loader, generating datasets for the consecutive tasks.
+
+    Scenario: Classes and instances changes and may reappear (NIC scenario)
+    """
+    # TODO
