955b130c8e62eb59f856f93e6e76e8b626ab99bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:08 +0000

    Add package aurcore/1.1.37/aurcore-1.1.37-py3-none-any.whl

---
+++ packages/aurcore/aurcore-1.1.37-py3-none-any.whl/aurcore/__init__.py
@@ -0,0 +1,28 @@
+import asyncio
+import contextlib
+
+from .event import Event, EventRouter, EventRouterHost, Eventful
+from . import util
+from . import log
+
+
+def aiorun(startup, cleanup):
+   loop = asyncio.get_event_loop()
+   try:
+      loop.create_task(startup)
+      loop.run_forever()
+   except KeyboardInterrupt:
+      pass
+   finally:
+      loop.run_until_complete(cleanup)
+      try:
+         all_tasks = asyncio.gather(*asyncio.all_tasks(loop), return_exceptions=True)
+         all_tasks.cancel()
+         with contextlib.suppress(asyncio.CancelledError):
+            loop.run_until_complete(all_tasks)
+         loop.run_until_complete(loop.shutdown_asyncgens())
+      finally:
+         loop.close()
+
+
+__all__ = ["Event", "Eventful", "EventRouterHost", "EventRouter", "log", "util"]
