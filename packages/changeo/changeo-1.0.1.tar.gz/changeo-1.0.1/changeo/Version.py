aabbe00c8dd1aa062303c913f86d508d36addcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:57 +0000

    Add package changeo/1.0.1/changeo-1.0.1.tar.gz

---
+++ packages/changeo/changeo-1.0.1.tar.gz/changeo-1.0.1/changeo/Version.py
@@ -0,0 +1,9 @@
+"""
+Version and authorship information
+"""
+
+__author__    = 'Namita Gupta, Jason Anthony Vander Heiden'
+__copyright__ = 'Copyright 2020 Kleinstein Lab, Yale University. All rights reserved.'
+__license__   = 'GNU Affero General Public License 3 (AGPL-3)'
+__version__   = '1.0.1'
+__date__      = '2020.10.13'
