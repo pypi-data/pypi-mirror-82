ee2322168f758c0aad3ea8d6f3d769688e2a339b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package fedocal-messages/1.4.0/fedocal-messages-1.4.0.tar.gz

---
+++ packages/fedocal-messages/fedocal-messages-1.4.0.tar.gz/fedocal-messages-1.4.0/fedocal_messages/__init__.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2020  Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import pkg_resources
+from fedora_messaging import message
+
+
+from .messages import (  # noqa: F401
+    ReminderV1,
+    CalendarNewV1,
+    CalendarUpdateV1,
+    CalendarUploadV1,
+    CalendarDeleteV1,
+    CalendarClearV1,
+    MeetingNewV1,
+    MeetingUpdateV1,
+    MeetingDeleteV1,
+)
+
+
+def get_message_object_from_topic(topic):
+    """Returns the Message class corresponding to the topic."""
+
+    output = None
+
+    for entry_point in pkg_resources.iter_entry_points("fedora.messages"):
+        cls = entry_point.load()
+        if cls().topic == topic:
+            output = cls
+            break
+
+    if output is None:
+        output = message.Message
+
+    return output
