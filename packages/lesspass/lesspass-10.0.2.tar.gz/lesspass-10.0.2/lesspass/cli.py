6e60223e25bd7dfb3a8572c107ae2b337c006ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package lesspass/10.0.2/lesspass-10.0.2.tar.gz

---
+++ packages/lesspass/lesspass-10.0.2.tar.gz/lesspass-10.0.2/lesspass/cli.py
@@ -0,0 +1,145 @@
+import argparse
+import os
+
+from lesspass import version
+from lesspass import name
+from lesspass import description
+
+EXAMPLES = """
+examples:
+  # no symbols
+  lesspass site login masterpassword --no-symbols
+
+  # no symbols shortcut
+  lesspass site login masterpassword -lud
+
+  # only digits and length of 8
+  lesspass site login masterpassword -d -L8
+
+  # master password in env variable
+  LESSPASS_MASTER_PASSWORD="masterpassword" lesspass site login
+"""
+
+COPYRIGHT = """
+copyright:
+  Copyright © 2018 Guillaume Vincent <contact@lesspass.com>.  License GPLv3: GNU GPL version 3 <https://gnu.org/licenses/gpl.html>.
+  This is free software: you are free to change and redistribute it.  There is NO WARRANTY, to the extent permitted by law
+"""
+
+
+def range_type(value_string):
+    value = int(value_string)
+    if value not in range(5, 35 + 1):
+        raise argparse.ArgumentTypeError("%s is out of range, choose in [5-35]" % value)
+    return value
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        usage="lesspass SITE [LOGIN] [MASTER_PASSWORD] [OPTIONS]",
+        description=description,
+        epilog=EXAMPLES + COPYRIGHT,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=version.__version__
+    )
+    parser.add_argument(
+        "site", nargs="?", help="site used in the password generation (required)"
+    )
+    parser.add_argument(
+        "login", nargs="?", help="login used in the password generation. Default to ''."
+    )
+    parser.add_argument(
+        "master_password",
+        default=os.environ.get("LESSPASS_MASTER_PASSWORD", None),
+        nargs="?",
+        help="master password used in password generation. Default to LESSPASS_MASTER_PASSWORD env variable or prompt.",
+    )
+    parser.add_argument(
+        "-L",
+        "--length",
+        default=16,
+        choices=range(5, 35 + 1),
+        type=range_type,
+        help="password length (default: 16, min: 5, max: 35)",
+        metavar="[5-35]",
+    )
+    parser.add_argument(
+        "-C", "--counter", default=1, type=int, help="password counter (default: 1)"
+    )
+    parser.add_argument(
+        "-p",
+        "--prompt",
+        dest="prompt",
+        action="store_true",
+        help="prompt for values interactively",
+    )
+    parser.add_argument(
+        "-c",
+        "--copy",
+        dest="clipboard",
+        action="store_true",
+        help="copy the password to clipboard",
+    )
+    parser.add_argument(
+        "--exclude", default=None, help="exclude char from generated password",
+    )
+    parser.add_argument(
+        "--no-fingerprint",
+        dest="no_fingerprint",
+        action="store_true",
+        help="hide visual fingerprint of the master password when you type",
+    )
+    lowercase_group = parser.add_mutually_exclusive_group()
+    lowercase_group.add_argument(
+        "-l",
+        "--lowercase",
+        help="add lowercase in password",
+        dest="l",
+        action="store_true",
+    )
+    lowercase_group.add_argument(
+        "--no-lowercase",
+        help="remove lowercase from password",
+        dest="nl",
+        action="store_true",
+    )
+
+    uppercase_group = parser.add_mutually_exclusive_group()
+    uppercase_group.add_argument(
+        "-u",
+        "--uppercase",
+        dest="u",
+        help="add uppercase in password",
+        action="store_true",
+    )
+    uppercase_group.add_argument(
+        "--no-uppercase",
+        dest="nu",
+        help="remove uppercase from password",
+        action="store_true",
+    )
+
+    digits_group = parser.add_mutually_exclusive_group()
+    digits_group.add_argument(
+        "-d", "--digits", dest="d", help="add digits in password", action="store_true"
+    )
+    digits_group.add_argument(
+        "--no-digits",
+        dest="nd",
+        help="remove digits from password",
+        action="store_true",
+    )
+
+    symbols_group = parser.add_mutually_exclusive_group()
+    symbols_group.add_argument(
+        "-s", "--symbols", dest="s", help="add symbols in password", action="store_true"
+    )
+    symbols_group.add_argument(
+        "--no-symbols",
+        dest="ns",
+        help="remove symbols from password",
+        action="store_true",
+    )
+    return parser.parse_args(args)
---
+++ packages/lesspass/lesspass-10.0.2.tar.gz/lesspass-10.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from os import path
+
+from lesspass.version import __version__
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="lesspass",
+    version=__version__,
+    packages=["lesspass"],
+    author="Guillaume Vincent",
+    author_email="contact@lesspass.com",
+    description="LessPass stateless password generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    entry_points="""
+        [console_scripts]
+        lesspass=lesspass.core:main
+    """,
+    url="https://github.com/lesspass/lesspass",
+    license="GPL-3.0",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
