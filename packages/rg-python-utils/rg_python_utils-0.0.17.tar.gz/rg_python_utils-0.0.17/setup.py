893cff9889e9561103d77b0a41da0b549c1aa402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:46 +0000

    Add package rg-python-utils/0.0.17/rg_python_utils-0.0.17.tar.gz

---
+++ packages/rg-python-utils/rg_python_utils-0.0.17.tar.gz/rg_python_utils-0.0.17/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="rg_python_utils",
+    version="0.0.17",
+    author="Shailendra Sharma",
+    author_email="shail.rgames@gmail.com",
+    description="rg python util package",
+    long_description="None",
+    long_description_content_type="text/markdown",
+    url="https://github.com/shail-rgames/rg_python_utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
