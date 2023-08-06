2d22ef9c51153cd69e1c803ccbd75bd5ea159743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:02 +0000

    Add package keras4torch/0.3.0/keras4torch-0.3.0.tar.gz

---
+++ packages/keras4torch/keras4torch-0.3.0.tar.gz/keras4torch-0.3.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "rt", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="keras4torch",
+    version="0.3.0",
+    author="Keras4Torch Development Team",
+    author_email="blueloveTH@foxmail.com",
+    description="A Lightweight Keras API for Training PyTorch Models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blueloveTH/keras4torch",
+    packages=setuptools.find_packages(),
+    install_requires=['torch-summary==1.4.3', 'pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
