9e1e5ed9143ba2a30dfa286c082fb4ecd5737eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:28 +0000

    Add package carbonplan/0.1.0/carbonplan-0.1.0.tar.gz

---
+++ packages/carbonplan/carbonplan-0.1.0.tar.gz/carbonplan-0.1.0/carbonplan/tests/test_metapackage.py
@@ -0,0 +1,11 @@
+import importlib
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    'mod', ['carbonplan', 'carbonplan.data', 'carbonplan.forests', 'carbonplan.styles']
+)
+def test_import_submodules(mod):
+    mod = importlib.import_module(mod)
+    assert mod.version
