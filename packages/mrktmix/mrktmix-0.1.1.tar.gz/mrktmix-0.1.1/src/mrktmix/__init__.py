41c8d14782e329542fb8dd406a57f62fec98020c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:07 +0000

    Add package mrktmix/0.1.1/mrktmix-0.1.1.tar.gz

---
+++ packages/mrktmix/mrktmix-0.1.1.tar.gz/mrktmix-0.1.1/src/mrktmix/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.1'
+from mrktmix.transformation import apply_apl
+from mrktmix.transformation import apply_coef
+from mrktmix.transformation import assess_error
+from mrktmix.transformation import collapse_date
+from mrktmix.transformation import create_base
