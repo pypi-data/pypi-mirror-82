c500a0441006df2c66d66a288e98cbda5f2e8a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:54 +0000

    Add package potodo/0.14.1/potodo-0.14.1.tar.gz

---
+++ packages/potodo/potodo-0.14.1.tar.gz/potodo-0.14.1/potodo/_cache.py
@@ -0,0 +1,33 @@
+import logging
+import os
+import pickle
+from pathlib import Path
+from typing import cast
+from typing import Dict
+
+from potodo._po_file import PoFileStats
+
+
+def _get_cache_file_content(
+    path: str = ".potodo/cache.pickle",
+) -> Dict[Path, PoFileStats]:
+    logging.debug("Trying to load cache from %s", path)
+    try:
+        with open(path, "rb") as handle:
+            data = pickle.load(handle)
+    except FileNotFoundError:
+        logging.warning("No cache found")
+        return {}
+    else:
+        logging.debug("Found cache")
+        return cast(Dict[Path, PoFileStats], data)
+
+
+def _set_cache_content(
+    obj: Dict[Path, PoFileStats], path: str = ".potodo/cache.pickle"
+) -> None:
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    with open(path, "wb") as handle:
+        pickle.dump(obj, handle)
+    logging.debug("Set cache to %s", path)
---
+++ packages/potodo/potodo-0.14.1.tar.gz/potodo-0.14.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+import potodo
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="potodo",
+    version=potodo.__version__,
+    description="Will list all .po files that are to be transated",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # This is important!
+    author="Jules Lasne",
+    author_email="jules.lasne@gmail.com",
+    url="https://github.com/seluj78/potodo",
+    packages=["potodo"],
+    package_dir={"potodo": "potodo"},
+    entry_points={"console_scripts": ["potodo=potodo.potodo:main"]},
+    include_package_data=True,
+    install_requires=["polib", "requests", "simple-term-menu"],
+    license="MIT license",
+    zip_safe=False,
+    keywords="potodo",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires=">=3.6",
+)
