71dbbc9c2aeedbbd225e7f288f0e6754e3fa3349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:13 +0000

    Add package straxen/0.11.0/straxen-0.11.0.tar.gz

---
+++ packages/straxen/straxen-0.11.0.tar.gz/straxen-0.11.0/straxen/__init__.py
@@ -0,0 +1,21 @@
+__version__ = '0.11.0'
+
+from .common import *
+from .itp_map import *
+from .rundb import *
+from .online_monitor import *
+from .matplotlib_utils import *
+from .mini_analysis import *
+from .misc import *
+
+from .gain_models import *
+from .hitfinder_thresholds import *
+
+from . import plugins
+from .plugins import *
+
+from . import analyses
+
+# Do not make all contexts directly available under straxen.
+# Otherwise we have straxen.demo() etc.
+from . import contexts
