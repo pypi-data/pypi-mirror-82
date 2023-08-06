ff3f4db8b704a21ca1f4fef4751c701f19c1343e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:25 +0000

    Add package hi2020/1.0.1/hi2020-1.0.1-py3-none-any.whl

---
+++ packages/hi2020/hi2020-1.0.1-py3-none-any.whl/hi2020/say.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+@File  : helloworld.py
+@Author: HappyLay
+@Date  : 2020/10/15 13:33
+@Desc  : 安徽理工大学
+"""
+__author__ = 'HappyLay'
+
+
+def hi():
+    """
+    向世界的第一个问候(*^▽^*)
+    :return: None
+    """
+    print("hello world , 2020🇨🇳加油，世界ヾ(◍°∇°◍)ﾉﾞ。")
+    return None
