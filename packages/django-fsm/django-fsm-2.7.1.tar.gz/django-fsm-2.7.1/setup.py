486e945b002f0fbd2ceb7a0846d9d741cbfe8115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:49 +0000

    Add package django-fsm/2.7.1/django-fsm-2.7.1.tar.gz

---
+++ packages/django-fsm/django-fsm-2.7.1.tar.gz/django-fsm-2.7.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='django-fsm',
+    version='2.7.1',
+    description='Django friendly finite state machine support.',
+    author='Mikhail Podgurskiy',
+    author_email='kmmbvnr@gmail.com',
+    url='http://github.com/kmmbvnr/django-fsm',
+    keywords="django",
+    packages=['django_fsm', 'django_fsm.management', 'django_fsm.management.commands'],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT License',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        "Framework :: Django",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
