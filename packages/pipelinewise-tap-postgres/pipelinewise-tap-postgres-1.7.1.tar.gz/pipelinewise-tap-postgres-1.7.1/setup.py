0e4f5fe69248c1da541e3b555f53d644b83e8ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:47 +0000

    Add package pipelinewise-tap-postgres/1.7.1/pipelinewise-tap-postgres-1.7.1.tar.gz

---
+++ packages/pipelinewise-tap-postgres/pipelinewise-tap-postgres-1.7.1.tar.gz/pipelinewise-tap-postgres-1.7.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='pipelinewise-tap-postgres',
+      version='1.7.1',
+      description='Singer.io tap for extracting data from PostgresSQL - PipelineWise compatible',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='TransferWise',
+      url='https://github.com/transferwise/pipelinewise-tap-postgres',
+      classifiers=[
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      install_requires=[
+          'pipelinewise-singer-python==1.*',
+          'psycopg2-binary==2.8.5',
+          'strict-rfc3339==0.7',
+      ],
+      extras_require={
+          "test": [
+              'pytest==5.4',
+              'pylint==2.4.4',
+              'pytest-cov==2.10'
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-postgres=tap_postgres:main
+      ''',
+      packages=['tap_postgres', 'tap_postgres.sync_strategies']
+)
