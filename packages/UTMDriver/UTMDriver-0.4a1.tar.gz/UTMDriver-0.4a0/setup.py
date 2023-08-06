fc4e999aaa9a7f5b4a20ed40590831d7c2d3a24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:54 +0000

    Add package UTMDriver/0.4a0/UTMDriver-0.4a1.tar.gz

---
+++ packages/UTMDriver/UTMDriver-0.4a1.tar.gz/UTMDriver-0.4a0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import UTMDriver
+
+setup(
+    name='UTMDriver',
+    install_requires=[
+        'chardet==3.0.4',
+        'decorator==4.4.2',
+        'idna==2.10',
+        'Jinja2==2.11.2',
+        'lxml==4.5.2',
+        'MarkupSafe==1.1.1',
+        'requests==2.24.0',
+        'urllib3==1.25.10',
+    ],
+    version=UTMDriver.__version__,
+    packages=find_packages(),
+    url='https://github.com/maxpoint2point/UTMDriver',
+    license='Apache-2.0 License',
+    author='Максим',
+    author_email='maxpoint2point@gmail.com',
+    description='UTMDriver for EGAIS'
+)
