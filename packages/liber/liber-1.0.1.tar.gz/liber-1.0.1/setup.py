14ff98110f4cad369ce1e768d61c507994719517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:56 +0000

    Add package liber/1.0.1/liber-1.0.1.tar.gz

---
+++ packages/liber/liber-1.0.1.tar.gz/liber-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open('readme.md', encoding='utf8') as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='liber',
+    version='1.0.1',
+    author='RimoChan',
+    author_email='the@librian.net',
+    description='A library to compile liber.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/librian-center/liber-language',
+    packages=['liber'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
