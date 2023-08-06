57d95ec1149c33d284d5d704d904cc2a19121fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:14 +0000

    Add package prevision-quantum-nn/1.0/prevision-quantum-nn-1.0.tar.gz

---
+++ packages/prevision-quantum-nn/prevision-quantum-nn-1.0.tar.gz/prevision-quantum-nn-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(name='prevision-quantum-nn',
+      version='1.0',
+      description='Prevision Automating Quantum Neural Networks Applications',
+      author='prevision.io',
+      author_email='prevision@prevision.io',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      python_requires = ">=3.6.8"
+)
