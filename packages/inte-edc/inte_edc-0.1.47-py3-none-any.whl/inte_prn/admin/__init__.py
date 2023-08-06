921c3c21e18a4a0174335aace70af6a808368d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:10 +0000

    Add package inte-edc/0.1.47/inte_edc-0.1.47-py3-none-any.whl

---
+++ packages/inte-edc/inte_edc-0.1.47-py3-none-any.whl/inte_prn/admin/__init__.py
@@ -0,0 +1,12 @@
+from .autocomplete_admin import (
+    UnblindingRequestorUserAdmin,
+    UnblindingReviewerUserAdmin,
+)
+from .end_of_study_admin import EndOfStudyAdmin
+from .integrated_care_clinic_registration_admin import (
+    IntegratedCareClinicRegistrationAdmin,
+)
+from .onschedule_admin import OnScheduleHivAdmin, OnScheduleNcdAdmin
+from .protocol_deviation_violation_admin import ProtocolDeviationViolationAdmin
+from .unblinding_request_admin import UnblindingRequestAdmin
+from .unblinding_review_admin import UnblindingReviewAdmin
---
+++ packages/inte-edc/inte_edc-0.1.47-py3-none-any.whl/inte_subject/forms/subject_visit_form.py
@@ -0,0 +1,78 @@
+from django import forms
+from edc_constants.constants import OTHER, STUDY_DEFINED_TIMEPOINT
+from edc_form_validators import FormValidatorMixin
+from edc_sites.forms import SiteModelFormMixin
+from edc_visit_tracking.form_validators import VisitFormValidator
+from inte_prn.icc_registered import (
+    InterventionSiteNotRegistered,
+    is_icc_registered_site,
+)
+from inte_prn.models import IntegratedCareClinicRegistration
+from inte_sites.is_intervention_site import NotInterventionSite
+from inte_subject.constants import INTEGRATED
+
+from ..models import SubjectVisit
+
+
+class SubjectVisitFormValidator(VisitFormValidator):
+    validate_missed_visit_reason = False
+
+    def clean(self):
+        super().clean()
+        self.m2m_other_specify(
+            OTHER, m2m_field="clinic_services", field_other="clinic_services_other",
+        )
+
+        self.validate__clinic_services()
+        self.validate__health_services()
+
+    def validate__clinic_services(self):
+        selections = self.get_m2m_selected("clinic_services")
+        if (
+            self.cleaned_data.get("appointment").visit_code_sequence == 0
+            and STUDY_DEFINED_TIMEPOINT not in selections
+        ):
+            raise forms.ValidationError(
+                {"clinic_services": "This is scheduled study visit."}
+            )
+        elif (
+            self.cleaned_data.get("appointment").visit_code_sequence != 0
+            and STUDY_DEFINED_TIMEPOINT in selections
+        ):
+            raise forms.ValidationError(
+                {"clinic_services": "This is not a scheduled study visit."}
+            )
+
+    def validate__health_services(self):
+        selections = self.get_m2m_selected("health_services")
+        if INTEGRATED in selections:
+            try:
+                is_icc_registered_site(
+                    report_datetime=self.cleaned_data.get("report_datetime")
+                )
+            except NotInterventionSite:
+                raise forms.ValidationError(
+                    {
+                        "health_services": "This site does not have an integrated care clinic."
+                    }
+                )
+            except InterventionSiteNotRegistered:
+                raise forms.ValidationError(
+                    {
+                        "health_services": (
+                            "Integrated Care Clinic not "
+                            "open at the time of this report. See facility form "
+                            f"`{IntegratedCareClinicRegistration._meta.verbose_name}`."
+                        )
+                    }
+                )
+        self.m2m_single_selection_if(INTEGRATED, m2m_field="health_services")
+
+
+class SubjectVisitForm(SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
+
+    form_validator_cls = SubjectVisitFormValidator
+
+    class Meta:
+        model = SubjectVisit
+        fields = "__all__"
