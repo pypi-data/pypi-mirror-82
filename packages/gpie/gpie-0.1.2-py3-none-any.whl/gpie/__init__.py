b020250b6297bf55db1c12eb07ed83706542cc7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:51 +0000

    Add package gpie/0.1.2/gpie-0.1.2-py3-none-any.whl

---
+++ packages/gpie/gpie-0.1.2-py3-none-any.whl/gpie/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# GPie: Gaussian Process tiny explorer
+
+__version__ = '0.1.2'
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+from . import infer
+from . import kernel
