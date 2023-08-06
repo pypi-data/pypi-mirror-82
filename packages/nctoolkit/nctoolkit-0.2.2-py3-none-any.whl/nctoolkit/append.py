423d450a2f2a608ea2be68380a414ab1c7a84673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:33 +0000

    Add package nctoolkit/0.2.2/nctoolkit-0.2.2-py3-none-any.whl

---
+++ packages/nctoolkit/nctoolkit-0.2.2-py3-none-any.whl/nctoolkit/append.py
@@ -0,0 +1,51 @@
+import os
+import warnings
+
+
+def append(self, x=None):
+    """
+    Add new file(s) to a dataset
+
+    Parameters
+    -------------
+    x: str or list
+     File path(s) to add to the dataset
+    """
+
+    # run, as it makes no sense to add files while commands are waiting to run
+    self.run()
+
+    if x is None:
+        raise TypeError("Please supply files")
+
+    if type(x) is str:
+        x = [x]
+    len_x = len(x)
+
+    x = list(set(x))
+
+    if len(x) < len_x:
+        warnings.warn("Duplicates removed from files")
+
+    # check files are not already in the dataset
+
+    if type(self.current) is list:
+        check_list = self.current
+    else:
+        check_list = [self.current]
+
+    for ff in x:
+        if ff in check_list:
+            raise ValueError(
+                "You are trying to add a file that is already in the dataset"
+            )
+
+    for ff in x:
+        if os.path.exists(ff) is False:
+            raise ValueError(f"{ff} does not exist!")
+
+    for ff in x:
+        if type(self.current) is str:
+            self.current = [self.current, ff]
+        else:
+            self.current.append(ff)
---
+++ packages/nctoolkit/nctoolkit-0.2.2-py3-none-any.whl/nctoolkit/zip.py
@@ -0,0 +1,13 @@
+from nctoolkit.runthis import run_this
+
+
+def zip(self):
+    """
+    Zip the dataset
+    This will compress the files within the dataset. This works lazily.
+    """
+    self._zip = True
+
+    if len(self.history) == len(self._hold_history):
+        cdo_command = "cdo copy "
+        run_this(cdo_command, self, output="ensemble")
