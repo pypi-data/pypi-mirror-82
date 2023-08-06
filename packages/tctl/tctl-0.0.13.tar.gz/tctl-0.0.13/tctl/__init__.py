786dcbf2c25134614b0ee156c1c45e7192250543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package tctl/0.0.13/tctl-0.0.13.tar.gz

---
+++ packages/tctl/tctl-0.0.13.tar.gz/tctl-0.0.13/tctl/__init__.py
@@ -0,0 +1,45 @@
+import click
+import sys
+
+from . import version
+
+# internals
+from . import config
+from . import brokers
+from . import accounts
+from . import assets
+from . import strategies
+from . import tradehooks
+from . import monitors
+from . import orders
+from . import trading
+from . import tokens
+from . import upgrade
+
+
+if "--version" in sys.argv or "-V" in sys.argv:
+    click.echo("\ntctl {version}".format(version=version.version))
+    click.echo("Copyrights (c) Tradologics, Inc.")
+    click.echo("https://tradologics.com")
+    sys.exit()
+
+
+cli = click.CommandCollection(sources=[
+    config.cli,
+    brokers.cli,
+    accounts.cli,
+    assets.cli,
+    strategies.cli,
+    tradehooks.cli,
+    monitors.cli,
+    orders.cli,
+    trading.cli,
+    tokens.cli,
+    upgrade.cli
+])
+
+
+__version__ = version.version
+__author__ = "Tradologics, Inc"
+
+__all__ = ['cli']
