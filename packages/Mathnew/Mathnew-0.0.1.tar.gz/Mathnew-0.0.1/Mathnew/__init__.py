db5b7138e98a4038f1afbdb1ac6763c67638541c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:57 +0000

    Add package Mathnew/0.0.1/Mathnew-0.0.1.tar.gz

---
+++ packages/Mathnew/Mathnew-0.0.1.tar.gz/Mathnew-0.0.1/Mathnew/__init__.py
@@ -0,0 +1,16 @@
+import math
+
+def sqroot(a):
+    return math.sqrt(a)
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
---
+++ packages/Mathnew/Mathnew-0.0.1.tar.gz/Mathnew-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a very simple calculator that take two numbers for adding, subtracting, multiplying and dividing. Takes a single number for finding Square-root.
