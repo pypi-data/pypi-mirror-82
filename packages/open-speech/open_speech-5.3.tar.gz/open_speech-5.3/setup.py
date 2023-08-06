55b0770c1c8c13ab1c6d638a66d3701ca7c2053b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:27 +0000

    Add package open-speech/5.3/open_speech-5.3.tar.gz

---
+++ packages/open-speech/open_speech-5.3.tar.gz/open_speech-5.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(name="open_speech",
+    version="5.3",
+    author="Dimitry Ishenko",
+    author_email="dimitry.ishenko@gmail.com",
+    description="Open Speech Datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dimitry-ishenko-ml/open-speech",
+    packages=setuptools.find_packages(),
+    install_requires=[ "pandas", "tensorflow" ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
