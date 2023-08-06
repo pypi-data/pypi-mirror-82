1f2f429885e3d429979d7dee4115807fe28c327f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:43 +0000

    Add package django-mp-basement/1.9/django-mp-basement-1.9.tar.gz

---
+++ packages/django-mp-basement/django-mp-basement-1.9.tar.gz/django-mp-basement-1.9/basement/static/newtabaction.js
@@ -0,0 +1,19 @@
+
+NewTabAction = function ($target) {
+
+    function handleTargetClick() {
+
+        var url = $target.data('url');
+
+        $target.prop('disabled', true);
+
+        $.post(url, {}, function (response) {
+            var win = window.open(response, '_blank');
+            win.focus();
+        }).fail(function (response) {
+            $.notify({message: response.responseText}, {type: 'danger'});
+        });
+    }
+
+    $target.click(handleTargetClick);
+};
