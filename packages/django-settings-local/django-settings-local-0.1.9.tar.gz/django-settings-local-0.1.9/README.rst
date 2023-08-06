1beaecced5337c48bd437de019fcd3b1ec45207b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:43 +0000

    Add package django-settings-local/0.1.9/django-settings-local-0.1.9.tar.gz

---
+++ packages/django-settings-local/django-settings-local-0.1.9.tar.gz/django-settings-local-0.1.9/README.rst
@@ -0,0 +1,45 @@
+.. role:: python(code)
+   :language: python
+
+####################################################
+django-settings-local - Manage Django Local Settings
+####################################################
+
+.. image:: https://badge.fury.io/py/django-settings-local.svg
+        :target: http://badge.fury.io/py/django-settings-local
+.. image:: https://github.com/fasih/django-settings-local/workflows/Test%20Python%20Package/badge.svg
+        :target: https://github.com/fasih/django-settings-local/actions?query=workflow%3A%22Test+Python+Package%22
+.. image:: https://codecov.io/gh/fasih/django-settings-local/branch/develop/graph/badge.svg?token=YDO44RQBSS
+      :target: https://codecov.io/gh/fasih/django-settings-local
+    
+*****
+Usage
+*****
+
+0. Installation
+===============
+
+The preferred instalation method is directly from pypi:
+
+.. code:: console
+
+   pip install -U django-settings-local
+
+
+1. Quickstart
+=============
+
+In your Django project app directory:
+
+.. code:: console
+
+   dsl
+   
+2. Quickhelp
+=============
+
+Quick help from simple console script:
+
+.. code:: console
+
+   dsl -h
---
+++ packages/django-settings-local/django-settings-local-0.1.9.tar.gz/django-settings-local-0.1.9/src/django_settings_local/__init__.py
@@ -0,0 +1,34 @@
+import importlib
+import os
+
+from pathlib import Path
+from .cute_python import snek
+
+__version__ = '0.1.9'
+
+DSL_CACHE = os.environ.get("DSL_CACHE")
+
+def pritty_print(msg):
+    print('\n','#'*len(msg),'\n',msg,'\n','#'*len(msg))
+
+def import_local(g):
+    CWD = Path(os.getcwd())
+    PWD = Path(g['__file__']).resolve(strict=True).parent
+    PROJECT_APP = ".".join(PWD.relative_to(CWD).parts)
+    try:
+        msg = "Django local settings applied"
+        settings_local = importlib.import_module(".settings_local", PROJECT_APP)
+        for flag in dir(settings_local):
+            if flag.startswith("__"):
+                continue
+            g[flag] = getattr(settings_local, flag)
+        settings_local.patch_globals(g)
+    except ImportError:
+        msg = "Use `settings_local.py` in the project app to override global settings"
+    except (AttributeError, TypeError):
+        msg = "For global settings mutation, see format of settings_local.py `dsl -f`"
+    finally:
+        if not DSL_CACHE:
+            print(snek)
+            pritty_print(msg)
+        os.environ.setdefault("DSL_CACHE", "PRINTED")
