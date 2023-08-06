d8e68fe725a9f458d8243b9185ad3ad748a8856a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:05 +0000

    Add package djangocms-pathomation/1.1/djangocms-pathomation-1.1.tar.gz

---
+++ packages/djangocms-pathomation/djangocms-pathomation-1.1.tar.gz/djangocms-pathomation-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='djangocms-pathomation',
+    version='1.1',
+    author='Pathomation',
+    author_email='info@pathomation.com',
+    packages=find_packages(),
+)
+
+
+
