ef76488d05c50b960e2ade81ca12f9aa28abcabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:32 +0000

    Add package wifiqr/1.2/wifiqr-1.2.tar.gz

---
+++ packages/wifiqr/wifiqr-1.2.tar.gz/wifiqr-1.2/README.md
@@ -0,0 +1,5 @@
+# WIFI QR Create and Generate with QR for Debian based
+'''
+pip3 install wifiqr
+'''
+[pipy](https://pypi.org/project/wifiqr/)
---
+++ packages/wifiqr/wifiqr-1.2.tar.gz/wifiqr-1.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools 
+setuptools.setup (
+    name = "wifiqr",
+    version="1.2",
+    author="Pyae Phyo Hein",
+    author_email="pyaephyohein.info.3326@gmail.com",
+    descripton="Wifi Qr creater and generator for linux",
+    packages=["wifiqr"],
+    entry_points = {
+        'console_scripts' : ['wifiqr=wifiqr.wifiqr:wifiqr']
+    },
+    install_requires=[
+        'qrcode'
+    ]
+    )
