a5568f12568b9f9af9f57071014348710712ae8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package basic-probability-distributions/0.1/basic_probability_distributions-0.1.tar.gz

---
+++ packages/basic-probability-distributions/basic_probability_distributions-0.1.tar.gz/basic_probability_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='basic_probability_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['basic_probability_distributions'],
+      zip_safe=False)
