57cf7a7677871e8afcaa13c14534273cd5643d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:49 +0000

    Add package composable/0.2.1/composable-0.2.1.tar.gz

---
+++ packages/composable/composable-0.2.1.tar.gz/composable-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['composable']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-forge>=18.6,<19.0', 'toolz>=0.11.1,<0.12.0']
+
+setup_kwargs = {
+    'name': 'composable',
+    'version': '0.2.1',
+    'description': 'Callable functions that be composed/piped using >> and <<',
+    'long_description': None,
+    'author': 'Todd Iverson',
+    'author_email': 'tiverson@winona.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
