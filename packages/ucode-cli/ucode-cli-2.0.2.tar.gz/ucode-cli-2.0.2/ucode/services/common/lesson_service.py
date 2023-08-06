34682622b8d1a7f56e392c0ad86557ce31a59d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:22 +0000

    Add package ucode-cli/2.0.2/ucode-cli-2.0.2.tar.gz

---
+++ packages/ucode-cli/ucode-cli-2.0.2.tar.gz/ucode-cli-2.0.2/ucode/services/common/lesson_service.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import logging
+
+__author__ = 'ThucNC'
+_logger = logging.getLogger(__name__)
+
+
+class LessonService:
+    @staticmethod
+    def create_quiz():
+        pass
+
+
+if __name__ == "__main__":
+    base_folder = "../problems/lessons"
+    LessonService.create_quiz()
