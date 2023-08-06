6f7f8323e9c92d65360d83fd72d3429930600608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:46 +0000

    Add package observable-jupyter/0.1.8/observable_jupyter-0.1.8-py3-none-any.whl

---
+++ packages/observable-jupyter/observable_jupyter-0.1.8-py3-none-any.whl/observable_jupyter/wrapper_bundle.js
@@ -0,0 +1,30 @@
+var ObservableJupyterWrapper = (function (exports) {
+  'use strict';
+
+  function getFrameByEvent(event) {
+    return [...document.getElementsByTagName("iframe")].filter(iframe => {
+      return iframe.contentWindow === event.source;
+    })[0];
+  }
+
+  // Each embed gets its own event listener.
+  function listenToSize(iframe) {
+    function onMessage(msg) {
+      if (!document.body.contains(iframe)) {
+        // iframe is gone
+        removeEventListener("message", onMessage);
+      }
+      const senderIframe = getFrameByEvent(msg);
+      if (msg.data.type === "iframeSize" && senderIframe === iframe) {
+        iframe.height = msg.data.height;
+      }
+    }
+
+    window.addEventListener("message", onMessage);
+  }
+
+  exports.listenToSize = listenToSize;
+
+  return exports;
+
+}({}));
