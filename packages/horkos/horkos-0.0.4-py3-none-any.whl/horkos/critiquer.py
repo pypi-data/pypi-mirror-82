dc9bde25cd48587456da32abc31d4843f656f0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:40 +0000

    Add package horkos/0.0.4/horkos-0.0.4-py3-none-any.whl

---
+++ packages/horkos/horkos-0.0.4-py3-none-any.whl/horkos/critiquer.py
@@ -0,0 +1,18 @@
+############################
+#
+# WITHIN DATASET
+#
+############################
+#
+# Fields
+# - Naming
+# -
+
+# Data sets
+
+
+# Field naming
+# Data set naming
+# Names should mean the same thing
+# Type consistency
+# Documentation length
