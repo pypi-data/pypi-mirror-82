94f6899bd9d3bd4b726a267233fb61f9a74e67fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:36 +0000

    Add package ptonppl/0.0.5/ptonppl-0.0.5.tar.gz

---
+++ packages/ptonppl/ptonppl-0.0.5.tar.gz/ptonppl-0.0.5/src/ptonppl/__init__.py
@@ -0,0 +1,12 @@
+
+from ptonppl.control import search
+from ptonppl.abstract import AbstractPtonPerson
+
+__author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
+
+__version__ = "0.0.5"
+
+__all__ = [
+    "search",
+    "AbstractPtonPerson",
+]
