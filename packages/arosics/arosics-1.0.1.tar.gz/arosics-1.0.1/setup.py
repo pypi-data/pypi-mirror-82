573550d7a68be2233c2cab128d029b5c771ac399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:51 +0000

    Add package arosics/1.0.1/arosics-1.0.1.tar.gz

---
+++ packages/arosics/arosics-1.0.1.tar.gz/arosics-1.0.1/setup.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# AROSICS - Automated and Robust Open-Source Image Co-Registration Software
+#
+# Copyright (C) 2017-2020  Daniel Scheffler (GFZ Potsdam, daniel.scheffler@gfz-potsdam.de)
+#
+# This software was developed within the context of the GeoMultiSens project funded
+# by the German Federal Ministry of Education and Research
+# (project grant code: 01 IS 14 010 A-C).
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+version = {}
+with open("arosics/version.py") as version_file:
+    exec(version_file.read(), version)
+
+requirements = [
+    'cmocean',
+    'folium>=0.6.0',
+    'gdal',
+    'geojson',
+    'geoarray>=0.9.0',
+    'geopandas',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'plotly',
+    'pyfftw',
+    'pykrige',
+    'py_tools_ds>=0.14.28',
+    'scikit-image',
+    'shapely',
+    'six',
+]
+
+setup_requirements = [
+    'setuptools',
+    'setuptools-git'
+]
+
+test_requirements = requirements + ['coverage', 'nose', 'nose-htmloutput', 'rednose']
+
+setup(
+    name='arosics',
+    version=version['__version__'],
+    description="An Automated and Robust Open-Source Image Co-Registration Software for Multi-Sensor Satellite Data",
+    long_description=readme + '\n\n' + history,
+    author="Daniel Scheffler",
+    author_email='daniel.scheffler@gfz-potsdam.de',
+    url='https://gitext.gfz-potsdam.de/danschef/arosics',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    scripts=["bin/arosics_cli.py"],
+    install_requires=requirements,
+    license="GPL-3.0-or-later",
+    zip_safe=False,
+    keywords=['arosics', 'image co-registration', 'geometric pre-processing', 'remote sensing', 'sensor fusion'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
