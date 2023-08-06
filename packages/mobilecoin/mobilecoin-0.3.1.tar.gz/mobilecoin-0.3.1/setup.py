9499fd1a7e888525ab0cd01670ac66b83ee904fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package mobilecoin/0.3.1/mobilecoin-0.3.1.tar.gz

---
+++ packages/mobilecoin/mobilecoin-0.3.1.tar.gz/mobilecoin-0.3.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mobilecoin",
+    version="0.3.1",
+    author="MobileCoin",
+    author_email="support@mobilecoin.com",
+    description="Python bindings for the MobileCoin daemon API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mobilecoinofficial/mobilecoin/tree/master/mobilecoind/clients/python/lib",
+    packages=setuptools.find_packages(exclude=("test*",)),
+    package_data={'mobilecoin': ['py.typed']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=['grpcio', 'grpcio-tools'],
+)
