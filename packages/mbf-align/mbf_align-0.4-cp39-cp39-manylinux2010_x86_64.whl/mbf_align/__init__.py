d03a9eea5343f610c8726be91a8dc11568359abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:51 +0000

    Add package mbf-align/0.4/mbf_align-0.4-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/mbf-align/mbf_align-0.4-cp39-cp39-manylinux2010_x86_64.whl/mbf_align/__init__.py
@@ -0,0 +1,11 @@
+from . import fastq2, post_process
+from .lanes import Sample, AlignedSample
+from .exceptions import PairingError
+from . import strategies
+from .strategies import *  # noqa:F403,F401
+
+
+__all__ = ["Sample", "fastq2", "PairingError", "AlignedSample", "post_process"]
+__all__.extend([x for x in dir(strategies) if x.startswith("FASTQs")])
+
+__version__ = '0.4'
