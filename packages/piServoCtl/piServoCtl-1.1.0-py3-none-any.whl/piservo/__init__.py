0ac2ad2ab69fa5b0df7cf53acaf8449daf01bf9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:23 +0000

    Add package piServoCtl/1.1.0/piServoCtl-1.1.0-py3-none-any.whl

---
+++ packages/piServoCtl/piServoCtl-1.1.0-py3-none-any.whl/piservo/__init__.py
@@ -0,0 +1,10 @@
+from piservo.servo import *
+
+__copyright__    = 'Copyright (C) 2020 naoto64'
+__version__      = '1.1.0'
+__license__      = 'MIT License'
+__author__       = 'naoto64'
+__author_email__ = 'naoto64.2000@gmail.com'
+__url__          = 'https://github.com/naoto64/piServoCtl'
+
+__all__ = ['Servo', 'Drive']
