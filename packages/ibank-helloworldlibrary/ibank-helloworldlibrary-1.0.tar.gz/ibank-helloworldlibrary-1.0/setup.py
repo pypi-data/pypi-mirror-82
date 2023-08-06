449daf157c59ce472db4324223c9e4d8001f82cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:49 +0000

    Add package ibank-helloworldlibrary/1.0/ibank-helloworldlibrary-1.0.tar.gz

---
+++ packages/ibank-helloworldlibrary/ibank-helloworldlibrary-1.0.tar.gz/ibank-helloworldlibrary-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ibank-helloworldlibrary",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='Nattawut',
+    author_email='gaamo.22@gmail.com',
+)
