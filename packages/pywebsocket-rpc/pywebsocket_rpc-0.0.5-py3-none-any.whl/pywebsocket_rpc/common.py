785017280a41fa1c6fe1571fd0de2a3e6c9864b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:29 +0000

    Add package pywebsocket-rpc/0.0.5/pywebsocket_rpc-0.0.5-py3-none-any.whl

---
+++ packages/pywebsocket-rpc/pywebsocket_rpc-0.0.5-py3-none-any.whl/pywebsocket_rpc/common.py
@@ -0,0 +1,22 @@
+import uuid
+from typing import Awaitable, Callable, NamedTuple
+
+from .proto.gen.node_pb2 import Direction, NodeMessage
+
+IncomingRequestHandler = Callable[[bytes], Awaitable[bytes]]
+
+
+class Token(NamedTuple):
+    value: str
+
+
+def _contstruct_node_message(data: bytes, direction: Direction, id=None) -> NodeMessage:
+    node_msg = NodeMessage()
+    if id is None:
+        node_msg.id = str(uuid.uuid4())
+    else:
+        node_msg.id = id
+    node_msg.bytes = data
+    node_msg.direction = direction
+
+    return node_msg
