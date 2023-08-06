1019fffc28ee77f84dbea0b1dd31506296d2fcc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:00 +0000

    Add package pyChakraCore/0.1.0/pyChakraCore-0.1.0.tar.gz

---
+++ packages/pyChakraCore/pyChakraCore-0.1.0.tar.gz/pyChakraCore-0.1.0/test.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+"""
+Author:wistn
+since:2020-10-20
+LastEditTime:2020-10-20
+LastEditors:Do not edit
+Description:
+"""
+import sys
+import os
+from pyChakraCore import pyChakraCore
+
+
+if sys.argv.__len__() > 1 and os.path.isfile(sys.argv[1]) and ".js" in sys.argv[1]:
+    with open(sys.argv[1], "r", encoding="utf-8") as fs:
+        jscode = fs.read()
+else:
+    jscode = "console.log(typeof (1 + 1) == 'number')"
+vm = pyChakraCore()
+vm.run(jscode)
+
