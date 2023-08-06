f3ec65097fbf43667593ffdb368da5997921c9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package papajohnsapi-mehtaarn000/0.2/papajohnsapi-mehtaarn000-0.2.tar.gz

---
+++ packages/papajohnsapi-mehtaarn000/papajohnsapi-mehtaarn000-0.2.tar.gz/papajohnsapi-mehtaarn000-0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="papajohnsapi-mehtaarn000", # Replace with your own username
+    version="0.2",
+    author="mehtaarn000",
+    author_email="arnavmehta1977@gmail.com",
+    description="A python module that will allow users to use selenium to order pizza from Papa Johns.",
+    long_description = long_description,
+    url="https://github.com/mehtaarn000/papajohnsapi.com",
+    packages=['papajohnsapi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
