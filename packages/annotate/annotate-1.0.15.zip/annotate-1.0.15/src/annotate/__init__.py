f6dcfdc29a87fed02809373d358af52fc0074332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package annotate/1.0.15/annotate-1.0.15.zip

---
+++ packages/annotate/annotate-1.0.15.zip/annotate-1.0.15/src/annotate/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2012-2020 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+from .__about__ import * ; del __about__  # noqa
+from ._annotate import * ; del _annotate  # noqa
