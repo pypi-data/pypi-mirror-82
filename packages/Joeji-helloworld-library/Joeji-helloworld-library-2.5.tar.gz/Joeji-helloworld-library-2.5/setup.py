a1e79279afadcc39a9cb6584709f4b8133d8342e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:51 +0000

    Add package Joeji-helloworld-library/2.5/Joeji-helloworld-library-2.5.tar.gz

---
+++ packages/Joeji-helloworld-library/Joeji-helloworld-library-2.5.tar.gz/Joeji-helloworld-library-2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Joeji-helloworld-library",
+    version='2.5',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='JOEJIKIMUJI',
+    author_email='jirayusanrit@gmail.com',
+)
