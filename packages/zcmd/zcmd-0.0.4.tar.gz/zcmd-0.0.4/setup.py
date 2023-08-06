125607f0eaae117861dcea6d2a7d22b452fb8fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:07 +0000

    Add package zcmd/0.0.4/zcmd-0.0.4.tar.gz

---
+++ packages/zcmd/zcmd-0.0.4.tar.gz/zcmd-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zcmd",
+    version="0.0.4",
+    author="ouczbs",
+    author_email="ouczbs@qq.com",
+    description="A package use for jupyter notebook",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ouczbs/zcmd",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
