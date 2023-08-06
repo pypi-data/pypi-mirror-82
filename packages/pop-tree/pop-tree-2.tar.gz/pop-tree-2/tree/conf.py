0fdd95db2ee8369b9735cb1f239a75958be373e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:48 +0000

    Add package pop-tree/2/pop-tree-2.tar.gz

---
+++ packages/pop-tree/pop-tree-2.tar.gz/pop-tree-2/tree/conf.py
@@ -0,0 +1,24 @@
+CLI_CONFIG = {
+    "format_plugin": {"options": ["-p"]},
+    "output": {"source": "rend", "default": None,},
+    "add_sub": {"nargs": "*"},
+    "sub": {"positional": True, "nargs": "?",},
+    "doc": {"action": "store_true",},
+    "file": {"action": "store_true",},
+}
+CONFIG = {
+    "add_sub": {"help": "Add a sub to the hub", "default": [],},
+    "sub": {"type": str, "help": "The sub on the hub to parse", "default": None,},
+    "doc": {
+        "help": "Show the available docstrings of all functions",
+        "default": False,
+    },
+    "file": {
+        "help": "Show the file locations of available functions",
+        "default": False,
+    },
+}
+SUBCOMMANDS = {}
+DYNE = {
+    "tree": ["tree"],
+}
