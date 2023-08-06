56a6132300f881050b0c96f980ec465fde4d3a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:36 +0000

    Add package safetylinespackage/0.1/safetylinespackage-0.1.tar.gz

---
+++ packages/safetylinespackage/safetylinespackage-0.1.tar.gz/safetylinespackage-0.1/safetylinespackage/__init__.py
@@ -0,0 +1,11 @@
+
+import json
+import numpy as np
+from sklearn.datasets import make_classification, make_regression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.metrics import confusion_matrix
+
+from safetylinespackage import generate,get_metric,learn,learn,predict,target_statistics,features_statistics,correlation,statistics,print_correlations
