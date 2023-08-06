0bc880060fd330924a8be92f911cb139e3745dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package naas/0.10.0/naas-0.10.0.tar.gz

---
+++ packages/naas/naas-0.10.0.tar.gz/naas-0.10.0/naas/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) Naas Team.
+# Distributed under the terms of the GNU AGPL License.
+
+from .runner.notifications import Notifications
+from .dependency import Dependency
+from .scheduler import Scheduler
+from .assets import Assets
+from .secret import Secret
+from .runner import Runner
+from .api import Api
+import os
+
+__version__ = "0.10.0"
+__location__ = os.getcwd()
+scheduler = Scheduler()
+secret = Secret()
+runner = Runner()
+api = Api()
+assets = Assets()
+dependency = Dependency()
+notifications = Notifications()
+
+
+def is_production():
+    return api.manager.is_production()
