963092696d289f5db47aecc311594ab5c4137007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:35 +0000

    Add package tensorly/0.5.0/tensorly-0.5.0.tar.gz

---
+++ packages/tensorly/tensorly-0.5.0.tar.gz/tensorly-0.5.0/tensorly/decomposition/__init__.py
@@ -0,0 +1,13 @@
+"""
+The :mod:`tensorly.decomposition` module includes utilities for performing
+tensor decomposition such as CANDECOMP-PARAFAC and Tucker.                                                                                               
+"""
+
+from ._cp import (parafac, non_negative_parafac, CP, RandomisedCP,
+                                randomised_parafac, sample_khatri_rao)
+from ._tucker import tucker, partial_tucker, non_negative_tucker, Tucker
+from .robust_decomposition import robust_pca
+from ._tt import matrix_product_state, TensorTrain
+from .parafac2 import parafac2, Parafac2
+from ._symmetric_cp import symmetric_parafac_power_iteration, symmetric_power_iteration, SymmetricCP
+from ._cp_power import parafac_power_iteration, power_iteration, CPPower
